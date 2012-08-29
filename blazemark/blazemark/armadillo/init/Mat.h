6b516142fc86d524ad43366120e12be5edb0d501
Klaus Iglberger <klaus.iglberger@gmail.com>
2012-08-29 10:04:32 +0200

    Add initialization functions for the Armadillo benchmarks

---
+++ blazemark/blazemark/armadillo/init/Mat.h
@@ -0,0 +1,80 @@
+//=================================================================================================
+/*!
+//  \file blazemark/armadillo/init/Mat.h
+//  \brief Header file for the Armadillo dense matrix initialization functions
+//
+//  Copyright (C) 2011 Klaus Iglberger - All Rights Reserved
+//
+//  This file is part of the Blaze library. This library is free software; you can redistribute
+//  it and/or modify it under the terms of the GNU General Public License as published by the
+//  Free Software Foundation; either version 3, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+//  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//  See the GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along with a special
+//  exception for linking and compiling against the Blaze library, the so-called "runtime
+//  exception"; see the file COPYING. If not, see http://www.gnu.org/licenses/.
+*/
+//=================================================================================================
+
+#ifndef _BLAZEMARK_ARMADILLO_INIT_MAT_H_
+#define _BLAZEMARK_ARMADILLO_INIT_MAT_H_
+
+
+//*************************************************************************************************
+// Includes
+//*************************************************************************************************
+
+#include <armadillo>
+#include <blaze/util/Random.h>
+#include <blazemark/system/Types.h>
+
+
+namespace blazemark {
+
+namespace armadillo {
+
+//=================================================================================================
+//
+//  INITIALIZATION FUNCTIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\name Armadillo initialization functions */
+//@{
+template< typename Type >
+void init( ::arma::Mat<Type>& m );
+//@}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Random initialization of the given column-major dense matrix.
+//
+// \param m The dense column-major dense matrix to be initialized.
+// \return void
+//
+// This function initializes the given dense column-major dense matrix with random values.
+*/
+template< typename Type >  // Data type of the matrix
+void init( ::arma::Mat<Type>& m )
+{
+   const size_t M( m.n_rows );
+   const size_t N( m.n_cols );
+
+   for( size_t j=0UL; j<N; ++j ) {
+      for( size_t i=0UL; i<M; ++i ) {
+         m(i,j) = ::blaze::rand<Type>();
+      }
+   }
+}
+//*************************************************************************************************
+
+} // namespace armadillo
+
+} // namespace blazemark
+
+#endif
---
+++ blazemark/blazemark/armadillo/init/Row.h
@@ -0,0 +1,77 @@
+//=================================================================================================
+/*!
+//  \file blazemark/armadillo/init/Row.h
+//  \brief Header file for the Armadillo dense row vector initialization functions
+//
+//  Copyright (C) 2011 Klaus Iglberger - All Rights Reserved
+//
+//  This file is part of the Blaze library. This library is free software; you can redistribute
+//  it and/or modify it under the terms of the GNU General Public License as published by the
+//  Free Software Foundation; either version 3, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+//  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//  See the GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along with a special
+//  exception for linking and compiling against the Blaze library, the so-called "runtime
+//  exception"; see the file COPYING. If not, see http://www.gnu.org/licenses/.
+*/
+//=================================================================================================
+
+#ifndef _BLAZEMARK_ARMADILLO_INIT_ROW_H_
+#define _BLAZEMARK_ARMADILLO_INIT_ROW_H_
+
+
+//*************************************************************************************************
+// Includes
+//*************************************************************************************************
+
+#include <armadillo>
+#include <blaze/util/Random.h>
+#include <blazemark/system/Types.h>
+
+
+namespace blazemark {
+
+namespace armadillo {
+
+//=================================================================================================
+//
+//  INITIALIZATION FUNCTIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\name Armadillo initialization functions */
+//@{
+template< typename Type >
+void init( ::arma::Row<Type>& v );
+//@}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Random initialization of the given row vector.
+//
+// \param v The row vector to be initialized.
+// \return void
+//
+// This function initializes the given row vector with random values.
+*/
+template< typename Type >  // Data type of the vector
+void init( ::arma::Row<Type>& v )
+{
+   const size_t N( v.n_elem );
+
+   for( size_t i=0UL; i<N; ++i ) {
+      v[i] = ::blaze::rand<Type>();
+   }
+}
+//*************************************************************************************************
+
+} // namespace armadillo
+
+} // namespace blazemark
+
+#endif
