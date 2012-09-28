bbe729addc69df9b9e6274d1a4354900f0390687
Klaus Iglberger <klaus.iglberger@gmail.com>
2012-09-28 10:53:20 +0200

    Add the 'SVecSVecCross' test scenario along with all test cases

---
+++ blazetest/src/mathtest/svecsveccross/VCbVCb.cpp
@@ -0,0 +1,71 @@
+//=================================================================================================
+/*!
+//  \file src/mathtest/svecsveccross/VCbVCb.cpp
+//  \brief Source file for the VCbVCb sparse vector/sparse vector cross product math test
+//
+//  Copyright (C) 2011 Klaus Iglberger - All Rights Reserved
+//
+//  This file is part of the Blaze library. This library is free software; you can redistribute
+//  it and/or modify it under the terms of the GNU General Public License as published by the
+//  Free Software Foundation; either version 3, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+//  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//  See the GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along with a special
+//  exception for linking and compiling against the Blaze library, the so-called "runtime
+//  exception"; see the file COPYING. If not, see http://www.gnu.org/licenses/.
+*/
+//=================================================================================================
+
+
+//*************************************************************************************************
+// Includes
+//*************************************************************************************************
+
+#include <cstdlib>
+#include <iostream>
+#include <blaze/math/CompressedVector.h>
+#include <blazetest/mathtest/SVecSVecCross.h>
+#include <blazetest/system/MathTest.h>
+#include <blazetest/util/Creator.h>
+
+
+//=================================================================================================
+//
+//  MAIN FUNCTION
+//
+//=================================================================================================
+
+//*************************************************************************************************
+int main()
+{
+   std::cout << "   Running 'VCbVCb'..." << std::endl;
+
+   using blazetest::mathtest::TypeB;
+
+   try
+   {
+      // Vector type definitions
+      typedef blaze::CompressedVector<TypeB>  VCb;
+
+      // Creator type definitions
+      typedef blazetest::Creator<VCb>  CVCb;
+
+      // Running the tests
+      for( size_t i=0UL; i<=3UL; ++i ) {
+         for( size_t j=0UL; j<=3UL; ++j ) {
+            RUN_SVECSVECCROSS_TEST( CVCb( 3UL, i ), CVCb( 3UL, j ) );
+         }
+      }
+   }
+   catch( std::exception& ex ) {
+      std::cerr << "\n\n ERROR DETECTED during sparse vector/sparse vector cross product:\n"
+                << ex.what() << "\n";
+      return EXIT_FAILURE;
+   }
+
+   return EXIT_SUCCESS;
+}
+//*************************************************************************************************
