e47009d13cc4c3b1da73059719b999f807e97b30
Klaus Iglberger <klaus.iglberger@gmail.com>
2012-10-05 13:30:42 +0200

    Add the dense vector/sparse vector cross product benchmark (dvecsveccross)

---
+++ blazemark/src/blaze/DVecSVecCross.cpp
@@ -0,0 +1,109 @@
+//=================================================================================================
+/*!
+//  \file src/blaze/DVecSVecCross.cpp
+//  \brief Source file for the Blaze dense vector/sparse vector cross product kernel
+//
+//  Copyright (C) 2011 Klaus Iglberger - All Rights Reserved
+//
+//  This file is part of the Blaze library. This library is free software; you can redistribute
+//  it and/or modify it under the terms of the GNU General Public License as published by the
+//  Free Software Foundation; either version 3, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+//  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//  See the GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along with a special
+//  exception for linking and compiling against the Blaze library, the so-called "runtime
+//  exception"; see the file COPYING. If not, see http://www.gnu.org/licenses/.
+*/
+//=================================================================================================
+
+
+//*************************************************************************************************
+// Includes
+//*************************************************************************************************
+
+#include <iostream>
+#include <vector>
+#include <blaze/math/CompressedVector.h>
+#include <blaze/math/StaticVector.h>
+#include <blaze/util/Timing.h>
+#include <blazemark/blaze/DVecSVecCross.h>
+#include <blazemark/blaze/init/CompressedVector.h>
+#include <blazemark/blaze/init/StaticVector.h>
+#include <blazemark/system/Config.h>
+
+
+namespace blazemark {
+
+namespace blaze {
+
+//=================================================================================================
+//
+//  KERNEL FUNCTIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\brief Blaze dense vector/sparse vector cross product kernel.
+//
+// \param N The number of cross products to be computed.
+// \param F The number of non-zero elements for the sparse vector.
+// \param steps The number of iteration steps to perform.
+// \return Minimum runtime of the kernel function.
+//
+// This kernel function implements the dense vector/sparse vector cross product by means of
+// the Blaze functionality.
+*/
+double dvecsveccross( size_t N, size_t F, size_t steps )
+{
+   using ::blazemark::element_t;
+   using ::blaze::columnVector;
+
+   ::blaze::setSeed( seed );
+
+   ::std::vector< ::blaze::StaticVector<element_t,3UL,columnVector> > a( N ), c( N );
+   ::std::vector< ::blaze::CompressedVector<element_t,columnVector> > b( N );
+   ::blaze::timing::WcTimer timer;
+
+   for( size_t i=0UL; i<N; ++i ) {
+      b[i].resize( 3UL );
+      init( a[i] );
+      init( b[i], F );
+   }
+
+   for( size_t i=0UL; i<N; ++i ) {
+      c[i] = a[i] % b[i];
+   }
+
+   for( size_t rep=0UL; rep<reps; ++rep )
+   {
+      timer.start();
+      for( size_t step=0UL, i=0UL; step<steps; ++step, ++i ) {
+         if( i == N ) i = 0UL;
+         c[i] = a[i] % b[i];
+      }
+      timer.end();
+
+      for( size_t i=0UL; i<N; ++i )
+         if( c[i].size() != 3UL )
+            std::cerr << " Line " << __LINE__ << ": ERROR detected!!!\n";
+
+      if( timer.last() > maxtime )
+         break;
+   }
+
+   const double minTime( timer.min()     );
+   const double avgTime( timer.average() );
+
+   if( minTime * ( 1.0 + deviation*0.01 ) < avgTime )
+      std::cerr << " Blaze kernel 'dvecsveccross': Time deviation too large!!!\n";
+
+   return minTime;
+}
+//*************************************************************************************************
+
+} // namespace blaze
+
+} // namespace blazemark
---
+++ blazemark/src/main/DVecSVecCross.cpp
@@ -0,0 +1,243 @@
+//=================================================================================================
+/*!
+//  \file src/blaze/DVecSVecCross.cpp
+//  \brief Source file for the dense vector/sparse vector cross product benchmark
+//
+//  Copyright (C) 2011 Klaus Iglberger - All Rights Reserved
+//
+//  This file is part of the Blaze library. This library is free software; you can redistribute
+//  it and/or modify it under the terms of the GNU General Public License as published by the
+//  Free Software Foundation; either version 3, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+//  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//  See the GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along with a special
+//  exception for linking and compiling against the Blaze library, the so-called "runtime
+//  exception"; see the file COPYING. If not, see http://www.gnu.org/licenses/.
+*/
+//=================================================================================================
+
+
+//*************************************************************************************************
+// Includes
+//*************************************************************************************************
+
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
+#include <blaze/math/CompressedVector.h>
+#include <blaze/math/Functions.h>
+#include <blaze/math/Infinity.h>
+#include <blaze/math/StaticVector.h>
+#include <blaze/util/Timing.h>
+#include <blazemark/blaze/DVecSVecCross.h>
+#include <blazemark/system/Config.h>
+#include <blazemark/system/Types.h>
+#include <blazemark/util/Benchmarks.h>
+#include <blazemark/util/Indices.h>
+#include <blazemark/util/Parser.h>
+#include <blazemark/util/StaticSparseRun.h>
+
+
+//*************************************************************************************************
+// Using declarations
+//*************************************************************************************************
+
+using blazemark::Benchmarks;
+using blazemark::Parser;
+using blazemark::StaticSparseRun;
+
+
+
+
+//=================================================================================================
+//
+//  TYPE DEFINITIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\brief Type of a benchmark run.
+//
+// This type definition specifies the type of a single benchmark run for the dense vector/sparse
+// vector cross product benchmark.
+*/
+typedef StaticSparseRun<3UL>  Run;
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  UTILITY FUNCTIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\brief Estimating the necessary number of steps for each benchmark.
+//
+// \param run The parameters for the benchmark run.
+// \return void
+//
+// This function estimates the necessary number of steps for the given benchmark based on the
+// performance of the Blaze library.
+*/
+void estimateSteps( Run& run )
+{
+   using blazemark::element_t;
+   using blaze::columnVector;
+
+   const size_t N( run.getSize() );
+   const size_t F( run.getNonZeros() );
+
+   blaze::StaticVector<element_t,3UL,columnVector> init( 0.1 );
+   std::vector< blaze::StaticVector<element_t,3UL,columnVector> > a( N, init ), c( N );
+   std::vector< blaze::CompressedVector<element_t,columnVector> > b( N );
+   blaze::timing::WcTimer timer;
+   double wct( 0.0 );
+   size_t steps( 1UL );
+
+   for( size_t i=0UL; i<N; ++i ) {
+      b[i].resize( 3UL );
+      b[i].reserve( F );
+      blazemark::Indices indices( N, F );
+      for( blazemark::Indices::Iterator it=indices.begin(); it!=indices.end(); ++it ) {
+         b[i][*it] = element_t(0.1);
+      }
+   }
+
+   while( true ) {
+      timer.start();
+      for( size_t step=0UL, i=0UL; step<steps; ++step, ++i ) {
+         if( i == N ) i = 0UL;
+         c[i] = a[i] % b[i];
+      }
+      timer.end();
+      wct = timer.last();
+      if( wct >= 0.2 ) break;
+      steps *= 2UL;
+   }
+
+   for( size_t i=0UL; i<N; ++i )
+      if( c[i].size() != 3UL )
+         std::cerr << " Line " << __LINE__ << ": ERROR detected!!!\n";
+
+   run.setSteps( blaze::max( 1UL, ( blazemark::runtime * steps ) / timer.last() ) );
+}
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  BENCHMARK FUNCTIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\brief Dense vector/sparse vector cross product benchmark function.
+//
+// \param runs The specified benchmark runs.
+// \param benchmarks The selection of benchmarks.
+// \return void
+*/
+void dvecsveccross( std::vector<Run>& runs, Benchmarks benchmarks )
+{
+   std::cout << std::left;
+
+   std::sort( runs.begin(), runs.end() );
+
+   size_t slowSize( blaze::inf );
+   for( std::vector<Run>::iterator run=runs.begin(); run!=runs.end(); ++run ) {
+      if( run->getSteps() == 0UL ) {
+         if( run->getSize() < slowSize ) {
+            estimateSteps( *run );
+            if( run->getSteps() == 1UL )
+               slowSize = run->getSize();
+         }
+         else run->setSteps( 1UL );
+      }
+   }
+
+   if( benchmarks.runBlaze ) {
+      std::vector<Run>::iterator run=runs.begin();
+      while( run != runs.end() ) {
+         const float fill( run->getFillingDegree() );
+         std::cout << "   Blaze (" << fill << "% filled) [MFlop/s]:\n";
+         for( ; run!=runs.end(); ++run ) {
+            if( run->getFillingDegree() != fill ) break;
+            const size_t N     ( run->getNumber()   );
+            const size_t F     ( run->getNonZeros() );
+            const size_t steps ( run->getSteps()    );
+            run->setBlazeResult( blazemark::blaze::dvecsveccross( N, F, steps ) );
+            const double mflops( ( F * 3UL ) * steps / run->getBlazeResult() / 1E6 );
+            std::cout << "     " << std::setw(12) << N << mflops << std::endl;
+         }
+      }
+   }
+
+   for( std::vector<Run>::iterator run=runs.begin(); run!=runs.end(); ++run ) {
+      std::cout << *run;
+   }
+}
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  MAIN FUNCTION
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\brief The main function for the dense vector/sparse vector cross product benchmark.
+//
+// \param argc The total number of command line arguments.
+// \param argv The array of command line arguments.
+// \return void
+*/
+int main( int argc, char** argv )
+{
+   std::cout << "\n Dense Vector/Sparse Vector Cross Product:\n";
+
+   Benchmarks benchmarks;
+
+   try {
+      parseCommandLineArguments( argc, argv, benchmarks );
+   }
+   catch( std::exception& ex ) {
+      std::cerr << "   " << ex.what() << "\n";
+      return EXIT_FAILURE;
+   }
+
+   const std::string installPath( INSTALL_PATH );
+   const std::string parameterFile( installPath + "/params/dvecsveccross.prm" );
+   Parser<Run> parser;
+   std::vector<Run> runs;
+
+   try {
+      parser.parse( parameterFile.c_str(), runs );
+   }
+   catch( std::exception& ex ) {
+      std::cerr << "   Error during parameter extraction: " << ex.what() << "\n";
+      return EXIT_FAILURE;
+   }
+
+   try {
+      dvecsveccross( runs, benchmarks );
+   }
+   catch( std::exception& ex ) {
+      std::cerr << "   Error during benchmark execution: " << ex.what() << "\n";
+      return EXIT_FAILURE;
+   }
+}
+//*************************************************************************************************
