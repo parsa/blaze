96e47b05f002a429399fb1a82e10812a0c83bca5
Klaus Iglberger <klaus.iglberger@gmail.com>
2013-01-03 09:42:05 +0100

    Integrate extended aliasing detection in the 'DMatDMatSubExpr' expression

--- blaze/math/expressions/DMatDMatSubExpr.h
+++ blaze/math/expressions/DMatDMatSubExpr.h
@@ -274,9 +274,14 @@ class DMatDMatSubExpr : public DenseMatrix< DMatDMatSubExpr<MT1,MT2,SO>, SO >
    {
       BLAZE_INTERNAL_ASSERT( (~lhs).rows()    == rhs.rows()   , "Invalid number of rows"    );
       BLAZE_INTERNAL_ASSERT( (~lhs).columns() == rhs.columns(), "Invalid number of columns" );
-
-      assign   ( ~lhs, rhs.lhs_ );
-      subAssign( ~lhs, rhs.rhs_ );
+      
+      if( !IsExpression<MT1>::value && (~lhs).isAliased( &rhs.lhs_ ) ) {
+         subAssign( ~lhs, rhs.rhs_ );
+      }
+      else {
+         assign   ( ~lhs, rhs.lhs_ );
+         subAssign( ~lhs, rhs.rhs_ );
+      }
    }
    /*! \endcond */
    //**********************************************************************************************
