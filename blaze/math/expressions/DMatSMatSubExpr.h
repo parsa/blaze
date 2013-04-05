f9b63547489abe8fa6948949c6fe567fe3adb3d2
Klaus Iglberger <klaus.iglberger@gmail.com>
2013-04-05 17:58:09 +0200

    Fix the detection of aliasing in the 'DMatSMatSubExpr' expression template

--- blaze/math/expressions/DMatSMatSubExpr.h
+++ blaze/math/expressions/DMatSMatSubExpr.h
@@ -120,7 +120,7 @@ class DMatSMatSubExpr : public DenseMatrix< DMatSMatSubExpr<MT1,MT2,SO>, SO >
    enum { vectorizable = 0 };
 
    //! Compilation flag for the detection of aliasing effects.
-   enum { canAlias = IsExpression<MT1>::value };
+   enum { canAlias = 1 };
    //**********************************************************************************************
 
    //**Constructor*********************************************************************************
@@ -200,7 +200,8 @@ class DMatSMatSubExpr : public DenseMatrix< DMatSMatSubExpr<MT1,MT2,SO>, SO >
    */
    template< typename T >
    inline bool isAliased( const T* alias ) const {
-      return IsExpression<MT1>::value && lhs_.isAliased( alias );
+      return ( IsExpression<MT1>::value && lhs_.isAliased( alias ) ) ||
+             ( rhs_.isAliased( alias ) );
    }
    //**********************************************************************************************
 
