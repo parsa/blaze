7fecbaa4b04af1609b790446b67c10698cb8d831
Klaus Iglberger <klaus.iglberger@gmail.com>
2013-04-07 09:26:39 +0200

    Fix the detection of aliasing in the 'SMatTSMatMultExpr' expression template

--- blaze/math/expressions/SMatTSMatMultExpr.h
+++ blaze/math/expressions/SMatTSMatMultExpr.h
@@ -109,8 +109,8 @@ class SMatTSMatMultExpr : public SparseMatrix< SMatTSMatMultExpr<MT1,MT2>, false
 
    //**Compilation flags***************************************************************************
    //! Compilation flag for the detection of aliasing effects.
-   enum { canAlias = ( !RequiresEvaluation<MT1>::value && ( !IsComputation<MT1>::value || CanAlias<MT1>::value ) ) ||
-                     ( !RequiresEvaluation<MT2>::value && ( !IsComputation<MT2>::value || CanAlias<MT2>::value ) ) };
+   enum { canAlias = ( !IsComputation<MT1>::value || CanAlias<MT1>::value ) ||
+                     ( !IsComputation<MT2>::value || CanAlias<MT2>::value ) };
    //**********************************************************************************************
 
    //**Constructor*********************************************************************************
@@ -328,10 +328,8 @@ class SMatTSMatMultExpr : public SparseMatrix< SMatTSMatMultExpr<MT1,MT2>, false
    */
    template< typename T >
    inline bool isAliased( const T* alias ) const {
-      return ( ( !IsComputation<MT1>::value || CanAlias<MT1>::value ) &&
-               !RequiresEvaluation<MT1>::value && lhs_.isAliased( alias ) ) ||
-             ( ( !IsComputation<MT2>::value || CanAlias<MT2>::value ) &&
-               !RequiresEvaluation<MT2>::value && rhs_.isAliased( alias ) );
+      return ( ( !IsComputation<MT1>::value || CanAlias<MT1>::value ) && lhs_.isAliased( alias ) ) ||
+             ( ( !IsComputation<MT2>::value || CanAlias<MT2>::value ) && rhs_.isAliased( alias ) );
    }
    //**********************************************************************************************
 
