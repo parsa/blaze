8f6783ed5d592e6a49bbb4cf611f511153ad93e3
Klaus Iglberger <klaus.iglberger@gmail.com>
2013-11-04 07:33:58 +0100

    Add a header file for every view in the math include directory

---
+++ blaze/math/SparseColumn.h
@@ -0,0 +1,33 @@
+//=================================================================================================
+/*!
+//  \file blaze/math/SparseColumn.h
+//  \brief Header file for the complete SparseColumn implementation
+//
+//  Copyright (C) 2011 Klaus Iglberger - All Rights Reserved
+//
+//  This file is part of the Blaze library. This library is free software; you can redistribute
+//  it and/or modify it under the terms of the GNU General Public License as published by the
+//  Free Software Foundation; either version 3, or (at your option) any later version.
+//
+//  This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+//  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+//  See the GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along with a special
+//  exception for linking and compiling against the Blaze library, the so-called "runtime
+//  exception"; see the file COPYING. If not, see http://www.gnu.org/licenses/.
+*/
+//=================================================================================================
+
+#ifndef _BLAZE_MATH_SPARSECOLUMN_H_
+#define _BLAZE_MATH_SPARSECOLUMN_H_
+
+
+//*************************************************************************************************
+// Includes
+//*************************************************************************************************
+
+#include <blaze/math/views/SparseColumn.h>
+#include <blaze/math/views/SparseRow.h>
+
+#endif
