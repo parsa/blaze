fb6451a1c9e52a2a4eef71f527f2c06cda68cad8
Klaus Iglberger <klaus.iglberger@gmail.com>
2013-11-11 14:51:05 +0100

    Switch from the GPLv3 license to the new, revised BSD license

--- blazemark/blazemark/blaze/SVecSVecAdd.h
+++ blazemark/blazemark/blaze/SVecSVecAdd.h
@@ -3,19 +3,32 @@
 //  \file blazemark/blaze/SVecSVecAdd.h
 //  \brief Header file for the Blaze sparse vector/sparse vector addition kernel
 //
-//  Copyright (C) 2011 Klaus Iglberger - All Rights Reserved
+//  Copyright (C) 2013 Klaus Iglberger - All Rights Reserved
 //
-//  This file is part of the Blaze library. This library is free software; you can redistribute
-//  it and/or modify it under the terms of the GNU General Public License as published by the
-//  Free Software Foundation; either version 3, or (at your option) any later version.
+//  This file is part of the Blaze library. You can redistribute it and/or modify it under
+//  the terms of the New (Revised) BSD License. Redistribution and use in source and binary
+//  forms, with or without modification, are permitted provided that the following conditions
+//  are met:
 //
-//  This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-//  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-//  See the GNU General Public License for more details.
+//  1. Redistributions of source code must retain the above copyright notice, this list of
+//     conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright notice, this list
+//     of conditions and the following disclaimer in the documentation and/or other materials
+//     provided with the distribution.
+//  3. Neither the names of the Blaze development group nor the names of its contributors
+//     may be used to endorse or promote products derived from this software without specific
+//     prior written permission.
 //
-//  You should have received a copy of the GNU General Public License along with a special
-//  exception for linking and compiling against the Blaze library, the so-called "runtime
-//  exception"; see the file COPYING. If not, see http://www.gnu.org/licenses/.
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+//  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+//  SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+//  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+//  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+//  DAMAGE.
 */
 //=================================================================================================
 
