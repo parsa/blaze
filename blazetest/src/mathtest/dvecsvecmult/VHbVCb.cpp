f0d7d4769c6899eea2f049b16a2e1dc53a61fb0a
Klaus Iglberger <klaus.iglberger@gmail.com>
2013-11-15 17:27:43 +0100

    Add dense vector/sparse vector multiplication tests using the 'HybridVector' class template

---
+++ blazetest/src/mathtest/dvecsvecmult/VHbVCb.cpp
@@ -0,0 +1,91 @@
+//=================================================================================================
+/*!
+//  \file src/mathtest/dvecsvecmult/VHbVCb.cpp
+//  \brief Source file for the VHbVCb dense vector/sparse vector multiplication math test
+//
+//  Copyright (C) 2013 Klaus Iglberger - All Rights Reserved
+//
+//  This file is part of the Blaze library. You can redistribute it and/or modify it under
+//  the terms of the New (Revised) BSD License. Redistribution and use in source and binary
+//  forms, with or without modification, are permitted provided that the following conditions
+//  are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this list of
+//     conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright notice, this list
+//     of conditions and the following disclaimer in the documentation and/or other materials
+//     provided with the distribution.
+//  3. Neither the names of the Blaze development group nor the names of its contributors
+//     may be used to endorse or promote products derived from this software without specific
+//     prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+//  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+//  SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+//  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+//  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+//  DAMAGE.
+*/
+//=================================================================================================
+
+
+//*************************************************************************************************
+// Includes
+//*************************************************************************************************
+
+#include <cstdlib>
+#include <iostream>
+#include <blaze/math/CompressedVector.h>
+#include <blaze/math/HybridVector.h>
+#include <blazetest/mathtest/Creator.h>
+#include <blazetest/mathtest/dvecsvecmult/OperationTest.h>
+#include <blazetest/system/MathTest.h>
+
+
+//=================================================================================================
+//
+//  MAIN FUNCTION
+//
+//=================================================================================================
+
+//*************************************************************************************************
+int main()
+{
+   std::cout << "   Running 'VHbVCb'..." << std::endl;
+
+   using blazetest::mathtest::TypeB;
+
+   try
+   {
+      // Vector type definitions
+      typedef blaze::HybridVector<TypeB,128UL>  VHb;
+      typedef blaze::CompressedVector<TypeB>    VCb;
+
+      // Creator type definitions
+      typedef blazetest::Creator<VHb>  CVHb;
+      typedef blazetest::Creator<VCb>  CVCb;
+
+      // Running tests with small vectors
+      for( size_t i=0UL; i<=6UL; ++i ) {
+         for( size_t j=0UL; j<=i; ++j ) {
+            RUN_DVECSVECMULT_OPERATION_TEST( CVHb( i ), CVCb( i, j ) );
+         }
+      }
+
+      // Running tests with large vectors
+      RUN_DVECSVECMULT_OPERATION_TEST( CVHb( 127UL ), CVCb( 127UL, 13UL ) );
+      RUN_DVECSVECMULT_OPERATION_TEST( CVHb( 128UL ), CVCb( 128UL, 16UL ) );
+   }
+   catch( std::exception& ex ) {
+      std::cerr << "\n\n ERROR DETECTED during dense vector/sparse vector multiplication:\n"
+                << ex.what() << "\n";
+      return EXIT_FAILURE;
+   }
+
+   return EXIT_SUCCESS;
+}
+//*************************************************************************************************
