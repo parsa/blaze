600aca54d11eb9aec7a644c1e2f11aec49e1ee77
Klaus Iglberger <klaus.iglberger@gmail.com>
2013-12-21 10:07:32 +0100

    Add a compile time switch for the configuration of the streaming behavior

---
+++ blaze/config/Streaming.h
@@ -0,0 +1,53 @@
+//=================================================================================================
+/*!
+//  \file blaze/config/Streaming.h
+//  \brief Configuration of the streaming (non-temporal store) behavior
+//
+//  Copyright (C) 2013 Klaus Iglberger - All Rights Reserved
+//
+//  This file is part of the Blaze library. You can redistribute it and/or modify it under
+//  the terms of the New (Revised) BSD License. Redistribution and use in source and binary
+//  forms, with or without modification, are permitted provided that the following conditions
+//  are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this list of
+//     conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright notice, this list
+//     of conditions and the following disclaimer in the documentation and/or other materials
+//     provided with the distribution.
+//  3. Neither the names of the Blaze development group nor the names of its contributors
+//     may be used to endorse or promote products derived from this software without specific
+//     prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+//  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+//  SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+//  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+//  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+//  DAMAGE.
+*/
+//=================================================================================================
+
+
+namespace blaze {
+
+//*************************************************************************************************
+/*!\brief Configuration of the streaming behavior.
+// \ingroup config
+//
+// For large vectors and matrices non-temporal stores can provide a significant performance
+// advantage of about 20%. However, this advantage is only in effect in case the memory bandwidth
+// of the target architecture is maxed out. If the target architecture's memory bandwidth cannot
+// be exhausted the use of non-temporal stores can decrease performance instead of increase it.
+//
+// Via this compilation switch streaming  (i.e. non-temporal stores) can be (de-)activated. If
+// set to \a true streaming is enabled, if set to \a false streaming is disabled.
+*/
+const bool useStreaming = true;
+//*************************************************************************************************
+
+} // namespace blaze
