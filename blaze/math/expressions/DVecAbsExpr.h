72231b9b98ef420a87699ad2629f435056364d64
Klaus Iglberger <klaus.iglberger@gmail.com>
2013-12-26 18:16:03 +0100

    Vectorize the 'DVecAbsExpr' expression class

--- blaze/math/expressions/DVecAbsExpr.h
+++ blaze/math/expressions/DVecAbsExpr.h
@@ -47,11 +47,13 @@
 #include <blaze/math/expressions/DenseVector.h>
 #include <blaze/math/expressions/Forward.h>
 #include <blaze/math/expressions/VecAbsExpr.h>
+#include <blaze/math/Intrinsics.h>
 #include <blaze/math/traits/AbsExprTrait.h>
 #include <blaze/math/traits/DVecAbsExprTrait.h>
 #include <blaze/math/traits/SubvectorExprTrait.h>
 #include <blaze/math/traits/TDVecAbsExprTrait.h>
 #include <blaze/math/typetraits/IsColumnVector.h>
+#include <blaze/math/typetraits/IsComputation.h>
 #include <blaze/math/typetraits/IsDenseVector.h>
 #include <blaze/math/typetraits/IsExpression.h>
 #include <blaze/math/typetraits/IsTemporary.h>
@@ -89,11 +91,8 @@ class DVecAbsExpr : public DenseVector< DVecAbsExpr<VT,TF>, TF >
 {
  private:
    //**Type definitions****************************************************************************
-   typedef typename VT::ResultType     RT;  //!< Result type of the dense vector expression.
-   typedef typename VT::ReturnType     RN;  //!< Return type of the dense vector expression.
-   typedef typename VT::CompositeType  CT;  //!< Composite type of the dense vector expression.
-   typedef typename VT::TransposeType  TT;  //!< Transpose type of the dense vector expression.
-   typedef typename VT::ElementType    ET;  //!< Element type of the dense vector expression.
+   typedef typename VT::ReturnType   RN;  //!< Return type of the dense vector expression.
+   typedef typename VT::ElementType  ET;  //!< Element type of the dense vector expression.
    //**********************************************************************************************
 
    //**Return type evaluation**********************************************************************
@@ -130,10 +129,11 @@ class DVecAbsExpr : public DenseVector< DVecAbsExpr<VT,TF>, TF >
 
  public:
    //**Type definitions****************************************************************************
-   typedef DVecAbsExpr<VT,TF>  This;           //!< Type of this DVecAbsExpr instance.
-   typedef RT                  ResultType;     //!< Result type for expression template evaluations.
-   typedef TT                  TransposeType;  //!< Transpose type for expression template evaluations.
-   typedef ET                  ElementType;    //!< Resulting element type.
+   typedef DVecAbsExpr<VT,TF>                 This;           //!< Type of this DVecAbsExpr instance.
+   typedef typename VT::ResultType            ResultType;     //!< Result type for expression template evaluations.
+   typedef typename VT::TransposeType         TransposeType;  //!< Transpose type for expression template evaluations.
+   typedef typename VT::ElementType           ElementType;    //!< Resulting element type.
+   typedef typename IntrinsicTrait<ET>::Type  IntrinsicType;  //!< Resulting intrinsic element type.
 
    //! Return type for expression template evaluations.
    typedef const typename SelectType< returnExpr, ExprReturnType, ElementType >::Type  ReturnType;
@@ -145,9 +145,263 @@ class DVecAbsExpr : public DenseVector< DVecAbsExpr<VT,TF>, TF >
    typedef typename SelectType< IsExpression<VT>::value, const VT, const VT& >::Type  Operand;
    //**********************************************************************************************
 
+   //**ConstIterator class definition**************************************************************
+   /*!\brief Iterator over the elements of the dense vector.
+   */
+   class ConstIterator
+   {
+    public:
+      //**Type definitions*************************************************************************
+      typedef std::random_access_iterator_tag  IteratorCategory;  //!< The iterator category.
+      typedef ElementType                      ValueType;         //!< Type of the underlying elements.
+      typedef ElementType*                     PointerType;       //!< Pointer return type.
+      typedef ElementType&                     ReferenceType;     //!< Reference return type.
+      typedef ptrdiff_t                        DifferenceType;    //!< Difference between two iterators.
+
+      // STL iterator requirements
+      typedef IteratorCategory  iterator_category;  //!< The iterator category.
+      typedef ValueType         value_type;         //!< Type of the underlying elements.
+      typedef PointerType       pointer;            //!< Pointer return type.
+      typedef ReferenceType     reference;          //!< Reference return type.
+      typedef DifferenceType    difference_type;    //!< Difference between two iterators.
+
+      //! ConstIterator type of the left-hand side dense vector expression.
+      typedef typename VT::ConstIterator  IteratorType;
+      //*******************************************************************************************
+
+      //**Constructor******************************************************************************
+      /*!\brief Constructor for the ConstIterator class.
+      //
+      // \param it Iterator to the initial vector element.
+      */
+      explicit inline ConstIterator( IteratorType it )
+         : it_( it  )  // Iterator to the current vector element
+      {}
+      //*******************************************************************************************
+
+      //**Addition assignment operator*************************************************************
+      /*!\brief Addition assignment operator.
+      //
+      // \param inc The increment of the iterator.
+      // \return The incremented iterator.
+      */
+      inline ConstIterator& operator+=( size_t inc ) {
+         it_ += inc;
+         return *this;
+      }
+      //*******************************************************************************************
+
+      //**Subtraction assignment operator**********************************************************
+      /*!\brief Subtraction assignment operator.
+      //
+      // \param dec The decrement of the iterator.
+      // \return The decremented iterator.
+      */
+      inline ConstIterator& operator-=( size_t dec ) {
+         it_ -= dec;
+         return *this;
+      }
+      //*******************************************************************************************
+
+      //**Prefix increment operator****************************************************************
+      /*!\brief Pre-increment operator.
+      //
+      // \return Reference to the incremented iterator.
+      */
+      inline ConstIterator& operator++() {
+         ++it_;
+         return *this;
+      }
+      //*******************************************************************************************
+
+      //**Postfix increment operator***************************************************************
+      /*!\brief Post-increment operator.
+      //
+      // \return The previous position of the iterator.
+      */
+      inline const ConstIterator operator++( int ) {
+         return ConstIterator( it_++ );
+      }
+      //*******************************************************************************************
+
+      //**Prefix decrement operator****************************************************************
+      /*!\brief Pre-decrement operator.
+      //
+      // \return Reference to the decremented iterator.
+      */
+      inline ConstIterator& operator--() {
+         --it_;
+         return *this;
+      }
+      //*******************************************************************************************
+
+      //**Postfix decrement operator***************************************************************
+      /*!\brief Post-decrement operator.
+      //
+      // \return The previous position of the iterator.
+      */
+      inline const ConstIterator operator--( int ) {
+         return ConstIterator( it_-- );
+      }
+      //*******************************************************************************************
+
+      //**Element access operator******************************************************************
+      /*!\brief Direct access to the element at the current iterator position.
+      //
+      // \return The resulting value.
+      */
+      inline ReturnType operator*() const {
+         using std::abs;
+         return abs( *it_ );
+      }
+      //*******************************************************************************************
+
+      //**Load function****************************************************************************
+      /*!\brief Access to the intrinsic elements of the vector.
+      //
+      // \return The resulting intrinsic value.
+      */
+      inline IntrinsicType load() const {
+         return abs( it_.load() );
+      }
+      //*******************************************************************************************
+
+      //**Equality operator************************************************************************
+      /*!\brief Equality comparison between two ConstIterator objects.
+      //
+      // \param rhs The right-hand side iterator.
+      // \return \a true if the iterators refer to the same element, \a false if not.
+      */
+      inline bool operator==( const ConstIterator& rhs ) const {
+         return it_ == rhs.it_;
+      }
+      //*******************************************************************************************
+
+      //**Inequality operator**********************************************************************
+      /*!\brief Inequality comparison between two ConstIterator objects.
+      //
+      // \param rhs The right-hand side iterator.
+      // \return \a true if the iterators don't refer to the same element, \a false if they do.
+      */
+      inline bool operator!=( const ConstIterator& rhs ) const {
+         return it_ != rhs.it_;
+      }
+      //*******************************************************************************************
+
+      //**Less-than operator***********************************************************************
+      /*!\brief Less-than comparison between two ConstIterator objects.
+      //
+      // \param rhs The right-hand side iterator.
+      // \return \a true if the left-hand side iterator is smaller, \a false if not.
+      */
+      inline bool operator<( const ConstIterator& rhs ) const {
+         return it_ < rhs.it_;
+      }
+      //*******************************************************************************************
+
+      //**Greater-than operator********************************************************************
+      /*!\brief Greater-than comparison between two ConstIterator objects.
+      //
+      // \param rhs The right-hand side iterator.
+      // \return \a true if the left-hand side iterator is greater, \a false if not.
+      */
+      inline bool operator>( const ConstIterator& rhs ) const {
+         return it_ > rhs.it_;
+      }
+      //*******************************************************************************************
+
+      //**Less-or-equal-than operator**************************************************************
+      /*!\brief Less-than comparison between two ConstIterator objects.
+      //
+      // \param rhs The right-hand side iterator.
+      // \return \a true if the left-hand side iterator is smaller or equal, \a false if not.
+      */
+      inline bool operator<=( const ConstIterator& rhs ) const {
+         return it_ <= rhs.it_;
+      }
+      //*******************************************************************************************
+
+      //**Greater-or-equal-than operator***********************************************************
+      /*!\brief Greater-than comparison between two ConstIterator objects.
+      //
+      // \param rhs The right-hand side iterator.
+      // \return \a true if the left-hand side iterator is greater or equal, \a false if not.
+      */
+      inline bool operator>=( const ConstIterator& rhs ) const {
+         return it_ >= rhs.it_;
+      }
+      //*******************************************************************************************
+
+      //**Subtraction operator*********************************************************************
+      /*!\brief Calculating the number of elements between two iterators.
+      //
+      // \param rhs The right-hand side iterator.
+      // \return The number of elements between the two iterators.
+      */
+      inline DifferenceType operator-( const ConstIterator& rhs ) const {
+         return it_ - rhs.it_;
+      }
+      //*******************************************************************************************
+
+      //**Addition operator************************************************************************
+      /*!\brief Addition between a ConstIterator and an integral value.
+      //
+      // \param it The iterator to be incremented.
+      // \param inc The number of elements the iterator is incremented.
+      // \return The incremented iterator.
+      */
+      friend inline const ConstIterator operator+( const ConstIterator& it, size_t inc ) {
+         return ConstIterator( it.it_ + inc );
+      }
+      //*******************************************************************************************
+
+      //**Addition operator************************************************************************
+      /*!\brief Addition between an integral value and a ConstIterator.
+      //
+      // \param inc The number of elements the iterator is incremented.
+      // \param it The iterator to be incremented.
+      // \return The incremented iterator.
+      */
+      friend inline const ConstIterator operator+( size_t inc, const ConstIterator& it ) {
+         return ConstIterator( it.it_ + inc );
+      }
+      //*******************************************************************************************
+
+      //**Subtraction operator*********************************************************************
+      /*!\brief Subtraction between a ConstIterator and an integral value.
+      //
+      // \param it The iterator to be decremented.
+      // \param dec The number of elements the iterator is decremented.
+      // \return The decremented iterator.
+      */
+      friend inline const ConstIterator operator-( const ConstIterator& it, size_t dec ) {
+         return ConstIterator( it.it_ - dec );
+      }
+      //*******************************************************************************************
+
+      //**Subtraction operator*********************************************************************
+      /*!\brief Subtraction between an integral value and a ConstIterator.
+      //
+      // \param dec The number of elements the iterator is decremented.
+      // \param it The iterator to be decremented.
+      // \return The decremented iterator.
+      */
+      friend inline const ConstIterator operator-( size_t dec, const ConstIterator& it ) {
+         return ConstIterator( it.it_ - dec );
+      }
+      //*******************************************************************************************
+
+    private:
+      //**Member variables*************************************************************************
+      IteratorType it_;  //!< Iterator to the current vector element.
+      //*******************************************************************************************
+   };
+   //**********************************************************************************************
+
    //**Compilation flags***************************************************************************
    //! Compilation switch for the expression template evaluation strategy.
-   enum { vectorizable = 0 };
+   enum { vectorizable = VT::vectorizable &&
+                         IntrinsicTrait<ET>::absoluteValue };
    //**********************************************************************************************
 
    //**Constructor*********************************************************************************
@@ -170,6 +424,40 @@ class DVecAbsExpr : public DenseVector< DVecAbsExpr<VT,TF>, TF >
       using std::abs;
       BLAZE_INTERNAL_ASSERT( index < dv_.size(), "Invalid vector access index" );
       return abs( dv_[index] );
+   }
+   //**********************************************************************************************
+
+   //**Load function*******************************************************************************
+   /*!\brief Access to the intrinsic elements of the vector.
+   //
+   // \param index Access index. The index has to be in the range \f$[0..N-1]\f$.
+   // \return Reference to the accessed values.
+   */
+   inline IntrinsicType load( size_t index ) const {
+      typedef IntrinsicTrait<ElementType>  IT;
+      BLAZE_INTERNAL_ASSERT( index < dv_.size()     , "Invalid vector access index" );
+      BLAZE_INTERNAL_ASSERT( index % IT::size == 0UL, "Invalid vector access index" );
+      return abs( dv_.load( index ) );
+   }
+   //**********************************************************************************************
+
+   //**Begin function******************************************************************************
+   /*!\brief Returns an iterator to the first non-zero element of the dense vector.
+   //
+   // \return Iterator to the first non-zero element of the dense vector.
+   */
+   inline ConstIterator begin() const {
+      return ConstIterator( dv_.begin() );
+   }
+   //**********************************************************************************************
+
+   //**End function********************************************************************************
+   /*!\brief Returns an iterator just past the last non-zero element of the dense vector.
+   //
+   // \return Iterator just past the last non-zero element of the dense vector.
+   */
+   inline ConstIterator end() const {
+      return ConstIterator( dv_.end() );
    }
    //**********************************************************************************************
 
@@ -201,7 +489,7 @@ class DVecAbsExpr : public DenseVector< DVecAbsExpr<VT,TF>, TF >
    */
    template< typename T >
    inline bool canAlias( const T* alias ) const {
-      return dv_.canAlias( alias );
+      return IsComputation<VT>::value && dv_.canAlias( alias );
    }
    //**********************************************************************************************
 
@@ -242,16 +530,10 @@ class DVecAbsExpr : public DenseVector< DVecAbsExpr<VT,TF>, TF >
    {
       BLAZE_FUNCTION_TRACE;
 
-      using std::abs;
-
       BLAZE_INTERNAL_ASSERT( (~lhs).size() == rhs.size(), "Invalid vector sizes" );
 
       assign( ~lhs, rhs.dv_ );
-
-      const size_t size( rhs.size() );
-      for( size_t i=0UL; i<size; ++i ) {
-         (~lhs)[i] = abs( (~lhs)[i] );
-      }
+      assign( ~lhs, abs( ~lhs ) );
    }
    /*! \endcond */
    //**********************************************************************************************
