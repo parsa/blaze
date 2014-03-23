56f62d368005cdcb0503bb2fb64e221bd501061e
Klaus Iglberger <klaus.iglberger@gmail.com>
2014-03-23 07:17:06 +0100

    Change the version information to 2.0

--- blaze/system/Version.h
+++ blaze/system/Version.h
@@ -47,9 +47,9 @@
 // \ingroup system
 //
 // This value corresponds to the major version of the Blaze library. For instance, for Blaze
-// version 1.5, the BLAZE_MAJOR_VERSION corresponds to 1.
+// version 2.0, the BLAZE_MAJOR_VERSION corresponds to 2.
 */
-#define BLAZE_MAJOR_VERSION 1
+#define BLAZE_MAJOR_VERSION 2
 //*************************************************************************************************
 
 
@@ -58,9 +58,9 @@
 // \ingroup system
 //
 // This value corresponds to the minor version of the Blaze library. For instance, for Blaze
-// version 1.5, the BLAZE_MINOR_VERSION corresponds to 5.
+// version 2.0, the BLAZE_MINOR_VERSION corresponds to 0.
 */
-#define BLAZE_MINOR_VERSION 5
+#define BLAZE_MINOR_VERSION 0
 //*************************************************************************************************
 
 #endif
