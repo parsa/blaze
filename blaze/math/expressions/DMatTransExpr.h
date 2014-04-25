84e7445a1b79781c7a45268bdfcda4470d0ab34d
Klaus Iglberger <klaus.iglberger@gmail.com>
2014-04-25 18:52:18 +0200

    Split 'assign' and 'smpAssign' functionality in the dense matrix transpose expression class ('DMatTransExpr')

--- blaze/math/expressions/DMatTransExpr.h
+++ blaze/math/expressions/DMatTransExpr.h
@@ -90,23 +90,35 @@ class DMatTransExpr : public DenseMatrix< DMatTransExpr<MT,SO>, SO >
    typedef typename MT::CompositeType  CT;  //!< Composite type of the dense matrix expression.
    //**********************************************************************************************
 
-   //**********************************************************************************************
-   //! Compilation switch for the evaluation strategy of the transposition expression.
+   //**Serial evaluation strategy******************************************************************
+   //! Compilation switch for the serial evaluation strategy of the transposition expression.
    /*! The \a useAssign compile time constant expression represents a compilation switch for
-       the evaluation strategy of the transposition expression. In case the given dense matrix
-       expression of type \a MT requires an intermediate evaluation, \a useAssign will be set
-       to 1 and the transposition expression will be evaluated via the \a assign function
+       the serial evaluation strategy of the transposition expression. In case the given dense
+       matrix expression of type \a MT requires an intermediate evaluation, \a useAssign will
+       be set to 1 and the transposition expression will be evaluated via the \a assign function
        family. Otherwise \a useAssign will be set to 0 and the expression will be evaluated
        via the subscript operator. */
    enum { useAssign = RequiresEvaluation<MT>::value };
-   //**********************************************************************************************
 
-   //**********************************************************************************************
    /*! \cond BLAZE_INTERNAL */
    //! Helper structure for the explicit application of the SFINAE principle.
    template< typename MT2 >
    struct UseAssign {
       enum { value = useAssign };
+   };
+   /*! \endcond */
+   //**********************************************************************************************
+
+   //**Parallel evaluation strategy****************************************************************
+   /*! \cond BLAZE_INTERNAL */
+   //! Helper structure for the explicit application of the SFINAE principle.
+   /*! The UseSMPAssign struct is a helper struct for the selection of the parallel evaluation
+       strategy. In case the target matrix is SMP assignable but the dense matrix operand is not,
+       \a value is set to 1 and the expression specific evaluation strategy is selected. Otherwise
+       \a value is set to 0 and the default strategy is chosen. */
+   template< typename MT2 >
+   struct UseSMPAssign {
+      enum { value = MT2::smpAssignable && !MT::smpAssignable };
    };
    /*! \endcond */
    //**********************************************************************************************
@@ -681,6 +693,151 @@ class DMatTransExpr : public DenseMatrix< DMatTransExpr<MT,SO>, SO >
 
    //**Multiplication assignment to sparse matrices************************************************
    // No special implementation for the multiplication assignment to sparse matrices.
+   //**********************************************************************************************
+
+   //**SMP assignment to dense matrices************************************************************
+   /*! \cond BLAZE_INTERNAL */
+   /*!\brief SMP assignment of a dense matrix transposition expression to a dense matrix.
+   // \ingroup dense_matrix
+   //
+   // \param lhs The target left-hand side dense matrix.
+   // \param rhs The right-hand side transposition expression to be assigned.
+   // \return void
+   //
+   // This function implements the performance optimized SMP assignment of a dense matrix
+   // transposition expression to a dense matrix. Due to the explicit application of the
+   // SFINAE principle, this operator can only be selected by the compiler in case the
+   // target matrix is SMP assignable but the dense matrix operand is not.
+   */
+   template< typename MT2  // Type of the target dense matrix
+           , bool SO2 >    // Storage order of the target dense matrix
+   friend inline typename EnableIf< UseSMPAssign<MT2> >::Type
+      smpAssign( DenseMatrix<MT2,SO2>& lhs, const DMatTransExpr& rhs )
+   {
+      BLAZE_FUNCTION_TRACE;
+
+      BLAZE_INTERNAL_ASSERT( (~lhs).rows()    == rhs.rows()   , "Invalid number of rows"    );
+      BLAZE_INTERNAL_ASSERT( (~lhs).columns() == rhs.columns(), "Invalid number of columns" );
+
+      DMatTransposer<MT2,!SO2> tmp( ~lhs );
+      smpAssign( tmp, rhs.dm_ );
+   }
+   /*! \endcond */
+   //**********************************************************************************************
+
+   //**SMP assignment to sparse matrices***********************************************************
+   /*! \cond BLAZE_INTERNAL */
+   /*!\brief SMP assignment of a dense matrix transposition expression to a sparse matrix.
+   // \ingroup dense_matrix
+   //
+   // \param lhs The target left-hand side sparse matrix.
+   // \param rhs The right-hand side transposition expression to be assigned.
+   // \return void
+   //
+   // This function implements the performance optimized SMP assignment of a dense matrix
+   // transposition expression to a sparse matrix. Due to the explicit application of the
+   // SFINAE principle, this operator can only be selected by the compiler in case the
+   // target matrix is SMP assignable but the dense matrix operand is not.
+   */
+   template< typename MT2  // Type of the target sparse matrix
+           , bool SO2 >    // Storage order of the target sparse matrix
+   friend inline typename EnableIf< UseSMPAssign<MT2> >::Type
+      smpAssign( SparseMatrix<MT2,SO2>& lhs, const DMatTransExpr& rhs )
+   {
+      BLAZE_FUNCTION_TRACE;
+
+      typedef typename SelectType< SO == SO2, ResultType, OppositeType >::Type  TmpType;
+
+      BLAZE_CONSTRAINT_MUST_BE_DENSE_MATRIX_TYPE( ResultType );
+      BLAZE_CONSTRAINT_MUST_BE_DENSE_MATRIX_TYPE( OppositeType );
+      BLAZE_CONSTRAINT_MUST_BE_MATRIX_WITH_STORAGE_ORDER( ResultType, SO );
+      BLAZE_CONSTRAINT_MUST_BE_MATRIX_WITH_STORAGE_ORDER( OppositeType, !SO );
+      BLAZE_CONSTRAINT_MATRICES_MUST_HAVE_SAME_STORAGE_ORDER( MT2, TmpType );
+      BLAZE_CONSTRAINT_MUST_BE_REFERENCE_TYPE( typename TmpType::CompositeType );
+
+      BLAZE_INTERNAL_ASSERT( (~lhs).rows()    == rhs.rows()   , "Invalid number of rows"    );
+      BLAZE_INTERNAL_ASSERT( (~lhs).columns() == rhs.columns(), "Invalid number of columns" );
+
+      const TmpType tmp( rhs );
+      smpAssign( ~lhs, tmp );
+   }
+   /*! \endcond */
+   //**********************************************************************************************
+
+   //**SMP addition assignment to dense matrices***************************************************
+   /*! \cond BLAZE_INTERNAL */
+   /*!\brief SMP addition assignment of a dense matrix transposition expression to a dense matrix.
+   // \ingroup dense_matrix
+   //
+   // \param lhs The target left-hand side dense matrix.
+   // \param rhs The right-hand side transposition expression to be added.
+   // \return void
+   //
+   // This function implements the performance optimized SMP addition assignment of a dense
+   // matrix transposition expression to a dense matrix. Due to the explicit application of
+   // the SFINAE principle, this operator can only be selected by the compiler in case the
+   // target matrix is SMP assignable but the dense matrix operand is not.
+   */
+   template< typename MT2  // Type of the target dense matrix
+           , bool SO2 >    // Storage order of the target dense matrix
+   friend inline typename EnableIf< UseSMPAssign<MT2> >::Type
+      smpAddAssign( DenseMatrix<MT2,SO2>& lhs, const DMatTransExpr& rhs )
+   {
+      BLAZE_FUNCTION_TRACE;
+
+      BLAZE_INTERNAL_ASSERT( (~lhs).rows()    == rhs.rows()   , "Invalid number of rows"    );
+      BLAZE_INTERNAL_ASSERT( (~lhs).columns() == rhs.columns(), "Invalid number of columns" );
+
+      DMatTransposer<MT2,!SO2> tmp( ~lhs );
+      smpAddAssign( tmp, rhs.dm_ );
+   }
+   /*! \endcond */
+   //**********************************************************************************************
+
+   //**SMP addition assignment to sparse matrices**************************************************
+   // No special implementation for the SMP addition assignment to sparse matrices.
+   //**********************************************************************************************
+
+   //**SMP subtraction assignment to dense matrices************************************************
+   /*! \cond BLAZE_INTERNAL */
+   /*!\brief SMP subtraction assignment of a dense matrix transposition expression to a dense matrix.
+   // \ingroup dense_matrix
+   //
+   // \param lhs The target left-hand side dense matrix.
+   // \param rhs The right-hand side transposition expression to be subtracted.
+   // \return void
+   //
+   // This function implements the performance optimized SMP subtraction assignment of a dense
+   // matrix transposition expression to a dense matrix. Due to the explicit application of
+   // the SFINAE principle, this operator can only be selected by the compiler in case the
+   // target matrix is SMP assignable but the dense matrix operand is not.
+   */
+   template< typename MT2  // Type of the target dense matrix
+           , bool SO2 >    // Storage order of the target dense matrix
+   friend inline typename EnableIf< UseSMPAssign<MT2> >::Type
+      smpSubAssign( DenseMatrix<MT2,SO2>& lhs, const DMatTransExpr& rhs )
+   {
+      BLAZE_FUNCTION_TRACE;
+
+      BLAZE_INTERNAL_ASSERT( (~lhs).rows()    == rhs.rows()   , "Invalid number of rows"    );
+      BLAZE_INTERNAL_ASSERT( (~lhs).columns() == rhs.columns(), "Invalid number of columns" );
+
+      DMatTransposer<MT2,!SO2> tmp( ~lhs );
+      smpSubAssign( tmp, rhs.dm_ );
+   }
+   /*! \endcond */
+   //**********************************************************************************************
+
+   //**SMP subtraction assignment to sparse matrices***********************************************
+   // No special implementation for the SMP subtraction assignment to sparse matrices.
+   //**********************************************************************************************
+
+   //**SMP multiplication assignment to dense matrices*********************************************
+   // No special implementation for the SMP multiplication assignment to dense matrices.
+   //**********************************************************************************************
+
+   //**SMP multiplication assignment to sparse matrices********************************************
+   // No special implementation for the SMP multiplication assignment to sparse matrices.
    //**********************************************************************************************
 
    //**Trans function******************************************************************************
