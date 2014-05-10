f43e0183b0a2340899685d359bfda113fd610319
Klaus Iglberger <klaus.iglberger@gmail.com>
2014-05-10 19:48:46 +0200

    Improve the condition for the expression specific parallel evaluation strategy of the 'DMatAbsExpr' expression class

--- blaze/math/expressions/DMatAbsExpr.h
+++ blaze/math/expressions/DMatAbsExpr.h
@@ -133,13 +133,13 @@ class DMatAbsExpr : public DenseMatrix< DMatAbsExpr<MT,SO>, SO >
    /*! \cond BLAZE_INTERNAL */
    //! Helper structure for the explicit application of the SFINAE principle.
    /*! The UseSMPAssign struct is a helper struct for the selection of the parallel evaluation
-       strategy. In case the target matrix is SMP assignable but the dense matrix operand is
-       not and additionally requires an intermediate evaluation, \a value is set to 1 and the
-       expression specific evaluation strategy is selected. Otherwise \a value is set to 0 and
-       the default strategy is chosen. */
+       strategy. In case either the target matrix or the dense matrix operand is not SMP
+       assignable and the matrix operand requires an intermediate evaluation, \a value is set
+       to 1 and the expression specific evaluation strategy is selected. Otherwise \a value is
+       set to 0 and the default strategy is chosen. */
    template< typename MT2 >
    struct UseSMPAssign {
-      enum { value = MT2::smpAssignable && !MT::smpAssignable && useAssign };
+      enum { value = ( !MT2::smpAssignable || !MT::smpAssignable ) && useAssign };
    };
    /*! \endcond */
    //**********************************************************************************************
