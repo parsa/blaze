6974b7c123d07c3feb38ebcce38194a9742ae63b
Klaus Iglberger <klaus.iglberger@gmail.com>
2014-05-10 09:25:50 +0200

    Refine the condition for the expression specific parallel evaluation strategy of the sparse vector transpose expression ('SVecTransExpr')

--- blaze/math/expressions/SVecTransExpr.h
+++ blaze/math/expressions/SVecTransExpr.h
@@ -110,12 +110,13 @@ class SVecTransExpr : public SparseVector< SVecTransExpr<VT,TF>, TF >
    /*! \cond BLAZE_INTERNAL */
    //! Helper structure for the explicit application of the SFINAE principle.
    /*! The UseSMPAssign struct is a helper struct for the selection of the parallel evaluation
-       strategy. In case the target vector is SMP assignable but the sparse vector operand is not,
-       \a value is set to 1 and the expression specific evaluation strategy is selected. Otherwise
-       \a value is set to 0 and the default strategy is chosen. */
+       strategy. In case the target vector is SMP assignable but the sparse vector operand is not
+       and additionally requires an intermediate evaluation, \a value is set to 1 and the expression
+       specific evaluation strategy is selected. Otherwise \a value is set to 0 and the default
+       strategy is chosen. */
    template< typename VT2 >
    struct UseSMPAssign {
-      enum { value = VT2::smpAssignable && !VT::smpAssignable };
+      enum { value = VT2::smpAssignable && !VT::smpAssignable && useAssign };
    };
    /*! \endcond */
    //**********************************************************************************************
@@ -538,7 +539,7 @@ class SVecTransExpr : public SparseVector< SVecTransExpr<VT,TF>, TF >
    // This function implements the performance optimized SMP assignment of a sparse vector
    // transposition expression to a dense vector. Due to the explicit application of the
    // SFINAE principle, this operator can only be selected by the compiler in case the
-   // target vector is SMP assignable but the sparse vector operand is not.
+   // expression specific parallel evaluation strategy is selected.
    */
    template< typename VT2 >  // Type of the target dense vector
    friend inline typename EnableIf< UseSMPAssign<VT2> >::Type
@@ -566,7 +567,7 @@ class SVecTransExpr : public SparseVector< SVecTransExpr<VT,TF>, TF >
    // This function implements the performance optimized SMP assignment of a sparse vector
    // transposition expression to a sparse vector. Due to the explicit application of the
    // SFINAE principle, this operator can only be selected by the compiler in case the
-   // target vector is SMP assignable but the sparse vector operand is not.
+   // expression specific parallel evaluation strategy is selected.
    */
    template< typename VT2 >  // Type of the target sparse vector
    friend inline typename EnableIf< UseSMPAssign<VT2> >::Type
@@ -594,7 +595,7 @@ class SVecTransExpr : public SparseVector< SVecTransExpr<VT,TF>, TF >
    // This function implements the performance optimized SMP addition assignment of a sparse
    // vector transposition expression to a dense vector. Due to the explicit application of
    // the SFINAE principle, this operator can only be selected by the compiler in case the
-   // target vector is SMP assignable but the sparse vector operand is not.
+   // expression specific parallel evaluation strategy is selected.
    */
    template< typename VT2 >  // Type of the target dense vector
    friend inline typename EnableIf< UseSMPAssign<VT2> >::Type
@@ -625,8 +626,8 @@ class SVecTransExpr : public SparseVector< SVecTransExpr<VT,TF>, TF >
    //
    // This function implements the performance optimized SMP subtraction assignment of a sparse
    // vector transposition expression to a dense vector. Due to the explicit application of the
-   // SFINAE principle, this operator can only be selected by the compiler in case the target
-   // vector is SMP assignable but the sparse vector operand is not.
+   // SFINAE principle, this operator can only be selected by the compiler in case the expression
+   // specific parallel evaluation strategy is selected.
    */
    template< typename VT2 >  // Type of the target dense vector
    friend inline typename EnableIf< UseSMPAssign<VT2> >::Type
@@ -658,8 +659,8 @@ class SVecTransExpr : public SparseVector< SVecTransExpr<VT,TF>, TF >
    //
    // This function implements the performance optimized SMP multiplication assignment of a sparse
    // vector transposition expression to a dense vector. Due to the explicit application of the
-   // SFINAE principle, this operator can only be selected by the compiler in case the target
-   // vector is SMP assignable but the sparse vector operand is not.
+   // SFINAE principle, this operator can only be selected by the compiler in case the expression
+   // specific parallel evaluation strategy is selected.
    */
    template< typename VT2 >  // Type of the target dense vector
    friend inline typename EnableIf< UseSMPAssign<VT2> >::Type
