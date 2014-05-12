0bc4229c24abc396552603301e97e96782766519
Klaus Iglberger <klaus.iglberger@gmail.com>
2014-05-12 21:26:47 +0200

    Remove the unneeded <shims/Reset.h> header from the 'TSMatSMatMultExpr' expression template

--- blaze/math/expressions/TSMatSMatMultExpr.h
+++ blaze/math/expressions/TSMatSMatMultExpr.h
@@ -53,7 +53,6 @@
 #include <blaze/math/expressions/MatMatMultExpr.h>
 #include <blaze/math/expressions/SparseMatrix.h>
 #include <blaze/math/shims/IsDefault.h>
-#include <blaze/math/shims/Reset.h>
 #include <blaze/math/shims/Serial.h>
 #include <blaze/math/traits/ColumnExprTrait.h>
 #include <blaze/math/traits/MultExprTrait.h>
