5e4cedbd3bfcd36aac6fc1d759b578d481515273
Klaus Iglberger <klaus.iglberger@gmail.com>
2014-05-12 21:26:30 +0200

    Add the missing <shims/Reset.h> header to several expression templates

--- blaze/math/expressions/SVecSVecMultExpr.h
+++ blaze/math/expressions/SVecSVecMultExpr.h
@@ -48,6 +48,7 @@
 #include <blaze/math/expressions/SparseVector.h>
 #include <blaze/math/expressions/VecVecMultExpr.h>
 #include <blaze/math/Functions.h>
+#include <blaze/math/shims/Reset.h>
 #include <blaze/math/shims/Serial.h>
 #include <blaze/math/traits/MultExprTrait.h>
 #include <blaze/math/traits/MultTrait.h>
