de3a9a79f4644c3eabe100615b6996ce2a915977
Klaus Iglberger <klaus.iglberger@gmail.com>
2014-05-28 21:17:11 +0200

    Add the 'maxThreads' configuration variable

--- blaze/math/smp/default/Functions.h
+++ blaze/math/smp/default/Functions.h
@@ -40,7 +40,7 @@
 // Includes
 //*************************************************************************************************
 
-#include <stdexcept>
+#include <blaze/util/Unused.h>
 
 
 namespace blaze {
@@ -80,19 +80,18 @@ inline size_t getNumThreads()
 /*!\brief Sets the number of threads to be used for thread parallel operations.
 // \ingroup smp
 //
-// \param number The given number of threads \f$[1..\infty)\f$.
+// \param number The given number of threads \f$[1..maxThreads]\f$.
 // \return void
 // \exception std::invalid_argument Invalid number of threads.
 //
 // Via this function the maximum number of threads for thread parallel operations can be specified.
-// Note that the given \a number must be in the range \f$[1..\infty)\f$. In case an invalid number
-// of threads is specified, a \a std::invalid_argument exception is thrown. Also note that in case
-// no parallelization is active, the function has no effect.
+// Note that the given \a number must be in the range \f$[1..maxThreads]\f$. In case an invalid
+// number of threads is specified, a \a std::invalid_argument exception is thrown. Also note that
+// in case no parallelization is active, the function has no effect.
 */
 inline void setNumThreads( size_t number )
 {
-   if( number == 0UL )
-      throw std::invalid_argument( "Invalid number of threads" );
+   UNUSED_PARAMETER( number );
 }
 //*************************************************************************************************
 
