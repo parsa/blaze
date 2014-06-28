ab7a92712c5b53ccf34cb3ca82457b48ec89f683
Klaus Iglberger <klaus.iglberger@gmail.com>
2014-06-28 21:09:47 +0200

    Remove all column specific tests for the 'isnan', 'min', and 'max' functions

--- blazetest/blazetest/mathtest/sparsecolumn/ClassTest.h
+++ blazetest/blazetest/mathtest/sparsecolumn/ClassTest.h
@@ -107,10 +107,7 @@ class ClassTest
    void testLowerBound  ();
    void testUpperBound  ();
    void testIsDefault   ();
-   void testIsNan       ();
    void testIsSame      ();
-   void testMinimum     ();
-   void testMaximum     ();
    void testSubvector   ();
 
    template< typename Type >
