583c161c7b37e08e87dc8c9d8dd7d0338270f431
Klaus Iglberger <klaus.iglberger@gmail.com>
2014-07-30 08:06:25 +0200

    Add dense matrix/sparse vector multiplication tests for the 'SymmetricMatrix' class template

---
+++ blazetest/src/mathtest/dmatsvecmult/SDaVCa.cpp
@@ -0,0 +1,94 @@
+//=================================================================================================
+/*!
+//  \file src/mathtest/dmatsvecmult/SDaVCa.cpp
+//  \brief Source file for the SDaVCa dense matrix/sparse vector multiplication math test
+//
+//  Copyright (C) 2013 Klaus Iglberger - All Rights Reserved
+//
+//  This file is part of the Blaze library. You can redistribute it and/or modify it under
+//  the terms of the New (Revised) BSD License. Redistribution and use in source and binary
+//  forms, with or without modification, are permitted provided that the following conditions
+//  are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this list of
+//     conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright notice, this list
+//     of conditions and the following disclaimer in the documentation and/or other materials
+//     provided with the distribution.
+//  3. Neither the names of the Blaze development group nor the names of its contributors
+//     may be used to endorse or promote products derived from this software without specific
+//     prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+//  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+//  SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+//  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+//  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+//  DAMAGE.
+*/
+//=================================================================================================
+
+
+//*************************************************************************************************
+// Includes
+//*************************************************************************************************
+
+#include <cstdlib>
+#include <iostream>
+#include <blaze/math/CompressedVector.h>
+#include <blaze/math/DynamicMatrix.h>
+#include <blaze/math/SymmetricMatrix.h>
+#include <blazetest/mathtest/Creator.h>
+#include <blazetest/mathtest/dmatsvecmult/OperationTest.h>
+#include <blazetest/system/MathTest.h>
+
+
+//=================================================================================================
+//
+//  MAIN FUNCTION
+//
+//=================================================================================================
+
+//*************************************************************************************************
+int main()
+{
+   std::cout << "   Running 'SDaVCa'..." << std::endl;
+
+   using blazetest::mathtest::TypeA;
+
+   try
+   {
+      // Matrix type definitions
+      typedef blaze::SymmetricMatrix< blaze::DynamicMatrix<TypeA> >  SDa;
+      typedef blaze::CompressedVector<TypeA>                         VCa;
+
+      // Creator type definitions
+      typedef blazetest::Creator<SDa>  CSDa;
+      typedef blazetest::Creator<VCa>  CVCa;
+
+      // Running tests with large matrices and vectors
+      for( size_t i=0UL; i<=6UL; ++i ) {
+         for( size_t j=0UL; j<=i; ++j ) {
+            RUN_DMATSVECMULT_OPERATION_TEST( CSDa( i ), CVCa( i, j ) );
+         }
+      }
+
+      // Running tests with large matrices and vectors
+      RUN_DMATSVECMULT_OPERATION_TEST( CSDa(  67UL ), CVCa(  67UL,  7UL ) );
+      RUN_DMATSVECMULT_OPERATION_TEST( CSDa( 127UL ), CVCa( 127UL, 13UL ) );
+      RUN_DMATSVECMULT_OPERATION_TEST( CSDa(  64UL ), CVCa(  64UL,  8UL ) );
+      RUN_DMATSVECMULT_OPERATION_TEST( CSDa( 128UL ), CVCa( 128UL, 16UL ) );
+   }
+   catch( std::exception& ex ) {
+      std::cerr << "\n\n ERROR DETECTED during dense matrix/sparse vector multiplication:\n"
+                << ex.what() << "\n";
+      return EXIT_FAILURE;
+   }
+
+   return EXIT_SUCCESS;
+}
+//*************************************************************************************************
