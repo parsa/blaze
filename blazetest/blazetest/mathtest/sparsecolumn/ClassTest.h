1619d74fdd4862ee754a5aadc9af6cc16bd1e8ca
Klaus Iglberger <klaus.iglberger@gmail.com>
2014-08-02 14:30:15 +0200

    Extend the 'reset', 'clear', and 'isDefault' tests of the 'SparseColumn' class template

--- blazetest/blazetest/mathtest/sparsecolumn/ClassTest.h
+++ blazetest/blazetest/mathtest/sparsecolumn/ClassTest.h
@@ -99,6 +99,7 @@ class ClassTest
    void testIterator    ();
    void testNonZeros    ();
    void testReset       ();
+   void testClear       ();
    void testAppend      ();
    void testInsert      ();
    void testErase       ();
