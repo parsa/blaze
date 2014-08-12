4e20aec6989a8cd4a6bdd0afc9639ad48ab7e6ff
Klaus Iglberger <klaus.iglberger@gmail.com>
2014-08-12 20:56:02 +0200

    Add the 'find', 'lowerBound', and 'upperBound' functions to the 'SVecScalarMultExpr' expression class

--- blaze/math/expressions/SVecScalarMultExpr.h
+++ blaze/math/expressions/SVecScalarMultExpr.h
@@ -41,6 +41,7 @@
 //*************************************************************************************************
 
 #include <iterator>
+#include <blaze/math/constraints/RequiresEvaluation.h>
 #include <blaze/math/constraints/SparseVector.h>
 #include <blaze/math/constraints/TransposeFlag.h>
 #include <blaze/math/expressions/Computation.h>
@@ -372,6 +373,42 @@ class SVecScalarMultExpr : public SparseVector< SVecScalarMultExpr<VT,ST,TF>, TF
    */
    inline size_t nonZeros() const {
       return vector_.nonZeros();
+   }
+   //**********************************************************************************************
+
+   //**Find function*******************************************************************************
+   /*!\brief Searches for a specific vector element.
+   //
+   // \param index The index of the search element.
+   // \return Iterator to the element in case the index is found, end() iterator otherwise.
+   */
+   inline ConstIterator find( size_t index ) const {
+      BLAZE_CONSTRAINT_MUST_NOT_REQUIRE_EVALUATION( VT );
+      return ConstIterator( vector_.find( index ) );
+   }
+   //**********************************************************************************************
+
+   //**LowerBound function*************************************************************************
+   /*!\brief Returns an iterator to the first index not less then the given index.
+   //
+   // \param index The index of the search element.
+   // \return Iterator to the first index not less then the given index, end() iterator otherwise.
+   */
+   inline ConstIterator lowerBound( size_t index ) const {
+      BLAZE_CONSTRAINT_MUST_NOT_REQUIRE_EVALUATION( VT );
+      return ConstIterator( vector_.lowerBound( index ) );
+   }
+   //**********************************************************************************************
+
+   //**UpperBound function*************************************************************************
+   /*!\brief Returns an iterator to the first index greater then the given index.
+   //
+   // \param index The index of the search element.
+   // \return Iterator to the first index greater then the given index, end() iterator otherwise.
+   */
+   inline ConstIterator upperBound( size_t index ) const {
+      BLAZE_CONSTRAINT_MUST_NOT_REQUIRE_EVALUATION( VT );
+      return ConstIterator( vector_.upperBound( index ) );
    }
    //**********************************************************************************************
 
