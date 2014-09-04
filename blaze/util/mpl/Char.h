30db4eae24c36d453ae2313c52c6704b06042daa
Klaus Iglberger <klaus.iglberger@gmail.com>
2014-09-04 16:56:46 +0200

    Add the 'Char' class template

--- blaze/util/mpl/Char.h
+++ blaze/util/mpl/Char.h
@@ -1,7 +1,7 @@
 //=================================================================================================
 /*!
-//  \file blaze/util/MPL.h
-//  \brief Header file for all meta-programming tools
+//  \file blaze/util/mpl/Char.h
+//  \brief Header file for the Char class template
 //
 //  Copyright (C) 2013 Klaus Iglberger - All Rights Reserved
 //
@@ -32,22 +32,46 @@
 */
 //=================================================================================================
 
-#ifndef _BLAZE_UTIL_MPL_H_
-#define _BLAZE_UTIL_MPL_H_
+#ifndef _BLAZE_UTIL_MPL_CHAR_H_
+#define _BLAZE_UTIL_MPL_CHAR_H_
 
+
+namespace blaze {
+
+//=================================================================================================
+//
+//  CLASS DEFINITION
+//
+//=================================================================================================
 
 //*************************************************************************************************
-// Includes
+/*!\brief Compile time integral constant wrapper for \a char.
+// \ingroup mpl
+//
+// The Char class template represents an integral wrapper for a compile time constant expression
+// of type \a char. The value of an Char can be accessed via the nested \a value (which is guaranteed
+// to be of type \a char), the type can be accessed via the nested type definition \a ValueType.
+
+   \code
+   using namespace blaze;
+
+   Char<3>::value      // Evaluates to 3
+   Char<5>::ValueType  // Results in char
+   \endcode
+*/
+template< char N >
+struct Char
+{
+ public:
+   //**********************************************************************************************
+   /*! \cond BLAZE_INTERNAL */
+   static const char value = N;
+   typedef char  ValueType;
+   /*! \endcond */
+   //**********************************************************************************************
+};
 //*************************************************************************************************
 
-#include <blaze/util/mpl/And.h>
-#include <blaze/util/mpl/Bool.h>
-#include <blaze/util/mpl/Char.h>
-#include <blaze/util/mpl/EqualTo.h>
-#include <blaze/util/mpl/If.h>
-#include <blaze/util/mpl/Int.h>
-#include <blaze/util/mpl/Long.h>
-#include <blaze/util/mpl/Not.h>
-#include <blaze/util/mpl/Or.h>
+} // namespace blaze
 
 #endif
