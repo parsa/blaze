9d739dd1ab93334f8d00e790d27c8e8c4016e745
Klaus Iglberger <klaus.iglberger@gmail.com>
2014-11-26 07:43:09 +0100

    Add the missing <blaze/math/SparseMatrix.h> include to the <blaze/math/SymmetricMatrix.h> header

--- blaze/math/SymmetricMatrix.h
+++ blaze/math/SymmetricMatrix.h
@@ -47,6 +47,7 @@
 #include <blaze/math/constraints/Resizable.h>
 #include <blaze/math/constraints/SparseMatrix.h>
 #include <blaze/math/DenseMatrix.h>
+#include <blaze/math/SparseMatrix.h>
 #include <blaze/math/typetraits/IsDenseMatrix.h>
 #include <blaze/util/FalseType.h>
 #include <blaze/util/Random.h>
