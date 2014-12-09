964d05365e00cc09910521d1de37edefd756bac5
Klaus Iglberger <klaus.iglberger@gmail.com>
2014-12-09 10:36:42 +0100

    Add a specialization of the 'Rand' class template for the 'LowerMatrix' adaptor

--- blaze/math/LowerMatrix.h
+++ blaze/math/LowerMatrix.h
@@ -40,6 +40,507 @@
 // Includes
 //*************************************************************************************************
 
+#include <cmath>
+#include <stdexcept>
 #include <blaze/math/adaptors/LowerMatrix.h>
+#include <blaze/math/constraints/DenseMatrix.h>
+#include <blaze/math/constraints/Resizable.h>
+#include <blaze/math/constraints/SparseMatrix.h>
+#include <blaze/math/DenseMatrix.h>
+#include <blaze/math/SparseMatrix.h>
+#include <blaze/math/typetraits/IsDenseMatrix.h>
+#include <blaze/util/FalseType.h>
+#include <blaze/util/Random.h>
+#include <blaze/util/TrueType.h>
+#include <blaze/util/Types.h>
+
+
+namespace blaze {
+
+//=================================================================================================
+//
+//  RAND SPECIALIZATION
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Specialization of the Rand class template for LowerMatrix.
+// \ingroup random
+//
+// This specialization of the Rand class creates random instances of LowerMatrix.
+*/
+template< typename MT  // Type of the adapted matrix
+        , bool SO      // Storage order of the adapted matrix
+        , bool DF >    // Numeric flag
+class Rand< LowerMatrix<MT,SO,DF> >
+{
+ public:
+   //**Generate functions**************************************************************************
+   /*!\name Generate functions */
+   //@{
+   inline const LowerMatrix<MT,SO,DF> generate() const;
+   inline const LowerMatrix<MT,SO,DF> generate( size_t n ) const;
+   inline const LowerMatrix<MT,SO,DF> generate( size_t n, size_t nonzeros ) const;
+
+   template< typename Arg >
+   inline const LowerMatrix<MT,SO,DF> generate( const Arg& min, const Arg& max ) const;
+
+   template< typename Arg >
+   inline const LowerMatrix<MT,SO,DF> generate( size_t n, const Arg& min, const Arg& max ) const;
+
+   template< typename Arg >
+   inline const LowerMatrix<MT,SO,DF> generate( size_t n, size_t nonzeros,
+                                                const Arg& min, const Arg& max ) const;
+   //@}
+   //**********************************************************************************************
+
+   //**Randomize functions*************************************************************************
+   /*!\name Randomize functions */
+   //@{
+   inline void randomize( LowerMatrix<MT,SO,DF>& matrix ) const;
+   inline void randomize( LowerMatrix<MT,SO,DF>& matrix, size_t nonzeros ) const;
+
+   template< typename Arg >
+   inline void randomize( LowerMatrix<MT,SO,DF>& matrix, const Arg& min, const Arg& max ) const;
+
+   template< typename Arg >
+   inline void randomize( LowerMatrix<MT,SO,DF>& matrix, size_t nonzeros,
+                          const Arg& min, const Arg& max ) const;
+   //@}
+   //**********************************************************************************************
+
+ private:
+   //**Randomize functions*************************************************************************
+   /*!\name Randomize functions */
+   //@{
+   inline void randomize( LowerMatrix<MT,SO,DF>& matrix, TrueType  ) const;
+   inline void randomize( LowerMatrix<MT,SO,DF>& matrix, FalseType ) const;
+
+   template< typename Arg >
+   inline void randomize( LowerMatrix<MT,SO,DF>& matrix, const Arg& min, const Arg& max, TrueType ) const;
+
+   template< typename Arg >
+   inline void randomize( LowerMatrix<MT,SO,DF>& matrix, const Arg& min, const Arg& max, FalseType ) const;
+   //@}
+   //**********************************************************************************************
+};
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Generation of a random LowerMatrix.
+//
+// \return The generated random matrix.
+*/
+template< typename MT  // Type of the adapted matrix
+        , bool SO      // Storage order of the adapted matrix
+        , bool DF >    // Numeric flag
+inline const LowerMatrix<MT,SO,DF> Rand< LowerMatrix<MT,SO,DF> >::generate() const
+{
+   BLAZE_CONSTRAINT_MUST_NOT_BE_RESIZABLE( MT );
+
+   LowerMatrix<MT,SO,DF> matrix;
+   randomize( matrix );
+   return matrix;
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Generation of a random LowerMatrix.
+//
+// \param n The number of rows and columns of the random matrix.
+// \return The generated random matrix.
+*/
+template< typename MT  // Type of the adapted matrix
+        , bool SO      // Storage order of the adapted matrix
+        , bool DF >    // Numeric flag
+inline const LowerMatrix<MT,SO,DF>
+   Rand< LowerMatrix<MT,SO,DF> >::generate( size_t n ) const
+{
+   BLAZE_CONSTRAINT_MUST_BE_RESIZABLE( MT );
+
+   LowerMatrix<MT,SO,DF> matrix( n );
+   randomize( matrix );
+   return matrix;
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Generation of a random LowerMatrix.
+//
+// \param n The number of rows and columns of the random matrix.
+// \param nonzeros The number of non-zero elements of the random matrix.
+// \return The generated random matrix.
+// \exception std::invalid_argument Invalid number of non-zero elements.
+*/
+template< typename MT  // Type of the adapted matrix
+        , bool SO      // Storage order of the adapted matrix
+        , bool DF >    // Numeric flag
+inline const LowerMatrix<MT,SO,DF>
+   Rand< LowerMatrix<MT,SO,DF> >::generate( size_t n, size_t nonzeros ) const
+{
+   BLAZE_CONSTRAINT_MUST_BE_RESIZABLE         ( MT );
+   BLAZE_CONSTRAINT_MUST_BE_SPARSE_MATRIX_TYPE( MT );
+
+   if( nonzeros > ( ( n + 1UL ) * n ) / 2UL )
+      throw std::invalid_argument( "Invalid number of non-zero elements" );
+
+   LowerMatrix<MT,SO,DF> matrix( n );
+   randomize( matrix, nonzeros );
+
+   return matrix;
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Generation of a random LowerMatrix.
+//
+// \param min The smallest possible value for a matrix element.
+// \param max The largest possible value for a matrix element.
+// \return The generated random matrix.
+*/
+template< typename MT     // Type of the adapted matrix
+        , bool SO         // Storage order of the adapted matrix
+        , bool DF >       // Numeric flag
+template< typename Arg >  // Min/max argument type
+inline const LowerMatrix<MT,SO,DF>
+   Rand< LowerMatrix<MT,SO,DF> >::generate( const Arg& min, const Arg& max ) const
+{
+   BLAZE_CONSTRAINT_MUST_NOT_BE_RESIZABLE( MT );
+
+   LowerMatrix<MT,SO,DF> matrix;
+   randomize( matrix, min, max );
+   return matrix;
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Generation of a random LowerMatrix.
+//
+// \param n The number of rows and columns of the random matrix.
+// \param min The smallest possible value for a matrix element.
+// \param max The largest possible value for a matrix element.
+// \return The generated random matrix.
+*/
+template< typename MT     // Type of the adapted matrix
+        , bool SO         // Storage order of the adapted matrix
+        , bool DF >       // Numeric flag
+template< typename Arg >  // Min/max argument type
+inline const LowerMatrix<MT,SO,DF>
+   Rand< LowerMatrix<MT,SO,DF> >::generate( size_t n, const Arg& min, const Arg& max ) const
+{
+   BLAZE_CONSTRAINT_MUST_BE_RESIZABLE( MT );
+
+   LowerMatrix<MT,SO,DF> matrix( n );
+   randomize( matrix, min, max );
+   return matrix;
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Generation of a random LowerMatrix.
+//
+// \param n The number of rows and columns of the random matrix.
+// \param nonzeros The number of non-zero elements of the random matrix.
+// \param min The smallest possible value for a matrix element.
+// \param max The largest possible value for a matrix element.
+// \return The generated random matrix.
+// \exception std::invalid_argument Invalid number of non-zero elements.
+*/
+template< typename MT     // Type of the adapted matrix
+        , bool SO         // Storage order of the adapted matrix
+        , bool DF >       // Numeric flag
+template< typename Arg >  // Min/max argument type
+inline const LowerMatrix<MT,SO,DF>
+   Rand< LowerMatrix<MT,SO,DF> >::generate( size_t n, size_t nonzeros,
+                                            const Arg& min, const Arg& max ) const
+{
+   BLAZE_CONSTRAINT_MUST_BE_RESIZABLE         ( MT );
+   BLAZE_CONSTRAINT_MUST_BE_SPARSE_MATRIX_TYPE( MT );
+
+   if( nonzeros > ( ( n + 1UL ) * n ) / 2UL )
+      throw std::invalid_argument( "Invalid number of non-zero elements" );
+
+   LowerMatrix<MT,SO,DF> matrix( n );
+   randomize( matrix, nonzeros, min, max );
+
+   return matrix;
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Randomization of a LowerMatrix.
+//
+// \param matrix The matrix to be randomized.
+// \return void
+*/
+template< typename MT  // Type of the adapted matrix
+        , bool SO      // Storage order of the adapted matrix
+        , bool DF >    // Numeric flag
+inline void Rand< LowerMatrix<MT,SO,DF> >::randomize( LowerMatrix<MT,SO,DF>& matrix ) const
+{
+   randomize( matrix, typename IsDenseMatrix<MT>::Type() );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Randomization of a dense LowerMatrix.
+//
+// \param matrix The matrix to be randomized.
+// \return void
+*/
+template< typename MT  // Type of the adapted matrix
+        , bool SO      // Storage order of the adapted matrix
+        , bool DF >    // Numeric flag
+inline void Rand< LowerMatrix<MT,SO,DF> >::randomize( LowerMatrix<MT,SO,DF>& matrix, TrueType ) const
+{
+   BLAZE_CONSTRAINT_MUST_BE_DENSE_MATRIX_TYPE( MT );
+
+   typedef typename MT::ElementType  ET;
+
+   const size_t n( matrix.rows() );
+
+   for( size_t i=0UL; i<n; ++i ) {
+      for( size_t j=0UL; j<=i; ++j ) {
+         matrix(i,j) = rand<ET>();
+      }
+   }
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Randomization of a sparse LowerMatrix.
+//
+// \param matrix The matrix to be randomized.
+// \return void
+*/
+template< typename MT  // Type of the adapted matrix
+        , bool SO      // Storage order of the adapted matrix
+        , bool DF >    // Numeric flag
+inline void Rand< LowerMatrix<MT,SO,DF> >::randomize( LowerMatrix<MT,SO,DF>& matrix, FalseType ) const
+{
+   BLAZE_CONSTRAINT_MUST_BE_SPARSE_MATRIX_TYPE( MT );
+
+   typedef typename MT::ElementType  ET;
+
+   const size_t n( matrix.rows() );
+
+   if( n == 0UL ) return;
+
+   const size_t nonzeros( rand<size_t>( 1UL, std::ceil( 0.3*n*n ) ) );
+
+   matrix.reset();
+   matrix.reserve( nonzeros );
+
+   while( matrix.nonZeros() < nonzeros ) {
+      const size_t row( rand<size_t>( 0UL, n-1UL ) );
+      const size_t col( rand<size_t>( 0UL, row ) );
+      matrix(row,col) = rand<ET>();
+   }
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Randomization of a sparse LowerMatrix.
+//
+// \param matrix The matrix to be randomized.
+// \param nonzeros The number of non-zero elements of the random matrix.
+// \return void
+// \exception std::invalid_argument Invalid number of non-zero elements.
+*/
+template< typename MT  // Type of the adapted matrix
+        , bool SO      // Storage order of the adapted matrix
+        , bool DF >    // Numeric flag
+inline void Rand< LowerMatrix<MT,SO,DF> >::randomize( LowerMatrix<MT,SO,DF>& matrix, size_t nonzeros ) const
+{
+   BLAZE_CONSTRAINT_MUST_BE_SPARSE_MATRIX_TYPE( MT );
+
+   typedef typename MT::ElementType  ET;
+
+   const size_t n( matrix.rows() );
+
+   if( nonzeros > ( ( n + 1UL ) * n ) / 2UL )
+      throw std::invalid_argument( "Invalid number of non-zero elements" );
+
+   if( n == 0UL ) return;
+
+   matrix.reset();
+   matrix.reserve( nonzeros );
+
+   while( matrix.nonZeros() < nonzeros ) {
+      const size_t row( rand<size_t>( 0UL, n-1UL ) );
+      const size_t col( rand<size_t>( 0UL, row ) );
+      matrix(row,col) = rand<ET>();
+   }
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Randomization of a LowerMatrix.
+//
+// \param matrix The matrix to be randomized.
+// \param min The smallest possible value for a matrix element.
+// \param max The largest possible value for a matrix element.
+// \return void
+*/
+template< typename MT     // Type of the adapted matrix
+        , bool SO         // Storage order of the adapted matrix
+        , bool DF >       // Numeric flag
+template< typename Arg >  // Min/max argument type
+inline void Rand< LowerMatrix<MT,SO,DF> >::randomize( LowerMatrix<MT,SO,DF>& matrix,
+                                                      const Arg& min, const Arg& max ) const
+{
+   randomize( matrix, min, max, typename IsDenseMatrix<MT>::Type() );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Randomization of a dense LowerMatrix.
+//
+// \param matrix The matrix to be randomized.
+// \param min The smallest possible value for a matrix element.
+// \param max The largest possible value for a matrix element.
+// \return void
+*/
+template< typename MT     // Type of the adapted matrix
+        , bool SO         // Storage order of the adapted matrix
+        , bool DF >       // Numeric flag
+template< typename Arg >  // Min/max argument type
+inline void Rand< LowerMatrix<MT,SO,DF> >::randomize( LowerMatrix<MT,SO,DF>& matrix,
+                                                      const Arg& min, const Arg& max, TrueType ) const
+{
+   BLAZE_CONSTRAINT_MUST_BE_DENSE_MATRIX_TYPE( MT );
+
+   typedef typename MT::ElementType  ET;
+
+   const size_t n( matrix.rows() );
+
+   for( size_t i=0UL; i<n; ++i ) {
+      for( size_t j=0UL; j<=i; ++j ) {
+         matrix(i,j) = rand<ET>( min, max );
+      }
+   }
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Randomization of a sparse LowerMatrix.
+//
+// \param matrix The matrix to be randomized.
+// \param min The smallest possible value for a matrix element.
+// \param max The largest possible value for a matrix element.
+// \return void
+*/
+template< typename MT     // Type of the adapted matrix
+        , bool SO         // Storage order of the adapted matrix
+        , bool DF >       // Numeric flag
+template< typename Arg >  // Min/max argument type
+inline void Rand< LowerMatrix<MT,SO,DF> >::randomize( LowerMatrix<MT,SO,DF>& matrix,
+                                                      const Arg& min, const Arg& max, FalseType ) const
+{
+   BLAZE_CONSTRAINT_MUST_BE_SPARSE_MATRIX_TYPE( MT );
+
+   typedef typename MT::ElementType  ET;
+
+   const size_t n( matrix.rows() );
+
+   if( n == 0UL ) return;
+
+   const size_t nonzeros( rand<size_t>( 1UL, std::ceil( 0.3*n*n ) ) );
+
+   matrix.reset();
+   matrix.reserve( nonzeros );
+
+   while( matrix.nonZeros() < nonzeros ) {
+      const size_t row( rand<size_t>( 0UL, n-1UL ) );
+      const size_t col( rand<size_t>( 0UL, row ) );
+      matrix(row,col) = rand<ET>( min, max );
+   }
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Randomization of a sparse LowerMatrix.
+//
+// \param matrix The matrix to be randomized.
+// \param nonzeros The number of non-zero elements of the random matrix.
+// \param min The smallest possible value for a matrix element.
+// \param max The largest possible value for a matrix element.
+// \return void
+// \exception std::invalid_argument Invalid number of non-zero elements.
+*/
+template< typename MT     // Type of the adapted matrix
+        , bool SO         // Storage order of the adapted matrix
+        , bool DF >       // Numeric flag
+template< typename Arg >  // Min/max argument type
+inline void Rand< LowerMatrix<MT,SO,DF> >::randomize( LowerMatrix<MT,SO,DF>& matrix,
+                                                      size_t nonzeros, const Arg& min, const Arg& max ) const
+{
+   BLAZE_CONSTRAINT_MUST_BE_SPARSE_MATRIX_TYPE( MT );
+
+   typedef typename MT::ElementType  ET;
+
+   const size_t n( matrix.rows() );
+
+   if( nonzeros > ( ( n + 1UL ) * n ) / 2UL )
+      throw std::invalid_argument( "Invalid number of non-zero elements" );
+
+   if( n == 0UL ) return;
+
+   matrix.reset();
+   matrix.reserve( nonzeros );
+
+   while( matrix.nonZeros() < nonzeros ) {
+      const size_t row( rand<size_t>( 0UL, n-1UL ) );
+      const size_t col( rand<size_t>( 0UL, row ) );
+      matrix(row,col) = rand<ET>( min, max );
+   }
+}
+/*! \endcond */
+//*************************************************************************************************
+
+} // namespace blaze
 
 #endif
