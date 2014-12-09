3a28b92ef661db6fdf93a52bffe79296d776a4b3
Klaus Iglberger <klaus.iglberger@gmail.com>
2014-12-09 07:25:59 +0100

    Fix the header guard of the dense matrix specialization of the 'UpperMatrix' class template

--- blaze/math/adaptors/uppermatrix/Dense.h
+++ blaze/math/adaptors/uppermatrix/Dense.h
@@ -32,8 +32,8 @@
 */
 //=================================================================================================
 
-#ifndef _BLAZE_MATH_ADAPTORS_LOWERMATRIX_DENSE_H_
-#define _BLAZE_MATH_ADAPTORS_LOWERMATRIX_DENSE_H_
+#ifndef _BLAZE_MATH_ADAPTORS_UPPERMATRIX_DENSE_H_
+#define _BLAZE_MATH_ADAPTORS_UPPERMATRIX_DENSE_H_
 
 
 //*************************************************************************************************
