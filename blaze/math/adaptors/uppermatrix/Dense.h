a28ca17a4ccd85b5fe64e7ee0f9b2bcf5e4dddf8
Klaus Iglberger <klaus.iglberger@gmail.com>
2014-12-28 07:44:03 +0100

    Add an overload of the 'derestrict' shim for the 'UpperMatrix' class template

--- blaze/math/adaptors/uppermatrix/Dense.h
+++ blaze/math/adaptors/uppermatrix/Dense.h
@@ -701,6 +701,9 @@ class UpperMatrix<MT,SO,true>
    //**Friend declarations*************************************************************************
    template< typename MT2, bool SO2, bool DF2 >
    friend bool isDefault( const UpperMatrix<MT2,SO2,DF2>& m );
+
+   template< typename MT2, bool SO2, bool DF2 >
+   friend MT2& derestrict( UpperMatrix<MT2,SO2,DF2>& m );
    //**********************************************************************************************
 
    //**Compile time checks*************************************************************************
