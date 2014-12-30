62b411bd2c559c475ef7463f4a74a2a4e79342a7
Klaus Iglberger <klaus.iglberger@gmail.com>
2014-12-30 06:46:23 +0100

    Add the 'DerestrictTrait' class template

--- blaze/math/Traits.h
+++ blaze/math/Traits.h
@@ -47,6 +47,7 @@
 #include <blaze/math/traits/ColumnTrait.h>
 #include <blaze/math/traits/CrossExprTrait.h>
 #include <blaze/math/traits/CrossTrait.h>
+#include <blaze/math/traits/DerestrictTrait.h>
 #include <blaze/math/traits/DivExprTrait.h>
 #include <blaze/math/traits/DivTrait.h>
 #include <blaze/math/traits/DMatAbsExprTrait.h>
