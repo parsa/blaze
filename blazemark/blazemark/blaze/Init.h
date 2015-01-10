652822cce8abf4efef2af9f74ca8095aa995f083
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-01-10 14:19:48 +0100

    Add an 'init' function for the initialization of upper dynamic matrices in benchmarks

--- blazemark/blazemark/blaze/Init.h
+++ blazemark/blazemark/blaze/Init.h
@@ -44,6 +44,7 @@
 #include <blazemark/blaze/init/CompressedVector.h>
 #include <blazemark/blaze/init/DynamicLower.h>
 #include <blazemark/blaze/init/DynamicMatrix.h>
+#include <blazemark/blaze/init/DynamicUpper.h>
 #include <blazemark/blaze/init/DynamicVector.h>
 #include <blazemark/blaze/init/StaticLower.h>
 #include <blazemark/blaze/init/StaticMatrix.h>
