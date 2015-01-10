5091b5309775837e53682cf9d9ed19cc3aae8c71
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-01-10 05:55:45 +0100

    Improve all inner product tests

--- blazetest/blazetest/mathtest/tdvecsvecmult/OperationTest.h
+++ blazetest/blazetest/mathtest/tdvecsvecmult/OperationTest.h
@@ -83,14 +83,13 @@ class OperationTest
 {
  private:
    //**Type definitions****************************************************************************
+   typedef typename VT1::ElementType  ET1;  //!< Element type 1
+   typedef typename VT2::ElementType  ET2;  //!< Element type 2
+
    typedef typename VT1::TransposeType                TVT1;  //!< Transpose vector type 1
    typedef typename VT2::TransposeType                TVT2;  //!< Transpose vector type 2
-   typedef typename blaze::MultTrait<TVT1,VT2>::Type  RE;    //!< Default result type
-   //**********************************************************************************************
+   typedef typename blaze::MultTrait<TVT1,VT2>::Type  RE;    //!< Result type
 
-   //**Type definitions****************************************************************************
-   typedef typename VT1::ElementType        ET1;  //!< Element type 1
-   typedef typename VT2::ElementType        ET2;  //!< Element type 2
    typedef blaze::DynamicVector<ET1,true>   RT1;  //!< Reference type 1
    typedef blaze::DynamicVector<ET2,false>  RT2;  //!< Reference type 2
    //**********************************************************************************************
@@ -155,12 +154,14 @@ class OperationTest
    BLAZE_CONSTRAINT_MUST_BE_SPARSE_VECTOR_TYPE( TVT2  );
    BLAZE_CONSTRAINT_MUST_BE_DENSE_VECTOR_TYPE ( RT1   );
    BLAZE_CONSTRAINT_MUST_BE_DENSE_VECTOR_TYPE ( RT2   );
+
    BLAZE_CONSTRAINT_MUST_BE_COLUMN_VECTOR_TYPE( VT1   );
    BLAZE_CONSTRAINT_MUST_BE_COLUMN_VECTOR_TYPE( VT2   );
    BLAZE_CONSTRAINT_MUST_BE_ROW_VECTOR_TYPE   ( TVT1  );
    BLAZE_CONSTRAINT_MUST_BE_ROW_VECTOR_TYPE   ( TVT2  );
    BLAZE_CONSTRAINT_MUST_BE_ROW_VECTOR_TYPE   ( RT1   );
    BLAZE_CONSTRAINT_MUST_BE_COLUMN_VECTOR_TYPE( RT2   );
+
    BLAZE_CONSTRAINT_MUST_BE_SAME_TYPE( ET1, typename TVT1::ElementType );
    BLAZE_CONSTRAINT_MUST_BE_SAME_TYPE( ET2, typename TVT2::ElementType );
    /*! \endcond */
