bf1d7ac942c20babb71d591865c1130eb183787d
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-03-21 20:07:25 +0100

    Add a specialization of the 'Creator' class for diagonal hybrid matrices

--- blazetest/blazetest/mathtest/Creator.h
+++ blazetest/blazetest/mathtest/Creator.h
@@ -52,6 +52,7 @@
 #include <blazetest/mathtest/creator/DynamicSymmetric.h>
 #include <blazetest/mathtest/creator/DynamicUpper.h>
 #include <blazetest/mathtest/creator/DynamicVector.h>
+#include <blazetest/mathtest/creator/HybridDiagonal.h>
 #include <blazetest/mathtest/creator/HybridLower.h>
 #include <blazetest/mathtest/creator/HybridMatrix.h>
 #include <blazetest/mathtest/creator/HybridSymmetric.h>
