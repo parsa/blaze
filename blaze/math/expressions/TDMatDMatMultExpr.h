79b664353405de795d604f5ac36fb934f6b0e5b1
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-04-08 06:02:31 +0200

    Add a specialization of the 'IsIdentity' type trait for the 'TDMatDMatMultExpr' expression class

--- blaze/math/expressions/TDMatDMatMultExpr.h
+++ blaze/math/expressions/TDMatDMatMultExpr.h
@@ -73,6 +73,7 @@
 #include <blaze/math/typetraits/IsDenseMatrix.h>
 #include <blaze/math/typetraits/IsDenseVector.h>
 #include <blaze/math/typetraits/IsExpression.h>
+#include <blaze/math/typetraits/IsIdentity.h>
 #include <blaze/math/typetraits/IsLower.h>
 #include <blaze/math/typetraits/IsRowMajorMatrix.h>
 #include <blaze/math/typetraits/IsRowVector.h>
@@ -6702,6 +6703,24 @@ struct IsLower< TDMatDMatMultExpr<MT1,MT2> >
 template< typename MT1, typename MT2 >
 struct IsUpper< TDMatDMatMultExpr<MT1,MT2> >
    : public IsTrue< IsUpper<MT1>::value && IsUpper<MT2>::value >
+{};
+/*! \endcond */
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  ISIDENTITY SPECIALIZATIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+template< typename MT1, typename MT2 >
+struct IsIdentity< TDMatDMatMultExpr<MT1,MT2> >
+   : public IsTrue< IsIdentity<MT1>::value && IsIdentity<MT2>::value >
 {};
 /*! \endcond */
 //*************************************************************************************************
