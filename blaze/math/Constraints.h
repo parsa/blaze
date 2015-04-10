841849ce4ce67d8e64bbbb3e8efc48b73644a1b5
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-04-10 05:36:54 +0200

    Add the 'IsUniUpper' type trait along with the according compile time constraints

--- blaze/math/Constraints.h
+++ blaze/math/Constraints.h
@@ -89,6 +89,7 @@
 #include <blaze/math/constraints/Triangular.h>
 #include <blaze/math/constraints/TVecMatMultExpr.h>
 #include <blaze/math/constraints/UniLower.h>
+#include <blaze/math/constraints/UniUpper.h>
 #include <blaze/math/constraints/Upper.h>
 #include <blaze/math/constraints/VecAbsExpr.h>
 #include <blaze/math/constraints/VecEvalExpr.h>
