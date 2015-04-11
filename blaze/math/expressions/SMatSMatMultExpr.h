3c2ad8b354f016d17ea534aa50207afbbe5efd17
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-04-11 06:13:24 +0200

    Add specializations of the 'IsUniLower' and 'IsUniUpper' type traits for the 'SMatSMatMultExpr' expression class

--- blaze/math/expressions/SMatSMatMultExpr.h
+++ blaze/math/expressions/SMatSMatMultExpr.h
@@ -71,7 +71,6 @@
 #include <blaze/math/typetraits/IsComputation.h>
 #include <blaze/math/typetraits/IsDenseVector.h>
 #include <blaze/math/typetraits/IsExpression.h>
-#include <blaze/math/typetraits/IsIdentity.h>
 #include <blaze/math/typetraits/IsLower.h>
 #include <blaze/math/typetraits/IsResizable.h>
 #include <blaze/math/typetraits/IsRowMajorMatrix.h>
@@ -79,6 +78,8 @@
 #include <blaze/math/typetraits/IsSparseMatrix.h>
 #include <blaze/math/typetraits/IsSparseVector.h>
 #include <blaze/math/typetraits/IsSymmetric.h>
+#include <blaze/math/typetraits/IsUniLower.h>
+#include <blaze/math/typetraits/IsUniUpper.h>
 #include <blaze/math/typetraits/IsUpper.h>
 #include <blaze/math/typetraits/RequiresEvaluation.h>
 #include <blaze/math/typetraits/Rows.h>
@@ -1198,6 +1199,24 @@ struct IsLower< SMatSMatMultExpr<MT1,MT2> >
 
 //=================================================================================================
 //
+//  ISUNILOWER SPECIALIZATIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+template< typename MT1, typename MT2 >
+struct IsUniLower< SMatSMatMultExpr<MT1,MT2> >
+   : public IsTrue< IsUniLower<MT1>::value && IsUniLower<MT2>::value >
+{};
+/*! \endcond */
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
 //  ISUPPER SPECIALIZATIONS
 //
 //=================================================================================================
@@ -1216,15 +1235,15 @@ struct IsUpper< SMatSMatMultExpr<MT1,MT2> >
 
 //=================================================================================================
 //
-//  ISIDENTITY SPECIALIZATIONS
+//  ISUNIUPPER SPECIALIZATIONS
 //
 //=================================================================================================
 
 //*************************************************************************************************
 /*! \cond BLAZE_INTERNAL */
 template< typename MT1, typename MT2 >
-struct IsIdentity< SMatSMatMultExpr<MT1,MT2> >
-   : public IsTrue< IsIdentity<MT1>::value && IsIdentity<MT2>::value >
+struct IsUniUpper< SMatSMatMultExpr<MT1,MT2> >
+   : public IsTrue< IsUniUpper<MT1>::value && IsUniUpper<MT2>::value >
 {};
 /*! \endcond */
 //*************************************************************************************************
