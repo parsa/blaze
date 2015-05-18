b7989b7066439351336e87a91575d88b8fda56ee
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-05-18 06:05:20 +0300

    Add a specialization of the 'Creator' class for lower unitriangular static matrices

--- blazetest/blazetest/mathtest/Creator.h
+++ blazetest/blazetest/mathtest/Creator.h
@@ -64,6 +64,7 @@
 #include <blazetest/mathtest/creator/StaticLower.h>
 #include <blazetest/mathtest/creator/StaticMatrix.h>
 #include <blazetest/mathtest/creator/StaticSymmetric.h>
+#include <blazetest/mathtest/creator/StaticUniLower.h>
 #include <blazetest/mathtest/creator/StaticUpper.h>
 #include <blazetest/mathtest/creator/StaticVector.h>
 
