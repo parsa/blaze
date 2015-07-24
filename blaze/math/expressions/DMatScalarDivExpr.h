93b248d58aa0cf59405b4bb147493113980cd54e
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-07-24 06:05:21 +0200

    Add a specialization of the 'IsHermitian' type trait for the 'DMatScalarDivExpr' expression class

--- blaze/math/expressions/DMatScalarDivExpr.h
+++ blaze/math/expressions/DMatScalarDivExpr.h
@@ -62,6 +62,7 @@
 #include <blaze/math/typetraits/IsComputation.h>
 #include <blaze/math/typetraits/IsDenseMatrix.h>
 #include <blaze/math/typetraits/IsExpression.h>
+#include <blaze/math/typetraits/IsHermitian.h>
 #include <blaze/math/typetraits/IsLower.h>
 #include <blaze/math/typetraits/IsMultExpr.h>
 #include <blaze/math/typetraits/IsRowMajorMatrix.h>
@@ -1107,6 +1108,23 @@ struct Columns< DMatScalarDivExpr<MT,ST,SO> > : public Rows<MT>
 /*! \cond BLAZE_INTERNAL */
 template< typename MT, typename ST, bool SO >
 struct IsSymmetric< DMatScalarDivExpr<MT,ST,SO> > : public IsTrue< IsSymmetric<MT>::value >
+{};
+/*! \endcond */
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  ISHERMITIAN SPECIALIZATIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+template< typename MT, typename ST, bool SO >
+struct IsHermitian< DMatScalarDivExpr<MT,ST,SO> > : public IsTrue< IsHermitian<MT>::value >
 {};
 /*! \endcond */
 //*************************************************************************************************
