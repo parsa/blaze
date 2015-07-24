80fb54349529cc78df5bf3408582b26d2ed5839e
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-07-24 06:09:37 +0200

    Add a specialization of the 'IsHermitian' type trait for the 'SMatSMatSubExpr' expression class

--- blaze/math/expressions/SMatSMatSubExpr.h
+++ blaze/math/expressions/SMatSMatSubExpr.h
@@ -59,6 +59,7 @@
 #include <blaze/math/typetraits/Columns.h>
 #include <blaze/math/typetraits/IsComputation.h>
 #include <blaze/math/typetraits/IsExpression.h>
+#include <blaze/math/typetraits/IsHermitian.h>
 #include <blaze/math/typetraits/IsLower.h>
 #include <blaze/math/typetraits/IsResizable.h>
 #include <blaze/math/typetraits/IsStrictlyLower.h>
@@ -846,6 +847,24 @@ struct Columns< SMatSMatSubExpr<MT1,MT2> >
 template< typename MT1, typename MT2 >
 struct IsSymmetric< SMatSMatSubExpr<MT1,MT2> >
    : public IsTrue< IsSymmetric<MT1>::value && IsSymmetric<MT2>::value >
+{};
+/*! \endcond */
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  ISHERMITIAN SPECIALIZATIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+template< typename MT1, typename MT2 >
+struct IsHermitian< SMatSMatSubExpr<MT1,MT2> >
+   : public IsTrue< IsHermitian<MT1>::value && IsHermitian<MT2>::value >
 {};
 /*! \endcond */
 //*************************************************************************************************
