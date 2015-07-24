dc1f20539edf9522de1a4aff08a387c7d421117c
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-07-24 06:07:27 +0200

    Add a specialization of the 'IsHermitian' type trait for the 'DMatSMatAddExpr' expression class

--- blaze/math/expressions/DMatSMatAddExpr.h
+++ blaze/math/expressions/DMatSMatAddExpr.h
@@ -60,6 +60,7 @@
 #include <blaze/math/typetraits/IsColumnMajorMatrix.h>
 #include <blaze/math/typetraits/IsDenseMatrix.h>
 #include <blaze/math/typetraits/IsExpression.h>
+#include <blaze/math/typetraits/IsHermitian.h>
 #include <blaze/math/typetraits/IsLower.h>
 #include <blaze/math/typetraits/IsRowMajorMatrix.h>
 #include <blaze/math/typetraits/IsSparseMatrix.h>
@@ -776,6 +777,24 @@ struct Columns< DMatSMatAddExpr<MT1,MT2,SO> >
 template< typename MT1, typename MT2, bool SO >
 struct IsSymmetric< DMatSMatAddExpr<MT1,MT2,SO> >
    : public IsTrue< IsSymmetric<MT1>::value && IsSymmetric<MT2>::value >
+{};
+/*! \endcond */
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  ISHERMITIAN SPECIALIZATIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+template< typename MT1, typename MT2, bool SO >
+struct IsHermitian< DMatSMatAddExpr<MT1,MT2,SO> >
+   : public IsTrue< IsHermitian<MT1>::value && IsHermitian<MT2>::value >
 {};
 /*! \endcond */
 //*************************************************************************************************
