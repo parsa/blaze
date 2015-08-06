8ad0a1c35d47b3b8c2cb4ff4e283a7082ba2ac12
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-08-06 06:09:18 +0200

    Add the 'TSMatRealExprTrait' class template

--- blaze/math/Traits.h
+++ blaze/math/Traits.h
@@ -199,6 +199,7 @@
 #include <blaze/math/traits/TSMatDMatMultExprTrait.h>
 #include <blaze/math/traits/TSMatDMatSubExprTrait.h>
 #include <blaze/math/traits/TSMatEvalExprTrait.h>
+#include <blaze/math/traits/TSMatRealExprTrait.h>
 #include <blaze/math/traits/TSMatScalarDivExprTrait.h>
 #include <blaze/math/traits/TSMatScalarMultExprTrait.h>
 #include <blaze/math/traits/TSMatSerialExprTrait.h>
