d62b97c2db42a01fc23823bc010f25b977380a95
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-08-06 06:08:18 +0200

    Add the 'TSVecRealExprTrait' class template

--- blaze/math/Traits.h
+++ blaze/math/Traits.h
@@ -219,6 +219,7 @@
 #include <blaze/math/traits/TSVecDMatMultExprTrait.h>
 #include <blaze/math/traits/TSVecDVecMultExprTrait.h>
 #include <blaze/math/traits/TSVecEvalExprTrait.h>
+#include <blaze/math/traits/TSVecRealExprTrait.h>
 #include <blaze/math/traits/TSVecScalarDivExprTrait.h>
 #include <blaze/math/traits/TSVecScalarMultExprTrait.h>
 #include <blaze/math/traits/TSVecSerialExprTrait.h>
