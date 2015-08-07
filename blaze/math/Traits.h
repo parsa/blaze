aa8312bbed46d8a7f8d6225c5f304eafb2e695db
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-08-07 06:13:19 +0200

    Add the 'TDMatImagExprTrait' class template

--- blaze/math/Traits.h
+++ blaze/math/Traits.h
@@ -158,6 +158,7 @@
 #include <blaze/math/traits/TDMatDMatMultExprTrait.h>
 #include <blaze/math/traits/TDMatDMatSubExprTrait.h>
 #include <blaze/math/traits/TDMatEvalExprTrait.h>
+#include <blaze/math/traits/TDMatImagExprTrait.h>
 #include <blaze/math/traits/TDMatRealExprTrait.h>
 #include <blaze/math/traits/TDMatScalarDivExprTrait.h>
 #include <blaze/math/traits/TDMatScalarMultExprTrait.h>
