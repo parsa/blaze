dfe55560b9e40f37e683c37435a54925684d7486
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-08-07 06:18:31 +0200

    Add the 'IsVecImagExpr' type trait along with the according compile time constraints

--- blaze/math/Constraints.h
+++ blaze/math/Constraints.h
@@ -106,6 +106,7 @@
 #include <blaze/math/constraints/VecAbsExpr.h>
 #include <blaze/math/constraints/VecConjExpr.h>
 #include <blaze/math/constraints/VecEvalExpr.h>
+#include <blaze/math/constraints/VecImagExpr.h>
 #include <blaze/math/constraints/VecRealExpr.h>
 #include <blaze/math/constraints/VecScalarDivExpr.h>
 #include <blaze/math/constraints/VecScalarMultExpr.h>
--- blaze/math/TypeTraits.h
+++ blaze/math/TypeTraits.h
@@ -107,6 +107,7 @@
 #include <blaze/math/typetraits/IsVecAbsExpr.h>
 #include <blaze/math/typetraits/IsVecConjExpr.h>
 #include <blaze/math/typetraits/IsVecEvalExpr.h>
+#include <blaze/math/typetraits/IsVecImagExpr.h>
 #include <blaze/math/typetraits/IsVecRealExpr.h>
 #include <blaze/math/typetraits/IsVecScalarDivExpr.h>
 #include <blaze/math/typetraits/IsVecScalarMultExpr.h>
