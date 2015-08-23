f58e3c86f895a247a0e80472ec9261bd6d15d049
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-08-23 05:16:48 +0200

    Add overloads of the 'tryAddAssign' and 'trySubAssign' functions for diagonal matrices

--- blaze/math/adaptors/DiagonalMatrix.h
+++ blaze/math/adaptors/DiagonalMatrix.h
@@ -228,8 +228,8 @@ template< typename MT    // Type of the adapted matrix
         , bool SO        // Storage order of the adapted matrix
         , bool DF        // Density flag
         , typename VT >  // Type of the right-hand side dense vector
-BLAZE_ALWAYS_INLINE bool tryAssign( const DiagonalMatrix<MT,SO,DF>& lhs,
-                                    const DenseVector<VT,false>& rhs, size_t row, size_t column )
+inline bool tryAssign( const DiagonalMatrix<MT,SO,DF>& lhs,
+                       const DenseVector<VT,false>& rhs, size_t row, size_t column )
 {
    BLAZE_CONSTRAINT_MUST_NOT_REQUIRE_EVALUATION( VT );
 
@@ -277,8 +277,8 @@ template< typename MT    // Type of the adapted matrix
         , bool SO        // Storage order of the adapted matrix
         , bool DF        // Density flag
         , typename VT >  // Type of the right-hand side dense vector
-BLAZE_ALWAYS_INLINE bool tryAssign( const DiagonalMatrix<MT,SO,DF>& lhs,
-                                    const DenseVector<VT,true>& rhs, size_t row, size_t column )
+inline bool tryAssign( const DiagonalMatrix<MT,SO,DF>& lhs,
+                       const DenseVector<VT,true>& rhs, size_t row, size_t column )
 {
    BLAZE_CONSTRAINT_MUST_NOT_REQUIRE_EVALUATION( VT );
 
@@ -326,8 +326,8 @@ template< typename MT    // Type of the adapted matrix
         , bool SO        // Storage order of the adapted matrix
         , bool DF        // Density flag
         , typename VT >  // Type of the right-hand side sparse vector
-BLAZE_ALWAYS_INLINE bool tryAssign( const DiagonalMatrix<MT,SO,DF>& lhs,
-                                    const SparseVector<VT,false>& rhs, size_t row, size_t column )
+inline bool tryAssign( const DiagonalMatrix<MT,SO,DF>& lhs,
+                       const SparseVector<VT,false>& rhs, size_t row, size_t column )
 {
    BLAZE_CONSTRAINT_MUST_NOT_REQUIRE_EVALUATION( VT );
 
@@ -372,8 +372,8 @@ template< typename MT    // Type of the adapted matrix
         , bool SO        // Storage order of the adapted matrix
         , bool DF        // Density flag
         , typename VT >  // Type of the right-hand side sparse vector
-BLAZE_ALWAYS_INLINE bool tryAssign( const DiagonalMatrix<MT,SO,DF>& lhs,
-                                    const SparseVector<VT,true>& rhs, size_t row, size_t column )
+inline bool tryAssign( const DiagonalMatrix<MT,SO,DF>& lhs,
+                       const SparseVector<VT,true>& rhs, size_t row, size_t column )
 {
    BLAZE_CONSTRAINT_MUST_NOT_REQUIRE_EVALUATION( VT );
 
@@ -418,8 +418,8 @@ template< typename MT1    // Type of the adapted matrix
         , bool SO         // Storage order of the adapted matrix
         , bool DF         // Density flag
         , typename MT2 >  // Type of the right-hand side dense matrix
-BLAZE_ALWAYS_INLINE bool tryAssign( const DiagonalMatrix<MT1,SO,DF>& lhs,
-                                    const DenseMatrix<MT2,false>& rhs, size_t row, size_t column )
+inline bool tryAssign( const DiagonalMatrix<MT1,SO,DF>& lhs,
+                       const DenseMatrix<MT2,false>& rhs, size_t row, size_t column )
 {
    BLAZE_CONSTRAINT_MUST_NOT_REQUIRE_EVALUATION( MT2 );
 
@@ -466,8 +466,8 @@ template< typename MT1    // Type of the adapted matrix
         , bool SO         // Storage order of the adapted matrix
         , bool DF         // Density flag
         , typename MT2 >  // Type of the right-hand side dense matrix
-BLAZE_ALWAYS_INLINE bool tryAssign( const DiagonalMatrix<MT1,SO,DF>& lhs,
-                                    const DenseMatrix<MT2,true>& rhs, size_t row, size_t column )
+inline bool tryAssign( const DiagonalMatrix<MT1,SO,DF>& lhs,
+                       const DenseMatrix<MT2,true>& rhs, size_t row, size_t column )
 {
    BLAZE_CONSTRAINT_MUST_NOT_REQUIRE_EVALUATION( MT2 );
 
@@ -514,8 +514,8 @@ template< typename MT1    // Type of the adapted matrix
         , bool SO         // Storage order of the adapted matrix
         , bool DF         // Density flag
         , typename MT2 >  // Type of the right-hand side sparse matrix
-BLAZE_ALWAYS_INLINE bool tryAssign( const DiagonalMatrix<MT1,SO,DF>& lhs,
-                                    const SparseMatrix<MT2,false>& rhs, size_t row, size_t column )
+inline bool tryAssign( const DiagonalMatrix<MT1,SO,DF>& lhs,
+                       const SparseMatrix<MT2,false>& rhs, size_t row, size_t column )
 {
    BLAZE_CONSTRAINT_MUST_NOT_REQUIRE_EVALUATION( MT2 );
 
@@ -564,8 +564,8 @@ template< typename MT1    // Type of the adapted matrix
         , bool SO         // Storage order of the adapted matrix
         , bool DF         // Density flag
         , typename MT2 >  // Type of the right-hand side sparse matrix
-BLAZE_ALWAYS_INLINE bool tryAssign( const DiagonalMatrix<MT1,SO,DF>& lhs,
-                                    const SparseMatrix<MT2,true>& rhs, size_t row, size_t column )
+inline bool tryAssign( const DiagonalMatrix<MT1,SO,DF>& lhs,
+                       const SparseMatrix<MT2,true>& rhs, size_t row, size_t column )
 {
    BLAZE_CONSTRAINT_MUST_NOT_REQUIRE_EVALUATION( MT2 );
 
@@ -589,6 +589,128 @@ BLAZE_ALWAYS_INLINE bool tryAssign( const DiagonalMatrix<MT1,SO,DF>& lhs,
    }
 
    return true;
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Predict invariant violations by the addition assignment of a vector to a diagonal matrix.
+// \ingroup diagonal_matrix
+//
+// \param lhs The target left-hand side diagonal matrix.
+// \param rhs The right-hand side vector to be added.
+// \param row The row index of the first element to be modified.
+// \param column The column index of the first element to be modified.
+// \return \a true in case the assignment would be successful, \a false if not.
+//
+// This function must \b NOT be called explicitly! It is used internally for the performance
+// optimized evaluation of expression templates. Calling this function explicitly might result
+// in erroneous results and/or in compilation errors. Instead of using this function use the
+// assignment operator.
+*/
+template< typename MT  // Type of the adapted matrix
+        , bool SO      // Storage order of the adapted matrix
+        , bool DF      // Density flag
+        , typename VT  // Type of the right-hand side vector
+        , bool TF >    // Transpose flag of the right-hand side vector
+inline bool tryAddAssign( const DiagonalMatrix<MT,SO,DF>& lhs,
+                          const Vector<VT,TF>& rhs, size_t row, size_t column )
+{
+   return tryAssign( lhs, ~rhs, row, column );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Predict invariant violations by the addition assignment of a matrix to a diagonal matrix.
+// \ingroup diagonal_matrix
+//
+// \param lhs The target left-hand side diagonal matrix.
+// \param rhs The right-hand side matrix to be added.
+// \param row The row index of the first element to be modified.
+// \param column The column index of the first element to be modified.
+// \return \a true in case the assignment would be successful, \a false if not.
+//
+// This function must \b NOT be called explicitly! It is used internally for the performance
+// optimized evaluation of expression templates. Calling this function explicitly might result
+// in erroneous results and/or in compilation errors. Instead of using this function use the
+// assignment operator.
+*/
+template< typename MT1  // Type of the adapted matrix
+        , bool SO1      // Storage order of the adapted matrix
+        , bool DF       // Density flag
+        , typename MT2  // Type of the right-hand side matrix
+        , bool SO2 >    // Storage order of the right-hand side matrix
+inline bool tryAddAssign( const DiagonalMatrix<MT1,SO1,DF>& lhs,
+                          const Matrix<MT2,SO2>& rhs, size_t row, size_t column )
+{
+   return tryAssign( lhs, ~rhs, row, column );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Predict invariant violations by the subtraction assignment of a vector to a diagonal
+//        matrix.
+// \ingroup diagonal_matrix
+//
+// \param lhs The target left-hand side diagonal matrix.
+// \param rhs The right-hand side vector to be subtracted.
+// \param row The row index of the first element to be modified.
+// \param column The column index of the first element to be modified.
+// \return \a true in case the assignment would be successful, \a false if not.
+//
+// This function must \b NOT be called explicitly! It is used internally for the performance
+// optimized evaluation of expression templates. Calling this function explicitly might result
+// in erroneous results and/or in compilation errors. Instead of using this function use the
+// assignment operator.
+*/
+template< typename MT  // Type of the adapted matrix
+        , bool SO      // Storage order of the adapted matrix
+        , bool DF      // Density flag
+        , typename VT  // Type of the right-hand side vector
+        , bool TF >    // Transpose flag of the right-hand side vector
+inline bool trySubAssign( const DiagonalMatrix<MT,SO,DF>& lhs,
+                          const Vector<VT,TF>& rhs, size_t row, size_t column )
+{
+   return tryAssign( lhs, ~rhs, row, column );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Predict invariant violations by the subtraction assignment of a matrix to a diagonal
+//        matrix.
+// \ingroup diagonal_matrix
+//
+// \param lhs The target left-hand side diagonal matrix.
+// \param rhs The right-hand side matrix to be subtracted.
+// \param row The row index of the first element to be modified.
+// \param column The column index of the first element to be modified.
+// \return \a true in case the assignment would be successful, \a false if not.
+//
+// This function must \b NOT be called explicitly! It is used internally for the performance
+// optimized evaluation of expression templates. Calling this function explicitly might result
+// in erroneous results and/or in compilation errors. Instead of using this function use the
+// assignment operator.
+*/
+template< typename MT1  // Type of the adapted matrix
+        , bool SO1      // Storage order of the adapted matrix
+        , bool DF       // Density flag
+        , typename MT2  // Type of the right-hand side matrix
+        , bool SO2 >    // Storage order of the right-hand side matrix
+inline bool trySubAssign( const DiagonalMatrix<MT1,SO1,DF>& lhs,
+                          const Matrix<MT2,SO2>& rhs, size_t row, size_t column )
+{
+   return tryAssign( lhs, ~rhs, row, column );
 }
 /*! \endcond */
 //*************************************************************************************************
