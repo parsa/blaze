583dd5a31e2b2c0c6dc351a990aba63ea6c23713
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-09-01 05:53:28 +0200

    Extend the set of free functions for the 'UniUpperProxy' class template

--- blaze/math/adaptors/uniuppermatrix/UniUpperProxy.h
+++ blaze/math/adaptors/uniuppermatrix/UniUpperProxy.h
@@ -50,7 +50,15 @@
 #include <blaze/math/proxy/Proxy.h>
 #include <blaze/math/shims/Clear.h>
 #include <blaze/math/shims/IsDefault.h>
+#include <blaze/math/shims/IsNaN.h>
+#include <blaze/math/shims/IsOne.h>
+#include <blaze/math/shims/IsReal.h>
+#include <blaze/math/shims/IsZero.h>
 #include <blaze/math/shims/Reset.h>
+#include <blaze/math/traits/AbsExprTrait.h>
+#include <blaze/math/traits/ConjExprTrait.h>
+#include <blaze/math/traits/ImagExprTrait.h>
+#include <blaze/math/traits/RealExprTrait.h>
 #include <blaze/util/constraints/Const.h>
 #include <blaze/util/constraints/Numeric.h>
 #include <blaze/util/constraints/Pointer.h>
@@ -842,6 +850,22 @@ inline std::ostream& operator<<( std::ostream& os, const UniUpperProxy<MT>& prox
 /*!\name UniUpperProxy global functions */
 //@{
 template< typename MT >
+inline typename AbsExprTrait< typename MT::ElementType >::Type
+   abs( const UniUpperProxy<MT>& proxy );
+
+template< typename MT >
+inline typename ConjExprTrait< typename MT::ElementType >::Type
+   conj( const UniUpperProxy<MT>& proxy );
+
+template< typename MT >
+inline typename RealExprTrait< typename MT::ElementType >::Type
+   real( const UniUpperProxy<MT>& proxy );
+
+template< typename MT >
+inline typename ImagExprTrait< typename MT::ElementType >::Type
+   imag( const UniUpperProxy<MT>& proxy );
+
+template< typename MT >
 inline void reset( const UniUpperProxy<MT>& proxy );
 
 template< typename MT >
@@ -849,7 +873,107 @@ inline void clear( const UniUpperProxy<MT>& proxy );
 
 template< typename MT >
 inline bool isDefault( const UniUpperProxy<MT>& proxy );
+
+template< typename MT >
+inline bool isReal( const UniUpperProxy<MT>& proxy );
+
+template< typename MT >
+inline bool isZero( const UniUpperProxy<MT>& proxy );
+
+template< typename MT >
+inline bool isOne( const UniUpperProxy<MT>& proxy );
+
+template< typename MT >
+inline bool isnan( const UniUpperProxy<MT>& proxy );
 //@}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Computing the absolute value of the represented element.
+// \ingroup uniupper_matrix
+//
+// \param proxy The given access proxy.
+// \return The absolute value of the represented element.
+//
+// This function computes the absolute value of the element represented by the access proxy. In
+// case the access proxy represents a vector- or matrix-like data structure the function returns
+// an expression representing the absolute values of the elements of the vector/matrix.
+*/
+template< typename MT >
+inline typename AbsExprTrait< typename MT::ElementType >::Type
+   abs( const UniUpperProxy<MT>& proxy )
+{
+   using std::abs;
+
+   return abs( proxy.get() );
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Computing the complex conjugate of the represented element.
+// \ingroup uniupper_matrix
+//
+// \param proxy The given access proxy.
+// \return The complex conjugate of the represented element.
+//
+// This function computes the complex conjugate of the element represented by the access proxy.
+// In case the access proxy represents a vector- or matrix-like data structure the function
+// returns an expression representing the complex conjugate of the vector/matrix.
+*/
+template< typename MT >
+inline typename ConjExprTrait< typename MT::ElementType >::Type
+   conj( const UniUpperProxy<MT>& proxy )
+{
+   using blaze::conj;
+
+   return conj( proxy.get() );
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Computing the real part of the represented element.
+// \ingroup uniupper_matrix
+//
+// \param proxy The given access proxy.
+// \return The real part of the represented element.
+//
+// This function returns the real part of the element represented by the access proxy. In case
+// the access proxy represents a vector- or matrix-like data structure the function returns an
+// expression representing the real part of each each element of the vector/matrix.
+*/
+template< typename MT >
+inline typename RealExprTrait< typename MT::ElementType >::Type
+   real( const UniUpperProxy<MT>& proxy )
+{
+   using blaze::real;
+
+   return real( proxy.get() );
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Computing the imaginary part of the represented element.
+// \ingroup uniupper_matrix
+//
+// \param proxy The given access proxy.
+// \return The imaginary part of the represented element.
+//
+// This function returns the imaginary part of the element represented by the access proxy. In
+// case the access proxy represents a vector- or matrix-like data structure the function returns
+// an expression representing the real part of each each element of the vector/matrix.
+*/
+template< typename MT >
+inline typename ImagExprTrait< typename MT::ElementType >::Type
+   imag( const UniUpperProxy<MT>& proxy )
+{
+   using blaze::imag;
+
+   return imag( proxy.get() );
+}
 //*************************************************************************************************
 
 
@@ -911,6 +1035,88 @@ inline bool isDefault( const UniUpperProxy<MT>& proxy )
    using blaze::isDefault;
 
    return isDefault( proxy.get() );
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Returns whether the matrix element represents a real number.
+// \ingroup uniupper_matrix
+//
+// \param proxy The given access proxy.
+// \return \a true in case the matrix element represents a real number, \a false otherwise.
+//
+// This function checks whether the element represented by the access proxy represents the a
+// real number. In case the element is of built-in type, the function returns \a true. In case
+// the element is of complex type, the function returns \a true if the imaginary part is equal
+// to 0. Otherwise it returns \a false.
+*/
+template< typename MT >
+inline bool isReal( const UniUpperProxy<MT>& proxy )
+{
+   using blaze::isReal;
+
+   return isReal( proxy.get() );
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Returns whether the represented element is 0.
+// \ingroup uniupper_matrix
+//
+// \param proxy The given access proxy.
+// \return \a true in case the represented element is 0, \a false otherwise.
+//
+// This function checks whether the element represented by the access proxy represents the numeric
+// value 0. In case it is 0, the function returns \a true, otherwise it returns \a false.
+*/
+template< typename MT >
+inline bool isZero( const UniUpperProxy<MT>& proxy )
+{
+   using blaze::isZero;
+
+   return isZero( proxy.get() );
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Returns whether the represented element is 1.
+// \ingroup uniupper_matrix
+//
+// \param proxy The given access proxy.
+// \return \a true in case the represented element is 1, \a false otherwise.
+//
+// This function checks whether the element represented by the access proxy represents the numeric
+// value 1. In case it is 1, the function returns \a true, otherwise it returns \a false.
+*/
+template< typename MT >
+inline bool isOne( const UniUpperProxy<MT>& proxy )
+{
+   using blaze::isOne;
+
+   return isOne( proxy.get() );
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Returns whether the represented element is not a number.
+// \ingroup uniupper_matrix
+//
+// \param proxy The given access proxy.
+// \return \a true in case the represented element is in not a number, \a false otherwise.
+//
+// This function checks whether the element represented by the access proxy is not a number (NaN).
+// In case it is not a number, the function returns \a true, otherwise it returns \a false.
+*/
+template< typename MT >
+inline bool isnan( const UniUpperProxy<MT>& proxy )
+{
+   using blaze::isnan;
+
+   return isnan( proxy.get() );
 }
 //*************************************************************************************************
 
