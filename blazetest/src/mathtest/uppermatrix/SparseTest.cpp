b8217a4d2e5de323e6beeaf4e9dedfd2a8394873
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-09-03 06:12:00 +0200

    Add default constructor tests to the iterator test for the 'UpperMatrix' class template

--- blazetest/src/mathtest/uppermatrix/SparseTest.cpp
+++ blazetest/src/mathtest/uppermatrix/SparseTest.cpp
@@ -5306,6 +5306,34 @@ void SparseTest::testIterator()
       upper(1,1) = -2;
       upper(2,2) =  4;
 
+      // Testing the Iterator default constructor
+      {
+         test_ = "Row-major Iterator default constructor";
+
+         Iterator it = Iterator();
+
+         if( it != Iterator() ) {
+            std::ostringstream oss;
+            oss << " Test: " << test_ << "\n"
+                << " Error: Failed iterator default constructor\n";
+            throw std::runtime_error( oss.str() );
+         }
+      }
+
+      // Testing the ConstIterator default constructor
+      {
+         test_ = "Row-major ConstIterator default constructor";
+
+         ConstIterator it = ConstIterator();
+
+         if( it != ConstIterator() ) {
+            std::ostringstream oss;
+            oss << " Test: " << test_ << "\n"
+                << " Error: Failed iterator default constructor\n";
+            throw std::runtime_error( oss.str() );
+         }
+      }
+
       // Testing conversion from Iterator to ConstIterator
       {
          test_ = "Row-major Iterator/ConstIterator conversion";
@@ -5513,6 +5541,34 @@ void SparseTest::testIterator()
       upper(0,2) =  3;
       upper(1,1) = -2;
       upper(2,2) =  4;
+
+      // Testing the Iterator default constructor
+      {
+         test_ = "Column-major Iterator default constructor";
+
+         Iterator it = Iterator();
+
+         if( it != Iterator() ) {
+            std::ostringstream oss;
+            oss << " Test: " << test_ << "\n"
+                << " Error: Failed iterator default constructor\n";
+            throw std::runtime_error( oss.str() );
+         }
+      }
+
+      // Testing the ConstIterator default constructor
+      {
+         test_ = "Column-major ConstIterator default constructor";
+
+         ConstIterator it = ConstIterator();
+
+         if( it != ConstIterator() ) {
+            std::ostringstream oss;
+            oss << " Test: " << test_ << "\n"
+                << " Error: Failed iterator default constructor\n";
+            throw std::runtime_error( oss.str() );
+         }
+      }
 
       // Testing conversion from Iterator to ConstIterator
       {
