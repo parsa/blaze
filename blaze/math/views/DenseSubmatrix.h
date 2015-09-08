f47e7a56ffc3940d3c7a92b548293a48a16d1dd4
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-09-08 04:24:18 +0200

    Add an overload of the 'isStrictlyUpper' function for the 'DenseSubmatrix' class template

--- blaze/math/views/DenseSubmatrix.h
+++ blaze/math/views/DenseSubmatrix.h
@@ -10334,6 +10334,9 @@ template< typename MT, bool AF, bool SO >
 inline bool isUniUpper( const DenseSubmatrix<MT,AF,SO>& dm );
 
 template< typename MT, bool AF, bool SO >
+inline bool isStrictlyUpper( const DenseSubmatrix<MT,AF,SO>& dm );
+
+template< typename MT, bool AF, bool SO >
 inline bool isSame( const DenseSubmatrix<MT,AF,SO>& a, const DenseMatrix<MT,SO>& b );
 
 template< typename MT, bool AF, bool SO >
@@ -10772,6 +10775,51 @@ inline bool isUniUpper( const DenseSubmatrix<MT,AF,SO>& dm )
    if( IsUniUpper<MT>::value && dm.row() == dm.column() && dm.rows() == dm.columns() )
       return true;
    else return isUniUpper( static_cast<const BaseType&>( dm ) );
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Checks if the given dense submatrix is a strictly upper triangular matrix.
+// \ingroup dense_submatrix
+//
+// \param dm The dense submatrix to be checked.
+// \return \a true if the submatrix is a strictly upper triangular matrix, \a false if not.
+//
+// This function checks if the given sparse submatrix is a strictly upper triangular matrix. The
+// matrix is considered to be upper triangular if it is a square matrix of the form
+
+                        \f[\left(\begin{array}{*{5}{c}}
+                        0      & u_{0,1} & u_{0,2} & \cdots & u_{0,N} \\
+                        0      & 0       & u_{1,2} & \cdots & u_{1,N} \\
+                        0      & 0       & 0       & \cdots & u_{2,N} \\
+                        \vdots & \vdots  & \vdots  & \ddots & \vdots  \\
+                        0      & 0       & 0       & \cdots & 0       \\
+                        \end{array}\right).\f]
+
+// The following code example demonstrates the use of the function:
+
+   \code
+   typedef blaze::DynamicMatrix<int,blaze::rowMajor>  Matrix;
+
+   Matrix A( 32UL, 16UL );
+   // ... Initialization
+
+   blaze::DenseSubmatrix<Matrix> sm( A, 8UL, 8UL, 16UL, 16UL );
+
+   if( isStrictlyUpper( sm ) ) { ... }
+   \endcode
+*/
+template< typename MT  // Type of the dense matrix
+        , bool AF      // Alignment flag
+        , bool SO >    // Storage order
+inline bool isStrictlyUpper( const DenseSubmatrix<MT,AF,SO>& dm )
+{
+   typedef DenseMatrix< DenseSubmatrix<MT,AF,SO>, SO >  BaseType;
+
+   if( IsStrictlyUpper<MT>::value && dm.row() == dm.column() && dm.rows() == dm.columns() )
+      return true;
+   else return isStrictlyUpper( static_cast<const BaseType&>( dm ) );
 }
 //*************************************************************************************************
 
