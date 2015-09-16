a57e092b98e6480e36628d817ddb1f13a18e838f
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-09-16 11:07:55 +0200

    Add a specialization of the 'IsPadded' type trait for the 'DVecDVecSubExpr' class template

--- blaze/math/expressions/DVecDVecSubExpr.h
+++ blaze/math/expressions/DVecDVecSubExpr.h
@@ -55,6 +55,7 @@
 #include <blaze/math/traits/SubvectorExprTrait.h>
 #include <blaze/math/typetraits/IsComputation.h>
 #include <blaze/math/typetraits/IsExpression.h>
+#include <blaze/math/typetraits/IsPadded.h>
 #include <blaze/math/typetraits/IsTemporary.h>
 #include <blaze/math/typetraits/RequiresEvaluation.h>
 #include <blaze/math/typetraits/Size.h>
@@ -65,10 +66,12 @@
 #include <blaze/util/EnableIf.h>
 #include <blaze/util/Exception.h>
 #include <blaze/util/logging/FunctionTrace.h>
+#include <blaze/util/mpl/And.h>
 #include <blaze/util/mpl/Max.h>
 #include <blaze/util/SelectType.h>
 #include <blaze/util/Types.h>
 #include <blaze/util/typetraits/IsSame.h>
+#include <blaze/util/valuetraits/IsTrue.h>
 
 
 namespace blaze {
@@ -982,6 +985,24 @@ inline const DVecDVecSubExpr<T1,T2,TF>
 template< typename VT1, typename VT2, bool TF >
 struct Size< DVecDVecSubExpr<VT1,VT2,TF> >
    : public Max< Size<VT1>, Size<VT2> >::Type
+{};
+/*! \endcond */
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  ISPADDED SPECIALIZATIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+template< typename VT1, typename VT2, bool TF >
+struct IsPadded< DVecDVecSubExpr<VT1,VT2,TF> >
+   : public IsTrue< And< IsPadded<VT1>, IsPadded<VT2> >::value  >
 {};
 /*! \endcond */
 //*************************************************************************************************
