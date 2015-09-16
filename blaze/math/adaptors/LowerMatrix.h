1c508c983648e07b9a69ce78aafbe66c7cd2f5a0
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-09-16 08:48:19 +0200

    Add a specialization of the 'IsPadded' type trait for the 'LowerMatrix' class template

--- blaze/math/adaptors/LowerMatrix.h
+++ blaze/math/adaptors/LowerMatrix.h
@@ -60,6 +60,7 @@
 #include <blaze/math/typetraits/Columns.h>
 #include <blaze/math/typetraits/IsAdaptor.h>
 #include <blaze/math/typetraits/IsLower.h>
+#include <blaze/math/typetraits/IsPadded.h>
 #include <blaze/math/typetraits/IsResizable.h>
 #include <blaze/math/typetraits/IsRestricted.h>
 #include <blaze/math/typetraits/IsSquare.h>
@@ -921,6 +922,23 @@ struct IsRestricted< LowerMatrix<MT,SO,DF> > : public IsTrue<true>
 /*! \cond BLAZE_INTERNAL */
 template< typename MT, bool SO >
 struct HasConstDataAccess< LowerMatrix<MT,SO,true> > : public IsTrue<true>
+{};
+/*! \endcond */
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  ISPADDED SPECIALIZATIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+template< typename MT, bool SO, bool DF >
+struct IsPadded< LowerMatrix<MT,SO,DF> > : public IsTrue< IsPadded<MT>::value >
 {};
 /*! \endcond */
 //*************************************************************************************************
