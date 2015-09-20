85918e2ede164a7c2fbb5b30092430fa0cda7057
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-09-20 06:23:04 +0200

    Add intrinsic 'store' functions for integral complex numbers

--- blaze/math/intrinsics/Store.h
+++ blaze/math/intrinsics/Store.h
@@ -211,6 +211,100 @@ BLAZE_ALWAYS_INLINE void store( double* address, const sse_double_t& value )
 
 
 //*************************************************************************************************
+/*!\brief Aligned store of a vector of 2-byte integral complex values.
+// \ingroup intrinsics
+//
+// \param address The target address.
+// \param value The 2-byte integral complex vector to be stored.
+// \return void
+//
+// This function stores a vector of 2-byte integral complex values. The given address must be
+// aligned according to the enabled instruction set (16-byte alignment in case of SSE, 32-byte
+// alignment in case of AVX, and 64-byte alignment in case of MIC.
+*/
+template< typename T >  // Type of the integral value
+BLAZE_ALWAYS_INLINE typename EnableIf< And< IsIntegral<T>, HasSize<T,2UL> > >::Type
+   store( complex<T>* address, const sse_cint16_t& value )
+{
+   BLAZE_STATIC_ASSERT( sizeof( complex<T> ) == 2UL*sizeof( T ) );
+   BLAZE_INTERNAL_ASSERT( checkAlignment( address ), "Invalid alignment detected" );
+
+#if BLAZE_AVX2_MODE
+   _mm256_store_si256( reinterpret_cast<__m256i*>( address ), value.value );
+#elif BLAZE_SSE2_MODE
+   _mm_store_si128( reinterpret_cast<__m128i*>( address ), value.value );
+#else
+   *address = value.value;
+#endif
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Aligned store of a vector of 4-byte integral complex values.
+// \ingroup intrinsics
+//
+// \param address The target address.
+// \param value The 4-byte integral complex vector to be stored.
+// \return void
+//
+// This function stores a vector of 4-byte integral complex values. The given address must be
+// aligned according to the enabled instruction set (16-byte alignment in case of SSE, 32-byte
+// alignment in case of AVX, and 64-byte alignment in case of MIC.
+*/
+template< typename T >  // Type of the integral value
+BLAZE_ALWAYS_INLINE typename EnableIf< And< IsIntegral<T>, HasSize<T,4UL> > >::Type
+   store( complex<T>* address, const sse_cint32_t& value )
+{
+   BLAZE_STATIC_ASSERT( sizeof( complex<T> ) == 2UL*sizeof( T ) );
+   BLAZE_INTERNAL_ASSERT( checkAlignment( address ), "Invalid alignment detected" );
+
+#if BLAZE_MIC_MODE
+   _mm512_store_epi32( address, value.value );
+#elif BLAZE_AVX2_MODE
+   _mm256_store_si256( reinterpret_cast<__m256i*>( address ), value.value );
+#elif BLAZE_SSE2_MODE
+   _mm_store_si128( reinterpret_cast<__m128i*>( address ), value.value );
+#else
+   *address = value.value;
+#endif
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Aligned store of a vector of 8-byte integral complex values.
+// \ingroup intrinsics
+//
+// \param address The target address.
+// \param value The 8-byte integral complex vector to be stored.
+// \return void
+//
+// This function stores a vector of 8-byte integral complex values. The given address must be
+// aligned according to the enabled instruction set (16-byte alignment in case of SSE, 32-byte
+// alignment in case of AVX, and 64-byte alignment in case of MIC.
+*/
+template< typename T >  // Type of the integral value
+BLAZE_ALWAYS_INLINE typename EnableIf< And< IsIntegral<T>, HasSize<T,8UL> > >::Type
+   store( complex<T>* address, const sse_cint64_t& value )
+{
+   BLAZE_STATIC_ASSERT( sizeof( complex<T> ) == 2UL*sizeof( T ) );
+   BLAZE_INTERNAL_ASSERT( checkAlignment( address ), "Invalid alignment detected" );
+
+#if BLAZE_MIC_MODE
+   _mm512_store_epi64( address, value.value );
+#elif BLAZE_AVX2_MODE
+   _mm256_store_si256( reinterpret_cast<__m256i*>( address ), value.value );
+#elif BLAZE_SSE2_MODE
+   _mm_store_si128( reinterpret_cast<__m128i*>( address ), value.value );
+#else
+   *address = value.value;
+#endif
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
 /*!\brief Aligned store of a vector of 'complex<float>' values.
 // \ingroup intrinsics
 //
