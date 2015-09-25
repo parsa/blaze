d8416c731144e66ce9e781f014a663932a351725
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-09-25 20:19:23 +0200

    Add an overload of the 'conj' function for the 'SymmetricValue' class template

--- blaze/math/adaptors/symmetricmatrix/SymmetricValue.h
+++ blaze/math/adaptors/symmetricmatrix/SymmetricValue.h
@@ -48,12 +48,14 @@
 #include <blaze/math/constraints/Upper.h>
 #include <blaze/math/proxy/Proxy.h>
 #include <blaze/math/shims/Clear.h>
+#include <blaze/math/shims/Conjugate.h>
 #include <blaze/math/shims/IsDefault.h>
 #include <blaze/math/shims/IsNaN.h>
 #include <blaze/math/shims/IsOne.h>
 #include <blaze/math/shims/IsReal.h>
 #include <blaze/math/shims/IsZero.h>
 #include <blaze/math/shims/Reset.h>
+#include <blaze/math/traits/ConjExprTrait.h>
 #include <blaze/math/typetraits/IsRowMajorMatrix.h>
 #include <blaze/util/constraints/Const.h>
 #include <blaze/util/constraints/Numeric.h>
@@ -559,6 +561,10 @@ inline void SymmetricValue<MT>::imag( ValueType value ) const
 /*!\name SymmetricValue global functions */
 //@{
 template< typename MT >
+inline typename ConjExprTrait< typename SymmetricValue<MT>::RepresentedType >::Type
+   conj( const SymmetricValue<MT>& value );
+
+template< typename MT >
 inline void reset( const SymmetricValue<MT>& value );
 
 template< typename MT >
@@ -579,6 +585,28 @@ inline bool isOne( const SymmetricValue<MT>& value );
 template< typename MT >
 inline bool isnan( const SymmetricValue<MT>& value );
 //@}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Computing the complex conjugate of the symmetric value.
+// \ingroup symmetric_matrix
+//
+// \param value The given symmetric value.
+// \return The complex conjugate of the symmetric value.
+//
+// This function computes the complex conjugate of the symmetric value. In case the value
+// represents a vector- or matrix-like data structure the function returns an expression
+// representing the complex conjugate of the vector/matrix.
+*/
+template< typename MT >
+inline typename ConjExprTrait< typename SymmetricValue<MT>::RepresentedType >::Type
+   conj( const SymmetricValue<MT>& value )
+{
+   using blaze::conj;
+
+   return conj( (~value).get() );
+}
 //*************************************************************************************************
 
 
