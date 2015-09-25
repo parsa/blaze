ace2348f592cbf769c1151c08a5723fb6bcf3050
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-09-25 20:20:48 +0200

    Add an overload of the 'conj' function for the 'StrictlyLowerProxy' class template

--- blaze/math/adaptors/strictlylowermatrix/StrictlyLowerProxy.h
+++ blaze/math/adaptors/strictlylowermatrix/StrictlyLowerProxy.h
@@ -48,12 +48,14 @@
 #include <blaze/math/constraints/Upper.h>
 #include <blaze/math/proxy/Proxy.h>
 #include <blaze/math/shims/Clear.h>
+#include <blaze/math/shims/Conjugate.h>
 #include <blaze/math/shims/IsDefault.h>
 #include <blaze/math/shims/IsNaN.h>
 #include <blaze/math/shims/IsOne.h>
 #include <blaze/math/shims/IsReal.h>
 #include <blaze/math/shims/IsZero.h>
 #include <blaze/math/shims/Reset.h>
+#include <blaze/math/traits/ConjExprTrait.h>
 #include <blaze/util/constraints/Const.h>
 #include <blaze/util/constraints/Pointer.h>
 #include <blaze/util/constraints/Reference.h>
@@ -441,6 +443,10 @@ inline StrictlyLowerProxy<MT>::operator RawReference() const
 /*!\name StrictlyLowerProxy global functions */
 //@{
 template< typename MT >
+inline typename ConjExprTrait< typename StrictlyLowerProxy<MT>::RepresentedType >::Type
+   conj( const StrictlyLowerProxy<MT>& proxy );
+
+template< typename MT >
 inline void reset( const StrictlyLowerProxy<MT>& proxy );
 
 template< typename MT >
@@ -461,6 +467,28 @@ inline bool isOne( const StrictlyLowerProxy<MT>& proxy );
 template< typename MT >
 inline bool isnan( const StrictlyLowerProxy<MT>& proxy );
 //@}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Computing the complex conjugate of the represented element.
+// \ingroup strictly_lower_matrix
+//
+// \param proxy The given proxy instance.
+// \return The complex conjugate of the represented element.
+//
+// This function computes the complex conjugate of the element represented by the access proxy.
+// In case the proxy represents a vector- or matrix-like data structure the function returns an
+// expression representing the complex conjugate of the vector/matrix.
+*/
+template< typename MT >
+inline typename ConjExprTrait< typename StrictlyLowerProxy<MT>::RepresentedType >::Type
+   conj( const StrictlyLowerProxy<MT>& proxy )
+{
+   using blaze::conj;
+
+   return conj( (~proxy).get() );
+}
 //*************************************************************************************************
 
 
