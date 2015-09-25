3b868d59ed851aedb037e6586a4c7fb6d109fad0
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-09-25 20:21:27 +0200

    Add an overload of the 'conj' function for the 'UniUpperValue' class template

--- blaze/math/adaptors/uniuppermatrix/UniUpperValue.h
+++ blaze/math/adaptors/uniuppermatrix/UniUpperValue.h
@@ -48,12 +48,14 @@
 #include <blaze/math/constraints/Upper.h>
 #include <blaze/math/proxy/Proxy.h>
 #include <blaze/math/shims/Clear.h>
+#include <blaze/math/shims/Conjugate.h>
 #include <blaze/math/shims/IsDefault.h>
 #include <blaze/math/shims/IsNaN.h>
 #include <blaze/math/shims/IsOne.h>
 #include <blaze/math/shims/IsReal.h>
 #include <blaze/math/shims/IsZero.h>
 #include <blaze/math/shims/Reset.h>
+#include <blaze/math/traits/ConjExprTrait.h>
 #include <blaze/math/typetraits/IsRowMajorMatrix.h>
 #include <blaze/util/constraints/Const.h>
 #include <blaze/util/constraints/Numeric.h>
@@ -554,6 +556,10 @@ inline void UniUpperValue<MT>::imag( ValueType value ) const
 /*!\name UniUpperValue global functions */
 //@{
 template< typename MT >
+inline typename ConjExprTrait< typename UniUpperValue<MT>::RepresentedType >::Type
+   conj( const UniUpperValue<MT>& value );
+
+template< typename MT >
 inline void reset( const UniUpperValue<MT>& value );
 
 template< typename MT >
@@ -574,6 +580,28 @@ inline bool isOne( const UniUpperValue<MT>& value );
 template< typename MT >
 inline bool isnan( const UniUpperValue<MT>& value );
 //@}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Computing the complex conjugate of the uniupper value.
+// \ingroup uniupper_matrix
+//
+// \param value The given uniupper value.
+// \return The complex conjugate of the uniupper value.
+//
+// This function computes the complex conjugate of the uniupper value. In case the value
+// represents a vector- or matrix-like data structure the function returns an expression
+// representing the complex conjugate of the vector/matrix.
+*/
+template< typename MT >
+inline typename ConjExprTrait< typename UniUpperValue<MT>::RepresentedType >::Type
+   conj( const UniUpperValue<MT>& value )
+{
+   using blaze::conj;
+
+   return conj( (~value).get() );
+}
 //*************************************************************************************************
 
 
