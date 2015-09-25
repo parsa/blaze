e0314f79ec760671d6c763ef86ae18b143cb34d2
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-09-25 20:21:01 +0200

    Add an overload of the 'conj' function for the 'UpperProxy' class template

--- blaze/math/adaptors/uppermatrix/UpperProxy.h
+++ blaze/math/adaptors/uppermatrix/UpperProxy.h
@@ -48,12 +48,14 @@
 #include <blaze/math/constraints/Upper.h>
 #include <blaze/math/proxy/Proxy.h>
 #include <blaze/math/shims/Clear.h>
+#include <blaze/math/shims/Conjugate.h>
 #include <blaze/math/shims/IsDefault.h>
 #include <blaze/math/shims/IsNaN.h>
 #include <blaze/math/shims/IsOne.h>
 #include <blaze/math/shims/IsReal.h>
 #include <blaze/math/shims/IsZero.h>
 #include <blaze/math/shims/Reset.h>
+#include <blaze/math/traits/ConjExprTrait.h>
 #include <blaze/util/constraints/Const.h>
 #include <blaze/util/constraints/Pointer.h>
 #include <blaze/util/constraints/Reference.h>
@@ -439,6 +441,10 @@ inline UpperProxy<MT>::operator RawReference() const
 /*!\name UpperProxy global functions */
 //@{
 template< typename MT >
+inline typename ConjExprTrait< typename UpperProxy<MT>::RepresentedType >::Type
+   conj( const UpperProxy<MT>& proxy );
+
+template< typename MT >
 inline void reset( const UpperProxy<MT>& proxy );
 
 template< typename MT >
@@ -459,6 +465,28 @@ inline bool isOne( const UpperProxy<MT>& proxy );
 template< typename MT >
 inline bool isnan( const UpperProxy<MT>& proxy );
 //@}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Computing the complex conjugate of the represented element.
+// \ingroup upper_matrix
+//
+// \param proxy The given proxy instance.
+// \return The complex conjugate of the represented element.
+//
+// This function computes the complex conjugate of the element represented by the access proxy.
+// In case the proxy represents a vector- or matrix-like data structure the function returns an
+// expression representing the complex conjugate of the vector/matrix.
+*/
+template< typename MT >
+inline typename ConjExprTrait< typename UpperProxy<MT>::RepresentedType >::Type
+   conj( const UpperProxy<MT>& proxy )
+{
+   using blaze::conj;
+
+   return conj( (~proxy).get() );
+}
 //*************************************************************************************************
 
 
