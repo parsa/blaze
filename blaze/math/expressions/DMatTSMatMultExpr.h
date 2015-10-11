e29b35101a1a75e8b9b440284ac4162d1b981076
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-10-11 10:10:48 +0200

    Add a specialization of the 'IsAligned' type trait for the 'DMatTSMatMultExpr' class template

--- blaze/math/expressions/DMatTSMatMultExpr.h
+++ blaze/math/expressions/DMatTSMatMultExpr.h
@@ -66,6 +66,7 @@
 #include <blaze/math/traits/TDVecTSMatMultExprTrait.h>
 #include <blaze/math/traits/TSVecDMatMultExprTrait.h>
 #include <blaze/math/typetraits/Columns.h>
+#include <blaze/math/typetraits/IsAligned.h>
 #include <blaze/math/typetraits/IsColumnMajorMatrix.h>
 #include <blaze/math/typetraits/IsColumnVector.h>
 #include <blaze/math/typetraits/IsComputation.h>
@@ -1801,8 +1802,7 @@ inline const DMatTSMatMultExpr<T1,T2>
 //*************************************************************************************************
 /*! \cond BLAZE_INTERNAL */
 template< typename MT1, typename MT2 >
-struct Rows< DMatTSMatMultExpr<MT1,MT2> >
-   : public Rows<MT1>
+struct Rows< DMatTSMatMultExpr<MT1,MT2> > : public Rows<MT1>
 {};
 /*! \endcond */
 //*************************************************************************************************
@@ -1819,8 +1819,24 @@ struct Rows< DMatTSMatMultExpr<MT1,MT2> >
 //*************************************************************************************************
 /*! \cond BLAZE_INTERNAL */
 template< typename MT1, typename MT2 >
-struct Columns< DMatTSMatMultExpr<MT1,MT2> >
-   : public Columns<MT2>
+struct Columns< DMatTSMatMultExpr<MT1,MT2> > : public Columns<MT2>
+{};
+/*! \endcond */
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  ISALIGNED SPECIALIZATIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+template< typename MT1, typename MT2 >
+struct IsAligned< DMatTSMatMultExpr<MT1,MT2> > : public IsTrue< IsAligned<MT1>::value >
 {};
 /*! \endcond */
 //*************************************************************************************************
