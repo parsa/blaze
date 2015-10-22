1cc58de09d2d31ef1a82f14b43d759ecbc3460c0
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-10-22 05:18:10 +0200

    Add the 'NextMultiple' class template

--- blaze/util/mpl/NextMultiple.h
+++ blaze/util/mpl/NextMultiple.h
@@ -1,7 +1,7 @@
 //=================================================================================================
 /*!
-//  \file blaze/util/MPL.h
-//  \brief Header file for all meta-programming tools
+//  \file blaze/util/mpl/NextMultiple.h
+//  \brief Header file for the NextMultiple class template
 //
 //  Copyright (C) 2013 Klaus Iglberger - All Rights Reserved
 //
@@ -32,33 +32,63 @@
 */
 //=================================================================================================
 
-#ifndef _BLAZE_UTIL_MPL_H_
-#define _BLAZE_UTIL_MPL_H_
+#ifndef _BLAZE_UTIL_MPL_NEXTMULTIPLE_H_
+#define _BLAZE_UTIL_MPL_NEXTMULTIPLE_H_
 
 
 //*************************************************************************************************
 // Includes
 //*************************************************************************************************
 
-#include <blaze/util/mpl/And.h>
-#include <blaze/util/mpl/Bool.h>
-#include <blaze/util/mpl/Char.h>
-#include <blaze/util/mpl/Equal.h>
-#include <blaze/util/mpl/Greater.h>
-#include <blaze/util/mpl/If.h>
-#include <blaze/util/mpl/Int.h>
+#include <boost/typeof/typeof.hpp>
 #include <blaze/util/mpl/IntegralC.h>
-#include <blaze/util/mpl/Less.h>
-#include <blaze/util/mpl/Long.h>
-#include <blaze/util/mpl/Max.h>
-#include <blaze/util/mpl/Min.h>
 #include <blaze/util/mpl/Minus.h>
 #include <blaze/util/mpl/Modulus.h>
-#include <blaze/util/mpl/NextMultiple.h>
-#include <blaze/util/mpl/Not.h>
-#include <blaze/util/mpl/Or.h>
 #include <blaze/util/mpl/Plus.h>
-#include <blaze/util/mpl/SizeT.h>
-#include <blaze/util/mpl/Times.h>
+#include <blaze/util/StaticAssert.h>
+
+
+namespace blaze {
+
+//=================================================================================================
+//
+//  CLASS DEFINITION
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\brief Compile time integral round up operation.
+// \ingroup mpl
+//
+// The NextMultiple class template rounds up the given template argument of type \a T1 to the
+// next multiple of the given template argument of type \a T2. In case \a T1 already represents
+// a multiple of \a T2, the result is \a T1. In order for NextMultiple to be able to perform the
+// round up operation, both arguments are required to have a nested member \a value. The result
+// of the operation can be accessed via the nested member \a value, the resulting type is
+// available via the nested type \a ValueType.
+
+   \code
+   blaze::NextMultiple< Int<3> , Int<2>  >::value      // Results in 4
+   blaze::NextMultiple< Long<3>, Int<2>  >::ValueType  // Results in long
+   blaze::NextMultiple< Int<3> , Long<2> >::ValueType  // Results in long
+   \endcode
+
+// Note that both \a T1 and \a T2 are expected to represent positive integrals. The attempt to
+// use NextMultiple with a negative or zero integral results in a compilation error!
+*/
+template< typename T1    // Type of the first compile time value
+        , typename T2 >  // Type of the second compile time value
+struct NextMultiple
+   : public Plus< T1, Modulus< Minus< T2, Modulus< T1, T2 > >, T2 > >
+{
+   //**********************************************************************************************
+   /*! \cond BLAZE_INTERNAL */
+   BLAZE_STATIC_ASSERT( T1::value > 0 && T2::value > 0 );
+   /*! \endcond */
+   //**********************************************************************************************
+};
+//*************************************************************************************************
+
+} // namespace blaze
 
 #endif
