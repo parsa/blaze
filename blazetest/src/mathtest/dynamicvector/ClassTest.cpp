6e70a9bf0e402f4ea4bfcead47522cd146d26b6d
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-10-23 18:27:15 +0200

    Extend the 'DynamicVector' class test for unaligned/unpadded vectors

--- blazetest/src/mathtest/dynamicvector/ClassTest.cpp
+++ blazetest/src/mathtest/dynamicvector/ClassTest.cpp
@@ -40,9 +40,11 @@
 #include <cstdlib>
 #include <iostream>
 #include <blaze/math/CompressedVector.h>
+#include <blaze/math/CustomVector.h>
 #include <blaze/math/StaticVector.h>
 #include <blaze/math/shims/Equal.h>
 #include <blaze/util/Complex.h>
+#include <blaze/util/policies/Deallocate.h>
 #include <blaze/util/Random.h>
 #include <blaze/util/UniqueArray.h>
 #include <blazetest/mathtest/dynamicvector/ClassTest.h>
@@ -300,9 +302,51 @@ void ClassTest::testConstructors()
    //=====================================================================================
 
    {
-      test_ = "DynamicVector dense vector constructor";
+      test_ = "DynamicVector dense vector constructor (aligned/padded)";
 
-      blaze::StaticVector<int,5UL,blaze::rowVector> vec1( 1, 2, 3, 4, 5 );
+      using blaze::aligned;
+      using blaze::padded;
+      using blaze::rowVector;
+
+      typedef blaze::CustomVector<int,aligned,padded,rowVector>  AlignedPadded;
+      AlignedPadded vec1( blaze::allocate<int>( 16UL ), 5UL, 16UL, blaze::Deallocate() );
+      vec1[0] = 1;
+      vec1[1] = 2;
+      vec1[2] = 3;
+      vec1[3] = 4;
+      vec1[4] = 5;
+      blaze::DynamicVector<int,blaze::rowVector> vec2( vec1 );
+
+      checkSize    ( vec2, 5UL );
+      checkCapacity( vec2, 5UL );
+      checkNonZeros( vec2, 5UL );
+
+      if( vec2[0] != 1 || vec2[1] != 2 || vec2[2] != 3 || vec2[3] != 4 || vec2[4] != 5 ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: Construction failed\n"
+             << " Details:\n"
+             << "   Result:\n" << vec2 << "\n"
+             << "   Expected result:\n( 1 2 3 4 5 )\n";
+         throw std::runtime_error( oss.str() );
+      }
+   }
+
+   {
+      test_ = "DynamicVector dense vector constructor (unaligned/unpadded)";
+
+      using blaze::unaligned;
+      using blaze::unpadded;
+      using blaze::rowVector;
+
+      typedef blaze::CustomVector<int,unaligned,unpadded,rowVector>  UnalignedUnpadded;
+      blaze::UniqueArray<int> array( new int[6] );
+      UnalignedUnpadded vec1( array.get()+1UL, 5UL );
+      vec1[0] = 1;
+      vec1[1] = 2;
+      vec1[2] = 3;
+      vec1[3] = 4;
+      vec1[4] = 5;
       blaze::DynamicVector<int,blaze::rowVector> vec2( vec1 );
 
       checkSize    ( vec2, 5UL );
@@ -481,9 +525,47 @@ void ClassTest::testAssignment()
    //=====================================================================================
 
    {
-      test_ = "DynamicVector dense vector assignment";
+      test_ = "DynamicVector dense vector assignment (aligned/padded)";
 
-      blaze::StaticVector<int,5UL,blaze::rowVector> vec1;
+      using blaze::aligned;
+      using blaze::padded;
+      using blaze::rowVector;
+
+      typedef blaze::CustomVector<int,aligned,padded,rowVector>  AlignedPadded;
+      AlignedPadded vec1( blaze::allocate<int>( 16UL ), 5UL, 16UL, blaze::Deallocate() );
+      vec1[0] = 1;
+      vec1[1] = 2;
+      vec1[2] = 3;
+      vec1[3] = 4;
+      vec1[4] = 5;
+      blaze::DynamicVector<int,blaze::rowVector> vec2;
+      vec2 = vec1;
+
+      checkSize    ( vec2, 5UL );
+      checkCapacity( vec2, 5UL );
+      checkNonZeros( vec2, 5UL );
+
+      if( vec2[0] != 1 || vec2[1] != 2 || vec2[2] != 3 || vec2[3] != 4 || vec2[4] != 5 ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: Assignment failed\n"
+             << " Details:\n"
+             << "   Result:\n" << vec2 << "\n"
+             << "   Expected result:\n( 1 2 3 4 5 )\n";
+         throw std::runtime_error( oss.str() );
+      }
+   }
+
+   {
+      test_ = "DynamicVector dense vector assignment (unaligned/unpadded)";
+
+      using blaze::unaligned;
+      using blaze::unpadded;
+      using blaze::rowVector;
+
+      typedef blaze::CustomVector<int,unaligned,unpadded,rowVector>  UnalignedUnpadded;
+      blaze::UniqueArray<int> array( new int[6] );
+      UnalignedUnpadded vec1( array.get()+1UL, 5UL );
       vec1[0] = 1;
       vec1[1] = 2;
       vec1[2] = 3;
@@ -612,12 +694,57 @@ void ClassTest::testAddAssign()
    //=====================================================================================
 
    {
-      test_ = "DynamicVector dense vector addition assignment";
+      test_ = "DynamicVector dense vector addition assignment (aligned/padded)";
 
-      blaze::DynamicVector<int,blaze::rowVector> vec1( 5UL, 0 );
+      using blaze::aligned;
+      using blaze::padded;
+      using blaze::rowVector;
+
+      typedef blaze::CustomVector<int,aligned,padded,rowVector>  AlignedPadded;
+      AlignedPadded vec1( blaze::allocate<int>( 16UL ), 5UL, 16UL, blaze::Deallocate() );
       vec1[0] =  1;
+      vec1[1] =  0;
       vec1[2] = -2;
       vec1[3] =  3;
+      vec1[4] =  0;
+      blaze::DynamicVector<int,blaze::rowVector> vec2( 5UL, 0 );
+      vec2[1] =  4;
+      vec2[2] =  2;
+      vec2[3] = -6;
+      vec2[4] =  7;
+
+      vec2 += vec1;
+
+      checkSize    ( vec2, 5UL );
+      checkCapacity( vec2, 5UL );
+      checkNonZeros( vec2, 4UL );
+
+      if( vec2[0] != 1 || vec2[1] != 4 || vec2[2] != 0 || vec2[3] != -3 || vec2[4] != 7 ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: Addition assignment failed\n"
+             << " Details:\n"
+             << "   Result:\n" << vec2 << "\n"
+             << "   Expected result:\n( 1 4 0 -3 7 )\n";
+         throw std::runtime_error( oss.str() );
+      }
+   }
+
+   {
+      test_ = "DynamicVector dense vector addition assignment (unaligned/unpadded)";
+
+      using blaze::unaligned;
+      using blaze::unpadded;
+      using blaze::rowVector;
+
+      typedef blaze::CustomVector<int,unaligned,unpadded,rowVector>  UnalignedUnpadded;
+      blaze::UniqueArray<int> array( new int[6] );
+      UnalignedUnpadded vec1( array.get()+1UL, 5UL );
+      vec1[0] =  1;
+      vec1[1] =  0;
+      vec1[2] = -2;
+      vec1[3] =  3;
+      vec1[4] =  0;
       blaze::DynamicVector<int,blaze::rowVector> vec2( 5UL, 0 );
       vec2[1] =  4;
       vec2[2] =  2;
@@ -695,12 +822,57 @@ void ClassTest::testSubAssign()
    //=====================================================================================
 
    {
-      test_ = "DynamicVector dense vector subtraction assignment";
+      test_ = "DynamicVector dense vector subtraction assignment (aligned/padded)";
 
-      blaze::DynamicVector<int,blaze::rowVector> vec1( 5UL, 0 );
+      using blaze::aligned;
+      using blaze::padded;
+      using blaze::rowVector;
+
+      typedef blaze::CustomVector<int,aligned,padded,rowVector>  AlignedPadded;
+      AlignedPadded vec1( blaze::allocate<int>( 16UL ), 5UL, 16UL, blaze::Deallocate() );
       vec1[0] = -1;
+      vec1[1] =  0;
       vec1[2] =  2;
       vec1[3] = -3;
+      vec1[4] =  0;
+      blaze::DynamicVector<int,blaze::rowVector> vec2( 5UL, 0 );
+      vec2[1] =  4;
+      vec2[2] =  2;
+      vec2[3] = -6;
+      vec2[4] =  7;
+
+      vec2 -= vec1;
+
+      checkSize    ( vec2, 5UL );
+      checkCapacity( vec2, 5UL );
+      checkNonZeros( vec2, 4UL );
+
+      if( vec2[0] != 1 || vec2[1] != 4 || vec2[2] != 0 || vec2[3] != -3 || vec2[4] != 7 ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: Subtraction assignment failed\n"
+             << " Details:\n"
+             << "   Result:\n" << vec2 << "\n"
+             << "   Expected result:\n( 1 4 0 -3 7 )\n";
+         throw std::runtime_error( oss.str() );
+      }
+   }
+
+   {
+      test_ = "DynamicVector dense vector subtraction assignment (unaligned/unpadded)";
+
+      using blaze::unaligned;
+      using blaze::unpadded;
+      using blaze::rowVector;
+
+      typedef blaze::CustomVector<int,unaligned,unpadded,rowVector>  UnalignedUnpadded;
+      blaze::UniqueArray<int> array( new int[6] );
+      UnalignedUnpadded vec1( array.get()+1UL, 5UL );
+      vec1[0] = -1;
+      vec1[1] =  0;
+      vec1[2] =  2;
+      vec1[3] = -3;
+      vec1[4] =  0;
       blaze::DynamicVector<int,blaze::rowVector> vec2( 5UL, 0 );
       vec2[1] =  4;
       vec2[2] =  2;
@@ -778,12 +950,57 @@ void ClassTest::testMultAssign()
    //=====================================================================================
 
    {
-      test_ = "DynamicVector dense vector multiplication assignment";
+      test_ = "DynamicVector dense vector multiplication assignment (aligned/padded)";
 
-      blaze::DynamicVector<int,blaze::rowVector> vec1( 5UL, 0 );
+      using blaze::aligned;
+      using blaze::padded;
+      using blaze::rowVector;
+
+      typedef blaze::CustomVector<int,aligned,padded,rowVector>  AlignedPadded;
+      AlignedPadded vec1( blaze::allocate<int>( 16UL ), 5UL, 16UL, blaze::Deallocate() );
       vec1[0] =  1;
+      vec1[1] =  0;
       vec1[2] = -2;
       vec1[3] =  3;
+      vec1[4] =  0;
+      blaze::DynamicVector<int,blaze::rowVector> vec2( 5UL, 0 );
+      vec2[1] =  4;
+      vec2[2] =  2;
+      vec2[3] = -6;
+      vec2[4] =  7;
+
+      vec2 *= vec1;
+
+      checkSize    ( vec2, 5UL );
+      checkCapacity( vec2, 5UL );
+      checkNonZeros( vec2, 2UL );
+
+      if( vec2[0] != 0 || vec2[1] != 0 || vec2[2] != -4 || vec2[3] != -18 || vec2[4] != 0 ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: Multiplication assignment failed\n"
+             << " Details:\n"
+             << "   Result:\n" << vec2 << "\n"
+             << "   Expected result:\n( 0 0 -4 -18 0 )\n";
+         throw std::runtime_error( oss.str() );
+      }
+   }
+
+   {
+      test_ = "DynamicVector dense vector multiplication assignment (unaligned/unpadded)";
+
+      using blaze::unaligned;
+      using blaze::unpadded;
+      using blaze::rowVector;
+
+      typedef blaze::CustomVector<int,unaligned,unpadded,rowVector>  UnalignedUnpadded;
+      blaze::UniqueArray<int> array( new int[6] );
+      UnalignedUnpadded vec1( array.get()+1UL, 5UL );
+      vec1[0] =  1;
+      vec1[1] =  0;
+      vec1[2] = -2;
+      vec1[3] =  3;
+      vec1[4] =  0;
       blaze::DynamicVector<int,blaze::rowVector> vec2( 5UL, 0 );
       vec2[1] =  4;
       vec2[2] =  2;
