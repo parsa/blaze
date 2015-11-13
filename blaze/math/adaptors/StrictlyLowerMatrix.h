9e61fba3e374f4d5c3952e89ff921e9b83f8d85e
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-11-13 05:51:42 +0100

    Add type trait specializations for combinations of 'CustomMatrix' and 'StrictlyLowerMatrix'

--- blaze/math/adaptors/StrictlyLowerMatrix.h
+++ blaze/math/adaptors/StrictlyLowerMatrix.h
@@ -1101,6 +1101,18 @@ struct AddTrait< DynamicMatrix<T,SO1>, StrictlyLowerMatrix<MT,SO2,DF> >
    typedef typename AddTrait< DynamicMatrix<T,SO1>, MT >::Type  Type;
 };
 
+template< typename MT, bool SO1, bool DF, typename T, bool AF, bool PF, bool SO2 >
+struct AddTrait< StrictlyLowerMatrix<MT,SO1,DF>, CustomMatrix<T,AF,PF,SO2> >
+{
+   typedef typename AddTrait< MT, CustomMatrix<T,AF,PF,SO2> >::Type  Type;
+};
+
+template< typename T, bool AF, bool PF, bool SO1, typename MT, bool SO2, bool DF >
+struct AddTrait< CustomMatrix<T,AF,PF,SO1>, StrictlyLowerMatrix<MT,SO2,DF> >
+{
+   typedef typename AddTrait< CustomMatrix<T,AF,PF,SO1>, MT >::Type  Type;
+};
+
 template< typename MT, bool SO1, bool DF, typename T, bool SO2 >
 struct AddTrait< StrictlyLowerMatrix<MT,SO1,DF>, CompressedMatrix<T,SO2> >
 {
@@ -1214,6 +1226,18 @@ template< typename T, bool SO1, typename MT, bool SO2, bool DF >
 struct SubTrait< DynamicMatrix<T,SO1>, StrictlyLowerMatrix<MT,SO2,DF> >
 {
    typedef typename SubTrait< DynamicMatrix<T,SO1>, MT >::Type  Type;
+};
+
+template< typename MT, bool SO1, bool DF, typename T, bool AF, bool PF, bool SO2 >
+struct SubTrait< StrictlyLowerMatrix<MT,SO1,DF>, CustomMatrix<T,AF,PF,SO2> >
+{
+   typedef typename SubTrait< MT, CustomMatrix<T,AF,PF,SO2> >::Type  Type;
+};
+
+template< typename T, bool AF, bool PF, bool SO1, typename MT, bool SO2, bool DF >
+struct SubTrait< CustomMatrix<T,AF,PF,SO1>, StrictlyLowerMatrix<MT,SO2,DF> >
+{
+   typedef typename SubTrait< CustomMatrix<T,AF,PF,SO1>, MT >::Type  Type;
 };
 
 template< typename MT, bool SO1, bool DF, typename T, bool SO2 >
@@ -1403,6 +1427,18 @@ template< typename T, bool SO1, typename MT, bool SO2, bool DF >
 struct MultTrait< DynamicMatrix<T,SO1>, StrictlyLowerMatrix<MT,SO2,DF> >
 {
    typedef typename MultTrait< DynamicMatrix<T,SO1>, MT >::Type  Type;
+};
+
+template< typename MT, bool SO1, bool DF, typename T, bool AF, bool PF, bool SO2 >
+struct MultTrait< StrictlyLowerMatrix<MT,SO1,DF>, CustomMatrix<T,AF,PF,SO2> >
+{
+   typedef typename MultTrait< MT, CustomMatrix<T,AF,PF,SO2> >::Type  Type;
+};
+
+template< typename T, bool AF, bool PF, bool SO1, typename MT, bool SO2, bool DF >
+struct MultTrait< CustomMatrix<T,AF,PF,SO1>, StrictlyLowerMatrix<MT,SO2,DF> >
+{
+   typedef typename MultTrait< CustomMatrix<T,AF,PF,SO1>, MT >::Type  Type;
 };
 
 template< typename MT, bool SO1, bool DF, typename T, bool SO2 >
