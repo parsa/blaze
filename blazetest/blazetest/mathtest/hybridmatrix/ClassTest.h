d6e463330c3733eba849a085bd62e2541a02a7e7
Klaus Iglberger <klaus.iglberger@gmail.com>
2015-11-20 13:26:54 +0100

    Add a test for the 'ctranspose' member function to the 'HybridMatrix' class test

--- blazetest/blazetest/mathtest/hybridmatrix/ClassTest.h
+++ blazetest/blazetest/mathtest/hybridmatrix/ClassTest.h
@@ -111,6 +111,7 @@ class ClassTest
    void testResize      ();
    void testExtend      ();
    void testTranspose   ();
+   void testCTranspose  ();
    void testSwap        ();
    void testIsDefault   ();
 
