6e7f3219ef4a2e4ae30c9c36a09c6289fad4690d
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-01-10 06:52:48 +0100

    Improve the LAPACK operation test

--- blazetest/blazetest/mathtest/lapack/OperationTest.h
+++ blazetest/blazetest/mathtest/lapack/OperationTest.h
@@ -40,7 +40,15 @@
 // Includes
 //*************************************************************************************************
 
+#include <sstream>
+#include <stdexcept>
 #include <string>
+#include <typeinfo>
+#include <blaze/math/LAPACK.h>
+#include <blaze/math/shims/IsDefault.h>
+#include <blaze/math/StaticMatrix.h>
+#include <blaze/math/StaticVector.h>
+#include <blazetest/system/LAPACK.h>
 
 
 namespace blazetest {
@@ -79,10 +87,11 @@ class OperationTest
    //**Test functions******************************************************************************
    /*!\name Test functions */
    //@{
-   void testQR();
-   void testPLU();
-   void testCholesky();
-   void testInversion();
+   template< typename Type > void testGeqrf();
+   template< typename Type > void testGetrf();
+   template< typename Type > void testPotrf();
+   template< typename Type > void testGetri();
+   template< typename Type > void testPotri();
    //@}
    //**********************************************************************************************
 
@@ -93,6 +102,447 @@ class OperationTest
    //@}
    //**********************************************************************************************
 };
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  TEST FUNCTIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\brief Test of the QR decomposition functionality (geqrf).
+//
+// \return void
+// \exception std::runtime_error Error detected.
+//
+// This function performs a test of the QR decomposition functions for various data types. In
+// case an error is detected, a \a std::runtime_error exception is thrown.
+*/
+template< typename Type >
+void OperationTest::testGeqrf()
+{
+#if BLAZETEST_MATHTEST_LAPACK_MODE
+
+   test_ = "QR decomposition";
+
+   {
+      blaze::StaticMatrix<Type,2UL,5UL,blaze::rowMajor> A;
+      randomize( A );
+
+      blaze::StaticMatrix<Type,5UL,2UL,blaze::columnMajor> B( trans( A ) );
+
+      blaze::StaticVector<Type,2UL,blaze::rowVector> tauA;
+      blaze::StaticVector<Type,2UL,blaze::rowVector> tauB;
+
+      blaze::geqrf( A, tauA.data() );
+      blaze::geqrf( B, tauB.data() );
+
+      if( A != trans( B ) || tauA != tauB ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: PLU decomposition failed\n"
+             << " Details:\n"
+             << "   Element type:\n"
+             << "     " << typeid( Type ).name() << "\n"
+             << "   Row-major decomposition:\n" << A << "\n"
+             << "   Row-major projectors:\n" << tauA << "\n"
+             << "   Column-major decomposition:\n" << B << "\n"
+             << "   Column-major projectors:\n" << tauB << "\n";
+         throw std::runtime_error( oss.str() );
+      }
+   }
+
+   {
+      blaze::StaticMatrix<Type,5UL,2UL,blaze::rowMajor> A;
+      randomize( A );
+
+      blaze::StaticMatrix<Type,2UL,5UL,blaze::columnMajor> B( trans( A ) );
+
+      blaze::StaticVector<Type,2UL,blaze::rowVector> tauA;
+      blaze::StaticVector<Type,2UL,blaze::rowVector> tauB;
+
+      blaze::geqrf( A, tauA.data() );
+      blaze::geqrf( B, tauB.data() );
+
+      if( A != trans( B ) || tauA != tauB ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: PLU decomposition failed\n"
+             << " Details:\n"
+             << "   Element type:\n"
+             << "     " << typeid( Type ).name() << "\n"
+             << "   Row-major decomposition:\n" << A << "\n"
+             << "   Row-major projectors:\n" << tauA << "\n"
+             << "   Column-major decomposition:\n" << B << "\n"
+             << "   Column-major projectors:\n" << tauB << "\n";
+         throw std::runtime_error( oss.str() );
+      }
+   }
+
+#endif
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Test of the PLU decomposition functionality (getrf).
+//
+// \return void
+// \exception std::runtime_error Error detected.
+//
+// This function performs a test of the PLU decomposition functions for various data types. In
+// case an error is detected, a \a std::runtime_error exception is thrown.
+*/
+template< typename Type >
+void OperationTest::testGetrf()
+{
+#if BLAZETEST_MATHTEST_LAPACK_MODE
+
+   test_ = "PLU decomposition";
+
+   {
+      blaze::StaticMatrix<Type,2UL,5UL,blaze::rowMajor> A;
+      randomize( A );
+
+      blaze::StaticMatrix<Type,5UL,2UL,blaze::columnMajor> B( trans( A ) );
+
+      blaze::StaticVector<int,2UL,blaze::columnVector> ipivA;
+      blaze::StaticVector<int,2UL,blaze::columnVector> ipivB;
+
+      blaze::getrf( A, ipivA.data() );
+      blaze::getrf( B, ipivB.data() );
+
+      if( A != trans( B ) || ipivA != ipivB ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: PLU decomposition failed\n"
+             << " Details:\n"
+             << "   Element type:\n"
+             << "     " << typeid( Type ).name() << "\n"
+             << "   Row-major decomposition:\n" << A << "\n"
+             << "   Row-major pivot elements:\n" << ipivA << "\n"
+             << "   Column-major decomposition:\n" << B << "\n"
+             << "   Column-major pivot elements:\n" << ipivB << "\n";
+         throw std::runtime_error( oss.str() );
+      }
+   }
+
+   {
+      blaze::StaticMatrix<Type,5UL,2UL,blaze::rowMajor> A;
+      randomize( A );
+
+      blaze::StaticMatrix<Type,2UL,5UL,blaze::columnMajor> B( trans( A ) );
+
+      blaze::StaticVector<int,2UL,blaze::columnVector> ipivA;
+      blaze::StaticVector<int,2UL,blaze::columnVector> ipivB;
+
+      blaze::getrf( A, ipivA.data() );
+      blaze::getrf( B, ipivB.data() );
+
+      if( A != trans( B ) || ipivA != ipivB ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: PLU decomposition failed\n"
+             << " Details:\n"
+             << "   Element type:\n"
+             << "     " << typeid( Type ).name() << "\n"
+             << "   Row-major decomposition:\n" << A << "\n"
+             << "   Row-major pivot elements:\n" << ipivA << "\n"
+             << "   Column-major decomposition:\n" << B << "\n"
+             << "   Column-major pivot elements:\n" << ipivB << "\n";
+         throw std::runtime_error( oss.str() );
+      }
+   }
+
+#endif
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Test of the Cholesky decomposition functionality (potrf).
+//
+// \return void
+// \exception std::runtime_error Error detected.
+//
+// This function performs a test of the Cholesky decomposition functions for various data types.
+// In case an error is detected, a \a std::runtime_error exception is thrown.
+*/
+template< typename Type >
+void OperationTest::testPotrf()
+{
+#if BLAZETEST_MATHTEST_LAPACK_MODE
+
+   test_ = "Cholesky decomposition";
+
+   {
+      blaze::StaticMatrix<Type,3UL,3UL,blaze::rowMajor> A( Type(1), Type( 2), Type( 4),
+                                                           Type(2), Type(13), Type(23),
+                                                           Type(4), Type(23), Type(77) );
+
+      blaze::StaticMatrix<Type,3UL,3UL,blaze::columnMajor> B( A );
+
+      blaze::potrf( A, 'L' );
+      blaze::potrf( B, 'L' );
+
+      if( A != B ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: Cholesky decomposition failed\n"
+             << " Details:\n"
+             << "   Element type:\n"
+             << "     " << typeid( Type ).name() << "\n"
+             << "   Row-major decomposition:\n" << A << "\n"
+             << "   Column-major decomposition:\n" << B << "\n";
+         throw std::runtime_error( oss.str() );
+      }
+   }
+
+   {
+      blaze::StaticMatrix<Type,3UL,3UL,blaze::rowMajor> A( Type(1), Type( 2), Type( 4),
+                                                           Type(2), Type(13), Type(23),
+                                                           Type(4), Type(23), Type(77) );
+
+      blaze::StaticMatrix<Type,3UL,3UL,blaze::columnMajor> B( A );
+
+      blaze::potrf( A, 'U' );
+      blaze::potrf( B, 'U' );
+
+      if( A != B ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: Cholesky decomposition failed\n"
+             << " Details:\n"
+             << "   Element type:\n"
+             << "     " << typeid( Type ).name() << "\n"
+             << "   Row-major decomposition:\n" << A << "\n"
+             << "   Column-major decomposition:\n" << B << "\n";
+         throw std::runtime_error( oss.str() );
+      }
+   }
+
+#endif
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Test of the PLU-based matrix inversion functionality (getri).
+//
+// \return void
+// \exception std::runtime_error Error detected.
+//
+// This function performs a test of the PLU-based matrix inversion functions for various data
+// types. In case an error is detected, a \a std::runtime_error exception is thrown.
+*/
+template< typename Type >
+void OperationTest::testGetri()
+{
+   //=====================================================================================
+   // Row-major matrix tests
+   //=====================================================================================
+
+   {
+      test_ = "Row-major PLU-based matrix inversion";
+
+      blaze::StaticMatrix<Type,3UL,3UL,blaze::rowMajor> A;
+
+      do {
+         randomize( A );
+      }
+      while( blaze::isDefault( det( A ) ) );
+
+      blaze::StaticMatrix<Type,3UL,3UL,blaze::rowMajor> B( A );
+      blaze::StaticVector<int,3UL,blaze::rowVector> ipiv;
+
+      blaze::getrf( A, ipiv.data() );
+      blaze::getri( A, ipiv.data() );
+
+      if( !blaze::isIdentity( A * B ) ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: PLU-based matrix inversion failed\n"
+             << " Details:\n"
+             << "   Element type:\n"
+             << "     " << typeid( Type ).name() << "\n"
+             << "   Result:\n" << A << "\n";
+         throw std::runtime_error( oss.str() );
+      }
+   }
+
+
+   //=====================================================================================
+   // Column-major matrix tests
+   //=====================================================================================
+
+   {
+      test_ = "Column-major PLU-based matrix inversion";
+
+      blaze::StaticMatrix<Type,3UL,3UL,blaze::columnMajor> A;
+
+      do {
+         randomize( A );
+      }
+      while( blaze::isDefault( det( A ) ) );
+
+      blaze::StaticMatrix<Type,3UL,3UL,blaze::columnMajor> B( A );
+      blaze::StaticVector<int,3UL,blaze::rowVector> ipiv;
+
+      blaze::getrf( A, ipiv.data() );
+      blaze::getri( A, ipiv.data() );
+
+      if( !blaze::isIdentity( A * B ) ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: PLU-based matrix inversion failed\n"
+             << " Details:\n"
+             << "   Element type:\n"
+             << "     " << typeid( Type ).name() << "\n"
+             << "   Result:\n" << A << "\n";
+         throw std::runtime_error( oss.str() );
+      }
+   }
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Test of the Cholesky-based matrix inversion functionality (potri).
+//
+// \return void
+// \exception std::runtime_error Error detected.
+//
+// This function performs a test of the Cholesky-based matrix inversion functions for various
+// data types. In case an error is detected, a \a std::runtime_error exception is thrown.
+*/
+template< typename Type >
+void OperationTest::testPotri()
+{
+   //=====================================================================================
+   // Row-major matrix tests
+   //=====================================================================================
+
+   {
+      test_ = "Row-major Cholesky-based matrix inversion (lower part)";
+
+      blaze::StaticMatrix<Type,3UL,3UL,blaze::rowMajor> A( Type(1), Type(1), Type(1),
+                                                           Type(1), Type(2), Type(2),
+                                                           Type(1), Type(2), Type(4) );
+
+      blaze::StaticMatrix<Type,3UL,3UL,blaze::rowMajor> B( A );
+
+      blaze::potrf( A, 'L' );
+      blaze::potri( A, 'L' );
+
+      A(0,1) = A(1,0);
+      A(0,2) = A(2,0);
+      A(1,2) = A(2,1);
+
+      if( !blaze::isIdentity( A * B ) ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: Cholesky-based matrix inversion failed\n"
+             << " Details:\n"
+             << "   Element type:\n"
+             << "     " << typeid( Type ).name() << "\n"
+             << "   Result:\n" << A << "\n";
+         throw std::runtime_error( oss.str() );
+      }
+   }
+
+   {
+      test_ = "Row-major Cholesky-based matrix inversion (upper part)";
+
+      blaze::StaticMatrix<Type,3UL,3UL,blaze::rowMajor> A( Type(1), Type(1), Type(1),
+                                                           Type(1), Type(2), Type(2),
+                                                           Type(1), Type(2), Type(4) );
+
+      blaze::StaticMatrix<Type,3UL,3UL,blaze::rowMajor> B( A );
+
+      blaze::potrf( A, 'U' );
+      blaze::potri( A, 'U' );
+
+      A(1,0) = A(0,1);
+      A(2,0) = A(0,2);
+      A(2,1) = A(1,2);
+
+      if( !blaze::isIdentity( A * B ) ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: Cholesky-based matrix inversion failed\n"
+             << " Details:\n"
+             << "   Element type:\n"
+             << "     " << typeid( Type ).name() << "\n"
+             << "   Result:\n" << A << "\n";
+         throw std::runtime_error( oss.str() );
+      }
+   }
+
+
+   //=====================================================================================
+   // Column-major matrix tests
+   //=====================================================================================
+
+   {
+      test_ = "Column-major Cholesky-based matrix inversion (lower part)";
+
+      blaze::StaticMatrix<Type,3UL,3UL,blaze::columnMajor> A( Type(1), Type(1), Type(1),
+                                                              Type(1), Type(2), Type(2),
+                                                              Type(1), Type(2), Type(4) );
+
+      blaze::StaticMatrix<Type,3UL,3UL,blaze::columnMajor> B( A );
+
+      blaze::potrf( A, 'L' );
+      blaze::potri( A, 'L' );
+
+      A(0,1) = A(1,0);
+      A(0,2) = A(2,0);
+      A(1,2) = A(2,1);
+
+      if( !blaze::isIdentity( A * B ) ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: Cholesky-based matrix inversion failed\n"
+             << " Details:\n"
+             << "   Element type:\n"
+             << "     " << typeid( Type ).name() << "\n"
+             << "   Result:\n" << A << "\n";
+         throw std::runtime_error( oss.str() );
+      }
+   }
+
+   {
+      test_ = "Column-major Cholesky-based matrix inversion (upper part)";
+
+      blaze::StaticMatrix<Type,3UL,3UL,blaze::columnMajor> A( Type(1), Type(1), Type(1),
+                                                              Type(1), Type(2), Type(2),
+                                                              Type(1), Type(2), Type(4) );
+
+      blaze::StaticMatrix<Type,3UL,3UL,blaze::columnMajor> B( A );
+
+      blaze::potrf( A, 'U' );
+      blaze::potri( A, 'U' );
+
+      A(1,0) = A(0,1);
+      A(2,0) = A(0,2);
+      A(2,1) = A(1,2);
+
+      if( !blaze::isIdentity( A * B ) ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: Cholesky-based matrix inversion failed\n"
+             << " Details:\n"
+             << "   Element type:\n"
+             << "     " << typeid( Type ).name() << "\n"
+             << "   Result:\n" << A << "\n";
+         throw std::runtime_error( oss.str() );
+      }
+   }
+}
 //*************************************************************************************************
 
 
