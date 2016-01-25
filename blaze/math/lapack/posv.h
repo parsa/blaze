4f5f56bd03d100891ba375b60537585f26e66406
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-01-25 12:09:44 +0100

    Rework the 'posv' LAPACK wrapper functions

--- blaze/math/lapack/posv.h
+++ blaze/math/lapack/posv.h
@@ -47,8 +47,6 @@
 #include <blaze/math/constraints/MutableDataAccess.h>
 #include <blaze/math/expressions/DenseMatrix.h>
 #include <blaze/math/expressions/DenseVector.h>
-#include <blaze/math/StorageOrder.h>
-#include <blaze/math/TransposeFlag.h>
 #include <blaze/util/Assert.h>
 #include <blaze/util/Complex.h>
 #include <blaze/util/constraints/SameType.h>
@@ -82,12 +80,12 @@ void zposv_( char* uplo, int* n, int* nrhs, double* A, int* lda, double* b, int*
 
 //=================================================================================================
 //
-//  LAPACK WRAPPER FUNCTIONS (POSV)
+//  LAPACK LINEAR SYSTEM FUNCTIONS (POSV)
 //
 //=================================================================================================
 
 //*************************************************************************************************
-/*!\name LAPACK wrapper functions (posv) */
+/*!\name LAPACK linear system functions (posv) */
 //@{
 inline void posv( char uplo, int n, int nrhs, float* A, int lda, float* B, int ldb, int* info );
 
@@ -97,11 +95,11 @@ inline void posv( char uplo, int n, int nrhs, complex<float>* A, int lda, comple
 
 inline void posv( char uplo, int n, int nrhs, complex<double>* A, int lda, complex<double>* B, int ldb, int* info );
 
-template< typename MT, typename VT >
-inline void posv( DenseMatrix<MT,columnMajor>& A, DenseVector<VT,columnVector>& b, char uplo );
+template< typename MT, bool SO, typename VT, bool TF >
+inline void posv( DenseMatrix<MT,SO>& A, DenseVector<VT,TF>& b, char uplo );
 
-template< typename MT1, typename MT2 >
-inline void posv( DenseMatrix<MT1,columnMajor>& A, DenseMatrix<MT2,columnMajor>& B, char uplo );
+template< typename MT1, bool SO1, typename MT2, bool SO2 >
+inline void posv( DenseMatrix<MT1,SO1>& A, DenseMatrix<MT2,SO2>& B, char uplo );
 //@}
 //*************************************************************************************************
 
@@ -114,10 +112,10 @@ inline void posv( DenseMatrix<MT1,columnMajor>& A, DenseMatrix<MT2,columnMajor>&
 // \param uplo \c 'L' to use the lower part of the matrix, \c 'U' to use the upper part.
 // \param n The number of rows/columns of matrix \a A \f$[0..\infty)\f$.
 // \param nrhs The number of right-hand side vectors \f$[0..\infty)\f$.
-// \param A Pointer to the first element of the matrix.
-// \param lda The total number of elements between two rows/columns of matrix \a A \f$[0..\infty)\f$.
-// \param B Pointer to the first element of the matrix.
-// \param ldb The total number of elements between two rows/columns of matrix \a B \f$[0..\infty)\f$.
+// \param A Pointer to the first element of the single precision column-major square matrix.
+// \param lda The total number of elements between two columns of matrix \a A \f$[0..\infty)\f$.
+// \param B Pointer to the first element of the column-major matrix.
+// \param ldb The total number of elements between two columns of matrix \a B \f$[0..\infty)\f$.
 // \param info Return code of the function call.
 // \return void
 //
@@ -165,10 +163,10 @@ inline void posv( char uplo, int n, int nrhs, float* A, int lda, float* B, int l
 // \param uplo \c 'L' to use the lower part of the matrix, \c 'U' to use the upper part.
 // \param n The number of rows/columns of matrix \a A \f$[0..\infty)\f$.
 // \param nrhs The number of right-hand side vectors \f$[0..\infty)\f$.
-// \param A Pointer to the first element of the matrix.
-// \param lda The total number of elements between two rows/columns of matrix \a A \f$[0..\infty)\f$.
-// \param B Pointer to the first element of the matrix.
-// \param ldb The total number of elements between two rows/columns of matrix \a B \f$[0..\infty)\f$.
+// \param A Pointer to the first element of the double precision column-major square matrix.
+// \param lda The total number of elements between two columns of matrix \a A \f$[0..\infty)\f$.
+// \param B Pointer to the first element of the column-major matrix.
+// \param ldb The total number of elements between two columns of matrix \a B \f$[0..\infty)\f$.
 // \param info Return code of the function call.
 // \return void
 //
@@ -216,10 +214,10 @@ inline void posv( char uplo, int n, int nrhs, double* A, int lda, double* B, int
 // \param uplo \c 'L' to use the lower part of the matrix, \c 'U' to use the upper part.
 // \param n The number of rows/columns of matrix \a A \f$[0..\infty)\f$.
 // \param nrhs The number of right-hand side vectors \f$[0..\infty)\f$.
-// \param A Pointer to the first element of the matrix.
-// \param lda The total number of elements between two rows/columns of matrix \a A \f$[0..\infty)\f$.
-// \param B Pointer to the first element of the matrix.
-// \param ldb The total number of elements between two rows/columns of matrix \a B \f$[0..\infty)\f$.
+// \param A Pointer to the first element of the single precision complex column-major square matrix.
+// \param lda The total number of elements between two columns of matrix \a A \f$[0..\infty)\f$.
+// \param B Pointer to the first element of the column-major matrix.
+// \param ldb The total number of elements between two columns of matrix \a B \f$[0..\infty)\f$.
 // \param info Return code of the function call.
 // \return void
 //
@@ -270,10 +268,10 @@ inline void posv( char uplo, int n, int nrhs, complex<float>* A, int lda, comple
 // \param uplo \c 'L' to use the lower part of the matrix, \c 'U' to use the upper part.
 // \param n The number of rows/columns of matrix \a A \f$[0..\infty)\f$.
 // \param nrhs The number of right-hand side vectors \f$[0..\infty)\f$.
-// \param A Pointer to the first element of the matrix.
-// \param lda The total number of elements between two rows/columns of matrix \a A \f$[0..\infty)\f$.
-// \param B Pointer to the first element of the matrix.
-// \param ldb The total number of elements between two rows/columns of matrix \a B \f$[0..\infty)\f$.
+// \param A Pointer to the first element of the double precision complex column-major square matrix.
+// \param lda The total number of elements between two columns of matrix \a A \f$[0..\infty)\f$.
+// \param B Pointer to the first element of the column-major matrix.
+// \param ldb The total number of elements between two columns of matrix \a B \f$[0..\infty)\f$.
 // \param info Return code of the function call.
 // \return void
 //
@@ -324,15 +322,21 @@ inline void posv( char uplo, int n, int nrhs, complex<double>* A, int lda, compl
 // \param b The right-hand side vector.
 // \param uplo \c 'L' to use the lower part of the matrix, \c 'U' to use the upper part.
 // \return void
-// \exception std::invalid_argument Inversion of singular matrix failed.
 // \exception std::invalid_argument Invalid non-square matrix provided.
+// \exception std::invalid_argument Invalid uplo argument provided.
+// \exception std::invalid_argument Inversion of singular matrix failed.
 //
 // This function uses the LAPACK posv() functions to compute the solution to the positive definite
-// system of linear equations \f$ A*x=b \f$, where \a A is a column-major n-by-n matrix and \a x
-// and \a b are n-dimensional column vectors. Note that the function only works for general,
-// non-adapted matrices with \c float, \c double, \c complex<float>, or \c complex<double> element
-// type. The attempt to call the function with adaptors or matrices of any other element type
-// results in a compile time error!
+// system of linear equations:
+//
+//  - \f$ A  *x=b \f$ if \a A is column-major
+//  - \f$ A^T*x=b \f$ if \a A is row-major
+//
+// In this context the positive definite system matrix \a A is a n-by-n matrix and \a x and \a b
+// are n-dimensional vectors. Note that the function only works for general, non-adapted matrices
+// with \c float, \c double, \c complex<float>, or \c complex<double> element type. The attempt
+// to call the function with adaptors or matrices of any other element type results in a compile
+// time error!
 //
 // If the function exits successfully, the vector \a b contains the solution of the linear system
 // of equations and \a A has been decomposed by means of a Cholesky decomposition. The decomposition
@@ -350,6 +354,7 @@ inline void posv( char uplo, int n, int nrhs, complex<double>* A, int lda, compl
 // The function fails if ...
 //
 //  - ... the given system matrix is not a square matrix;
+//  - ... the given \a uplo argument is neither 'L' nor 'U';
 //  - ... the given system matrix is singular and not invertible.
 //
 // In all failure cases a \a std::invalid_argument exception is thrown.
@@ -365,8 +370,11 @@ inline void posv( char uplo, int n, int nrhs, complex<double>* A, int lda, compl
 // \note This function does not provide any exception safety guarantee, i.e. in case an exception
 // is thrown \a A may already have been modified.
 */
-template< typename MT, typename VT >
-inline void posv( DenseMatrix<MT,columnMajor>& A, DenseVector<VT,columnVector>& b, char uplo )
+template< typename MT  // Type of the system matrix
+        , bool SO      // Storage order of the system matrix
+        , typename VT  // Type of the right-hand side vector
+        , bool TF >    // Transpose flag of the right-hand side vector
+inline void posv( DenseMatrix<MT,SO>& A, DenseVector<VT,TF>& b, char uplo )
 {
    using boost::numeric_cast;
 
@@ -396,6 +404,10 @@ inline void posv( DenseMatrix<MT,columnMajor>& A, DenseVector<VT,columnVector>& 
       return;
    }
 
+   if( IsRowMajorMatrix<MT>::value ) {
+      ( uplo == 'L' )?( uplo = 'U' ):( uplo = 'L' );
+   }
+
    posv( uplo, n, nrhs, (~A).data(), lda, (~b).data(), ldb, &info );
 
    BLAZE_INTERNAL_ASSERT( info >= 0, "Invalid function argument" );
@@ -415,17 +427,25 @@ inline void posv( DenseMatrix<MT,columnMajor>& A, DenseVector<VT,columnVector>& 
 // \param B The matrix of right-hand sides.
 // \param uplo \c 'L' to use the lower part of the matrix, \c 'U' to use the upper part.
 // \return void
-// \exception std::invalid_argument Inversion of singular matrix failed.
 // \exception std::invalid_argument Invalid non-square matrix provided.
+// \exception std::invalid_argument Matrix sizes do not match.
+// \exception std::invalid_argument Inversion of singular matrix failed.
 //
 // This function uses the LAPACK posv() functions to compute the solution to the positive definite
-// system of linear equations \f$ A*X=B \f$, where \a A is a column-major n-by-n matrix and \a X
-// and \a B are column-major n-by-m matrices. Note that the function only works for general,
-// non-adapted matrices with \c float, \c double, \c complex<float>, or \c complex<double> element
-// type. The attempt to call the function with adaptors or matrices of any other element type
-// results in a compile time error!
+// system of linear equations:
 //
-// If the function exits successfully, the vector \a b contains the solution of the linear system
+//  - \f$ A  *X  =B   \f$ if both \a A and \a B are column-major
+//  - \f$ A^T*X  =B   \f$ if \a A is row-major and \a B is column-major
+//  - \f$ A  *X^T=B^T \f$ if \a A is column-major and \a B is row-major
+//  - \f$ A^T*X^T=B^T \f$ if both \a A and \a B are row-major
+//
+// In this context the positive definite system matrix \a A is a n-by-n matrix and \a X and \a B
+// are either row-major m-by-n matrices or column-major n-by-m matrices. Note that the function
+// only works for general, non-adapted matrices with \c float, \c double, \c complex<float>, or
+// \c complex<double> element type. The attempt to call the function with adaptors or matrices
+// of any other element type results in a compile time error!
+//
+// If the function exits successfully, the matrix \a B contains the solution of the linear system
 // of equations and \a A has been decomposed by means of a Cholesky decomposition. The decomposition
 // has the form
 
@@ -441,6 +461,7 @@ inline void posv( DenseMatrix<MT,columnMajor>& A, DenseVector<VT,columnVector>& 
 // The function fails if ...
 //
 //  - ... the given system matrix is not a square matrix;
+//  - ... the sizes of the two given matrices do not match;
 //  - ... the given system matrix is singular and not invertible.
 //
 // In all failure cases a \a std::invalid_argument exception is thrown.
@@ -456,8 +477,11 @@ inline void posv( DenseMatrix<MT,columnMajor>& A, DenseVector<VT,columnVector>& 
 // \note This function does not provide any exception safety guarantee, i.e. in case an exception
 // is thrown \a A may already have been modified.
 */
-template< typename MT1, typename MT2 >
-inline void posv( DenseMatrix<MT1,columnMajor>& A, DenseMatrix<MT2,columnMajor>& B, char uplo )
+template< typename MT1  // Type of the system matrix
+        , bool SO1      // Storage order of the system matrix
+        , typename MT2  // Type of the right-hand side matrix
+        , bool SO2 >    // Storage order of the right-hand side matrix
+inline void posv( DenseMatrix<MT1,SO1>& A, DenseMatrix<MT2,SO2>& B, char uplo )
 {
    using boost::numeric_cast;
 
@@ -478,14 +502,23 @@ inline void posv( DenseMatrix<MT1,columnMajor>& A, DenseMatrix<MT2,columnMajor>&
       BLAZE_THROW_INVALID_ARGUMENT( "Invalid uplo argument provided" );
    }
 
-   int n   ( numeric_cast<int>( (~A).rows()    ) );
-   int nrhs( numeric_cast<int>( (~B).columns() ) );
+   int n   ( numeric_cast<int>( (~A).rows() ) );
+   int mrhs( numeric_cast<int>( SO2 ? (~B).rows() : (~B).columns() ) );
+   int nrhs( numeric_cast<int>( SO2 ? (~B).columns() : (~B).rows() ) );
    int lda ( numeric_cast<int>( (~A).spacing() ) );
    int ldb ( numeric_cast<int>( (~B).spacing() ) );
    int info( 0 );
 
+   if( n != mrhs ) {
+      BLAZE_THROW_INVALID_ARGUMENT( "Matrix sizes do not match" );
+   }
+
    if( n == 0 ) {
       return;
+   }
+
+   if( IsRowMajorMatrix<MT1>::value ) {
+      ( uplo == 'L' )?( uplo = 'U' ):( uplo = 'L' );
    }
 
    posv( uplo, n, nrhs, (~A).data(), lda, (~B).data(), ldb, &info );
