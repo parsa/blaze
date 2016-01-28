17cca824af299ca32bbee3335cae0610a38169cb
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-01-28 13:27:06 +0100

    Add 'makeSymmetric', 'makeHermitian', and 'makePositiveDefinite' functions for the 'CustomMatrix' class template

--- blaze/math/CustomMatrix.h
+++ blaze/math/CustomMatrix.h
@@ -44,6 +44,12 @@
 #include <blaze/math/dense/DynamicMatrix.h>
 #include <blaze/math/DenseMatrix.h>
 #include <blaze/math/DynamicVector.h>
+#include <blaze/math/shims/Conjugate.h>
+#include <blaze/math/shims/Real.h>
+#include <blaze/math/typetraits/UnderlyingBuiltin.h>
+#include <blaze/util/Assert.h>
+#include <blaze/util/constraints/Numeric.h>
+#include <blaze/util/Exception.h>
 #include <blaze/util/Random.h>
 
 
@@ -138,6 +144,209 @@ inline void Rand< CustomMatrix<Type,AF,PF,SO> >::randomize( CustomMatrix<Type,AF
          randomize( matrix(i,j), min, max );
       }
    }
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  MAKE FUNCTIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Setup of a random symmetric CustomMatrix.
+//
+// \param matrix The matrix to be randomized.
+// \return void
+// \exception std::invalid_argument Invalid non-square matrix provided.
+*/
+template< typename Type  // Data type of the matrix
+        , bool AF        // Alignment flag
+        , bool PF        // Padding flag
+        , bool SO >      // Storage order
+void makeSymmetric( CustomMatrix<Type,AF,PF,SO>& matrix )
+{
+   using blaze::randomize;
+
+   if( !isSquare( ~matrix ) ) {
+      BLAZE_THROW_INVALID_ARGUMENT( "Invalid non-square matrix provided" );
+   }
+
+   const size_t n( matrix.rows() );
+
+   for( size_t i=0UL; i<n; ++i ) {
+      for( size_t j=0UL; j<i; ++j ) {
+         randomize( matrix(i,j) );
+         matrix(j,i) = matrix(i,j);
+      }
+      randomize( matrix(i,i) );
+   }
+
+   BLAZE_INTERNAL_ASSERT( isSymmetric( matrix ), "Non-symmetric matrix detected" );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Setup of a random symmetric CustomMatrix.
+//
+// \param matrix The matrix to be randomized.
+// \param min The smallest possible value for a matrix element.
+// \param max The largest possible value for a matrix element.
+// \return void
+// \exception std::invalid_argument Invalid non-square matrix provided.
+*/
+template< typename Type   // Data type of the matrix
+        , bool AF         // Alignment flag
+        , bool PF         // Padding flag
+        , bool SO         // Storage order
+        , typename Arg >  // Min/max argument type
+void makeSymmetric( CustomMatrix<Type,AF,PF,SO>& matrix, const Arg& min, const Arg& max )
+{
+   using blaze::randomize;
+
+   if( !isSquare( ~matrix ) ) {
+      BLAZE_THROW_INVALID_ARGUMENT( "Invalid non-square matrix provided" );
+   }
+
+   const size_t n( matrix.rows() );
+
+   for( size_t i=0UL; i<n; ++i ) {
+      for( size_t j=0UL; j<i; ++j ) {
+         randomize( matrix(i,j), min, max );
+         matrix(j,i) = matrix(i,j);
+      }
+      randomize( matrix(i,i), min, max );
+   }
+
+   BLAZE_INTERNAL_ASSERT( isSymmetric( matrix ), "Non-symmetric matrix detected" );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Setup of a random Hermitian CustomMatrix.
+//
+// \param matrix The matrix to be randomized.
+// \return void
+// \exception std::invalid_argument Invalid non-square matrix provided.
+*/
+template< typename Type  // Data type of the matrix
+        , bool AF        // Alignment flag
+        , bool PF        // Padding flag
+        , bool SO >      // Storage order
+void makeHermitian( CustomMatrix<Type,AF,PF,SO>& matrix )
+{
+   using blaze::randomize;
+
+   BLAZE_CONSTRAINT_MUST_BE_NUMERIC_TYPE( Type );
+
+   typedef typename UnderlyingBuiltin<Type>::Type  BT;
+
+   if( !isSquare( ~matrix ) ) {
+      BLAZE_THROW_INVALID_ARGUMENT( "Invalid non-square matrix provided" );
+   }
+
+   const size_t n( matrix.rows() );
+
+   for( size_t i=0UL; i<n; ++i ) {
+      for( size_t j=0UL; j<i; ++j ) {
+         randomize( matrix(i,j) );
+         matrix(j,i) = conj( matrix(i,j) );
+      }
+      matrix(i,i) = rand<BT>();
+   }
+
+   BLAZE_INTERNAL_ASSERT( isHermitian( matrix ), "Non-Hermitian matrix detected" );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Setup of a random Hermitian CustomMatrix.
+//
+// \param matrix The matrix to be randomized.
+// \param min The smallest possible value for a matrix element.
+// \param max The largest possible value for a matrix element.
+// \return void
+// \exception std::invalid_argument Invalid non-square matrix provided.
+*/
+template< typename Type   // Data type of the matrix
+        , bool AF         // Alignment flag
+        , bool PF         // Padding flag
+        , bool SO         // Storage order
+        , typename Arg >  // Min/max argument type
+void makeHermitian( CustomMatrix<Type,AF,PF,SO>& matrix, const Arg& min, const Arg& max )
+{
+   using blaze::randomize;
+
+   BLAZE_CONSTRAINT_MUST_BE_NUMERIC_TYPE( Type );
+
+   typedef typename UnderlyingBuiltin<Type>::Type  BT;
+
+   if( !isSquare( ~matrix ) ) {
+      BLAZE_THROW_INVALID_ARGUMENT( "Invalid non-square matrix provided" );
+   }
+
+   const size_t n( matrix.rows() );
+
+   for( size_t i=0UL; i<n; ++i ) {
+      for( size_t j=0UL; j<i; ++j ) {
+         randomize( matrix(i,j), min, max );
+         matrix(j,i) = conj( matrix(i,j) );
+      }
+      matrix(i,i) = rand<BT>( real( min ), real( max ) );
+   }
+
+   BLAZE_INTERNAL_ASSERT( isHermitian( matrix ), "Non-Hermitian matrix detected" );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Setup of a random (Hermitian) positive definite CustomMatrix.
+//
+// \param matrix The matrix to be randomized.
+// \return void
+// \exception std::invalid_argument Invalid non-square matrix provided.
+*/
+template< typename Type  // Data type of the matrix
+        , bool AF        // Alignment flag
+        , bool PF        // Padding flag
+        , bool SO >      // Storage order
+void makePositiveDefinite( CustomMatrix<Type,AF,PF,SO>& matrix )
+{
+   using blaze::randomize;
+
+   BLAZE_CONSTRAINT_MUST_BE_NUMERIC_TYPE( Type );
+
+   if( !isSquare( ~matrix ) ) {
+      BLAZE_THROW_INVALID_ARGUMENT( "Invalid non-square matrix provided" );
+   }
+
+   const size_t n( matrix.rows() );
+
+   randomize( matrix );
+   matrix *= ctrans( matrix );
+
+   for( size_t i=0UL; i<n; ++i ) {
+      matrix(i,i) += Type(n);
+   }
+
+   BLAZE_INTERNAL_ASSERT( isHermitian( matrix ), "Non-symmetric matrix detected" );
 }
 /*! \endcond */
 //*************************************************************************************************
