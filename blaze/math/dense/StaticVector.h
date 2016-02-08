dfc142852f6d46acaddda46be7bcf03d49ef849c
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-02-08 11:40:37 +0100

    Add the 'at' member functions to the 'StaticVector' class template

--- blaze/math/dense/StaticVector.h
+++ blaze/math/dense/StaticVector.h
@@ -255,6 +255,8 @@ class StaticVector : public DenseVector< StaticVector<Type,N,TF>, TF >
    //@{
    inline Reference      operator[]( size_t index );
    inline ConstReference operator[]( size_t index ) const;
+   inline Reference      at( size_t index );
+   inline ConstReference at( size_t index ) const;
    inline Pointer        data  ();
    inline ConstPointer   data  () const;
    inline Iterator       begin ();
@@ -852,7 +854,8 @@ inline StaticVector<Type,N,TF>::StaticVector( const Type& v1, const Type& v2, co
 // \param index Access index. The index has to be in the range \f$[0..N-1]\f$.
 // \return Reference to the accessed value.
 //
-// In case BLAZE_USER_ASSERT() is active, this operator performs an index check.
+// This function only performs an index check in case BLAZE_USER_ASSERT() is active. In contrast,
+// the at() function is guaranteed to perform a check of the given access index.
 */
 template< typename Type  // Data type of the vector
         , size_t N       // Number of elements
@@ -872,7 +875,8 @@ inline typename StaticVector<Type,N,TF>::Reference
 // \param index Access index. The index has to be in the range \f$[0..N-1]\f$.
 // \return Reference-to-const to the accessed value.
 //
-// In case BLAZE_USER_ASSERT() is active, this operator performs an index check.
+// This function only performs an index check in case BLAZE_USER_ASSERT() is active. In contrast,
+// the at() function is guaranteed to perform a check of the given access index.
 */
 template< typename Type  // Data type of the vector
         , size_t N       // Number of elements
@@ -882,6 +886,54 @@ inline typename StaticVector<Type,N,TF>::ConstReference
 {
    BLAZE_USER_ASSERT( index < N, "Invalid vector access index" );
    return v_[index];
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Checked access to the vector elements.
+//
+// \param index Access index. The index has to be in the range \f$[0..N-1]\f$.
+// \return Reference to the accessed value.
+// \exception std::out_of_range Invalid vector access index.
+//
+// In contrast to the subscript operator this function always performs a check of the given
+// access index.
+*/
+template< typename Type  // Data type of the vector
+        , size_t N       // Number of elements
+        , bool TF >      // Transpose flag
+inline typename StaticVector<Type,N,TF>::Reference
+   StaticVector<Type,N,TF>::at( size_t index )
+{
+   if( index >= N ) {
+      BLAZE_THROW_OUT_OF_RANGE( "Invalid vector access index" );
+   }
+   return (*this)[index];
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Checked access to the vector elements.
+//
+// \param index Access index. The index has to be in the range \f$[0..N-1]\f$.
+// \return Reference to the accessed value.
+// \exception std::out_of_range Invalid vector access index.
+//
+// In contrast to the subscript operator this function always performs a check of the given
+// access index.
+*/
+template< typename Type  // Data type of the vector
+        , size_t N       // Number of elements
+        , bool TF >      // Transpose flag
+inline typename StaticVector<Type,N,TF>::ConstReference
+   StaticVector<Type,N,TF>::at( size_t index ) const
+{
+   if( index >= N ) {
+      BLAZE_THROW_OUT_OF_RANGE( "Invalid vector access index" );
+   }
+   return (*this)[index];
 }
 //*************************************************************************************************
 
