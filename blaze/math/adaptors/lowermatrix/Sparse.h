bdb99fe725044429445b0d918cb8c7f343fedac0
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-02-08 11:59:04 +0100

    Add the 'at' member functions to the 'LowerMatrix' class template

--- blaze/math/adaptors/lowermatrix/Sparse.h
+++ blaze/math/adaptors/lowermatrix/Sparse.h
@@ -156,6 +156,8 @@ class LowerMatrix<MT,SO,false>
    //@{
    inline Reference      operator()( size_t i, size_t j );
    inline ConstReference operator()( size_t i, size_t j ) const;
+   inline Reference      at( size_t i, size_t j );
+   inline ConstReference at( size_t i, size_t j ) const;
    inline Iterator       begin ( size_t i );
    inline ConstIterator  begin ( size_t i ) const;
    inline ConstIterator  cbegin( size_t i ) const;
@@ -472,10 +474,14 @@ inline LowerMatrix<MT,SO,false>::LowerMatrix( const Matrix<MT2,SO2>& m )
 // \param i Access index for the row. The index has to be in the range \f$[0..N-1]\f$.
 // \param j Access index for the column. The index has to be in the range \f$[0..N-1]\f$.
 // \return Reference to the accessed value.
+// \exception std::invalid_argument Invalid assignment to upper matrix element.
 //
 // The function call operator provides access to the elements at position (i,j). The attempt to
 // assign to an element in the upper part of the matrix (i.e. above the diagonal) will result in
 // a \a std::invalid_argument exception.
+//
+// Note that this function only performs an index check in case BLAZE_USER_ASSERT() is active. In
+// contrast, the at() function is guaranteed to perform a check of the given access indices.
 */
 template< typename MT  // Type of the adapted sparse matrix
         , bool SO >    // Storage order of the adapted sparse matrix
@@ -498,10 +504,14 @@ inline typename LowerMatrix<MT,SO,false>::Reference
 // \param i Access index for the row. The index has to be in the range \f$[0..N-1]\f$.
 // \param j Access index for the column. The index has to be in the range \f$[0..N-1]\f$.
 // \return Reference to the accessed value.
+// \exception std::invalid_argument Invalid assignment to upper matrix element.
 //
 // The function call operator provides access to the elements at position (i,j). The attempt to
 // assign to an element in the upper part of the matrix (i.e. above the diagonal) will result in
 // a \a std::invalid_argument exception.
+//
+// Note that this function only performs an index check in case BLAZE_USER_ASSERT() is active. In
+// contrast, the at() function is guaranteed to perform a check of the given access indices.
 */
 template< typename MT  // Type of the adapted sparse matrix
         , bool SO >    // Storage order of the adapted sparse matrix
@@ -512,6 +522,74 @@ inline typename LowerMatrix<MT,SO,false>::ConstReference
    BLAZE_USER_ASSERT( j<columns(), "Invalid column access index" );
 
    return matrix_(i,j);
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Checked access to the matrix elements.
+//
+// \param i Access index for the row. The index has to be in the range \f$[0..N-1]\f$.
+// \param j Access index for the column. The index has to be in the range \f$[0..N-1]\f$.
+// \return Reference to the accessed value.
+// \exception std::out_of_range Invalid matrix access index.
+// \exception std::invalid_argument Invalid assignment to upper matrix element.
+//
+// The function call operator provides access to the elements at position (i,j). The attempt to
+// assign to an element in the upper part of the matrix (i.e. above the diagonal) will result in
+// a \a std::invalid_argument exception.
+//
+// Note that in contrast to the subscript operator this function always performs a check of the
+// given access indices.
+*/
+template< typename MT  // Type of the adapted sparse matrix
+        , bool SO >    // Storage order of the adapted sparse matrix
+inline typename LowerMatrix<MT,SO,false>::Reference
+   LowerMatrix<MT,SO,false>::at( size_t i, size_t j )
+{
+   if( i >= rows() ) {
+      BLAZE_THROW_OUT_OF_RANGE( "Invalid row access index" );
+   }
+   if( j >= columns() ) {
+      BLAZE_THROW_OUT_OF_RANGE( "Invalid column access index" );
+   }
+   return (*this)(i,j);
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Checked access to the matrix elements.
+//
+// \param i Access index for the row. The index has to be in the range \f$[0..N-1]\f$.
+// \param j Access index for the column. The index has to be in the range \f$[0..N-1]\f$.
+// \return Reference to the accessed value.
+// \exception std::out_of_range Invalid matrix access index.
+// \exception std::invalid_argument Invalid assignment to upper matrix element.
+//
+// The function call operator provides access to the elements at position (i,j). The attempt to
+// assign to an element in the upper part of the matrix (i.e. above the diagonal) will result in
+// a \a std::invalid_argument exception.
+//
+// Note that in contrast to the subscript operator this function always performs a check of the
+// given access indices.
+*/
+template< typename MT  // Type of the adapted sparse matrix
+        , bool SO >    // Storage order of the adapted sparse matrix
+inline typename LowerMatrix<MT,SO,false>::ConstReference
+   LowerMatrix<MT,SO,false>::at( size_t i, size_t j ) const
+{
+   if( i >= rows() ) {
+      BLAZE_THROW_OUT_OF_RANGE( "Invalid row access index" );
+   }
+   if( j >= columns() ) {
+      BLAZE_THROW_OUT_OF_RANGE( "Invalid column access index" );
+   }
+   return (*this)(i,j);
 }
 /*! \endcond */
 //*************************************************************************************************
