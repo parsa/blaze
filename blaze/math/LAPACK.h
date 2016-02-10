231904a4398355da7fd93c3b43fad2f8dc3fc1d0
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-02-10 14:20:06 +0100

    Add the 'gelqf' LAPACK wrapper functions

--- blaze/math/LAPACK.h
+++ blaze/math/LAPACK.h
@@ -40,6 +40,7 @@
 // Includes
 //*************************************************************************************************
 
+#include <blaze/math/lapack/gelqf.h>
 #include <blaze/math/lapack/geqrf.h>
 #include <blaze/math/lapack/gerqf.h>
 #include <blaze/math/lapack/gesv.h>
