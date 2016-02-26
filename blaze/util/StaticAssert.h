6f0dcd6c052777b5009a775fcc24caa0dfc9cb01
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-02-26 14:38:44 +0100

    Implement the 'BLAZE_STATIC_ASSERT' macro in terms of the C++11 'static_assert'

--- blaze/util/StaticAssert.h
+++ blaze/util/StaticAssert.h
@@ -55,82 +55,51 @@ namespace blaze {
 /*!\defgroup static_assert Compile time assertion
 // \ingroup assert
 //
-// Static assertion offers the possibility to stop the compilation process if a specific
-// compile time condition is not met. The blaze::BLAZE_STATIC_ASSERT macro can be used to check
-// an integral constant expression at compile time. If the expression evaluates to \a false,
-// a compilation error is generated that stops the compilation process. If the expression
-// (hopefully) evaluates to \a true, the compilation process is not aborted and the static
-// check leaves neither code nor data and is therefore not affecting the performance.\n
-// The blaze::BLAZE_STATIC_ASSERT macro can be used wherever a standard typedef statement can
-// be declared, i.e. in namespace scope, in class scope and in function scope. The following
-// examples illustrate the use of the static assertion macro: the type of the rotation matrix
-// is checked at compile time and restricted to be of floating point type.
+// Static assertion offers the possibility to stop the compilation process if a specific compile
+// time condition is not met. The blaze::BLAZE_STATIC_ASSERT and blaze::BLAZE_STATIC_ASSERT_MSG
+// macros can be used to check a constant expression at compile time. If the expression evaluates
+// to \a false, a compilation error is generated that stops the compilation process. If the
+// expression (hopefully) evaluates to \a true, the compilation process is not aborted and the
+// static check leaves neither code nor data and is therefore not affecting the performance.\n
+//
+// Both static assertion macros can be used wherever a standard typedef statement can be declared,
+// i.e. in namespace scope, in class scope and in function scope. The following examples illustrate
+// the use of the static assertion macros: The element type of the rotation matrix is checked at
+// compile time and restricted to be of floating point type.
 
    \code
-   #include <limits>
+   #include <blaze/util/StaticAssert.h>
+   #include <blaze/util/typetraits/FloatingPoint.h>
 
    template< typename T >
    class RotationMatrix {
       ...
-      BLAZE_STATIC_ASSERT( !std::numeric_limits<T>::is_integer );
+      BLAZE_STATIC_ASSERT( IsFloatingPoint<T>::value );
+      // ... or ...
+      BLAZE_STATIC_ASSERT_MSG( IsFloatingPoint<T>::value, "Given type is not a floating point type" );
       ...
    };
    \endcode
 
-// The static assertion is implemented in such a way that the created error messages for a
-// failed compile time check contains either the term STATIC_ASSERT or STATIC_ASSERT_FAILED.
-// The error message doesn't explicitly explain the source of the error, but is at least
-// useful to catch the eye. The following examples show possible error messages for the
-// GNU g++ and the Intel compiler:
+// The static assertion implementation is based on the C++11 \c static_assert declaration. Thus
+// the error message contains the violated compile time condition and directly refers to the
+// violated static assertion. The following examples show a possible error message from the GNU
+// g++ compiler:
 
    \code
-   incomplete type ‘blaze::STATIC_ASSERTION_FAILED<false>’ used in nested name specifier
+   error: static assertion failed: Compile time condition violated
+      static_assert( expr, "Compile time condition violated" )
+      ^
+   note: in expansion of macro 'BLAZE_STATIC_ASSERT'
+      BLAZE_STATIC_ASSERT( IsFloatingPoint<T>::value );
+
+   error: static assertion failed: Given type is not a floating point type
+      static_assert( expr, msg )
+      ^
+   note: in expansion of macro 'BLAZE_STATIC_ASSERT_MSG'
+      BLAZE_STATIC_ASSERT_MSG( IsFloatingPoint<T>::value, "Given type is not a floating point type" );
    \endcode
-
-   \code
-   error: incomplete type is not allowed
-      BLAZE_STATIC_ASSERT( !std::numeric_limits<T>::is_integer );
-   \endcode
-
-// \note: blaze::BLAZE_STATIC_ASSERT can only can expressions of integral type. Floating point
-// expressions cannot be checked at compile time!
-//
-// \b Acknowledgements: blaze::BLAZE_STATIC_ASSERT builds on ideas developed by John Maddock
-// within the Boost C++ framework (www.boost.org). However, it uses a slightly changed compile
-// time check that completely relies on a nested enum variable and doesn't use an old-style C
-// cast.
 */
-//*************************************************************************************************
-
-
-//*************************************************************************************************
-/*! \cond BLAZE_INTERNAL */
-/*!\brief Wrapper class for the static assertion check.
-// \ingroup static_assert
-//
-// This class is used as a wrapper for the instantiation of the STATIC_ASSERTION_FAILED
-// class template. It serves the purpose to force the instantiation of either the defined
-// specialization or the undefined basic template during the compilation. In case the
-// compile time condition is met, the type blaze::STATIC_ASSERTION_TEST<1> is defined.
-*/
-template< int > struct STATIC_ASSERTION_TEST {};
-/*! \endcond */
-//*************************************************************************************************
-
-
-//*************************************************************************************************
-/*! \cond BLAZE_INTERNAL */
-/*!\brief Compile time assertion
-// \ingroup static_assert
-//
-// Helper template class for the compile time assertion. Based on the compile time constant
-// expression used for the template instantiation, either the undefined basic template or the
-// specialization is selected. If the undefined basic template is selected, a compilation
-// error is created.
-*/
-template< bool > struct STATIC_ASSERTION_FAILED;
-template<> struct STATIC_ASSERTION_FAILED<true> { enum { value = 1 }; };
-/*! \endcond */
 //*************************************************************************************************
 
 
@@ -141,8 +110,18 @@ template<> struct STATIC_ASSERTION_FAILED<true> { enum { value = 1 }; };
 // In case of an invalid compile time expression, a compilation error is created.
 */
 #define BLAZE_STATIC_ASSERT(expr) \
-   typedef ::blaze::STATIC_ASSERTION_TEST< ::blaze::STATIC_ASSERTION_FAILED< (expr) != 0 >::value > \
-      BLAZE_JOIN( BLAZE_STATIC_ASSERTION_TYPEDEF, __LINE__ )
+   static_assert( expr, "Compile time condition violated" )
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Compile time assertion macro.
+// \ingroup static_assert
+//
+// In case of an invalid compile time expression, a compilation error is created.
+*/
+#define BLAZE_STATIC_ASSERT_MSG(expr,msg) \
+   static_assert( expr, msg )
 //*************************************************************************************************
 
 } // namespace blaze
