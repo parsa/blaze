9fbc2c58aa5caa40f9aee7038e368b5bcc31f3dd
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-03-08 11:22:59 +0100

    Extend the constructor and assignment operator tests for the 'DynamicVector' class template

--- blazetest/src/mathtest/dynamicvector/ClassTest.cpp
+++ blazetest/src/mathtest/dynamicvector/ClassTest.cpp
@@ -299,6 +299,47 @@ void ClassTest::testConstructors()
 
 
    //=====================================================================================
+   // Move constructor
+   //=====================================================================================
+
+   {
+      test_ = "DynamicVector move constructor (size 0)";
+
+      blaze::DynamicVector<int,blaze::rowVector> vec1( 0UL );
+      blaze::DynamicVector<int,blaze::rowVector> vec2( std::move( vec1 ) );
+
+      checkSize    ( vec2, 0UL );
+      checkNonZeros( vec2, 0UL );
+   }
+
+   {
+      test_ = "DynamicVector move constructor (size 5)";
+
+      blaze::DynamicVector<int,blaze::rowVector> vec1( 5UL );
+      vec1[0] = 1;
+      vec1[1] = 2;
+      vec1[2] = 3;
+      vec1[3] = 4;
+      vec1[4] = 5;
+      blaze::DynamicVector<int,blaze::rowVector> vec2( std::move( vec1 ) );
+
+      checkSize    ( vec2, 5UL );
+      checkCapacity( vec2, 5UL );
+      checkNonZeros( vec2, 5UL );
+
+      if( vec2[0] != 1 || vec2[1] != 2 || vec2[2] != 3 || vec2[3] != 4 || vec2[4] != 5 ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: Construction failed\n"
+             << " Details:\n"
+             << "   Result:\n" << vec2 << "\n"
+             << "   Expected result:\n( 1 2 3 4 5 )\n";
+         throw std::runtime_error( oss.str() );
+      }
+   }
+
+
+   //=====================================================================================
    // Dense vector constructor
    //=====================================================================================
 
@@ -517,6 +558,42 @@ void ClassTest::testAssignment()
                 << "   Expected result:\n" << vec2 << "\n";
             throw std::runtime_error( oss.str() );
          }
+      }
+   }
+
+
+   //=====================================================================================
+   // Move assignment
+   //=====================================================================================
+
+   {
+      test_ = "DynamicVector move assignment";
+
+      blaze::DynamicVector<int,blaze::rowVector> vec1( 5UL );
+      vec1[0] = 1;
+      vec1[1] = 2;
+      vec1[2] = 3;
+      vec1[3] = 4;
+      vec1[4] = 5;
+      blaze::DynamicVector<int,blaze::rowVector> vec2( 3UL );
+      vec2[0] = 11;
+      vec2[1] = 12;
+      vec2[2] = 13;
+
+      vec2 = std::move( vec1 );
+
+      checkSize    ( vec2, 5UL );
+      checkCapacity( vec2, 5UL );
+      checkNonZeros( vec2, 5UL );
+
+      if( vec2[0] != 1 || vec2[1] != 2 || vec2[2] != 3 || vec2[3] != 4 || vec2[4] != 5 ) {
+         std::ostringstream oss;
+         oss << " Test: " << test_ << "\n"
+             << " Error: Assignment failed\n"
+             << " Details:\n"
+             << "   Result:\n" << vec2 << "\n"
+             << "   Expected result:\n( 1 2 3 4 5 )\n";
+         throw std::runtime_error( oss.str() );
       }
    }
 
