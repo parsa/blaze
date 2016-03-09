70dd19cf5c424fb360941acc3eb48acde0143c90
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-03-09 15:13:36 +0100

    Remove the 'AlignedStorage' class template

--- blaze/Util.h
+++ blaze/Util.h
@@ -42,7 +42,6 @@
 
 #include <blaze/util/AlignedAllocator.h>
 #include <blaze/util/AlignedArray.h>
-#include <blaze/util/AlignedStorage.h>
 #include <blaze/util/AlignmentCheck.h>
 #include <blaze/util/Algorithm.h>
 #include <blaze/util/Assert.h>
