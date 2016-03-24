1b742de00bfbc1eab964643195a779e641fb83d1
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-03-24 10:38:33 +0100

    Add constructors and assignment operators for arrays to the 'UniUpperMatrix' specialization for dense matrices

--- blaze/math/adaptors/uniuppermatrix/Dense.h
+++ blaze/math/adaptors/uniuppermatrix/Dense.h
@@ -587,6 +587,12 @@ class UniUpperMatrix<MT,SO,true>
    template< typename A1 > explicit inline UniUpperMatrix( const A1& a1 );
                            explicit inline UniUpperMatrix( size_t n, const ElementType& init );
 
+   template< typename Other >
+   explicit inline UniUpperMatrix( size_t n, const Other* array );
+
+   template< typename Other, size_t N >
+   explicit inline UniUpperMatrix( const Other (&array)[N][N] );
+
    explicit inline UniUpperMatrix( ElementType* ptr, size_t n );
    explicit inline UniUpperMatrix( ElementType* ptr, size_t n, size_t nn );
 
@@ -626,6 +632,9 @@ class UniUpperMatrix<MT,SO,true>
    //**Assignment operators************************************************************************
    /*!\name Assignment operators */
    //@{
+   template< typename Other, size_t N >
+   inline UniUpperMatrix& operator=( const Other (&array)[N][N] );
+
    inline UniUpperMatrix& operator=( const ElementType& rhs );
    inline UniUpperMatrix& operator=( const UniUpperMatrix& rhs );
    inline UniUpperMatrix& operator=( UniUpperMatrix&& rhs ) noexcept;
@@ -831,6 +840,88 @@ inline UniUpperMatrix<MT,SO,true>::UniUpperMatrix( size_t n, const ElementType& 
    }
 
    BLAZE_INTERNAL_ASSERT( isSquare( matrix_ ), "Non-square uniupper matrix detected" );
+   BLAZE_INTERNAL_ASSERT( isIntact(), "Broken invariant detected" );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Array initialization of all uniupper matrix elements.
+//
+// \param n The number of rows and columns of the matrix.
+// \param array Dynamic array for the initialization.
+// \exception std::invalid_argument Invalid setup of uniupper matrix.
+//
+// This constructor offers the option to directly initialize the elements of the uniupper matrix
+// with a dynamic array:
+
+   \code
+   using blaze::rowMajor;
+
+   int* array = new int[16];
+   // ... Initialization of the dynamic array
+   blaze::UniUpperMatrix< blaze::DynamicMatrix<int,rowMajor> > v( 4UL, array );
+   delete[] array;
+   \endcode
+
+// The matrix is sized accoring to the given size of the array and initialized with the values
+// from the given array. Note that it is expected that the given \a array has at least \a m by
+// \a n elements. Providing an array with less elements results in undefined behavior! Also, in
+// case the given array does not represent a upper unitriangular matrix, a \a std::invalid_argument
+// exception is thrown.
+*/
+template< typename MT       // Type of the adapted dense matrix
+        , bool SO >         // Storage order of the adapted dense matrix
+template< typename Other >  // Data type of the initialization array
+inline UniUpperMatrix<MT,SO,true>::UniUpperMatrix( size_t n, const Other* array )
+   : matrix_( n, n, array )  // The adapted dense matrix
+{
+   if( !isUniUpper( matrix_ ) ) {
+      BLAZE_THROW_INVALID_ARGUMENT( "Invalid setup of uniupper matrix" );
+   }
+
+   BLAZE_INTERNAL_ASSERT( isIntact(), "Broken invariant detected" );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Array initialization of all uniupper matrix elements.
+//
+// \param array \f$ N \times N \f$ dimensional array for the initialization.
+// \exception std::invalid_argument Invalid setup of uniupper matrix.
+//
+// This constructor offers the option to directly initialize the elements of the uniupper matrix
+// with a static array:
+
+   \code
+   using blaze::rowMajor;
+
+   const int init[3][3] = { { 1, 2, 3 },
+                            { 0, 1 },
+                            { 0, 0, 1 } };
+   blaze::UniUpperMatrix< blaze::StaticMatrix<int,3,3,rowMajor> > A( init );
+   \endcode
+
+// The matrix is initialized with the values from the given array. Missing values are initialized
+// with default values. In case the given array does not represent a upper unitriangular matrix,
+// a \a std::invalid_argument exception is thrown.
+*/
+template< typename MT     // Type of the adapted dense matrix
+        , bool SO >       // Storage order of the adapted dense matrix
+template< typename Other  // Data type of the initialization array
+        , size_t N >      // Number of rows and columns of the initialization array
+inline UniUpperMatrix<MT,SO,true>::UniUpperMatrix( const Other (&array)[N][N] )
+   : matrix_( array )  // The adapted dense matrix
+{
+   if( !isUniUpper( matrix_ ) ) {
+      BLAZE_THROW_INVALID_ARGUMENT( "Invalid setup of uniupper matrix" );
+   }
+
    BLAZE_INTERNAL_ASSERT( isIntact(), "Broken invariant detected" );
 }
 /*! \endcond */
@@ -1353,6 +1444,54 @@ inline typename UniUpperMatrix<MT,SO,true>::ConstIterator
 //  ASSIGNMENT OPERATORS
 //
 //=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Array assignment to all uniupper matrix elements.
+//
+// \param array \f$ N \times N \f$ dimensional array for the assignment.
+// \return Reference to the assigned matrix.
+// \exception std::invalid_argument Invalid assignment to uniupper matrix.
+//
+// This assignment operator offers the option to directly set all elements of the uniupper matrix:
+
+   \code
+   using blaze::rowMajor;
+
+   const int init[3][3] = { { 1, 2, 3 },
+                            { 0, 1 },
+                            { 0, 0, 1 } };
+   blaze::UniUpperMatrix< blaze::StaticMatrix<int,3UL,3UL,rowMajor> > A;
+   A = init;
+   \endcode
+
+// The matrix is assigned the values from the given array. Missing values are initialized with
+// default values. In case the given array does not represent a uniupper triangular matrix, a
+// \a std::invalid_argument exception is thrown.
+*/
+template< typename MT     // Type of the adapted dense matrix
+        , bool SO >       // Storage order of the adapted dense matrix
+template< typename Other  // Data type of the initialization array
+        , size_t N >      // Number of rows and columns of the initialization array
+inline UniUpperMatrix<MT,SO,true>&
+   UniUpperMatrix<MT,SO,true>::operator=( const Other (&array)[N][N] )
+{
+   MT tmp( array );
+
+   if( !isUniUpper( tmp ) ) {
+      BLAZE_THROW_INVALID_ARGUMENT( "Invalid assignment to uniupper matrix" );
+   }
+
+   matrix_ = std::move( tmp );
+
+   BLAZE_INTERNAL_ASSERT( isSquare( matrix_ ), "Non-square uniupper matrix detected" );
+   BLAZE_INTERNAL_ASSERT( isIntact(), "Broken invariant detected" );
+
+   return *this;
+}
+/*! \endcond */
+//*************************************************************************************************
+
 
 //*************************************************************************************************
 /*! \cond BLAZE_INTERNAL */
