e360ffd387f951d3149463ea42d7d100b6cb6174
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-04-15 17:08:07 +0200

    Add the 'isDivisor' shim

--- blaze/math/shims/IsDivisor.h
+++ blaze/math/shims/IsDivisor.h
@@ -1,7 +1,7 @@
 //=================================================================================================
 /*!
-//  \file blaze/math/Shims.h
-//  \brief Header file for the mathematical shims
+//  \file blaze/math/shims/IsDivisor.h
+//  \brief Header file for the isDivisor shim
 //
 //  Copyright (C) 2013 Klaus Iglberger - All Rights Reserved
 //
@@ -32,28 +32,58 @@
 */
 //=================================================================================================
 
-#ifndef _BLAZE_MATH_SHIMS_H_
-#define _BLAZE_MATH_SHIMS_H_
+#ifndef _BLAZE_MATH_SHIMS_ISDIVISOR_H_
+#define _BLAZE_MATH_SHIMS_ISDIVISOR_H_
 
 
 //*************************************************************************************************
 // Includes
 //*************************************************************************************************
 
-#include <blaze/math/shims/Clear.h>
-#include <blaze/math/shims/Conjugate.h>
 #include <blaze/math/shims/Equal.h>
-#include <blaze/math/shims/Imaginary.h>
-#include <blaze/math/shims/Invert.h>
-#include <blaze/math/shims/IsDefault.h>
-#include <blaze/math/shims/IsDivisor.h>
-#include <blaze/math/shims/IsNaN.h>
-#include <blaze/math/shims/IsOne.h>
-#include <blaze/math/shims/IsReal.h>
-#include <blaze/math/shims/IsZero.h>
-#include <blaze/math/shims/Real.h>
-#include <blaze/math/shims/Reset.h>
-#include <blaze/math/shims/Serial.h>
-#include <blaze/math/shims/Square.h>
+#include <blaze/system/Inline.h>
+#include <blaze/util/DisableIf.h>
+#include <blaze/util/EnableIf.h>
+#include <blaze/util/typetraits/IsNumeric.h>
+#include <blaze/util/Unused.h>
+
+
+namespace blaze {
+
+//=================================================================================================
+//
+//  ISDIVISOR SHIM
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\brief Returns whether the given value/object is a valid divisor.
+// \ingroup math_shims
+//
+// \param v The value to be tested.
+// \return \a true in case the given value is a valid divisor, \a false otherwise.
+//
+// The \a isDivisor shim provides an abstract interface for testing a value/object of any type
+// whether it represents a valid divisor. In case the value/object can be used as divisor, the
+// function returns \a true, otherwise it returns \a false.
+
+   \code
+   const int i1 = 1;                 // isDivisor( i1 ) returns true
+   double    d1 = 0.1;               // isDivisor( d1 ) returns true
+   complex<double> c1( 0.2, -0.1 );  // isDivisor( c1 ) returns true
+
+   const int i2 = 0;                // isDivisor( i2 ) returns false
+   double    d2 = 0.0;              // isDivisor( d2 ) returns false
+   complex<double> c2( 0.0, 0.0 );  // isDivisor( c2 ) returns false
+   \endcode
+*/
+template< typename Type, typename = EnableIf_< IsNumeric<Type> > >
+BLAZE_ALWAYS_INLINE bool isDivisor( const Type& v )
+{
+   return v != Type(0);
+}
+//*************************************************************************************************
+
+} // namespace blaze
 
 #endif
