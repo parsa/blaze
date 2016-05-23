14c3e68e174b12964a28cc311f9a4126360da12d
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-05-23 11:58:08 +0200

    Add the SIMD 'cos()' functions

--- blaze/math/simd/Functions.h
+++ blaze/math/simd/Functions.h
@@ -47,6 +47,7 @@
 #include <blaze/math/simd/Cbrt.h>
 #include <blaze/math/simd/Ceil.h>
 #include <blaze/math/simd/Conj.h>
+#include <blaze/math/simd/Cos.h>
 #include <blaze/math/simd/Division.h>
 #include <blaze/math/simd/Exp.h>
 #include <blaze/math/simd/Floor.h>
