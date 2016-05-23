806d9ad94da626c80520d72be106aa7d5b58b125
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-05-23 12:03:05 +0200

    Add the SIMD 'inverfc()' functions

--- blaze/math/simd/Functions.h
+++ blaze/math/simd/Functions.h
@@ -60,6 +60,7 @@
 #include <blaze/math/simd/Floor.h>
 #include <blaze/math/simd/InvCbrt.h>
 #include <blaze/math/simd/InvErf.h>
+#include <blaze/math/simd/InvErfc.h>
 #include <blaze/math/simd/InvSqrt.h>
 #include <blaze/math/simd/Loada.h>
 #include <blaze/math/simd/Loadu.h>
