52a55322dde4afd388b451fdd42395a7f0290fd0
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-05-30 14:17:07 +0200

    Add the 'TDVecForEachExprTrait' class template

--- blaze/math/Traits.h
+++ blaze/math/Traits.h
@@ -190,6 +190,7 @@
 #include <blaze/math/traits/TDVecDMatMultExprTrait.h>
 #include <blaze/math/traits/TDVecDVecMultExprTrait.h>
 #include <blaze/math/traits/TDVecEvalExprTrait.h>
+#include <blaze/math/traits/TDVecForEachExprTrait.h>
 #include <blaze/math/traits/TDVecImagExprTrait.h>
 #include <blaze/math/traits/TDVecRealExprTrait.h>
 #include <blaze/math/traits/TDVecScalarDivExprTrait.h>
