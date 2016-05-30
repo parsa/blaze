4effa24d054e1ca601008e374613df15aaed8d92
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-05-30 14:50:29 +0200

    Add an overload of the 'subvector()' function for vector custom operations

--- blaze/math/views/Subvector.h
+++ blaze/math/views/Subvector.h
@@ -51,6 +51,7 @@
 #include <blaze/math/typetraits/IsVecAbsExpr.h>
 #include <blaze/math/typetraits/IsVecConjExpr.h>
 #include <blaze/math/typetraits/IsVecEvalExpr.h>
+#include <blaze/math/typetraits/IsVecForEachExpr.h>
 #include <blaze/math/typetraits/IsVecImagExpr.h>
 #include <blaze/math/typetraits/IsVecRealExpr.h>
 #include <blaze/math/typetraits/IsVecScalarDivExpr.h>
@@ -685,6 +686,33 @@ inline EnableIf_< IsVecImagExpr<VT>, SubvectorExprTrait_<VT,AF> >
    BLAZE_FUNCTION_TRACE;
 
    return imag( subvector<AF>( (~vector).operand(), index, size ) );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Creating a view on a specific subvector of the given vector custom operation.
+// \ingroup views
+//
+// \param vector The constant vector custom operation.
+// \param index The index of the first element of the subvector.
+// \param size The size of the subvector.
+// \return View on the specified subvector of the custom operation.
+//
+// This function returns an expression representing the specified subvector of the given vector
+// custom operation.
+*/
+template< bool AF      // Alignment flag
+        , typename VT  // Type of the vector
+        , bool TF >    // Transpose flag
+inline EnableIf_< IsVecForEachExpr<VT>, SubvectorExprTrait_<VT,AF> >
+   subvector( const Vector<VT,TF>& vector, size_t index, size_t size )
+{
+   BLAZE_FUNCTION_TRACE;
+
+   return forEach( subvector<AF>( (~vector).operand(), index, size ), (~vector).operation() );
 }
 /*! \endcond */
 //*************************************************************************************************
