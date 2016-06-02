a03cbccf89ad51c7a773fb68942a82ea0648b8e5
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-06-02 09:42:49 +0200

    Add the 'asinh()' shim

--- blaze/math/shims/Asinh.h
+++ blaze/math/shims/Asinh.h
@@ -1,7 +1,7 @@
 //=================================================================================================
 /*!
-//  \file blaze/math/Shims.h
-//  \brief Header file for the mathematical shims
+//  \file blaze/math/shims/Asinh.h
+//  \brief Header file for the asinh shim
 //
 //  Copyright (C) 2013 Klaus Iglberger - All Rights Reserved
 //
@@ -32,43 +32,32 @@
 */
 //=================================================================================================
 
-#ifndef _BLAZE_MATH_SHIMS_H_
-#define _BLAZE_MATH_SHIMS_H_
+#ifndef _BLAZE_MATH_SHIMS_ASINH_H_
+#define _BLAZE_MATH_SHIMS_ASINH_H_
 
 
 //*************************************************************************************************
 // Includes
 //*************************************************************************************************
 
-#include <blaze/math/shims/Abs.h>
-#include <blaze/math/shims/Asin.h>
-#include <blaze/math/shims/Asinh.h>
-#include <blaze/math/shims/Cbrt.h>
-#include <blaze/math/shims/Ceil.h>
-#include <blaze/math/shims/Clear.h>
-#include <blaze/math/shims/Conjugate.h>
-#include <blaze/math/shims/Equal.h>
-#include <blaze/math/shims/Exp.h>
-#include <blaze/math/shims/Floor.h>
-#include <blaze/math/shims/Imaginary.h>
-#include <blaze/math/shims/InvCbrt.h>
-#include <blaze/math/shims/Invert.h>
-#include <blaze/math/shims/InvSqrt.h>
-#include <blaze/math/shims/IsDefault.h>
-#include <blaze/math/shims/IsDivisor.h>
-#include <blaze/math/shims/IsNaN.h>
-#include <blaze/math/shims/IsOne.h>
-#include <blaze/math/shims/IsReal.h>
-#include <blaze/math/shims/IsZero.h>
-#include <blaze/math/shims/Log.h>
-#include <blaze/math/shims/Log10.h>
-#include <blaze/math/shims/Pow.h>
-#include <blaze/math/shims/Real.h>
-#include <blaze/math/shims/Reset.h>
-#include <blaze/math/shims/Serial.h>
-#include <blaze/math/shims/Sin.h>
-#include <blaze/math/shims/Sinh.h>
-#include <blaze/math/shims/Sqrt.h>
-#include <blaze/math/shims/Square.h>
+#include <cmath>
+
+
+namespace blaze {
+
+//=================================================================================================
+//
+//  ASINH SHIM
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\brief Import of the std::asinh() function into the Blaze namespace.
+// \ingroup math_shims
+*/
+using std::asinh;
+//*************************************************************************************************
+
+} // namespace blaze
 
 #endif
