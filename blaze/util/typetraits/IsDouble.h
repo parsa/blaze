a01cf425c9d55dca9568213594ffd348cdb26e2d
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-09-24 08:39:22 -0700

    Add the 'IsDouble_' alias declaration

--- blaze/util/typetraits/IsDouble.h
+++ blaze/util/typetraits/IsDouble.h
@@ -116,6 +116,24 @@ struct IsDouble<const volatile double> : public TrueType
 /*! \endcond */
 //*************************************************************************************************
 
+
+//*************************************************************************************************
+/*!\brief Auxiliary alias declaration for the IsDouble type trait.
+// \ingroup type_traits
+//
+// The IsDouble_ alias declaration provides a convenient shortcut to access the nested \a value
+// of the IsDouble class template. For instance, given the type \a T the following two statements
+// are identical:
+
+   \code
+   constexpr bool value1 = IsDouble<T>::value;
+   constexpr bool value2 = IsDouble_<T>;
+   \endcode
+*/
+template< typename T >
+constexpr bool IsDouble_ = IsDouble<T>::value;
+//*************************************************************************************************
+
 } // namespace blaze
 
 #endif
