1f32f82bb721bc9d61b4c398381aa4025facc303
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-10-31 10:17:57 +0100

    Add the 'HighType' type trait

--- blaze/math/TypeTraits.h
+++ blaze/math/TypeTraits.h
@@ -78,6 +78,7 @@
 #include <blaze/math/typetraits/HasSIMDTan.h>
 #include <blaze/math/typetraits/HasSIMDTanh.h>
 #include <blaze/math/typetraits/HasSIMDTrunc.h>
+#include <blaze/math/typetraits/HighType.h>
 #include <blaze/math/typetraits/IsAdaptor.h>
 #include <blaze/math/typetraits/IsAddExpr.h>
 #include <blaze/math/typetraits/IsAligned.h>
