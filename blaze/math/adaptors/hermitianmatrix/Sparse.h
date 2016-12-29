b04dc13d9439131fd2595fafa80f2b6942f0bef3
Klaus Iglberger <klaus.iglberger@gmail.com>
2016-12-29 13:59:15 +0100

    Add predicate-based 'erase()' functions to the 'HermitianMatrix' specialization for sparse matrices

--- blaze/math/adaptors/hermitianmatrix/Sparse.h
+++ blaze/math/adaptors/hermitianmatrix/Sparse.h
@@ -393,9 +393,6 @@ class HermitianMatrix<MT,SO,false>
                               inline void             clear();
                               inline Iterator         set( size_t i, size_t j, const ElementType& value );
                               inline Iterator         insert( size_t i, size_t j, const ElementType& value );
-                              inline void             erase( size_t i, size_t j );
-                              inline Iterator         erase( size_t i, Iterator pos );
-                              inline Iterator         erase( size_t i, Iterator first, Iterator last );
                               inline void             resize ( size_t n, bool preserve=true );
                               inline void             reserve( size_t nonzeros );
                               inline void             reserve( size_t i, size_t nonzeros );
@@ -406,6 +403,21 @@ class HermitianMatrix<MT,SO,false>
    template< typename Other > inline HermitianMatrix& scale( const Other& scalar );
    template< typename Other > inline HermitianMatrix& scaleDiagonal( Other scale );
                               inline void             swap( HermitianMatrix& m ) noexcept;
+   //@}
+   //**********************************************************************************************
+
+   //**Erase functions*****************************************************************************
+   /*!\name Erase functions */
+   //@{
+   inline void     erase( size_t i, size_t j );
+   inline Iterator erase( size_t i, Iterator pos );
+   inline Iterator erase( size_t i, Iterator first, Iterator last );
+
+   template< typename Pred >
+   inline void erase( Pred predicate );
+
+   template< typename Pred >
+   inline void erase( size_t i, Iterator first, Iterator last, Pred predicate );
    //@}
    //**********************************************************************************************
 
@@ -1663,114 +1675,6 @@ inline typename HermitianMatrix<MT,SO,false>::Iterator
 
 //*************************************************************************************************
 /*! \cond BLAZE_INTERNAL */
-/*!\brief Erasing elements from the Hermitian matrix.
-//
-// \param i The row index of the element to be erased. The index has to be in the range \f$[0..N-1]\f$.
-// \param j The column index of the element to be erased. The index has to be in the range \f$[0..N-1]\f$.
-// \return void
-//
-// This function erases both elements \f$ a_{ij} \f$ and \f$ a_{ji} \f$ from the Hermitian matrix.
-*/
-template< typename MT  // Type of the adapted sparse matrix
-        , bool SO >    // Storage order of the adapted sparse matrix
-inline void HermitianMatrix<MT,SO,false>::erase( size_t i, size_t j )
-{
-   matrix_.erase( i, j );
-   if( i != j )
-      matrix_.erase( j, i );
-}
-/*! \endcond */
-//*************************************************************************************************
-
-
-//*************************************************************************************************
-/*! \cond BLAZE_INTERNAL */
-/*!\brief Erasing elements from the Hermitian matrix.
-//
-// \param i The row/column index of the element to be erased. The index has to be in the range \f$[0..N-1]\f$.
-// \param pos Iterator to the element to be erased.
-// \return Iterator to the element after the erased element.
-//
-// This function erases both the specified element and its according Hermitian counterpart from
-// the Hermitian matrix. In case the storage order is set to \a rowMajor the given index \a i
-// refers to a row, in case the storage flag is set to \a columnMajor \a i refers to a column.
-*/
-template< typename MT  // Type of the adapted sparse matrix
-        , bool SO >    // Storage order of the adapted sparse matrix
-inline typename HermitianMatrix<MT,SO,false>::Iterator
-   HermitianMatrix<MT,SO,false>::erase( size_t i, Iterator pos )
-{
-   const Iterator_<MT> base( pos.base() );
-
-   if( base == matrix_.end( i ) )
-      return pos;
-
-   const size_t j( base->index() );
-
-   if( i == j ) {
-      BLAZE_INTERNAL_ASSERT( matrix_.find( i, i ) != matrix_.end( i ), "Missing element detected" );
-      return Iterator( matrix_.erase( i, base ), matrix_, i );
-   }
-
-   if( SO ) {
-      BLAZE_INTERNAL_ASSERT( matrix_.find( i, j ) != matrix_.end( j ), "Missing element detected" );
-      matrix_.erase( j, matrix_.find( i, j ) );
-      return Iterator( matrix_.erase( i, base ), matrix_, i );
-   }
-   else {
-      BLAZE_INTERNAL_ASSERT( matrix_.find( j, i ) != matrix_.end( j ), "Missing element detected" );
-      matrix_.erase( j, matrix_.find( j, i ) );
-      return Iterator( matrix_.erase( i, base ), matrix_, i );
-   }
-}
-/*! \endcond */
-//*************************************************************************************************
-
-
-//*************************************************************************************************
-/*! \cond BLAZE_INTERNAL */
-/*!\brief Erasing a range of elements from the Hermitian matrix.
-//
-// \param i The row/column index of the element to be erased. The index has to be in the range \f$[0..N-1]\f$.
-// \param first Iterator to first element to be erased.
-// \param last Iterator just past the last element to be erased.
-// \return Iterator to the element after the erased element.
-//
-// This function erases both the range of elements specified by the iterator pair \a first and
-// \a last and their according Hermitian counterparts from the Hermitian matrix. In case the
-// storage order is set to \a rowMajor the given index \a i refers to a row, in case the storage
-// flag is set to \a columnMajor \a i refers to a column.
-*/
-template< typename MT  // Type of the adapted sparse matrix
-        , bool SO >    // Storage order of the adapted sparse matrix
-inline typename HermitianMatrix<MT,SO,false>::Iterator
-   HermitianMatrix<MT,SO,false>::erase( size_t i, Iterator first, Iterator last )
-{
-   for( Iterator_<MT> it=first.base(); it!=last.base(); ++it )
-   {
-      const size_t j( it->index() );
-
-      if( i == j )
-         continue;
-
-      if( SO ) {
-         BLAZE_INTERNAL_ASSERT( matrix_.find( i, j ) != matrix_.end( j ), "Missing element detected" );
-         matrix_.erase( i, j );
-      }
-      else {
-         BLAZE_INTERNAL_ASSERT( matrix_.find( j, i ) != matrix_.end( j ), "Missing element detected" );
-         matrix_.erase( j, i );
-      }
-   }
-
-   return Iterator( matrix_.erase( i, first.base(), last.base() ), matrix_, i );
-}
-/*! \endcond */
-//*************************************************************************************************
-
-
-//*************************************************************************************************
-/*! \cond BLAZE_INTERNAL */
 /*!\brief Changing the size of the Hermitian matrix.
 //
 // \param n The new number of rows and columns of the matrix.
@@ -1976,6 +1880,211 @@ inline void HermitianMatrix<MT,SO,false>::swap( HermitianMatrix& m ) noexcept
    using std::swap;
 
    swap( matrix_, m.matrix_ );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  ERASE FUNCTIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Erasing elements from the Hermitian matrix.
+//
+// \param i The row index of the element to be erased. The index has to be in the range \f$[0..N-1]\f$.
+// \param j The column index of the element to be erased. The index has to be in the range \f$[0..N-1]\f$.
+// \return void
+//
+// This function erases both elements \f$ a_{ij} \f$ and \f$ a_{ji} \f$ from the Hermitian matrix.
+*/
+template< typename MT  // Type of the adapted sparse matrix
+        , bool SO >    // Storage order of the adapted sparse matrix
+inline void HermitianMatrix<MT,SO,false>::erase( size_t i, size_t j )
+{
+   matrix_.erase( i, j );
+   if( i != j )
+      matrix_.erase( j, i );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Erasing elements from the Hermitian matrix.
+//
+// \param i The row/column index of the element to be erased. The index has to be in the range \f$[0..N-1]\f$.
+// \param pos Iterator to the element to be erased.
+// \return Iterator to the element after the erased element.
+//
+// This function erases both the specified element and its according Hermitian counterpart from
+// the Hermitian matrix. In case the storage order is set to \a rowMajor the given index \a i
+// refers to a row, in case the storage flag is set to \a columnMajor \a i refers to a column.
+*/
+template< typename MT  // Type of the adapted sparse matrix
+        , bool SO >    // Storage order of the adapted sparse matrix
+inline typename HermitianMatrix<MT,SO,false>::Iterator
+   HermitianMatrix<MT,SO,false>::erase( size_t i, Iterator pos )
+{
+   const Iterator_<MT> base( pos.base() );
+
+   if( base == matrix_.end( i ) )
+      return pos;
+
+   const size_t j( base->index() );
+
+   if( i == j ) {
+      BLAZE_INTERNAL_ASSERT( matrix_.find( i, i ) != matrix_.end( i ), "Missing element detected" );
+      return Iterator( matrix_.erase( i, base ), matrix_, i );
+   }
+
+   if( SO ) {
+      BLAZE_INTERNAL_ASSERT( matrix_.find( i, j ) != matrix_.end( j ), "Missing element detected" );
+      matrix_.erase( j, matrix_.find( i, j ) );
+      return Iterator( matrix_.erase( i, base ), matrix_, i );
+   }
+   else {
+      BLAZE_INTERNAL_ASSERT( matrix_.find( j, i ) != matrix_.end( j ), "Missing element detected" );
+      matrix_.erase( j, matrix_.find( j, i ) );
+      return Iterator( matrix_.erase( i, base ), matrix_, i );
+   }
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Erasing a range of elements from the Hermitian matrix.
+//
+// \param i The row/column index of the element to be erased. The index has to be in the range \f$[0..N-1]\f$.
+// \param first Iterator to first element to be erased.
+// \param last Iterator just past the last element to be erased.
+// \return Iterator to the element after the erased element.
+//
+// This function erases both the range of elements specified by the iterator pair \a first and
+// \a last and their according Hermitian counterparts from the Hermitian matrix. In case the
+// storage order is set to \a rowMajor the given index \a i refers to a row, in case the storage
+// flag is set to \a columnMajor \a i refers to a column.
+*/
+template< typename MT  // Type of the adapted sparse matrix
+        , bool SO >    // Storage order of the adapted sparse matrix
+inline typename HermitianMatrix<MT,SO,false>::Iterator
+   HermitianMatrix<MT,SO,false>::erase( size_t i, Iterator first, Iterator last )
+{
+   for( Iterator_<MT> it=first.base(); it!=last.base(); ++it )
+   {
+      const size_t j( it->index() );
+
+      if( i == j )
+         continue;
+
+      if( SO ) {
+         BLAZE_INTERNAL_ASSERT( matrix_.find( i, j ) != matrix_.end( j ), "Missing element detected" );
+         matrix_.erase( i, j );
+      }
+      else {
+         BLAZE_INTERNAL_ASSERT( matrix_.find( j, i ) != matrix_.end( j ), "Missing element detected" );
+         matrix_.erase( j, i );
+      }
+   }
+
+   return Iterator( matrix_.erase( i, first.base(), last.base() ), matrix_, i );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Erasing specific elements from the Hermitian matrix.
+//
+// \param predicate The unary predicate for the element selection.
+// \return void.
+//
+// This function erases specific elements from the Hermitian matrix. The elements are selected
+// by the given unary predicate \a predicate, which is expected to accept a single argument of
+// the type of the elements and to be pure. The following example demonstrates how to remove
+// all elements that are smaller than a certain threshold value:
+
+   \code
+   blaze::HermitianMatrix< CompressedMatrix<double,blaze::rowMajor> > A;
+   // ... Resizing and initialization
+
+   A.erase( []( double value ){ return value < 1E-8; } );
+   \endcode
+
+// \note The predicate is required to be pure, i.e. to produce deterministic results for elements
+// with the same value. The attempt to use an impure predicate leads to undefined behavior!
+*/
+template< typename MT  // Type of the adapted sparse matrix
+        , bool SO >    // Storage order of the adapted sparse matrix
+template< typename Pred >  // Type of the unary predicate
+inline void HermitianMatrix<MT,SO,false>::erase( Pred predicate )
+{
+   matrix_.erase( [predicate=predicate]( const ElementType& value ) {
+                     return predicate( value ) || predicate( conj( value ) );
+                  } );
+
+   BLAZE_INTERNAL_ASSERT( isIntact(), "Broken invariant detected" );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Erasing specific elements from a range of the Hermitian matrix.
+//
+// \param i The row/column index of the elements to be erased. The index has to be in the range \f$[0..M-1]\f$.
+// \param first Iterator to first element of the range.
+// \param last Iterator just past the last element of the range.
+// \param predicate The unary predicate for the element selection.
+// \return void
+//
+// This function erases specific elements from a range of elements of the Hermitian matrix. The
+// elements are selected by the given unary predicate \a predicate, which is expected to accept
+// a single argument of the type of the elements and to be pure. In case the storage order is
+// set to \a rowMajor the function erases a range of elements from row \a i, in case the storage
+// flag is set to \a columnMajor the function erases a range of elements from column \a i. The
+// following example demonstrates how to remove all elements that are smaller than a certain
+// threshold value:
+
+   \code
+   blaze::HermitianMatrix< CompressedMatrix<double,blaze::rowMajor> > A;
+   // ... Resizing and initialization
+
+   A.erase( 2UL, A.begin(2UL), A.end(2UL), []( double value ){ return value < 1E-8; } );
+   \endcode
+
+// \note The predicate is required to be pure, i.e. to produce deterministic results for elements
+// with the same value. The attempt to use an impure predicate leads to undefined behavior!
+*/
+template< typename MT      // Type of the adapted sparse matrix
+        , bool SO >        // Storage order of the adapted sparse matrix
+template< typename Pred >  // Type of the unary predicate
+inline void
+   HermitianMatrix<MT,SO,false>::erase( size_t i, Iterator first, Iterator last, Pred predicate )
+{
+   for( Iterator it=first; it!=last; ++it ) {
+      const size_t j( it->index() );
+      if( i != j && predicate( it->value() ) ) {
+         if( SO )
+            matrix_.erase( i, j );
+         else
+            matrix_.erase( j, i );
+      }
+   }
+
+   matrix_.erase( i, first.base(), last.base(), predicate );
+
+   BLAZE_INTERNAL_ASSERT( isIntact(), "Broken invariant detected" );
 }
 /*! \endcond */
 //*************************************************************************************************
