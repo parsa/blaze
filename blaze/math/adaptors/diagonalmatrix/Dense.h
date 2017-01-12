99fb3c3a79964f128b44b0d180681983c84a9632
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-01-12 10:40:39 +0100

    Add the 'Resize' mechanism to the 'DiagonalMatrix' class template

--- blaze/math/adaptors/diagonalmatrix/Dense.h
+++ blaze/math/adaptors/diagonalmatrix/Dense.h
@@ -133,10 +133,21 @@ class DiagonalMatrix<MT,SO,true>
    //**Rebind struct definition********************************************************************
    /*!\brief Rebind mechanism to obtain a DiagonalMatrix with different data/element type.
    */
-   template< typename ET >  // Data type of the other matrix
+   template< typename NewType >  // Data type of the other matrix
    struct Rebind {
       //! The type of the other DiagonalMatrix.
-      typedef DiagonalMatrix< typename MT::template Rebind<ET>::Other >  Other;
+      typedef DiagonalMatrix< typename MT::template Rebind<NewType>::Other >  Other;
+   };
+   //**********************************************************************************************
+
+   //**Resize struct definition********************************************************************
+   /*!\brief Resize mechanism to obtain a DiagonalMatrix with different fixed dimensions.
+   */
+   template< size_t NewM    // Number of rows of the other matrix
+           , size_t NewN >  // Number of columns of the other matrix
+   struct Resize {
+      //! The type of the other DiagonalMatrix.
+      typedef DiagonalMatrix< typename MT::template Resize<NewM,NewN>::Other >  Other;
    };
    //**********************************************************************************************
 
