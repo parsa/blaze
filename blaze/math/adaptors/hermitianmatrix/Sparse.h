9f5632639471b991b68295eb5db5098ce0d3a2e0
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-01-12 10:38:27 +0100

    Add the 'Resize' mechanism to the 'HermitianMatrix' class template

--- blaze/math/adaptors/hermitianmatrix/Sparse.h
+++ blaze/math/adaptors/hermitianmatrix/Sparse.h
@@ -132,10 +132,21 @@ class HermitianMatrix<MT,SO,false>
    //**Rebind struct definition********************************************************************
    /*!\brief Rebind mechanism to obtain a HermitianMatrix with different data/element type.
    */
-   template< typename ET >  // Data type of the other matrix
+   template< typename NewType >  // Data type of the other matrix
    struct Rebind {
       //! The type of the other HermitianMatrix.
-      typedef HermitianMatrix< typename MT::template Rebind<ET>::Other >  Other;
+      typedef HermitianMatrix< typename MT::template Rebind<NewType>::Other >  Other;
+   };
+   //**********************************************************************************************
+
+   //**Resize struct definition********************************************************************
+   /*!\brief Resize mechanism to obtain a HermitianMatrix with different fixed dimensions.
+   */
+   template< size_t NewM    // Number of rows of the other matrix
+           , size_t NewN >  // Number of columns of the other matrix
+   struct Resize {
+      //! The type of the other HermitianMatrix.
+      typedef HermitianMatrix< typename MT::template Resize<NewM,NewN>::Other >  Other;
    };
    //**********************************************************************************************
 
