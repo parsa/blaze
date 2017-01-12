118ace5cef046d3c63bbe17bf8bc760b23bb8777
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-01-12 10:39:15 +0100

    Add the 'Resize' mechanism to the 'StrictlyLowerMatrix' class template

--- blaze/math/adaptors/strictlylowermatrix/Dense.h
+++ blaze/math/adaptors/strictlylowermatrix/Dense.h
@@ -134,10 +134,21 @@ class StrictlyLowerMatrix<MT,SO,true>
    //**Rebind struct definition********************************************************************
    /*!\brief Rebind mechanism to obtain a StrictlyLowerMatrix with different data/element type.
    */
-   template< typename ET >  // Data type of the other matrix
+   template< typename NewType >  // Data type of the other matrix
    struct Rebind {
       //! The type of the other StrictlyLowerMatrix.
-      typedef StrictlyLowerMatrix< typename MT::template Rebind<ET>::Other >  Other;
+      typedef StrictlyLowerMatrix< typename MT::template Rebind<NewType>::Other >  Other;
+   };
+   //**********************************************************************************************
+
+   //**Resize struct definition********************************************************************
+   /*!\brief Resize mechanism to obtain a StrictlyLowerMatrix with different fixed dimensions.
+   */
+   template< size_t NewM    // Number of rows of the other matrix
+           , size_t NewN >  // Number of columns of the other matrix
+   struct Resize {
+      //! The type of the other StrictlyLowerMatrix.
+      typedef StrictlyLowerMatrix< typename MT::template Resize<NewM,NewN>::Other >  Other;
    };
    //**********************************************************************************************
 
