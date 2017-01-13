222fd4db23bbb292a6c09cf255b30741ba74e5a2
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-01-13 14:39:29 +0100

    Improve the documentation of the 'isZero()' functions

--- blaze/math/shims/IsZero.h
+++ blaze/math/shims/IsZero.h
@@ -66,21 +66,37 @@ namespace blaze {
 //
 // The \a isZero shim provides an abstract interface for testing a value/object of any type
 // whether it represents the numeric value 0. In case the value/object is 0, the function
-// returns \a true, otherwise it returns \a false. For floating point built-in data types,
-// the function returns either \a true in case the current value is zero (strict semantics)
-// or \a true in case the current value is close to zero within a certain accuracy (relaxed
-// semantics).
+// returns \a true, otherwise it returns \a false:
+
+   \code
+   int i1 = 0;  // isZero( i1 ) returns true
+   int i2 = 1;  // isZero( i2 ) returns false
+   \endcode
+
+// For floating point built-in data types, the function by default uses relaxed semantics and
+// returns \a true in case the current value is close to zero within a certain accuracy:
+
+   \code
+   double d1 = 0.0;                  // isZero( d1 ) returns true
+   double d2 = 1E-9;                 // isZero( d2 ) returns true since d2 is below 1E-8
+   double d3 = 1.0;                  // isZero( d3 ) returns false
+   complex<double> c1( 0.0, 0.0  );  // isZero( c1 ) returns true
+   complex<double> c2( 0.0, 1E-9 );  // isZero( c2 ) returns true since imag(c2) is below 1E-8
+   complex<double> c3( 1.0, 0.0  );  // isZero( c3 ) returns false
+   \endcode
+
+// Optionally, it is possible to switch between relaxed semantics (blaze::relaxed) and strict
+// semantics (blaze::strict). In case of strict semantics, for floating point built-in data types
+// the function returns \a true in case the current value is exactly zero:
 
    \code
                                      // isZero<strict>( ... ) | isZero<relaxed>( ... )
-   int i1 = 0;                       //     true              |     true
-   int i2 = 1;                       //     false             |     false
-   double d1 = 0.0;                  //     true              |     true
-   double d2 = 1E-9;                 //     false             |     true (below 1E-8)
-   double d3 =  1.0;                 //     false             |     false
-   complex<double> c1( 0.0, 0.0 );   //     true              |     true
-   complex<double> c2( 0.0, 1E-9 );  //     false             |     true (below 1E-8)
-   complex<double> c3( 1.0, 0.0 );   //     false             |     false
+   double d1 = 0.0;                  //    true               |    true
+   double d2 = 1E-9;                 //    false (not 0.0)    |    true (below 1E-8)
+   double d3 = 1.0;                  //    false              |    false
+   complex<double> c1( 0.0, 0.0  );  //    true               |    true
+   complex<double> c2( 0.0, 1E-9 );  //    false              |    true (below 1E-8)
+   complex<double> c3( 1.0, 0.0  );  //    false              |    false
    \endcode
 */
 template< bool RF          // Relaxation flag
@@ -121,18 +137,37 @@ BLAZE_ALWAYS_INLINE DisableIf_< IsNumeric<Type>, bool > isZero( const Type& v ) 
 //
 // The \a isZero shim provides an abstract interface for testing a value/object of any type
 // whether it represents the numeric value 0. In case the value/object is 0, the function
-// returns \a true, otherwise it returns \a false. For floating point built-in data types,
-// the function returns \a true in case the current value is close to zero within a certain
-// accuracy (relaxed semantics).
+// returns \a true, otherwise it returns \a false:
 
    \code
-   const int i1 = 0;                // isZero( i1 ) returns true
-   double    d1 = 0.0;              // isZero( d1 ) returns true
-   complex<double> c1( 0.0, 0.0 );  // isZero( c1 ) returns true
+   int i1 = 0;  // isZero( i1 ) returns true
+   int i2 = 1;  // isZero( i2 ) returns false
+   \endcode
 
-   const int i2 = 1;                // isZero( i2 ) returns false
-   double    d2 = -1.0;             // isZero( d2 ) returns false
-   complex<double> c2( 1.0, 0.0 );  // isZero( c2 ) returns false
+// For floating point built-in data types, the function by default uses relaxed semantics and
+// returns \a true in case the current value is close to zero within a certain accuracy:
+
+   \code
+   double d1 = 0.0;                  // isZero( d1 ) returns true
+   double d2 = 1E-9;                 // isZero( d2 ) returns true since d2 is below 1E-8
+   double d3 = 1.0;                  // isZero( d3 ) returns false
+   complex<double> c1( 0.0, 0.0  );  // isZero( c1 ) returns true
+   complex<double> c2( 0.0, 1E-9 );  // isZero( c2 ) returns true since imag(c2) is below 1E-8
+   complex<double> c3( 1.0, 0.0  );  // isZero( c3 ) returns false
+   \endcode
+
+// Optionally, it is possible to switch between relaxed semantics (blaze::relaxed) and strict
+// semantics (blaze::strict). In case of strict semantics, for floating point built-in data types
+// the function returns \a true in case the current value is exactly zero:
+
+   \code
+                                     // isZero<strict>( ... ) | isZero<relaxed>( ... )
+   double d1 = 0.0;                  //     true              |     true
+   double d2 = 1E-9;                 //     false (not 0.0)   |     true (below 1E-8)
+   double d3 = 1.0;                  //     false             |     false
+   complex<double> c1( 0.0, 0.0  );  //     true              |     true
+   complex<double> c2( 0.0, 1E-9 );  //     false             |     true (below 1E-8)
+   complex<double> c3( 1.0, 0.0  );  //     false             |     false
    \endcode
 */
 template< typename Type >  // Type of the given value/object
