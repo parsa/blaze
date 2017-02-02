1e3c2c45a627e5a8076ffa0ce0dd4c0754dd714f
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-02-02 15:19:35 +0100

    Add the 'syevd()' LAPACK wrapper functions

--- blaze/math/LAPACK.h
+++ blaze/math/LAPACK.h
@@ -67,6 +67,7 @@
 #include <blaze/math/lapack/potri.h>
 #include <blaze/math/lapack/potrs.h>
 #include <blaze/math/lapack/syev.h>
+#include <blaze/math/lapack/syevd.h>
 #include <blaze/math/lapack/sysv.h>
 #include <blaze/math/lapack/sytrf.h>
 #include <blaze/math/lapack/sytri.h>
