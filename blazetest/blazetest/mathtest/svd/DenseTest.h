6bc4476388f73f97085444c9865b7a63056ab77d
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-02-03 14:35:05 +0100

    Add tests for the 'svd()' functions

---
+++ blazetest/blazetest/mathtest/svd/DenseTest.h
@@ -0,0 +1,141 @@
+//=================================================================================================
+/*!
+//  \file blazetest/mathtest/svd/DenseTest.h
+//  \brief Header file for the dense matrix singular value test
+//
+//  Copyright (C) 2013 Klaus Iglberger - All Rights Reserved
+//
+//  This file is part of the Blaze library. You can redistribute it and/or modify it under
+//  the terms of the New (Revised) BSD License. Redistribution and use in source and binary
+//  forms, with or without modification, are permitted provided that the following conditions
+//  are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this list of
+//     conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright notice, this list
+//     of conditions and the following disclaimer in the documentation and/or other materials
+//     provided with the distribution.
+//  3. Neither the names of the Blaze development group nor the names of its contributors
+//     may be used to endorse or promote products derived from this software without specific
+//     prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+//  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+//  SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+//  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+//  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+//  DAMAGE.
+*/
+//=================================================================================================
+
+#ifndef _BLAZETEST_MATHTEST_SVD_DENSETEST_H_
+#define _BLAZETEST_MATHTEST_SVD_DENSETEST_H_
+
+
+//*************************************************************************************************
+// Includes
+//*************************************************************************************************
+
+#include <string>
+
+
+namespace blazetest {
+
+namespace mathtest {
+
+namespace svd {
+
+//=================================================================================================
+//
+//  CLASS DEFINITION
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\brief Auxiliary class for all dense matrix singular value tests.
+//
+// This class represents a test suite for the dense matrix singular value functionality. It
+// performs a series of singular value computations on several dense matrix types of the Blaze
+// library.
+*/
+class DenseTest
+{
+ public:
+   //**Constructors********************************************************************************
+   /*!\name Constructors */
+   //@{
+   explicit DenseTest();
+   // No explicitly declared copy constructor.
+   //@}
+   //**********************************************************************************************
+
+   //**Destructor**********************************************************************************
+   // No explicitly declared destructor.
+   //**********************************************************************************************
+
+ private:
+   //**Test functions******************************************************************************
+   /*!\name Test functions */
+   //@{
+   void testGeneral();
+   //@}
+   //**********************************************************************************************
+
+   //**Member variables****************************************************************************
+   /*!\name Member variables */
+   //@{
+   std::string test_;  //!< Label of the currently performed test.
+   //@}
+   //**********************************************************************************************
+};
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  GLOBAL TEST FUNCTIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\brief Testing the dense matrix singular value functionality.
+//
+// \return void
+*/
+void runTest()
+{
+   DenseTest();
+}
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  MACRO DEFINITIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Macro for the execution of the dense matrix singular value test.
+*/
+#define RUN_DENSE_SVD_TEST \
+   blazetest::mathtest::svd::runTest()
+/*! \endcond */
+//*************************************************************************************************
+
+} // namespace svd
+
+} // namespace mathtest
+
+} // namespace blazetest
+
+#endif
