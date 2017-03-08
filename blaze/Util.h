cc4d4a18f9c0a0f110ac02aec935856c26851847
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-03-08 15:04:10 +0100

    Add the 'checkedDelete()' and 'checkedArrayDelete()' function templates

--- blaze/Util.h
+++ blaze/Util.h
@@ -45,6 +45,7 @@
 #include <blaze/util/AlignmentCheck.h>
 #include <blaze/util/Algorithm.h>
 #include <blaze/util/Assert.h>
+#include <blaze/util/CheckedDelete.h>
 #include <blaze/util/ColorMacros.h>
 #include <blaze/util/Complex.h>
 #include <blaze/util/Constraints.h>
