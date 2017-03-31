b3b754f401f1ac59d612f07bcab634791a98f337
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-03-31 13:43:15 +0200

    Improve the implementation of the nested 'SubvectorElement' class template

--- blaze/math/views/subvector/Sparse.h
+++ blaze/math/views/subvector/Sparse.h
@@ -132,33 +132,7 @@ class Subvector<VT,AF,TF,false>
            , typename IteratorType >  // Type of the sparse vector iterator
    class SubvectorElement : private SparseElement
    {
-    private:
-      //*******************************************************************************************
-      //! Compilation switch for the return type of the value member function.
-      /*! The \a returnConst compile time constant expression represents a compilation switch for
-          the return type of the value member function. In case the given vector type \a VectorType
-          is const qualified, \a returnConst will be set to 1 and the value member function will
-          return a reference to const. Otherwise \a returnConst will be set to 0 and the value
-          member function will offer write access to the sparse vector elements. */
-      enum : bool { returnConst = IsConst<VectorType>::value };
-      //*******************************************************************************************
-
-      //**Type definitions*************************************************************************
-      //! Type of the underlying sparse elements.
-      typedef typename std::iterator_traits<IteratorType>::value_type  SET;
-
-      typedef Reference_<SET>       RT;   //!< Reference type of the underlying sparse element.
-      typedef ConstReference_<SET>  CRT;  //!< Reference-to-const type of the underlying sparse element.
-      //*******************************************************************************************
-
     public:
-      //**Type definitions*************************************************************************
-      typedef ValueType_<SET>              ValueType;       //!< The value type of the row element.
-      typedef size_t                       IndexType;       //!< The index type of the row element.
-      typedef IfTrue_<returnConst,CRT,RT>  Reference;       //!< Reference return type
-      typedef CRT                          ConstReference;  //!< Reference-to-const return type.
-      //*******************************************************************************************
-
       //**Constructor******************************************************************************
       /*!\brief Constructor for the SubvectorElement class.
       //
@@ -246,7 +220,7 @@ class Subvector<VT,AF,TF,false>
       //
       // \return The current value of the sparse subvector element.
       */
-      inline Reference value() const {
+      inline decltype(auto) value() const {
          return pos_->value();
       }
       //*******************************************************************************************
@@ -256,7 +230,7 @@ class Subvector<VT,AF,TF,false>
       //
       // \return The current index of the sparse element.
       */
-      inline IndexType index() const {
+      inline size_t index() const {
          return pos_->index() - offset_;
       }
       //*******************************************************************************************
