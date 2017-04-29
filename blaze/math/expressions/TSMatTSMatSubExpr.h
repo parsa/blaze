4086e0d21e3384561a81c7c15057dc1b231e02fe
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-04-29 15:19:00 +0200

    Update the 'TSMatTSMatSubExpr' expression class for Schur product assignment

--- blaze/math/expressions/TSMatTSMatSubExpr.h
+++ blaze/math/expressions/TSMatTSMatSubExpr.h
@@ -477,7 +477,7 @@ class TSMatTSMatSubExpr : public SparseMatrix< TSMatTSMatSubExpr<MT1,MT2>, true 
                ++r;
             }
             else {
-               (~lhs).append( l->index(), j, l->value()-r->value() );
+               (~lhs).append( l->index(), j, l->value() - r->value() );
                ++l;
                ++r;
             }
@@ -583,7 +583,7 @@ class TSMatTSMatSubExpr : public SparseMatrix< TSMatTSMatSubExpr<MT1,MT2>, true 
                ++r;
             }
             else {
-               (~lhs).append( l->index(), j, l->value()-r->value() );
+               (~lhs).append( l->index(), j, l->value() - r->value() );
                ++l;
                ++r;
             }
@@ -667,6 +667,42 @@ class TSMatTSMatSubExpr : public SparseMatrix< TSMatTSMatSubExpr<MT1,MT2>, true 
 
    //**Subtraction assignment to sparse matrices***************************************************
    // No special implementation for the subtraction assignment to sparse matrices.
+   //**********************************************************************************************
+
+   //**Schur product assignment to dense matrices**************************************************
+   /*! \cond BLAZE_INTERNAL */
+   /*!\brief Schur product assignment of a transpose sparse matrix-transpose sparse matrix
+   //        subtraction to a dense matrix.
+   // \ingroup sparse_matrix
+   //
+   // \param lhs The target left-hand side dense matrix.
+   // \param rhs The right-hand side subtraction expression for the Schur product.
+   // \return void
+   //
+   // This function implements the performance optimized Schur product assignment of a transpose
+   // sparse matrix-transpose sparse matrix subtraction expression to a dense matrix.
+   */
+   template< typename MT  // Type of the target dense matrix
+           , bool SO >    // Storage order of the target dense matrix
+   friend inline void schurAssign( DenseMatrix<MT,SO>& lhs, const TSMatTSMatSubExpr& rhs )
+   {
+      BLAZE_FUNCTION_TRACE;
+
+      BLAZE_CONSTRAINT_MUST_BE_SPARSE_MATRIX_TYPE( ResultType );
+      BLAZE_CONSTRAINT_MUST_BE_COLUMN_MAJOR_MATRIX_TYPE( ResultType );
+      BLAZE_CONSTRAINT_MUST_BE_REFERENCE_TYPE( CompositeType_<ResultType> );
+
+      BLAZE_INTERNAL_ASSERT( (~lhs).rows()    == rhs.rows()   , "Invalid number of rows"    );
+      BLAZE_INTERNAL_ASSERT( (~lhs).columns() == rhs.columns(), "Invalid number of columns" );
+
+      const ResultType tmp( serial( rhs ) );
+      schurAssign( ~lhs, tmp );
+   }
+   /*! \endcond */
+   //**********************************************************************************************
+
+   //**Schur product assignment to sparse matrices*************************************************
+   // No special implementation for the Schur product assignment to sparse matrices.
    //**********************************************************************************************
 
    //**Multiplication assignment to dense matrices*************************************************
@@ -755,6 +791,45 @@ class TSMatTSMatSubExpr : public SparseMatrix< TSMatTSMatSubExpr<MT1,MT2>, true 
    // No special implementation for the SMP subtraction assignment to sparse matrices.
    //**********************************************************************************************
 
+   //**SMP Schur product assignment to dense matrices**********************************************
+   /*! \cond BLAZE_INTERNAL */
+   /*!\brief SMP Schur product assignment of a transpose sparse matrix-transpose sparse matrix
+   //        subtraction to a dense matrix.
+   // \ingroup sparse_matrix
+   //
+   // \param lhs The target left-hand side dense matrix.
+   // \param rhs The right-hand side subtraction expression for the Schur product.
+   // \return void
+   //
+   // This function implements the performance optimized SMP Schur product assignment of a
+   // transpose sparse matrix-transpose sparse matrix subtraction expression to a dense matrix.
+   // Due to the explicit application of the SFINAE principle, this function can only be selected
+   // by the compiler in case the expression specific parallel evaluation strategy is selected.
+   */
+   template< typename MT  // Type of the target dense matrix
+           , bool SO >    // Storage order of the target dense matrix
+   friend inline EnableIf_< UseSMPAssign<MT> >
+      smpSchurAssign( DenseMatrix<MT,SO>& lhs, const TSMatTSMatSubExpr& rhs )
+   {
+      BLAZE_FUNCTION_TRACE;
+
+      BLAZE_CONSTRAINT_MUST_BE_SPARSE_MATRIX_TYPE( ResultType );
+      BLAZE_CONSTRAINT_MUST_BE_COLUMN_MAJOR_MATRIX_TYPE( ResultType );
+      BLAZE_CONSTRAINT_MUST_BE_REFERENCE_TYPE( CompositeType_<ResultType> );
+
+      BLAZE_INTERNAL_ASSERT( (~lhs).rows()    == rhs.rows()   , "Invalid number of rows"    );
+      BLAZE_INTERNAL_ASSERT( (~lhs).columns() == rhs.columns(), "Invalid number of columns" );
+
+      const ResultType tmp( rhs );
+      smpSchurAssign( ~lhs, tmp );
+   }
+   /*! \endcond */
+   //**********************************************************************************************
+
+   //**SMP Schur product assignment to sparse matrices*********************************************
+   // No special implementation for the SMP Schur product assignment to sparse matrices.
+   //**********************************************************************************************
+
    //**SMP multiplication assignment to dense matrices*********************************************
    // No special implementation for the SMP multiplication assignment to dense matrices.
    //**********************************************************************************************
@@ -791,7 +866,7 @@ class TSMatTSMatSubExpr : public SparseMatrix< TSMatTSMatSubExpr<MT1,MT2>, true 
 //
 // \param lhs The left-hand side sparse matrix for the matrix subtraction.
 // \param rhs The right-hand side sparse matrix to be subtracted from the left-hand side matrix.
-// \return The sum of the two matrices.
+// \return The difference of the two matrices.
 // \exception std::invalid_argument Matrix sizes do not match.
 //
 // This operator represents the subtraction of two column-major sparse matrices:
