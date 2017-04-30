fc1cbe13209b82dad89138a38e60ef1e57985794
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-04-30 05:12:47 +0200

    Add the dense matrix/dense matrix Schur product ('dmatdmatschur') operation test

---
+++ blazetest/src/mathtest/dmatdmatschur/HHaHHb.cpp
@@ -0,0 +1,90 @@
+//=================================================================================================
+/*!
+//  \file src/mathtest/dmatdmatschur/HHaHHb.cpp
+//  \brief Source file for the HHaHHb dense matrix/dense matrix Schur product math test
+//
+//  Copyright (C) 2013 Klaus Iglberger - All Rights Reserved
+//
+//  This file is part of the Blaze library. You can redistribute it and/or modify it under
+//  the terms of the New (Revised) BSD License. Redistribution and use in source and binary
+//  forms, with or without modification, are permitted provided that the following conditions
+//  are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this list of
+//     conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright notice, this list
+//     of conditions and the following disclaimer in the documentation and/or other materials
+//     provided with the distribution.
+//  3. Neither the names of the Blaze development group nor the names of its contributors
+//     may be used to endorse or promote products derived from this software without specific
+//     prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+//  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+//  SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+//  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+//  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+//  DAMAGE.
+*/
+//=================================================================================================
+
+
+//*************************************************************************************************
+// Includes
+//*************************************************************************************************
+
+#include <cstdlib>
+#include <iostream>
+#include <blaze/math/HermitianMatrix.h>
+#include <blaze/math/HybridMatrix.h>
+#include <blazetest/mathtest/Creator.h>
+#include <blazetest/mathtest/dmatdmatschur/OperationTest.h>
+#include <blazetest/system/MathTest.h>
+
+
+//=================================================================================================
+//
+//  MAIN FUNCTION
+//
+//=================================================================================================
+
+//*************************************************************************************************
+int main()
+{
+   std::cout << "   Running 'HHaHHb'..." << std::endl;
+
+   using blazetest::mathtest::NumericA;
+   using blazetest::mathtest::NumericB;
+
+   try
+   {
+      // Matrix type definitions
+      typedef blaze::HermitianMatrix< blaze::HybridMatrix<NumericA,128UL,128UL> >  HHa;
+      typedef blaze::HermitianMatrix< blaze::HybridMatrix<NumericB,128UL,128UL> >  HHb;
+
+      // Creator type definitions
+      typedef blazetest::Creator<HHa>  CHHa;
+      typedef blazetest::Creator<HHb>  CHHb;
+
+      // Running tests with small matrices
+      for( size_t i=0UL; i<=9UL; ++i ) {
+         RUN_DMATDMATSCHUR_OPERATION_TEST( CHHa( i ), CHHb( i ) );
+      }
+
+      // Running tests with large matrices
+      RUN_DMATDMATSCHUR_OPERATION_TEST( CHHa(  67UL ), CHHb(  67UL ) );
+      RUN_DMATDMATSCHUR_OPERATION_TEST( CHHa( 128UL ), CHHb( 128UL ) );
+   }
+   catch( std::exception& ex ) {
+      std::cerr << "\n\n ERROR DETECTED during dense matrix/dense matrix Schur product:\n"
+                << ex.what() << "\n";
+      return EXIT_FAILURE;
+   }
+
+   return EXIT_SUCCESS;
+}
+//*************************************************************************************************
