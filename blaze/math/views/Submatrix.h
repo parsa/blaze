d8340e3425496b61da3adc3340c201124f4b5e7d
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-04-30 07:46:27 +0200

    Add an overload of the 'submatrix()' function for Schur product operations

--- blaze/math/views/Submatrix.h
+++ blaze/math/views/Submatrix.h
@@ -75,6 +75,7 @@
 #include <blaze/math/typetraits/IsMatTransExpr.h>
 #include <blaze/math/typetraits/IsMatVecMultExpr.h>
 #include <blaze/math/typetraits/IsRestricted.h>
+#include <blaze/math/typetraits/IsSchurExpr.h>
 #include <blaze/math/typetraits/IsStrictlyLower.h>
 #include <blaze/math/typetraits/IsStrictlyUpper.h>
 #include <blaze/math/typetraits/IsTransExpr.h>
@@ -629,6 +630,36 @@ inline const EnableIf_< IsMatMatSubExpr<MT>, SubmatrixExprTrait_<MT,AF> >
    BLAZE_FUNCTION_TRACE;
 
    return submatrix<AF>( (~matrix).leftOperand() , row, column, m, n ) -
+          submatrix<AF>( (~matrix).rightOperand(), row, column, m, n );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Creating a view on a specific submatrix of the given Schur product.
+// \ingroup views
+//
+// \param matrix The constant Schur product.
+// \param row The index of the first row of the submatrix.
+// \param column The index of the first column of the submatrix.
+// \param m The number of rows of the submatrix.
+// \param n The number of columns of the submatrix.
+// \return View on the specified submatrix of the Schur product.
+//
+// This function returns an expression representing the specified submatrix of the given Schur
+// product.
+*/
+template< bool AF      // Alignment flag
+        , typename MT  // Type of the matrix
+        , bool SO >    // Storage order
+inline const EnableIf_< IsSchurExpr<MT>, SubmatrixExprTrait_<MT,AF> >
+   submatrix( const Matrix<MT,SO>& matrix, size_t row, size_t column, size_t m, size_t n )
+{
+   BLAZE_FUNCTION_TRACE;
+
+   return submatrix<AF>( (~matrix).leftOperand() , row, column, m, n ) %
           submatrix<AF>( (~matrix).rightOperand(), row, column, m, n );
 }
 /*! \endcond */
