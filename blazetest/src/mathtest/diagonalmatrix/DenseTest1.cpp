ce8090ebef1767d0649f4234286247d27f45e007
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-06-20 05:34:25 +0200

    Update the tests for the 'DiagonalMatrix' class template

--- blazetest/src/mathtest/diagonalmatrix/DenseTest1.cpp
+++ blazetest/src/mathtest/diagonalmatrix/DenseTest1.cpp
@@ -511,12 +511,12 @@ void DenseTest::testConstructors()
       using blaze::rowMajor;
 
       typedef blaze::CustomMatrix<int,unaligned,unpadded,rowMajor>  UnalignedUnpadded;
-      std::unique_ptr<int[]> array( new int[5UL] );
-      array[1] = 1;
-      array[2] = 0;
-      array[3] = 0;
-      array[4] = 2;
-      const blaze::DiagonalMatrix<UnalignedUnpadded> diag( array.get()+1UL, 2UL );
+      std::unique_ptr<int[]> memory( new int[5UL] );
+      memory[1] = 1;
+      memory[2] = 0;
+      memory[3] = 0;
+      memory[4] = 2;
+      const blaze::DiagonalMatrix<UnalignedUnpadded> diag( memory.get()+1UL, 2UL );
 
       checkRows    ( diag, 2UL );
       checkColumns ( diag, 2UL );
@@ -544,78 +544,12 @@ void DenseTest::testConstructors()
       using blaze::rowMajor;
 
       typedef blaze::CustomMatrix<int,unaligned,unpadded,rowMajor>  UnalignedUnpadded;
-      std::unique_ptr<int[]> array( new int[11UL] );
-      array[1] = 1;
-      array[2] = 0;
-      array[6] = 0;
-      array[7] = 2;
-      const blaze::DiagonalMatrix<UnalignedUnpadded> diag( array.get()+1UL, 2UL, 5UL );
-
-      checkRows    ( diag, 2UL );
-      checkColumns ( diag, 2UL );
-      checkCapacity( diag, 4UL );
-      checkNonZeros( diag, 2UL );
-
-      if( diag(0,0) != 1 || diag(0,1) != 0 ||
-          diag(1,0) != 0 || diag(1,1) != 2 ) {
-         std::ostringstream oss;
-         oss << " Test: " << test_ << "\n"
-             << " Error: Construction failed\n"
-             << " Details:\n"
-             << "   Result:\n" << diag << "\n"
-             << "   Expected result:\n( 1 0 )\n( 0 2 )\n";
-         throw std::runtime_error( oss.str() );
-      }
-   }
-
-   // Custom matrix constructor (ElementType*, size_t, Deleter)
-   {
-      test_ = "Row-major DiagonalMatrix custom matrix constructor (ElementType*, size_t, Deleter)";
-
-      using blaze::unaligned;
-      using blaze::unpadded;
-      using blaze::rowMajor;
-
-      typedef blaze::CustomMatrix<int,unaligned,unpadded,rowMajor>  UnalignedUnpadded;
-      std::unique_ptr<int[]> array( new int[4UL] );
-      array[0] = 1;
-      array[1] = 0;
-      array[2] = 0;
-      array[3] = 2;
-      const blaze::DiagonalMatrix<UnalignedUnpadded> diag( array.release(), 2UL, blaze::ArrayDelete() );
-
-      checkRows    ( diag, 2UL );
-      checkColumns ( diag, 2UL );
-      checkCapacity( diag, 4UL );
-      checkNonZeros( diag, 2UL );
-
-      if( diag(0,0) != 1 || diag(0,1) != 0 ||
-          diag(1,0) != 0 || diag(1,1) != 2 ) {
-         std::ostringstream oss;
-         oss << " Test: " << test_ << "\n"
-             << " Error: Construction failed\n"
-             << " Details:\n"
-             << "   Result:\n" << diag << "\n"
-             << "   Expected result:\n( 1 0 )\n( 0 2 )\n";
-         throw std::runtime_error( oss.str() );
-      }
-   }
-
-   // Custom matrix constructor (ElementType*, size_t, size_t, Deleter)
-   {
-      test_ = "Row-major DiagonalMatrix custom matrix constructor (ElementType*, size_t, size_t, Deleter)";
-
-      using blaze::unaligned;
-      using blaze::unpadded;
-      using blaze::rowMajor;
-
-      typedef blaze::CustomMatrix<int,unaligned,unpadded,rowMajor>  UnalignedUnpadded;
-      std::unique_ptr<int[]> array( new int[10UL] );
-      array[0] = 1;
-      array[1] = 0;
-      array[5] = 0;
-      array[6] = 2;
-      const blaze::DiagonalMatrix<UnalignedUnpadded> diag( array.release(), 2UL, 5UL, blaze::ArrayDelete() );
+      std::unique_ptr<int[]> memory( new int[11UL] );
+      memory[1] = 1;
+      memory[2] = 0;
+      memory[6] = 0;
+      memory[7] = 2;
+      const blaze::DiagonalMatrix<UnalignedUnpadded> diag( memory.get()+1UL, 2UL, 5UL );
 
       checkRows    ( diag, 2UL );
       checkColumns ( diag, 2UL );
@@ -1148,12 +1082,12 @@ void DenseTest::testConstructors()
       using blaze::columnMajor;
 
       typedef blaze::CustomMatrix<int,unaligned,unpadded,columnMajor>  UnalignedUnpadded;
-      std::unique_ptr<int[]> array( new int[5UL] );
-      array[1] = 1;
-      array[2] = 0;
-      array[3] = 0;
-      array[4] = 2;
-      const blaze::DiagonalMatrix<UnalignedUnpadded> diag( array.get()+1UL, 2UL );
+      std::unique_ptr<int[]> memory( new int[5UL] );
+      memory[1] = 1;
+      memory[2] = 0;
+      memory[3] = 0;
+      memory[4] = 2;
+      const blaze::DiagonalMatrix<UnalignedUnpadded> diag( memory.get()+1UL, 2UL );
 
       checkRows    ( diag, 2UL );
       checkColumns ( diag, 2UL );
@@ -1181,78 +1115,12 @@ void DenseTest::testConstructors()
       using blaze::columnMajor;
 
       typedef blaze::CustomMatrix<int,unaligned,unpadded,columnMajor>  UnalignedUnpadded;
-      std::unique_ptr<int[]> array( new int[11UL] );
-      array[1] = 1;
-      array[2] = 0;
-      array[6] = 0;
-      array[7] = 2;
-      const blaze::DiagonalMatrix<UnalignedUnpadded> diag( array.get()+1UL, 2UL, 5UL );
-
-      checkRows    ( diag, 2UL );
-      checkColumns ( diag, 2UL );
-      checkCapacity( diag, 4UL );
-      checkNonZeros( diag, 2UL );
-
-      if( diag(0,0) != 1 || diag(0,1) != 0 ||
-          diag(1,0) != 0 || diag(1,1) != 2 ) {
-         std::ostringstream oss;
-         oss << " Test: " << test_ << "\n"
-             << " Error: Construction failed\n"
-             << " Details:\n"
-             << "   Result:\n" << diag << "\n"
-             << "   Expected result:\n( 1 0 )\n( 0 2 )\n";
-         throw std::runtime_error( oss.str() );
-      }
-   }
-
-   // Custom matrix constructor (ElementType*, size_t, Deleter)
-   {
-      test_ = "Column-major DiagonalMatrix custom matrix constructor (ElementType*, size_t, Deleter)";
-
-      using blaze::unaligned;
-      using blaze::unpadded;
-      using blaze::columnMajor;
-
-      typedef blaze::CustomMatrix<int,unaligned,unpadded,columnMajor>  UnalignedUnpadded;
-      std::unique_ptr<int[]> array( new int[4UL] );
-      array[0] = 1;
-      array[1] = 0;
-      array[2] = 0;
-      array[3] = 2;
-      const blaze::DiagonalMatrix<UnalignedUnpadded> diag( array.release(), 2UL, blaze::ArrayDelete() );
-
-      checkRows    ( diag, 2UL );
-      checkColumns ( diag, 2UL );
-      checkCapacity( diag, 4UL );
-      checkNonZeros( diag, 2UL );
-
-      if( diag(0,0) != 1 || diag(0,1) != 0 ||
-          diag(1,0) != 0 || diag(1,1) != 2 ) {
-         std::ostringstream oss;
-         oss << " Test: " << test_ << "\n"
-             << " Error: Construction failed\n"
-             << " Details:\n"
-             << "   Result:\n" << diag << "\n"
-             << "   Expected result:\n( 1 0 )\n( 0 2 )\n";
-         throw std::runtime_error( oss.str() );
-      }
-   }
-
-   // Custom matrix constructor (ElementType*, size_t, size_t, Deleter)
-   {
-      test_ = "Column-major DiagonalMatrix custom matrix constructor (ElementType*, size_t, size_t, Deleter)";
-
-      using blaze::unaligned;
-      using blaze::unpadded;
-      using blaze::columnMajor;
-
-      typedef blaze::CustomMatrix<int,unaligned,unpadded,columnMajor>  UnalignedUnpadded;
-      std::unique_ptr<int[]> array( new int[10UL] );
-      array[0] = 1;
-      array[1] = 0;
-      array[5] = 0;
-      array[6] = 2;
-      const blaze::DiagonalMatrix<UnalignedUnpadded> diag( array.release(), 2UL, 5UL, blaze::ArrayDelete() );
+      std::unique_ptr<int[]> memory( new int[11UL] );
+      memory[1] = 1;
+      memory[2] = 0;
+      memory[6] = 0;
+      memory[7] = 2;
+      const blaze::DiagonalMatrix<UnalignedUnpadded> diag( memory.get()+1UL, 2UL, 5UL );
 
       checkRows    ( diag, 2UL );
       checkColumns ( diag, 2UL );
