336808d6a66978e880ded6f66d29ba72a649566d
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-06-24 05:50:23 +0200

    Add specializations of the 'BinaryMapTrait' class template for 'HybridMatrix'

--- blaze/math/dense/HybridMatrix.h
+++ blaze/math/dense/HybridMatrix.h
@@ -58,6 +58,7 @@
 #include <blaze/math/shims/IsDefault.h>
 #include <blaze/math/SIMD.h>
 #include <blaze/math/traits/AddTrait.h>
+#include <blaze/math/traits/BinaryMapTrait.h>
 #include <blaze/math/traits/ColumnTrait.h>
 #include <blaze/math/traits/CTransExprTrait.h>
 #include <blaze/math/traits/DivTrait.h>
@@ -7077,6 +7078,55 @@ template< typename T, size_t M, size_t N, bool SO, typename OP >
 struct UnaryMapTrait< HybridMatrix<T,M,N,SO>, OP >
 {
    using Type = HybridMatrix< UnaryMapTrait_<T,OP>, M, N, SO >;
+};
+/*! \endcond */
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  BINARYMAPTRAIT SPECIALIZATIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+template< typename T1, size_t M1, size_t N1, bool SO, typename T2, size_t M2, size_t N2, typename OP >
+struct BinaryMapTrait< HybridMatrix<T1,M1,N1,SO>, StaticMatrix<T2,M2,N2,SO>, OP >
+{
+   using Type = StaticMatrix< BinaryMapTrait_<T1,T2,OP>, M2, N2, SO >;
+};
+
+template< typename T1, size_t M1, size_t N1, bool SO1, typename T2, size_t M2, size_t N2, bool SO2, typename OP >
+struct BinaryMapTrait< HybridMatrix<T1,M1,N1,SO1>, StaticMatrix<T2,M2,N2,SO2>, OP >
+{
+   using Type = StaticMatrix< BinaryMapTrait_<T1,T2,OP>, M2, N2, false >;
+};
+
+template< typename T1, size_t M1, size_t N1, bool SO, typename T2, size_t M2, size_t N2, typename OP >
+struct BinaryMapTrait< StaticMatrix<T1,M1,N1,SO>, HybridMatrix<T2,M2,N2,SO>, OP >
+{
+   using Type = StaticMatrix< BinaryMapTrait_<T1,T2,OP>, M2, N2, SO >;
+};
+
+template< typename T1, size_t M1, size_t N1, bool SO1, typename T2, size_t M2, size_t N2, bool SO2, typename OP >
+struct BinaryMapTrait< StaticMatrix<T1,M1,N1,SO1>, HybridMatrix<T2,M2,N2,SO2>, OP >
+{
+   using Type = StaticMatrix< BinaryMapTrait_<T1,T2,OP>, M2, N2, false >;
+};
+
+template< typename T1, size_t M1, size_t N1, bool SO, typename T2, size_t M2, size_t N2, typename OP >
+struct BinaryMapTrait< HybridMatrix<T1,M1,N1,SO>, HybridMatrix<T2,M2,N2,SO>, OP >
+{
+   using Type = HybridMatrix< BinaryMapTrait_<T1,T2,OP>, ( M1 < M2 )?( M1 ):( M2 ), ( N1 < N2 )?( N1 ):( N2 ), SO >;
+};
+
+template< typename T1, size_t M1, size_t N1, bool SO1, typename T2, size_t M2, size_t N2, bool SO2, typename OP >
+struct BinaryMapTrait< HybridMatrix<T1,M1,N1,SO1>, HybridMatrix<T2,M2,N2,SO2>, OP >
+{
+   using Type = HybridMatrix< BinaryMapTrait_<T1,T2,OP>, ( M1 < M2 )?( M1 ):( M2 ), ( N1 < N2 )?( N1 ):( N2 ), false >;
 };
 /*! \endcond */
 //*************************************************************************************************
