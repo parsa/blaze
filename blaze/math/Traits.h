06980971c5e6c5afb0890a18276fdf4318de1b1e
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-07-26 16:02:26 +0200

    Add the 'DeclIdTrait' class template

--- blaze/math/Traits.h
+++ blaze/math/Traits.h
@@ -53,6 +53,7 @@
 #include <blaze/math/traits/DeclDiagTrait.h>
 #include <blaze/math/traits/DeclHermExprTrait.h>
 #include <blaze/math/traits/DeclHermTrait.h>
+#include <blaze/math/traits/DeclIdTrait.h>
 #include <blaze/math/traits/DeclLowExprTrait.h>
 #include <blaze/math/traits/DeclLowTrait.h>
 #include <blaze/math/traits/DeclSymExprTrait.h>
---
+++ blaze/math/traits/DeclIdTrait.h
@@ -0,0 +1,183 @@
+//=================================================================================================
+/*!
+//  \file blaze/math/traits/DeclIdTrait.h
+//  \brief Header file for the declid trait
+//
+//  Copyright (C) 2013 Klaus Iglberger - All Rights Reserved
+//
+//  This file is part of the Blaze library. You can redistribute it and/or modify it under
+//  the terms of the New (Revised) BSD License. Redistribution and use in source and binary
+//  forms, with or without modification, are permitted provided that the following conditions
+//  are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this list of
+//     conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright notice, this list
+//     of conditions and the following disclaimer in the documentation and/or other materials
+//     provided with the distribution.
+//  3. Neither the names of the Blaze development group nor the names of its contributors
+//     may be used to endorse or promote products derived from this software without specific
+//     prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+//  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+//  SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+//  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+//  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+//  DAMAGE.
+*/
+//=================================================================================================
+
+#ifndef _BLAZE_MATH_TRAITS_DECLIDTRAIT_H_
+#define _BLAZE_MATH_TRAITS_DECLIDTRAIT_H_
+
+
+//*************************************************************************************************
+// Includes
+//*************************************************************************************************
+
+#include <blaze/math/Aliases.h>
+#include <blaze/math/sparse/Forward.h>
+#include <blaze/math/typetraits/IsMatrix.h>
+#include <blaze/math/typetraits/StorageOrder.h>
+#include <blaze/util/InvalidType.h>
+#include <blaze/util/mpl/If.h>
+#include <blaze/util/mpl/Or.h>
+#include <blaze/util/typetraits/Decay.h>
+#include <blaze/util/typetraits/IsConst.h>
+#include <blaze/util/typetraits/IsReference.h>
+#include <blaze/util/typetraits/IsVolatile.h>
+
+
+namespace blaze {
+
+//=================================================================================================
+//
+//  CLASS DEFINITION
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\brief Base template for the DeclIdTrait class.
+// \ingroup math_traits
+//
+// \section declidtrait_general General
+//
+// The DeclIdTrait class template offers the possibility to select the resulting data type
+// of a generic declid() operation on the given type \a MT. DeclIdTrait defines the nested
+// type \a Type, which represents the resulting data type of the declid() operation. In case
+// the given data type is not a dense or sparse matrix type, the resulting data type \a Type is
+// set to \a INVALID_TYPE. Note that \a const and \a volatile qualifiers and reference modifiers
+// are generally ignored.
+//
+// Per default, the DeclIdTrait template only supports the following matrix types:
+//
+// <ul>
+//    <li>blaze::StaticMatrix</li>
+//    <li>blaze::HybridMatrix</li>
+//    <li>blaze::DynamicMatrix</li>
+//    <li>blaze::CustomMatrix</li>
+//    <li>blaze::CompressedMatrix</li>
+//    <li>blaze::IdentityMatrix</li>
+//    <li>blaze::SymmetricMatrix</li>
+//    <li>blaze::HermitianMatrix</li>
+//    <li>blaze::LowerMatrix</li>
+//    <li>blaze::UniLowerMatrix</li>
+//    <li>blaze::StrictlyLowerMatrix</li>
+//    <li>blaze::UpperMatrix</li>
+//    <li>blaze::UniUpperMatrix</li>
+//    <li>blaze::StrictlyUpperMatrix</li>
+//    <li>blaze::DiagonalMatrix</li>
+//    <li>blaze::Submatrix</li>
+// </ul>
+//
+//
+// \section declidtrait_specializations Creating custom specializations
+//
+// It is possible to specialize the DeclIdTrait template for additional user-defined matrix types.
+// The following example shows the according specialization for the SymmetricMatrix class template:
+
+   \code
+   template< typename MT, bool SO, bool DF, bool NF >
+   struct DeclIdTrait< SymmetricMatrix<MT,SO,DF,NF> >
+   {
+      using Type = IdentityMatrix< ElementType_<MT>, SO >;
+   };
+   \endcode
+
+// \n \section declidtrait_examples Examples
+//
+// The following example demonstrates the use of the DeclIdTrait template, where depending on
+// the given matrix type the resulting type is selected:
+
+   \code
+   using blaze::DynamicMatrix;
+   using blaze::StaticMatrix;
+   using blaze::SymmetricMatrix;
+   using blaze::DeclIdTrait;
+   using blaze::rowMajor;
+   using blaze::columnMajor;
+
+   // Definition of the resulting type of a row-major dynamic matrix
+   using MatrixType1 = DynamicMatrix<int,rowMajor>;
+   using DeclIdType1 = typename DeclIdTrait<MatrixType1>::Type;
+
+   // Definition of the resulting type of a symmetric column-major static matrix
+   using MatrixType2 = SymmetricMatrix< StaticMatrix<int,3UL,3UL,columnMajor> >;
+   using DeclIdType2 = typename DeclIdTrait<MatrixType2>::Type;
+   \endcode
+*/
+template< typename MT >  // Type of the matrix
+struct DeclIdTrait
+{
+ private:
+   //**********************************************************************************************
+   /*! \cond BLAZE_INTERNAL */
+   struct Failure { using Type = INVALID_TYPE; };
+   /*! \endcond */
+   //**********************************************************************************************
+
+   //**********************************************************************************************
+   /*! \cond BLAZE_INTERNAL */
+   struct Result { using Type = IdentityMatrix< ElementType_<MT>, StorageOrder<MT>::value >; };
+   /*! \endcond */
+   //**********************************************************************************************
+
+ public:
+   //**********************************************************************************************
+   /*! \cond BLAZE_INTERNAL */
+   using Type = typename If_< Or< IsConst<MT>, IsVolatile<MT>, IsReference<MT> >
+                            , DeclIdTrait< Decay_<MT> >
+                            , If_< IsMatrix<MT>
+                                 , Result
+                                 , Failure > >::Type;
+   /*! \endcond */
+   //**********************************************************************************************
+};
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Auxiliary alias declaration for the DeclIdTrait type trait.
+// \ingroup math_traits
+//
+// The DeclIdTrait_ alias declaration provides a convenient shortcut to access the nested
+// \a Type of the DeclIdTrait class template. For instance, given the matrix type \a MT the
+// following two type definitions are identical:
+
+   \code
+   using Type1 = typename DeclIdTrait<MT>::Type;
+   using Type2 = DeclIdTrait_<MT>;
+   \endcode
+*/
+template< typename MT >  // Type of the matrix
+using DeclIdTrait_ = typename DeclIdTrait<MT>::Type;
+//*************************************************************************************************
+
+} // namespace blaze
+
+#endif
