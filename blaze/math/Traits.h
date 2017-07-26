7dc5bd2328476d49a5ce4064d24bc143890e2067
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-07-26 15:59:57 +0200

    Add the 'DeclDiagTrait' class template

--- blaze/math/Traits.h
+++ blaze/math/Traits.h
@@ -50,6 +50,7 @@
 #include <blaze/math/traits/CrossTrait.h>
 #include <blaze/math/traits/CTransExprTrait.h>
 #include <blaze/math/traits/DeclDiagExprTrait.h>
+#include <blaze/math/traits/DeclDiagTrait.h>
 #include <blaze/math/traits/DeclHermExprTrait.h>
 #include <blaze/math/traits/DeclHermTrait.h>
 #include <blaze/math/traits/DeclLowExprTrait.h>
