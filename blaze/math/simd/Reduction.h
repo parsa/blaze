f3ed9ac93b1609da4b716a838e62c6c7d340d376
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-07-27 11:50:43 +0200

    Add a SIMD 'sum()' function for 8-bit integral types

--- blaze/math/simd/Reduction.h
+++ blaze/math/simd/Reduction.h
@@ -54,6 +54,39 @@ namespace blaze {
 //=================================================================================================
 
 //*************************************************************************************************
+/*!\brief Returns the sum of all elements in the 8-bit integral SIMD vector.
+// \ingroup simd
+//
+// \param a The vector to be summed up.
+// \return The sum of all vector elements.
+*/
+BLAZE_ALWAYS_INLINE int8_t sum( const SIMDint8& a ) noexcept
+{
+#if BLAZE_AVX512BW_MODE
+   return a[ 0] + a[ 1] + a[ 2] + a[ 3] + a[ 4] + a[ 5] + a[ 6] + a[ 7] +
+          a[ 8] + a[ 9] + a[10] + a[11] + a[12] + a[13] + a[14] + a[15] +
+          a[16] + a[17] + a[18] + a[19] + a[20] + a[21] + a[22] + a[23] +
+          a[24] + a[25] + a[26] + a[27] + a[28] + a[29] + a[30] + a[31] +
+          a[32] + a[33] + a[34] + a[35] + a[36] + a[37] + a[38] + a[39] +
+          a[40] + a[41] + a[42] + a[43] + a[44] + a[45] + a[46] + a[47] +
+          a[48] + a[49] + a[50] + a[51] + a[52] + a[53] + a[54] + a[55] +
+          a[56] + a[57] + a[58] + a[59] + a[60] + a[61] + a[62] + a[63];
+#elif BLAZE_AVX2_MODE
+   return a[ 0] + a[ 1] + a[ 2] + a[ 3] + a[ 4] + a[ 5] + a[ 6] + a[ 7] +
+          a[ 8] + a[ 9] + a[10] + a[11] + a[12] + a[13] + a[14] + a[15] +
+          a[16] + a[17] + a[18] + a[19] + a[20] + a[21] + a[22] + a[23] +
+          a[24] + a[25] + a[26] + a[27] + a[28] + a[29] + a[30] + a[31];
+#elif BLAZE_SSE2_MODE
+   return a[ 0] + a[ 1] + a[ 2] + a[ 3] + a[ 4] + a[ 5] + a[ 6] + a[ 7] +
+          a[ 8] + a[ 9] + a[10] + a[11] + a[12] + a[13] + a[14] + a[15];
+#else
+   return a.value;
+#endif
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
 /*!\brief Returns the sum of all elements in the 8-bit integral complex SIMD vector.
 // \ingroup simd
 //
