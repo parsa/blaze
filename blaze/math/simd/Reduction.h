b824844253a9b9386b19fd6f0ddff8131a7498fc
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-07-27 13:32:16 +0200

    Rework the SIMD 'sum()' functions for integral types

--- blaze/math/simd/Reduction.h
+++ blaze/math/simd/Reduction.h
@@ -40,6 +40,7 @@
 // Includes
 //*************************************************************************************************
 
+#include <blaze/math/Aliases.h>
 #include <blaze/math/simd/BasicTypes.h>
 #include <blaze/system/Inline.h>
 #include <blaze/system/Vectorization.h>
@@ -60,27 +61,28 @@ namespace blaze {
 // \param a The vector to be summed up.
 // \return The sum of all vector elements.
 */
-BLAZE_ALWAYS_INLINE int8_t sum( const SIMDint8& a ) noexcept
+template< typename T >  // Type of the SIMD element
+BLAZE_ALWAYS_INLINE ValueType_<T> sum( const SIMDi8<T>& a ) noexcept
 {
 #if BLAZE_AVX512BW_MODE
-   return a[ 0] + a[ 1] + a[ 2] + a[ 3] + a[ 4] + a[ 5] + a[ 6] + a[ 7] +
-          a[ 8] + a[ 9] + a[10] + a[11] + a[12] + a[13] + a[14] + a[15] +
-          a[16] + a[17] + a[18] + a[19] + a[20] + a[21] + a[22] + a[23] +
-          a[24] + a[25] + a[26] + a[27] + a[28] + a[29] + a[30] + a[31] +
-          a[32] + a[33] + a[34] + a[35] + a[36] + a[37] + a[38] + a[39] +
-          a[40] + a[41] + a[42] + a[43] + a[44] + a[45] + a[46] + a[47] +
-          a[48] + a[49] + a[50] + a[51] + a[52] + a[53] + a[54] + a[55] +
-          a[56] + a[57] + a[58] + a[59] + a[60] + a[61] + a[62] + a[63];
+   return (~a)[ 0] + (~a)[ 1] + (~a)[ 2] + (~a)[ 3] + (~a)[ 4] + (~a)[ 5] + (~a)[ 6] + (~a)[ 7] +
+          (~a)[ 8] + (~a)[ 9] + (~a)[10] + (~a)[11] + (~a)[12] + (~a)[13] + (~a)[14] + (~a)[15] +
+          (~a)[16] + (~a)[17] + (~a)[18] + (~a)[19] + (~a)[20] + (~a)[21] + (~a)[22] + (~a)[23] +
+          (~a)[24] + (~a)[25] + (~a)[26] + (~a)[27] + (~a)[28] + (~a)[29] + (~a)[30] + (~a)[31] +
+          (~a)[32] + (~a)[33] + (~a)[34] + (~a)[35] + (~a)[36] + (~a)[37] + (~a)[38] + (~a)[39] +
+          (~a)[40] + (~a)[41] + (~a)[42] + (~a)[43] + (~a)[44] + (~a)[45] + (~a)[46] + (~a)[47] +
+          (~a)[48] + (~a)[49] + (~a)[50] + (~a)[51] + (~a)[52] + (~a)[53] + (~a)[54] + (~a)[55] +
+          (~a)[56] + (~a)[57] + (~a)[58] + (~a)[59] + (~a)[60] + (~a)[61] + (~a)[62] + (~a)[63];
 #elif BLAZE_AVX2_MODE
-   return a[ 0] + a[ 1] + a[ 2] + a[ 3] + a[ 4] + a[ 5] + a[ 6] + a[ 7] +
-          a[ 8] + a[ 9] + a[10] + a[11] + a[12] + a[13] + a[14] + a[15] +
-          a[16] + a[17] + a[18] + a[19] + a[20] + a[21] + a[22] + a[23] +
-          a[24] + a[25] + a[26] + a[27] + a[28] + a[29] + a[30] + a[31];
+   return (~a)[ 0] + (~a)[ 1] + (~a)[ 2] + (~a)[ 3] + (~a)[ 4] + (~a)[ 5] + (~a)[ 6] + (~a)[ 7] +
+          (~a)[ 8] + (~a)[ 9] + (~a)[10] + (~a)[11] + (~a)[12] + (~a)[13] + (~a)[14] + (~a)[15] +
+          (~a)[16] + (~a)[17] + (~a)[18] + (~a)[19] + (~a)[20] + (~a)[21] + (~a)[22] + (~a)[23] +
+          (~a)[24] + (~a)[25] + (~a)[26] + (~a)[27] + (~a)[28] + (~a)[29] + (~a)[30] + (~a)[31];
 #elif BLAZE_SSE2_MODE
-   return a[ 0] + a[ 1] + a[ 2] + a[ 3] + a[ 4] + a[ 5] + a[ 6] + a[ 7] +
-          a[ 8] + a[ 9] + a[10] + a[11] + a[12] + a[13] + a[14] + a[15];
+   return (~a)[ 0] + (~a)[ 1] + (~a)[ 2] + (~a)[ 3] + (~a)[ 4] + (~a)[ 5] + (~a)[ 6] + (~a)[ 7] +
+          (~a)[ 8] + (~a)[ 9] + (~a)[10] + (~a)[11] + (~a)[12] + (~a)[13] + (~a)[14] + (~a)[15];
 #else
-   return a.value;
+   return (~a).value;
 #endif
 }
 //*************************************************************************************************
@@ -93,20 +95,21 @@ BLAZE_ALWAYS_INLINE int8_t sum( const SIMDint8& a ) noexcept
 // \param a The vector to be summed up.
 // \return The sum of all vector elements.
 */
-BLAZE_ALWAYS_INLINE const complex<int8_t> sum( const SIMDcint8& a ) noexcept
+template< typename T >  // Type of the SIMD element
+BLAZE_ALWAYS_INLINE const ValueType_<T> sum( const SIMDci8<T>& a ) noexcept
 {
 #if BLAZE_AVX512BW_MODE
-   return complex<int8_t>( a[ 0] + a[ 1] + a[ 2] + a[ 3] + a[ 4] + a[ 5] + a[ 6] + a[ 7] +
-                           a[ 8] + a[ 9] + a[10] + a[11] + a[12] + a[13] + a[14] + a[15] +
-                           a[16] + a[17] + a[18] + a[19] + a[20] + a[21] + a[22] + a[23] +
-                           a[24] + a[25] + a[26] + a[27] + a[28] + a[29] + a[30] + a[31] );
+   return complex<int8_t>( (~a)[ 0] + (~a)[ 1] + (~a)[ 2] + (~a)[ 3] + (~a)[ 4] + (~a)[ 5] + (~a)[ 6] + (~a)[ 7] +
+                           (~a)[ 8] + (~a)[ 9] + (~a)[10] + (~a)[11] + (~a)[12] + (~a)[13] + (~a)[14] + (~a)[15] +
+                           (~a)[16] + (~a)[17] + (~a)[18] + (~a)[19] + (~a)[20] + (~a)[21] + (~a)[22] + (~a)[23] +
+                           (~a)[24] + (~a)[25] + (~a)[26] + (~a)[27] + (~a)[28] + (~a)[29] + (~a)[30] + (~a)[31] );
 #elif BLAZE_AVX2_MODE
-   return complex<int8_t>( a[0] + a[1] + a[ 2] + a[ 3] + a[ 4] + a[ 5] + a[ 6] + a[ 7] +
-                           a[8] + a[9] + a[10] + a[11] + a[12] + a[13] + a[14] + a[15] );
+   return complex<int8_t>( (~a)[0] + (~a)[1] + (~a)[ 2] + (~a)[ 3] + (~a)[ 4] + (~a)[ 5] + (~a)[ 6] + (~a)[ 7] +
+                           (~a)[8] + (~a)[9] + (~a)[10] + (~a)[11] + (~a)[12] + (~a)[13] + (~a)[14] + (~a)[15] );
 #elif BLAZE_SSE2_MODE
-   return complex<int8_t>( a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] );
+   return complex<int8_t>( (~a)[0] + (~a)[1] + (~a)[2] + (~a)[3] + (~a)[4] + (~a)[5] + (~a)[6] + (~a)[7] );
 #else
-   return a.value;
+   return (~a).value;
 #endif
 }
 //*************************************************************************************************
@@ -127,11 +130,12 @@ BLAZE_ALWAYS_INLINE const complex<int8_t> sum( const SIMDcint8& a ) noexcept
 // \param a The vector to be summed up.
 // \return The sum of all vector elements.
 */
-BLAZE_ALWAYS_INLINE int16_t sum( const SIMDint16& a ) noexcept
+template< typename T >  // Type of the SIMD element
+BLAZE_ALWAYS_INLINE ValueType_<T> sum( const SIMDi16<T>& a ) noexcept
 {
 #if BLAZE_AVX512BW_MODE
-   const __m256i low ( _mm512_castsi512_si256( a.value ) );
-   const __m256i high( _mm512_extracti64x4_epi64( a.value, 1 ) );
+   const __m256i low ( _mm512_castsi512_si256( (~a).value ) );
+   const __m256i high( _mm512_extracti64x4_epi64( (~a).value, 1 ) );
    const __m256i b   ( _mm256_hadd_epi16( low, high ) );
    const __m256i c   ( _mm256_hadd_epi16( b, b ) );
    const __m256i d   ( _mm256_hadd_epi16( c, c ) );
@@ -140,21 +144,21 @@ BLAZE_ALWAYS_INLINE int16_t sum( const SIMDint16& a ) noexcept
                                     , _mm256_castsi256_si128( e ) ) );
    return _mm_extract_epi16( f, 0 );
 #elif BLAZE_AVX2_MODE
-   const __m256i b( _mm256_hadd_epi16( a.value, a.value ) );
+   const __m256i b( _mm256_hadd_epi16( (~a).value, (~a).value ) );
    const __m256i c( _mm256_hadd_epi16( b, b ) );
    const __m256i d( _mm256_hadd_epi16( c, c ) );
    const __m128i e( _mm_add_epi16( _mm256_extracti128_si256( d, 1 )
                                  , _mm256_castsi256_si128( d ) ) );
    return _mm_extract_epi16( e, 0 );
 #elif BLAZE_SSSE3_MODE
-   const __m128i b( _mm_hadd_epi16( a.value, a.value ) );
+   const __m128i b( _mm_hadd_epi16( (~a).value, (~a).value ) );
    const __m128i c( _mm_hadd_epi16( b, b ) );
    const __m128i d( _mm_hadd_epi16( c, c ) );
    return _mm_extract_epi16( d, 0 );
 #elif BLAZE_SSE2_MODE
-   return a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7];
+   return (~a)[0] + (~a)[1] + (~a)[2] + (~a)[3] + (~a)[4] + (~a)[5] + (~a)[6] + (~a)[7];
 #else
-   return a.value;
+   return (~a).value;
 #endif
 }
 //*************************************************************************************************
@@ -167,17 +171,18 @@ BLAZE_ALWAYS_INLINE int16_t sum( const SIMDint16& a ) noexcept
 // \param a The vector to be summed up.
 // \return The sum of all vector elements.
 */
-BLAZE_ALWAYS_INLINE const complex<int16_t> sum( const SIMDcint16& a ) noexcept
+template< typename T >  // Type of the SIMD element
+BLAZE_ALWAYS_INLINE const ValueType_<T> sum( const SIMDci16<T>& a ) noexcept
 {
 #if BLAZE_AVX512BW_MODE
-   return complex<int16_t>( a[0] + a[1] + a[ 2] + a[ 3] + a[ 4] + a[ 5] + a[ 6] + a[ 7] +
-                            a[8] + a[9] + a[10] + a[11] + a[12] + a[13] + a[14] + a[15] );
+   return complex<int16_t>( (~a)[0] + (~a)[1] + (~a)[ 2] + (~a)[ 3] + (~a)[ 4] + (~a)[ 5] + (~a)[ 6] + (~a)[ 7] +
+                            (~a)[8] + (~a)[9] + (~a)[10] + (~a)[11] + (~a)[12] + (~a)[13] + (~a)[14] + (~a)[15] );
 #elif BLAZE_AVX2_MODE
-   return complex<int16_t>( a[0] + a[1] + a[ 2] + a[ 3] + a[ 4] + a[ 5] + a[ 6] + a[ 7] );
+   return complex<int16_t>( (~a)[0] + (~a)[1] + (~a)[2] + (~a)[3] + (~a)[4] + (~a)[5] + (~a)[6] + (~a)[7] );
 #elif BLAZE_SSE2_MODE
-   return complex<int16_t>( a[0] + a[1] + a[2] + a[3] );
+   return complex<int16_t>( (~a)[0] + (~a)[1] + (~a)[2] + (~a)[3] );
 #else
-   return a.value;
+   return (~a).value;
 #endif
 }
 //*************************************************************************************************
@@ -198,11 +203,12 @@ BLAZE_ALWAYS_INLINE const complex<int16_t> sum( const SIMDcint16& a ) noexcept
 // \param a The vector to be summed up.
 // \return The sum of all vector elements.
 */
-BLAZE_ALWAYS_INLINE int32_t sum( const SIMDint32& a ) noexcept
+template< typename T >  // Type of the SIMD element
+BLAZE_ALWAYS_INLINE ValueType_<T> sum( const SIMDi32<T>& a ) noexcept
 {
 #if BLAZE_AVX512F_MODE
-   const __m256i low ( _mm512_castsi512_si256( a.value ) );
-   const __m256i high( _mm512_extracti64x4_epi64( a.value, 1 ) );
+   const __m256i low ( _mm512_castsi512_si256( (~a).value ) );
+   const __m256i high( _mm512_extracti64x4_epi64( (~a).value, 1 ) );
    const __m256i b   ( _mm256_hadd_epi32( low, high ) );
    const __m256i c   ( _mm256_hadd_epi32( b, b ) );
    const __m256i d   ( _mm256_hadd_epi32( c, c ) );
@@ -210,21 +216,21 @@ BLAZE_ALWAYS_INLINE int32_t sum( const SIMDint32& a ) noexcept
                                     , _mm256_castsi256_si128( d ) ) );
    return _mm_extract_epi32( e, 0 );
 #elif BLAZE_MIC_MODE
-   return _mm512_reduce_add_epi32( a.value );
+   return _mm512_reduce_add_epi32( (~a).value );
 #elif BLAZE_AVX2_MODE
-   const __m256i b( _mm256_hadd_epi32( a.value, a.value ) );
+   const __m256i b( _mm256_hadd_epi32( (~a).value, (~a).value ) );
    const __m256i c( _mm256_hadd_epi32( b, b ) );
    const __m128i d( _mm_add_epi32( _mm256_extracti128_si256( c, 1 )
                                  , _mm256_castsi256_si128( c ) ) );
    return _mm_extract_epi32( d, 0 );
 #elif BLAZE_SSSE3_MODE
-   const __m128i b( _mm_hadd_epi32( a.value, a.value ) );
+   const __m128i b( _mm_hadd_epi32( (~a).value, (~a).value ) );
    const __m128i c( _mm_hadd_epi32( b, b ) );
    return _mm_cvtsi128_si32( c );
 #elif BLAZE_SSE2_MODE
-   return a[0] + a[1] + a[2] + a[3];
+   return (~a)[0] + (~a)[1] + (~a)[2] + (~a)[3];
 #else
-   return a.value;
+   return (~a).value;
 #endif
 }
 //*************************************************************************************************
@@ -237,16 +243,17 @@ BLAZE_ALWAYS_INLINE int32_t sum( const SIMDint32& a ) noexcept
 // \param a The vector to be summed up.
 // \return The sum of all vector elements.
 */
-BLAZE_ALWAYS_INLINE const complex<int32_t> sum( const SIMDcint32& a ) noexcept
+template< typename T >  // Type of the SIMD element
+BLAZE_ALWAYS_INLINE const ValueType_<T> sum( const SIMDci32<T>& a ) noexcept
 {
 #if BLAZE_AVX512F_MODE || BLAZE_MIC_MODE
-   return complex<int32_t>( a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] );
+   return complex<int32_t>( (~a)[0] + (~a)[1] + (~a)[2] + (~a)[3] + (~a)[4] + (~a)[5] + (~a)[6] + (~a)[7] );
 #elif BLAZE_AVX2_MODE
-   return complex<int32_t>( a[0] + a[1] + a[2] + a[3] );
+   return complex<int32_t>( (~a)[0] + (~a)[1] + (~a)[2] + (~a)[3] );
 #elif BLAZE_SSE2_MODE
-   return complex<int32_t>( a[0] + a[1] );
+   return complex<int32_t>( (~a)[0] + (~a)[1] );
 #else
-   return a.value;
+   return (~a).value;
 #endif
 }
 //*************************************************************************************************
@@ -267,16 +274,17 @@ BLAZE_ALWAYS_INLINE const complex<int32_t> sum( const SIMDcint32& a ) noexcept
 // \param a The vector to be summed up.
 // \return The sum of all vector elements.
 */
-BLAZE_ALWAYS_INLINE int64_t sum( const SIMDint64& a ) noexcept
+template< typename T >  // Type of the SIMD element
+BLAZE_ALWAYS_INLINE ValueType_<T> sum( const SIMDi64<T>& a ) noexcept
 {
 #if BLAZE_AVX512F_MODE || BLAZE_MIC_MODE
-   return a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7];
+   return (~a)[0] + (~a)[1] + (~a)[2] + (~a)[3] + (~a)[4] + (~a)[5] + (~a)[6] + (~a)[7];
 #elif BLAZE_AVX2_MODE
-   return a[0] + a[1] + a[2] + a[3];
+   return (~a)[0] + (~a)[1] + (~a)[2] + (~a)[3];
 #elif BLAZE_SSE2_MODE
-   return a[0] + a[1];
+   return (~a)[0] + (~a)[1];
 #else
-   return a.value;
+   return (~a).value;
 #endif
 }
 //*************************************************************************************************
@@ -289,16 +297,17 @@ BLAZE_ALWAYS_INLINE int64_t sum( const SIMDint64& a ) noexcept
 // \param a The vector to be summed up.
 // \return The sum of all vector elements.
 */
-BLAZE_ALWAYS_INLINE const complex<int64_t> sum( const SIMDcint64& a ) noexcept
+template< typename T >  // Type of the SIMD element
+BLAZE_ALWAYS_INLINE const ValueType_<T> sum( const SIMDci64<T>& a ) noexcept
 {
 #if BLAZE_AVX512F_MODE || BLAZE_MIC_MODE
-   return complex<int64_t>( a[0] + a[1] + a[2] + a[3] );
+   return complex<int64_t>( (~a)[0] + (~a)[1] + (~a)[2] + (~a)[3] );
 #elif BLAZE_AVX2_MODE
-   return complex<int64_t>( a[0] + a[1] );
+   return complex<int64_t>( (~a)[0] + (~a)[1] );
 #elif BLAZE_SSE2_MODE
-   return a[0];
+   return (~a)[0];
 #else
-   return a.value;
+   return (~a).value;
 #endif
 }
 //*************************************************************************************************
