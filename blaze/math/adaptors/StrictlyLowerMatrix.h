af44f4c38fc2f1e88a071ac065822f9f9dfd37b8
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-08-18 10:46:52 +0200

    Add a specialization of the 'BandTrait' class template for 'StrictlyLowerMatrix'

--- blaze/math/adaptors/StrictlyLowerMatrix.h
+++ blaze/math/adaptors/StrictlyLowerMatrix.h
@@ -47,6 +47,7 @@
 #include <blaze/math/Forward.h>
 #include <blaze/math/shims/IsDefault.h>
 #include <blaze/math/traits/AddTrait.h>
+#include <blaze/math/traits/BandTrait.h>
 #include <blaze/math/traits/BinaryMapTrait.h>
 #include <blaze/math/traits/ColumnTrait.h>
 #include <blaze/math/traits/DeclDiagTrait.h>
@@ -2149,6 +2150,25 @@ template< typename MT, bool SO, bool DF >
 struct ColumnTrait< StrictlyLowerMatrix<MT,SO,DF> >
 {
    using Type = ColumnTrait_<MT>;
+};
+/*! \endcond */
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  BANDTRAIT SPECIALIZATIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+template< typename MT, bool SO, bool DF, ptrdiff_t... BIs >
+struct BandTrait< StrictlyLowerMatrix<MT,SO,DF>, BIs... >
+{
+   using Type = BandTrait_<MT,BIs...>;
 };
 /*! \endcond */
 //*************************************************************************************************
