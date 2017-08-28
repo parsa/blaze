0e5f8c5518de8dda5f5f8adf672f171d6ff21d10
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-08-28 10:09:13 +0200

    Add the 'Band', 'DenseBand', and 'SparseBand' alias templates

--- blaze/math/views/band/BaseTemplate.h
+++ blaze/math/views/band/BaseTemplate.h
@@ -40,6 +40,9 @@
 // Includes
 //*************************************************************************************************
 
+#include <blaze/math/typetraits/IsDenseMatrix.h>
+#include <blaze/math/typetraits/IsMatMatMultExpr.h>
+#include <blaze/system/TransposeFlag.h>
 #include <blaze/util/Types.h>
 
 
@@ -59,6 +62,317 @@ template< typename MT         // Type of the matrix
         , ptrdiff_t... BIs >  // Band indices
 class BandImpl
 {};
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  ALIAS DECLARATIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\defgroup band Band
+// \ingroup views
+*/
+/*!\brief Reference to a specific band of a dense or sparse matrix.
+// \ingroup band
+//
+// The Band template represents a reference to a specific band of a dense or sparse matrix
+// primitive. The type of the matrix is specified via the first template parameter. Additionally
+// it is possible to specify the band index at compile time as the second template parameter:
+
+   \code
+   template< typename MT, ptrdiff_t... BIs >
+   class Band;
+   \endcode
+
+//  - MT: specifies the type of the matrix primitive. Band can be used with every matrix primitive,
+//        but does not work with any matrix expression type.
+//  - BIs: specifies zero or one band indices. This optional parameter can be used to provide the
+//         band index at compile time.
+//
+//
+// \n \section band_setup Setup of Bands
+//
+// \image html band.png
+// \image latex band.eps "Band view" width=250pt
+//
+// A reference to a dense or sparse band can be created very conveniently via the \c band()
+// function. The band index must be in the range from \f$[1-M..N-1]\f$, where \c M is the total
+// number of rows and \c N is the total number of columns, and can be specified both at compile
+// time or at runtime:
+
+   \code
+   using DenseMatrixType = blaze::DynamicMatrix<double,blaze::rowMajor>;
+
+   DenseMatrixType A;
+   // ... Resizing and initialization
+
+   // Creating a reference to the 1st lower band of matrix A (compile time index)
+   blaze::Band<DenseMatrixType,-1L> band1 = band<-1L>( A );
+
+   // Creating a reference to the 2nd upper band of matrix A (runtime index)
+   blaze::Band<DenseMatrixType> band2 = band( A, 2L );
+   \endcode
+
+// The resulting reference can be treated as any other vector, i.e. it can be assigned to, it can
+// be copied from, and it can be used in arithmetic operations. By default, bands are considered
+// column vectors, but this setting can be changed via the \c defaultTransposeFlag switch. The
+// reference can also be used on both sides of an assignment: The band can either be used as an
+// alias to grant write access to a specific band of a matrix primitive on the left-hand side of
+// an assignment or to grant read-access to a specific band of a matrix primitive or expression
+// on the right-hand side of an assignment. The following example demonstrates this in detail:
+
+   \code
+   using DenseVectorType  = blaze::DynamicVector<double,blaze::rowVector>;
+   using SparseVectorType = blaze::CompressedVector<double,blaze::rowVector>;
+   using DenseMatrixType  = blaze::DynamicMatrix<double,blaze::rowMajor>;
+   using SparseMatrixType = blaze::CompressedMatrix<double,blaze::rowMajor>;
+
+   DenseVectorType  x;
+   SparseVectorType y;
+   DenseMatrixType  A, B;
+   SparseMatrixType C, D;
+   // ... Resizing and initialization
+
+   // Setting the 2nd upper band of matrix A to x
+   blaze::Band<DenseMatrixType> band2 = band( A, 2L );
+   band2 = x;
+
+   // Setting the 3rd upper band of matrix B to y
+   band( B, 3L ) = y;
+
+   // Setting x to the 2nd lower band of the result of the matrix multiplication
+   x = band( A * B, -2L );
+
+   // Setting y to the 2nd upper band of the result of the sparse matrix multiplication
+   y = band( C * D, 2L );
+   \endcode
+
+// The \c band() function can be used on any dense or sparse matrix, including expressions, as
+// illustrated by the source code example. However, bands cannot be instantiated for expression
+// types, but only for matrix primitives, respectively, i.e. for matrix types that offer write
+// access.
+//
+//
+// \n \section band_element_access Element access
+//
+// A dense or sparse band can be used like any other vector. For instance, the elements of a band
+// can be directly accessed with the subscript operator:
+
+   \code
+   using MatrixType = blaze::DynamicMatrix<double,blaze::rowMajor>;
+   MatrixType A;
+   // ... Resizing and initialization
+
+   // Creating a view on the 4th upper band of matrix A
+   blaze::Band<MatrixType> band4 = band( A, 4L );
+
+   // Setting the 1st element of the dense band, which corresponds
+   // to the 1st element in the 4th upper band of matrix A
+   band4[1] = 2.0;
+   \endcode
+
+// The numbering of the band elements is
+
+                             \f[\left(\begin{array}{*{5}{c}}
+                             0 & 1 & 2 & \cdots & N-1 \\
+                             \end{array}\right),\f]
+
+// where N is the number of elements of the referenced band. Alternatively, the elements of a band
+// can be traversed via iterators. Just as with vectors, in case of non-const band, \c begin() and
+// \c end() return an Iterator, which allows a manipulation of the non-zero values, in case of
+// constant band a ConstIterator is returned:
+
+   \code
+   using MatrixType = blaze::DynamicMatrix<int,blaze::rowMajor>;
+   using BandType   = blaze::Band<MatrixType>;
+
+   MatrixType A( 128UL, 256UL );
+   // ... Resizing and initialization
+
+   // Creating a reference to the 5th upper band of matrix A
+   BandType band5 = band( A, 5L );
+
+   for( BandType::Iterator it=band5.begin(); it!=band5.end(); ++it ) {
+      *it = ...;  // OK; Write access to the dense band value
+      ... = *it;  // OK: Read access to the dense band value.
+   }
+
+   for( BandType::ConstIterator it=band5.begin(); it!=band5.end(); ++it ) {
+      *it = ...;  // Compilation error: Assignment to the value via a ConstIterator is invalid.
+      ... = *it;  // OK: Read access to the dense band value.
+   }
+   \endcode
+
+   \code
+   using MatrixType = blaze::CompressedMatrix<int,blaze::rowMajor>;
+   using BandType   = blaze::Band<MatrixType>;
+
+   MatrixType A( 128UL, 256UL );
+   // ... Resizing and initialization
+
+   // Creating a reference to the 5th band of matrix A
+   BandType band5 = band( A, 5L );
+
+   for( BandType::Iterator it=band5.begin(); it!=band5.end(); ++it ) {
+      it->value() = ...;  // OK: Write access to the value of the non-zero element.
+      ... = it->value();  // OK: Read access to the value of the non-zero element.
+      it->index() = ...;  // Compilation error: The index of a non-zero element cannot be changed.
+      ... = it->index();  // OK: Read access to the index of the sparse element.
+   }
+
+   for( BandType::ConstIterator it=band5.begin(); it!=band5.end(); ++it ) {
+      it->value() = ...;  // Compilation error: Assignment to the value via a ConstIterator is invalid.
+      ... = it->value();  // OK: Read access to the value of the non-zero element.
+      it->index() = ...;  // Compilation error: The index of a non-zero element cannot be changed.
+      ... = it->index();  // OK: Read access to the index of the sparse element.
+   }
+   \endcode
+
+// \n \section sparse_band_element_insertion Element Insertion
+//
+// Inserting/accessing elements in a sparse band can be done by several alternative functions.
+// The following example demonstrates all options:
+
+   \code
+   using MatrixType = blaze::CompressedMatrix<double,blaze::rowMajor>;
+   MatrixType A( 10UL, 100UL );  // Non-initialized 10x100 matrix
+
+   using BandType = blaze::Band<MatrixType>;
+   BandType diag( band( A, 0L ) );  // Reference to the diagonal of A
+
+   // The subscript operator provides access to all possible elements of the sparse band,
+   // including the zero elements. In case the subscript operator is used to access an element
+   // that is currently not stored in the sparse band, the element is inserted into the band.
+   diag[42] = 2.0;
+
+   // The second operation for inserting elements is the set() function. In case the element
+   // is not contained in the band it is inserted into the band, if it is already contained in
+   // the band its value is modified.
+   diag.set( 45UL, -1.2 );
+
+   // An alternative for inserting elements into the band is the insert() function. However,
+   // it inserts the element only in case the element is not already contained in the band.
+   diag.insert( 50UL, 3.7 );
+   \endcode
+
+// \n \section band_common_operations Common Operations
+//
+// The current number of band elements can be obtained via the \c size() function, the current
+// capacity via the \c capacity() function, and the number of non-zero elements via the
+// \c nonZeros() function. However, since bands are references to specific bands of a matrix,
+// several operations are not possible, such as resizing and swapping. The following example
+// shows this by means of a dense band view:
+
+   \code
+   using MatrixType = blaze::DynamicMatrix<int,blaze::rowMajor>;
+   using BandType = blaze::Band<MatrixType>;
+
+   MatrixType A( 42UL, 42UL );
+   // ... Resizing and initialization
+
+   // Creating a reference to the 2nd upper band of matrix A
+   BandType band2 = band( A, 2L );
+
+   band2.size();          // Returns the number of elements in the band
+   band2.capacity();      // Returns the capacity of the band
+   band2.nonZeros();      // Returns the number of non-zero elements contained in the band
+
+   band2.resize( 84UL );  // Compilation error: Cannot resize a single band of a matrix
+
+   BandType band3 = band( A, 3L );
+   swap( band2, band3 );   // Compilation error: Swap operation not allowed
+   \endcode
+
+// \n \section band_arithmetic_operations Arithmetic Operations
+//
+// Both dense and sparse bands can be used in all arithmetic operations that any other dense or
+// sparse vector can be used in. The following example gives an impression of the use of dense
+// bands within arithmetic operations. All operations (addition, subtraction, multiplication,
+// scaling, ...) can be performed on all possible combinations of dense and sparse bands with
+// fitting element types:
+
+   \code
+   blaze::DynamicVector<double,blaze::columnVector> a( 2UL, 2.0 ), b;
+   blaze::CompressedVector<double,blaze::columnVector> c( 2UL );
+   c[1] = 3.0;
+
+   using DenseMatrix = blaze::DynamicMatrix<double,blaze::rowMajor>;
+   DenseMatrix A( 4UL, 2UL );  // Non-initialized 4x2 matrix
+
+   using BandType = blaze::Band<DenseMatrix>;
+   BandType band1( band( A, 1L ) );  // Reference to the 1st upper band of A
+   BandType diag ( band( A, 0L ) );  // Reference to the diagonal of A
+
+   band1[0] = 0.0;      // Manual initialization of the 1st upper band of A
+   diag = 1.0;          // Homogeneous initialization of the diagonal of A
+   band( A, -1L ) = a;  // Dense vector initialization of the 1st lower band of A
+   band( A, -2L ) = c;  // Sparse vector initialization of the 2nd lower band of A
+
+   b = diag + a;               // Dense vector/dense vector addition
+   b = c + band( A, -1L );     // Sparse vector/dense vector addition
+   b = diag * band( A, -2L );  // Component-wise vector multiplication
+
+   band( A, -1L ) *= 2.0;     // In-place scaling of the 1st upper band
+   b = band( A, -1L ) * 2.0;  // Scaling of the 1st upper band
+   b = 2.0 * band( A, -1L );  // Scaling of the 1st upper band
+
+   band( A, -2L ) += a;              // Addition assignment
+   band( A, -2L ) -= c;              // Subtraction assignment
+   band( A, -2L ) *= band( A, 0L );  // Multiplication assignment
+
+   double scalar = trans( c ) * band( A, -1L );  // Scalar/dot/inner product between two vectors
+
+   A = band( A, -1L ) * trans( c );  // Outer product between two vectors
+   \endcode
+*/
+template< typename MT         // Type of the matrix
+        , ptrdiff_t... BIs >  // Band indices
+using Band = BandImpl< MT
+                     , defaultTransposeFlag
+                     , IsDenseMatrix<MT>::value
+                     , IsMatMatMultExpr<MT>::value
+                     , BIs... >;
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Reference to a specific band of a dense matrix.
+// \ingroup band
+//
+// The DenseBand template represents a reference to a specific band of a dense matrix primitive.
+*/
+template< typename MT         // Type of the matrix
+        , ptrdiff_t... BIs >  // Band indices
+using DenseBand = BandImpl< MT
+                          , defaultTransposeFlag
+                          , true
+                          , IsMatMatMultExpr<MT>::value
+                          , BIs... >;
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Reference to a specific band of a sparse matrix.
+// \ingroup band
+//
+// The SparseBand template represents a reference to a specific band of a sparse matrix primitive.
+*/
+template< typename MT         // Type of the matrix
+        , ptrdiff_t... BIs >  // Band indices
+using SparseBand = BandImpl< MT
+                           , defaultTransposeFlag
+                           , false
+                           , IsMatMatMultExpr<MT>::value
+                           , BIs... >;
+/*! \endcond */
 //*************************************************************************************************
 
 } // namespace blaze
