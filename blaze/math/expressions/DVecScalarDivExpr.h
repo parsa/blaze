655e8c61314df8f5ed7f799980da0dec25c13c25
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-11-07 14:00:16 +0100

    Revert the alias templates in the vector/scalar division expression classes

--- blaze/math/expressions/DVecScalarDivExpr.h
+++ blaze/math/expressions/DVecScalarDivExpr.h
@@ -76,7 +76,6 @@
 #include <blaze/util/constraints/SameType.h>
 #include <blaze/util/EnableIf.h>
 #include <blaze/util/FunctionTrace.h>
-#include <blaze/util/IntegralConstant.h>
 #include <blaze/util/mpl/And.h>
 #include <blaze/util/mpl/If.h>
 #include <blaze/util/mpl/Or.h>
@@ -141,24 +140,27 @@ class DVecScalarDivExpr
    enum : bool { useAssign = IsComputation<VT>::value && RequiresEvaluation<VT>::value };
 
    /*! \cond BLAZE_INTERNAL */
-   //! Helper alias template for the explicit application of the SFINAE principle.
-   /*! The UseAssign alias is a helper alias for the selection of the serial evaluation strategy.
+   //! Helper structure for the explicit application of the SFINAE principle.
+   /*! The UseAssign struct is a helper struct for the selection of the serial evaluation strategy.
        In case the expression specific serial evaluation strategy is selected, the \a value is set
        to 1. Otherwise \a value is set to 0 and the default strategy is chosen. */
    template< typename LHS, typename RHS >
-   using UseAssign = BoolConstant< RHS::useAssign >;
+   struct UseAssign {
+      enum : bool { value = RHS::useAssign };
+   };
    /*! \endcond */
    //**********************************************************************************************
 
    //**Parallel evaluation strategy****************************************************************
    /*! \cond BLAZE_INTERNAL */
-   //! Helper alias template for the explicit application of the SFINAE principle.
-   /*! The UseSMPAssign alias is a helper alias for the selection of the parallel evaluation
+   //! Helper structure for the explicit application of the SFINAE principle.
+   /*! The UseSMPAssign struct is a helper struct for the selection of the parallel evaluation
        strategy. In case the expression specific parallel evaluation strategy is selected, the
        \a value is set to 1. Otherwise \a value is set to 0 and the default strategy is chosen. */
    template< typename LHS, typename RHS >
-   using UseSMPAssign =
-      BoolConstant< ( !LHS::smpAssignable || !RHS::smpAssignable ) && RHS::useAssign >;
+   struct UseSMPAssign {
+      enum : bool { value = ( !LHS::smpAssignable || !RHS::smpAssignable ) && RHS::useAssign };
+   };
    /*! \endcond */
    //**********************************************************************************************
 
