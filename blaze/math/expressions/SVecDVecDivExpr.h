d602a64d01868f802babdf45db63098ec7a5d505
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-11-07 14:01:14 +0100

    Revert the alias templates in the vector/vector division expression classes

--- blaze/math/expressions/SVecDVecDivExpr.h
+++ blaze/math/expressions/SVecDVecDivExpr.h
@@ -64,7 +64,6 @@
 #include <blaze/util/Assert.h>
 #include <blaze/util/EnableIf.h>
 #include <blaze/util/FunctionTrace.h>
-#include <blaze/util/IntegralConstant.h>
 #include <blaze/util/mpl/If.h>
 #include <blaze/util/mpl/Maximum.h>
 #include <blaze/util/Types.h>
@@ -127,12 +126,14 @@ class SVecDVecDivExpr
    enum : bool { useAssign = ( RequiresEvaluation<VT1>::value || RequiresEvaluation<VT2>::value ) };
 
    /*! \cond BLAZE_INTERNAL */
-   //! Helper alias template for the explicit application of the SFINAE principle.
-   /*! The UseSMPAssign alias is a helper alias for the selection of the parallel evaluation
+   //! Helper structure for the explicit application of the SFINAE principle.
+   /*! The UseSMPAssign struct is a helper struct for the selection of the parallel evaluation
        strategy. In case the expression specific parallel evaluation strategy is selected, the
        \a value is set to 1. Otherwise \a value is set to 0 and the default strategy is chosen. */
    template< typename LHS, typename RHS >
-   using UseAssign = BoolConstant< RHS::useAssign >;
+   struct UseAssign {
+      enum : bool { value = RHS::useAssign };
+   };
    /*! \endcond */
    //**********************************************************************************************
 
