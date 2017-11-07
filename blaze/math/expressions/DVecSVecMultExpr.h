ba4d0afc17a2f2ca1e19c37e70e832c9a6d36721
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-11-07 14:01:31 +0100

    Revert the alias templates in the vector/vector multiplication expression classes

--- blaze/math/expressions/DVecSVecMultExpr.h
+++ blaze/math/expressions/DVecSVecMultExpr.h
@@ -64,7 +64,6 @@
 #include <blaze/util/Assert.h>
 #include <blaze/util/EnableIf.h>
 #include <blaze/util/FunctionTrace.h>
-#include <blaze/util/IntegralConstant.h>
 #include <blaze/util/mpl/If.h>
 #include <blaze/util/mpl/Maximum.h>
 #include <blaze/util/Types.h>
@@ -129,12 +128,14 @@ class DVecSVecMultExpr
    enum : bool { useAssign = ( RequiresEvaluation<VT1>::value || RequiresEvaluation<VT2>::value ) };
 
    /*! \cond BLAZE_INTERNAL */
-   //! Helper alias template for the explicit application of the SFINAE principle.
-   /*! The UseAssign alias is a helper alias for the selection of the serial evaluation strategy.
+   //! Helper structure for the explicit application of the SFINAE principle.
+   /*! The UseAssign struct is a helper struct for the selection of the serial evaluation strategy.
        In case the expression specific serial evaluation strategy is selected, the \a value is set
        to 1. Otherwise \a value is set to 0 and the default strategy is chosen. */
    template< typename LHS, typename RHS >
-   using UseAssign = BoolConstant< RHS::useAssign >;
+   struct UseAssign {
+      enum : bool { value = RHS::useAssign };
+   };
    /*! \endcond */
    //**********************************************************************************************
 
