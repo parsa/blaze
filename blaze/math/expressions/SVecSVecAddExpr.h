e940e7634966cf7b9a24841adc1390550a33b5c1
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-11-07 12:27:51 +0100

    Replace all auxiliary nested structs in the vector/vector addition expression classes by alias templates

--- blaze/math/expressions/SVecSVecAddExpr.h
+++ blaze/math/expressions/SVecSVecAddExpr.h
@@ -64,6 +64,7 @@
 #include <blaze/util/DisableIf.h>
 #include <blaze/util/EnableIf.h>
 #include <blaze/util/FunctionTrace.h>
+#include <blaze/util/IntegralConstant.h>
 #include <blaze/util/mpl/If.h>
 #include <blaze/util/mpl/Maximum.h>
 #include <blaze/util/Types.h>
@@ -117,14 +118,12 @@ class SVecSVecAddExpr
 
    //**Parallel evaluation strategy****************************************************************
    /*! \cond BLAZE_INTERNAL */
-   //! Helper structure for the explicit application of the SFINAE principle.
-   /*! The UseSMPAssign struct is a helper struct for the selection of the parallel evaluation
+   //! Helper alias template for the explicit application of the SFINAE principle.
+   /*! The UseSMPAssign alias is a helper alias for the selection of the parallel evaluation
        strategy. In case the expression specific parallel evaluation strategy is selected, the
        \a value is set to 1. Otherwise \a value is set to 0 and the default strategy is chosen. */
    template< typename LHS, typename RHS >
-   struct UseSMPAssign {
-      enum : bool { value = LHS::smpAssignable };
-   };
+   using UseSMPAssign = BoolConstant< LHS::smpAssignable >;
    /*! \endcond */
    //**********************************************************************************************
 
