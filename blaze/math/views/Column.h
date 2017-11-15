1720c0055311094e48503e8fe64dc5fae937c3fd
Klaus Iglberger <klaus.iglberger@gmail.com>
2017-11-15 11:50:32 +0100

    Add overloads of the 'tryAdd()', 'trySub()', 'tryMult()', and 'tryDiv()' functions for the 'Column' class template

--- blaze/math/views/Column.h
+++ blaze/math/views/Column.h
@@ -1104,8 +1104,8 @@ inline bool isSame( const Column<MT1,SO,DF,SF1,CCAs1...>& a,
 //
 // \param sv The target column.
 // \param index The index of the element to be set.
-// \param value The value of the element to be set.
-// \return \a true in case the set operation would be successful, \a false if not.
+// \param value The value to be set to the element.
+// \return \a true in case the operation would be successful, \a false if not.
 //
 // This function must \b NOT be called explicitly! It is used internally for the performance
 // optimized evaluation of expression templates. Calling this function explicitly might result
@@ -1123,6 +1123,130 @@ inline bool trySet( const Column<MT,SO,DF,SF,CCAs...>& column, size_t index, con
    BLAZE_INTERNAL_ASSERT( index <= column.size(), "Invalid vector access index" );
 
    return trySet( column.operand(), index, column.column(), value );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Predict invariant violations by adding to a single element of a column.
+// \ingroup column
+//
+// \param sv The target column.
+// \param index The index of the element to be modified.
+// \param value The value to be added to the element.
+// \return \a true in case the operation would be successful, \a false if not.
+//
+// This function must \b NOT be called explicitly! It is used internally for the performance
+// optimized evaluation of expression templates. Calling this function explicitly might result
+// in erroneous results and/or in compilation errors. Instead of using this function use the
+// assignment operator.
+*/
+template< typename MT     // Type of the matrix
+        , bool SO         // Storage order
+        , bool DF         // Density flag
+        , bool SF         // Symmetry flag
+        , size_t... CCAs  // Compile time column arguments
+        , typename ET >   // Type of the element
+inline bool tryAdd( const Column<MT,SO,DF,SF,CCAs...>& column, size_t index, const ET& value )
+{
+   BLAZE_INTERNAL_ASSERT( index <= column.size(), "Invalid vector access index" );
+
+   return tryAdd( column.operand(), index, column.column(), value );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Predict invariant violations by subtracting from a single element of a column.
+// \ingroup column
+//
+// \param sv The target column.
+// \param index The index of the element to be modified.
+// \param value The value to be subtracted from the element.
+// \return \a true in case the operation would be successful, \a false if not.
+//
+// This function must \b NOT be called explicitly! It is used internally for the performance
+// optimized evaluation of expression templates. Calling this function explicitly might result
+// in erroneous results and/or in compilation errors. Instead of using this function use the
+// assignment operator.
+*/
+template< typename MT     // Type of the matrix
+        , bool SO         // Storage order
+        , bool DF         // Density flag
+        , bool SF         // Symmetry flag
+        , size_t... CCAs  // Compile time column arguments
+        , typename ET >   // Type of the element
+inline bool trySub( const Column<MT,SO,DF,SF,CCAs...>& column, size_t index, const ET& value )
+{
+   BLAZE_INTERNAL_ASSERT( index <= column.size(), "Invalid vector access index" );
+
+   return trySub( column.operand(), index, column.column(), value );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Predict invariant violations by scaling a single element of a column.
+// \ingroup column
+//
+// \param sv The target column.
+// \param index The index of the element to be modified.
+// \param value The factor for the element.
+// \return \a true in case the operation would be successful, \a false if not.
+//
+// This function must \b NOT be called explicitly! It is used internally for the performance
+// optimized evaluation of expression templates. Calling this function explicitly might result
+// in erroneous results and/or in compilation errors. Instead of using this function use the
+// assignment operator.
+*/
+template< typename MT     // Type of the matrix
+        , bool SO         // Storage order
+        , bool DF         // Density flag
+        , bool SF         // Symmetry flag
+        , size_t... CCAs  // Compile time column arguments
+        , typename ET >   // Type of the element
+inline bool tryMult( const Column<MT,SO,DF,SF,CCAs...>& column, size_t index, const ET& value )
+{
+   BLAZE_INTERNAL_ASSERT( index <= column.size(), "Invalid vector access index" );
+
+   return tryMult( column.operand(), index, column.column(), value );
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Predict invariant violations by scaling a single element of a column.
+// \ingroup column
+//
+// \param sv The target column.
+// \param index The index of the element to be modified.
+// \param value The divisor for the element.
+// \return \a true in case the operation would be successful, \a false if not.
+//
+// This function must \b NOT be called explicitly! It is used internally for the performance
+// optimized evaluation of expression templates. Calling this function explicitly might result
+// in erroneous results and/or in compilation errors. Instead of using this function use the
+// assignment operator.
+*/
+template< typename MT     // Type of the matrix
+        , bool SO         // Storage order
+        , bool DF         // Density flag
+        , bool SF         // Symmetry flag
+        , size_t... CCAs  // Compile time column arguments
+        , typename ET >   // Type of the element
+inline bool tryDiv( const Column<MT,SO,DF,SF,CCAs...>& column, size_t index, const ET& value )
+{
+   BLAZE_INTERNAL_ASSERT( index <= column.size(), "Invalid vector access index" );
+
+   return tryDiv( column.operand(), index, column.column(), value );
 }
 /*! \endcond */
 //*************************************************************************************************
