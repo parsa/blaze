482267224ce88a3eba9e32727c9ca248e7727604
Klaus Iglberger <klaus.iglberger@gmail.com>
2018-01-08 14:36:50 +0100

    Fix some memory leaks in the tests for the 'Submatrix' class template

--- blazetest/src/mathtest/submatrix/DenseAlignedTest1.cpp
+++ blazetest/src/mathtest/submatrix/DenseAlignedTest1.cpp
@@ -3385,7 +3385,7 @@ void DenseAlignedTest::testMultAssign()
 
       using AlignedPadded = blaze::CustomMatrix<int,aligned,padded,rowMajor>;
       std::unique_ptr<int[],blaze::Deallocate> memory( blaze::allocate<int>( 128UL ) );
-      AlignedPadded mat( blaze::allocate<int>( 128UL ), 8UL, 8UL, 16UL );
+      AlignedPadded mat( memory.get(), 8UL, 8UL, 16UL );
       randomize( mat, int(randmin), int(randmax) );
 
       sm1 *= mat;
@@ -3479,7 +3479,7 @@ void DenseAlignedTest::testMultAssign()
 
       using AlignedPadded = blaze::CustomMatrix<int,aligned,padded,columnMajor>;
       std::unique_ptr<int[],blaze::Deallocate> memory( blaze::allocate<int>( 128UL ) );
-      AlignedPadded mat( blaze::allocate<int>( 128UL ), 8UL, 8UL, 16UL );
+      AlignedPadded mat( memory.get(), 8UL, 8UL, 16UL );
       randomize( mat, int(randmin), int(randmax) );
 
       sm1 *= mat;
@@ -3705,7 +3705,7 @@ void DenseAlignedTest::testMultAssign()
 
       using AlignedPadded = blaze::CustomMatrix<int,aligned,padded,rowMajor>;
       std::unique_ptr<int[],blaze::Deallocate> memory( blaze::allocate<int>( 128UL ) );
-      AlignedPadded mat( blaze::allocate<int>( 128UL ), 8UL, 8UL, 16UL );
+      AlignedPadded mat( memory.get(), 8UL, 8UL, 16UL );
       randomize( mat, int(randmin), int(randmax) );
 
       sm1 *= mat;
@@ -3799,7 +3799,7 @@ void DenseAlignedTest::testMultAssign()
 
       using AlignedPadded = blaze::CustomMatrix<int,aligned,padded,columnMajor>;
       std::unique_ptr<int[],blaze::Deallocate> memory( blaze::allocate<int>( 128UL ) );
-      AlignedPadded mat( blaze::allocate<int>( 128UL ), 8UL, 8UL, 16UL );
+      AlignedPadded mat( memory.get(), 8UL, 8UL, 16UL );
       randomize( mat, int(randmin), int(randmax) );
 
       sm1 *= mat;
