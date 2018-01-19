ee2fa26575bfea4793ee2d8974a1a8e3398ae50a
Klaus Iglberger <klaus.iglberger@gmail.com>
2018-01-19 14:57:20 +0100

    Restrict the selection of BLAS kernels for scaled dense matrix/dense vector multiplications to contiguous vectors and matrices

--- blaze/math/expressions/DMatDVecMultExpr.h
+++ blaze/math/expressions/DMatDVecMultExpr.h
@@ -2769,9 +2769,9 @@ class DVecScalarMultExpr< DMatDVecMultExpr<MT,VT>, ST, false >
    template< typename T1, typename T2, typename T3, typename T4 >
    struct UseBlasKernel {
       enum : bool { value = BLAZE_BLAS_MODE && BLAZE_USE_BLAS_MATRIX_VECTOR_MULTIPLICATION &&
-                            HasMutableDataAccess<T1>::value &&
-                            HasConstDataAccess<T2>::value &&
-                            HasConstDataAccess<T3>::value &&
+                            IsContiguous<T1>::value && HasMutableDataAccess<T1>::value &&
+                            IsContiguous<T2>::value && HasConstDataAccess<T2>::value &&
+                            IsContiguous<T3>::value && HasConstDataAccess<T3>::value &&
                             !IsDiagonal<T2>::value &&
                             T1::simdEnabled && T2::simdEnabled && T3::simdEnabled &&
                             IsBLASCompatible< ElementType_<T1> >::value &&
--- blaze/math/expressions/TDVecDMatMultExpr.h
+++ blaze/math/expressions/TDVecDMatMultExpr.h
@@ -2518,9 +2518,9 @@ class DVecScalarMultExpr< TDVecDMatMultExpr<VT,MT>, ST, true >
    template< typename T1, typename T2, typename T3, typename T4 >
    struct UseBlasKernel {
       enum : bool { value = BLAZE_BLAS_MODE && BLAZE_USE_BLAS_MATRIX_VECTOR_MULTIPLICATION &&
-                            HasMutableDataAccess<T1>::value &&
-                            HasConstDataAccess<T2>::value &&
-                            HasConstDataAccess<T3>::value &&
+                            IsContiguous<T1>::value && HasMutableDataAccess<T1>::value &&
+                            IsContiguous<T2>::value && HasConstDataAccess<T2>::value &&
+                            IsContiguous<T3>::value && HasConstDataAccess<T3>::value &&
                             !IsDiagonal<T3>::value &&
                             T1::simdEnabled && T2::simdEnabled && T3::simdEnabled &&
                             IsBLASCompatible< ElementType_<T1> >::value &&
