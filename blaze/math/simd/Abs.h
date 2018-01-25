2bd9ad18dfc50b910a15010ce6a71c4ff6140010
Klaus Iglberger <klaus.iglberger@gmail.com>
2018-01-25 11:45:45 +0100

    Add SIMD 'abs()' support for 'float' and 'double' on SSE2 and AVX architectures

--- blaze/math/simd/Abs.h
+++ blaze/math/simd/Abs.h
@@ -196,13 +196,23 @@ BLAZE_ALWAYS_INLINE const SIMDint64 abs( const SIMDint64& a ) noexcept
 // \param a The vector of single precision floating point values \f$[-1..1]\f$.
 // \return The resulting vector.
 //
-// This operation is only available for MIC and AVX-512.
+// This operation is only available for SSE2, AVX, MIC, and AVX-512.
 */
 template< typename T >  // Type of the operand
 BLAZE_ALWAYS_INLINE const SIMDfloat abs( const SIMDf32<T>& a ) noexcept
 #if BLAZE_AVX512F_MODE || BLAZE_MIC_MODE
 {
    return _mm512_abs_ps( (~a).eval().value );
+}
+#elif BLAZE_AVX_MODE
+{
+   const __m256 mask( _mm256_castsi256_ps( _mm256_set1_epi32( 0x80000000 ) ) );
+   return _mm256_andnot_ps( mask, (~a).eval().value );
+}
+#elif BLAZE_SSE2_MODE
+{
+   const __m128 mask( _mm_castsi128_ps( _mm_set1_epi32( 0x80000000 ) ) );
+   return _mm_andnot_ps( mask, (~a).eval().value );
 }
 #else
 = delete;
@@ -225,13 +235,24 @@ BLAZE_ALWAYS_INLINE const SIMDfloat abs( const SIMDf32<T>& a ) noexcept
 // \param a The vector of double precision floating point values \f$[-1..1]\f$.
 // \return The resulting vector.
 //
-// This operation is only available for MIC and AVX-512.
+// This operation is only available for SSE2, AVX, MIC, and AVX-512.
 */
 template< typename T >  // Type of the operand
 BLAZE_ALWAYS_INLINE const SIMDdouble abs( const SIMDf64<T>& a ) noexcept
 #if BLAZE_AVX512F_MODE || BLAZE_MIC_MODE
 {
    return _mm512_abs_pd( (~a).eval().value );
+}
+#elif BLAZE_AVX_MODE
+{
+   const __m256d mask( _mm256_castsi256_pd(
+      _mm256_set_epi32( 0x80000000, 0x0, 0x80000000, 0x0, 0x80000000, 0x0, 0x80000000, 0x0 ) ) );
+   return _mm256_andnot_pd( mask, (~a).eval().value );
+}
+#elif BLAZE_SSE2_MODE
+{
+   const __m128d mask( _mm_castsi128_pd( _mm_set_epi32( 0x80000000, 0x0, 0x80000000, 0x0 ) ) );
+   return _mm_andnot_pd( mask, (~a).eval().value );
 }
 #else
 = delete;
