7854e6d3dda8fbbe893159ca38328c154757c54e
Klaus Iglberger <klaus.iglberger@gmail.com>
2018-03-27 15:31:02 +0200

    Add SIMD optimized dense vector/dense vector 'equal()' functions

--- blaze/math/expressions/DVecDVecEqualExpr.h
+++ blaze/math/expressions/DVecDVecEqualExpr.h
@@ -44,10 +44,54 @@
 #include <blaze/math/expressions/DenseVector.h>
 #include <blaze/math/RelaxationFlag.h>
 #include <blaze/math/shims/Equal.h>
+#include <blaze/math/SIMD.h>
+#include <blaze/math/typetraits/HasSIMDEqual.h>
+#include <blaze/math/typetraits/IsPadded.h>
+#include <blaze/system/Optimizations.h>
+#include <blaze/util/DisableIf.h>
+#include <blaze/util/EnableIf.h>
 #include <blaze/util/Types.h>
+#include <blaze/util/typetraits/RemoveReference.h>
 
 
 namespace blaze {
+
+//=================================================================================================
+//
+//  CLASS DEFINITION
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Auxiliary helper struct for the dense vector/dense vector equality comparison.
+// \ingroup dense_vector
+*/
+template< typename VT1    // Type of the left-hand side dense vector
+        , typename VT2 >  // Type of the right-hand side dense vector
+struct DVecDVecEqualExprHelper
+{
+   //**Type definitions****************************************************************************
+   //! Composite type of the left-hand side dense vector expression.
+   using CT1 = RemoveReference_t< CompositeType_t<VT1> >;
+
+   //! Composite type of the right-hand side dense vector expression.
+   using CT2 = RemoveReference_t< CompositeType_t<VT2> >;
+   //**********************************************************************************************
+
+   //**********************************************************************************************
+   static constexpr bool value =
+      ( useOptimizedKernels &&
+        CT1::simdEnabled &&
+        CT2::simdEnabled &&
+        HasSIMDEqual_v< ElementType_t<CT1>, ElementType_t<CT2> > );
+   //**********************************************************************************************
+};
+/*! \endcond */
+//*************************************************************************************************
+
+
+
 
 //=================================================================================================
 //
@@ -57,7 +101,7 @@ namespace blaze {
 
 //*************************************************************************************************
 /*! \cond BLAZE_INTERNAL */
-/*!\brief Equality check of two dense vectors.
+/*!\brief Default equality check of two dense vectors.
 // \ingroup dense_vector
 //
 // \param a The left-hand side dense vector for the comparison.
@@ -73,7 +117,8 @@ template< bool RF       // Relaxation flag
         , bool TF1      // Transpose flag of the left-hand side dense vector
         , typename VT2  // Type of the right-hand side dense vector
         , bool TF2 >    // Transpose flag of the right-hand side dense vector
-inline bool equal( const DenseVector<VT1,TF1>& lhs, const DenseVector<VT2,TF2>& rhs )
+inline DisableIf_t< DVecDVecEqualExprHelper<VT1,VT2>::value, bool >
+   equal( const DenseVector<VT1,TF1>& lhs, const DenseVector<VT2,TF2>& rhs )
 {
    using CT1 = CompositeType_t<VT1>;
    using CT2 = CompositeType_t<VT2>;
@@ -89,6 +134,70 @@ inline bool equal( const DenseVector<VT1,TF1>& lhs, const DenseVector<VT2,TF2>& 
    // type are converted to the higher-order data type within the equal function.
    for( size_t i=0UL; i<a.size(); ++i )
       if( !equal<RF>( a[i], b[i] ) ) return false;
+   return true;
+}
+/*! \endcond */
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief SIMD optimized equality check of two dense vectors.
+// \ingroup dense_vector
+//
+// \param a The left-hand side dense vector for the comparison.
+// \param b The right-hand side dense vector for the comparison.
+// \return \a true if the two vectors are equal, \a false if not.
+//
+// Equal function for the comparison of two dense vectors. Due to the limited machine accuracy,
+// a direct comparison of two floating point numbers should be avoided. This function offers the
+// possibility to compare two floating-point vectors with a certain accuracy margin.
+*/
+template< bool RF       // Relaxation flag
+        , typename VT1  // Type of the left-hand side dense vector
+        , bool TF1      // Transpose flag of the left-hand side dense vector
+        , typename VT2  // Type of the right-hand side dense vector
+        , bool TF2 >    // Transpose flag of the right-hand side dense vector
+inline EnableIf_t< DVecDVecEqualExprHelper<VT1,VT2>::value, bool >
+   equal( const DenseVector<VT1,TF1>& lhs, const DenseVector<VT2,TF2>& rhs )
+{
+   using CT1 = CompositeType_t<VT1>;
+   using CT2 = CompositeType_t<VT2>;
+
+   // Early exit in case the vector sizes don't match
+   if( (~lhs).size() != (~rhs).size() ) return false;
+
+   // Evaluation of the two dense vector operands
+   CT1 a( ~lhs );
+   CT2 b( ~rhs );
+
+   constexpr size_t SIMDSIZE = SIMDTrait< ElementType_t<VT1> >::size;
+   constexpr bool remainder( !usePadding || !IsPadded_v<VT1> || !IsPadded_v<VT2> );
+
+   const size_t N( a.size() );
+
+   const size_t ipos( ( remainder )?( N & size_t(-SIMDSIZE) ):( N ) );
+   BLAZE_INTERNAL_ASSERT( !remainder || ( N - ( N % SIMDSIZE ) ) == ipos, "Invalid end calculation" );
+
+   size_t i( 0UL );
+
+   for( ; (i+SIMDSIZE*3UL) < ipos; i+=SIMDSIZE*4UL ) {
+      if( !equal<RF>( a.load(i             ), b.load(i             ) ) ) return false;
+      if( !equal<RF>( a.load(i+SIMDSIZE    ), b.load(i+SIMDSIZE    ) ) ) return false;
+      if( !equal<RF>( a.load(i+SIMDSIZE*2UL), b.load(i+SIMDSIZE*2UL) ) ) return false;
+      if( !equal<RF>( a.load(i+SIMDSIZE*3UL), b.load(i+SIMDSIZE*3UL) ) ) return false;
+   }
+   for( ; (i+SIMDSIZE) < ipos; i+=SIMDSIZE*2UL ) {
+      if( !equal<RF>( a.load(i         ), b.load(i         ) ) ) return false;
+      if( !equal<RF>( a.load(i+SIMDSIZE), b.load(i+SIMDSIZE) ) ) return false;
+   }
+   for( ; i<ipos; i+=SIMDSIZE ) {
+      if( !equal<RF>( a.load(i), b.load(i) ) ) return false;
+   }
+   for( ; remainder && i<N; ++i ) {
+      if( !equal<RF>( a[i], b[i] ) ) return false;
+   }
+
    return true;
 }
 /*! \endcond */
