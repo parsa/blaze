69f6c022916cacd7b82e64d7d4c3312b9be59ff1
Klaus Iglberger <klaus.iglberger@gmail.com>
2018-05-18 15:10:34 +0200

    Add the 'HasConj' type trait

--- blaze/math/TypeTraits.h
+++ blaze/math/TypeTraits.h
@@ -43,6 +43,7 @@
 #include <blaze/math/typetraits/HasAbs.h>
 #include <blaze/math/typetraits/HasAdd.h>
 #include <blaze/math/typetraits/HasCeil.h>
+#include <blaze/math/typetraits/HasConj.h>
 #include <blaze/math/typetraits/HasConstDataAccess.h>
 #include <blaze/math/typetraits/HasDiv.h>
 #include <blaze/math/typetraits/HasFloor.h>
