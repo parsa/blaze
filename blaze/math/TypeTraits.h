d0e9796ffc8c9e558d7ec153dacc20a07ddbd95d
Klaus Iglberger <klaus.iglberger@gmail.com>
2018-05-18 15:07:03 +0200

    Add the 'HasFloor' type trait

--- blaze/math/TypeTraits.h
+++ blaze/math/TypeTraits.h
@@ -44,6 +44,7 @@
 #include <blaze/math/typetraits/HasAdd.h>
 #include <blaze/math/typetraits/HasConstDataAccess.h>
 #include <blaze/math/typetraits/HasDiv.h>
+#include <blaze/math/typetraits/HasFloor.h>
 #include <blaze/math/typetraits/HasMax.h>
 #include <blaze/math/typetraits/HasMin.h>
 #include <blaze/math/typetraits/HasMult.h>
