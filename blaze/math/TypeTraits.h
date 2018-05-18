9a649b8425450b1ace87ba9932f0e32680c584c7
Klaus Iglberger <klaus.iglberger@gmail.com>
2018-05-18 15:35:31 +0200

    Add the 'HasErf' type trait

--- blaze/math/TypeTraits.h
+++ blaze/math/TypeTraits.h
@@ -55,6 +55,7 @@
 #include <blaze/math/typetraits/HasCos.h>
 #include <blaze/math/typetraits/HasCosh.h>
 #include <blaze/math/typetraits/HasDiv.h>
+#include <blaze/math/typetraits/HasErf.h>
 #include <blaze/math/typetraits/HasExp.h>
 #include <blaze/math/typetraits/HasExp2.h>
 #include <blaze/math/typetraits/HasExp10.h>
