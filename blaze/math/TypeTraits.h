a8e71e9486287e792987ddbf9222f3241eadd516
Klaus Iglberger <klaus.iglberger@gmail.com>
2018-05-18 15:07:38 +0200

    Add the 'HasCeil' type trait

--- blaze/math/TypeTraits.h
+++ blaze/math/TypeTraits.h
@@ -42,6 +42,7 @@
 
 #include <blaze/math/typetraits/HasAbs.h>
 #include <blaze/math/typetraits/HasAdd.h>
+#include <blaze/math/typetraits/HasCeil.h>
 #include <blaze/math/typetraits/HasConstDataAccess.h>
 #include <blaze/math/typetraits/HasDiv.h>
 #include <blaze/math/typetraits/HasFloor.h>
