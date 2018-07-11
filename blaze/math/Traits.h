99d742fd51e3f5b9892b81ac643e349afaffd10b
Klaus Iglberger <klaus.iglberger@gmail.com>
2018-07-11 20:20:55 +0200

    Remove the 'EvalExprTrait' class template

--- blaze/math/Traits.h
+++ blaze/math/Traits.h
@@ -69,7 +69,6 @@
 #include <blaze/math/traits/DivTrait.h>
 #include <blaze/math/traits/ElementsExprTrait.h>
 #include <blaze/math/traits/ElementsTrait.h>
-#include <blaze/math/traits/EvalExprTrait.h>
 #include <blaze/math/traits/ImagTrait.h>
 #include <blaze/math/traits/MultExprTrait.h>
 #include <blaze/math/traits/MultTrait.h>
