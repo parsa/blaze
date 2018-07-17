69d05d003f4b1bb12919ee3975cc1847f3757c25
Klaus Iglberger <klaus.iglberger@gmail.com>
2018-07-17 09:43:03 +0200

    Remove the 'RealTrait' class template

--- blaze/math/Traits.h
+++ blaze/math/Traits.h
@@ -57,7 +57,6 @@
 #include <blaze/math/traits/ImagTrait.h>
 #include <blaze/math/traits/MapTrait.h>
 #include <blaze/math/traits/MultTrait.h>
-#include <blaze/math/traits/RealTrait.h>
 #include <blaze/math/traits/RowsTrait.h>
 #include <blaze/math/traits/RowTrait.h>
 #include <blaze/math/traits/SchurTrait.h>
