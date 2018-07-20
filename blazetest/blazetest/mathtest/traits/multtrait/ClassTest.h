c76c599303ecd4e5a6da25f3c3eba8ad64920876
Klaus Iglberger <klaus.iglberger@gmail.com>
2018-07-20 11:10:37 +0200

    Add a test for the 'MultTrait' class template

---
+++ blazetest/blazetest/mathtest/traits/multtrait/ClassTest.h
@@ -0,0 +1,140 @@
+//=================================================================================================
+/*!
+//  \file blazetest/mathtest/traits/multtrait/ClassTest.h
+//  \brief Header file for the MultTrait class test
+//
+//  Copyright (C) 2012-2018 Klaus Iglberger - All Rights Reserved
+//
+//  This file is part of the Blaze library. You can redistribute it and/or modify it under
+//  the terms of the New (Revised) BSD License. Redistribution and use in source and binary
+//  forms, with or without modification, are permitted provided that the following conditions
+//  are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this list of
+//     conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright notice, this list
+//     of conditions and the following disclaimer in the documentation and/or other materials
+//     provided with the distribution.
+//  3. Neither the names of the Blaze development group nor the names of its contributors
+//     may be used to endorse or promote products derived from this software without specific
+//     prior written permission.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+//  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+//  OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+//  SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+//  TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+//  BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+//  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+//  DAMAGE.
+*/
+//=================================================================================================
+
+#ifndef _BLAZETEST_MATHTEST_TRAITS_MULTTRAIT_CLASSTEST_H_
+#define _BLAZETEST_MATHTEST_TRAITS_MULTTRAIT_CLASSTEST_H_
+
+
+namespace blazetest {
+
+namespace mathtest {
+
+namespace traits {
+
+namespace multtrait {
+
+//=================================================================================================
+//
+//  CLASS DEFINITION
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\brief Auxiliary class for all tests of the MultTrait class template.
+//
+// This class represents a test suite for the MultTrait class template. It performs a series of
+// mainly compile time tests to guarantee the correctness of the MultTrait.
+*/
+class ClassTest
+{
+ public:
+   //**Constructors********************************************************************************
+   /*!\name Constructors */
+   //@{
+   explicit ClassTest();
+   // No explicitly declared copy constructor.
+   //@}
+   //**********************************************************************************************
+
+   //**Destructor**********************************************************************************
+   // No explicitly declared destructor.
+   //**********************************************************************************************
+
+ private:
+   //**Test functions******************************************************************************
+   /*!\name Test functions */
+   //@{
+   void testScalarScalarMultiplication();
+   void testVectorScalarMultiplication();
+   void testScalarVectorMultiplication();
+   void testMatrixScalarMultiplication();
+   void testScalarMatrixMultiplication();
+   void testVectorVectorMultiplication();
+   void testInnerProduct();
+   void testOuterProduct();
+   void testMatrixVectorMultiplication();
+   void testVectorMatrixMultiplication();
+   void testMatrixMatrixMultiplication();
+   //@}
+   //**********************************************************************************************
+};
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  GLOBAL TEST FUNCTIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*!\brief Testing the MultTrait class template.
+//
+// \return void
+*/
+void runTest()
+{
+   ClassTest();
+}
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  MACRO DEFINITIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+/*!\brief Macro for the execution of the MultTrait class test.
+*/
+#define RUN_MULTTRAIT_CLASS_TEST \
+   blazetest::mathtest::traits::multtrait::runTest()
+/*! \endcond */
+//*************************************************************************************************
+
+} // namespace multtrait
+
+} // namespace traits
+
+} // namespace mathtest
+
+} // namespace blazetest
+
+#endif
