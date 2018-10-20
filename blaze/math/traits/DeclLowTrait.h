dc1ad32092582b32c2ce036b8031e938a68dc92e
Klaus Iglberger <klaus.iglberger@gmail.com>
2018-10-20 16:12:05 +0200

    Improve the specializations for the 'DeclLowTrait' class template

--- blaze/math/traits/DeclLowTrait.h
+++ blaze/math/traits/DeclLowTrait.h
@@ -188,7 +188,7 @@ struct DeclLowTraitEval< MT
                                        Size_v<MT,1UL> == DefaultSize_v ||
                                        Size_v<MT,0UL> == Size_v<MT,1UL> ) > >
 {
-   using Type = LowerMatrix<MT>;
+   using Type = LowerMatrix<typename MT::ResultType>;
 };
 /*! \endcond */
 //*************************************************************************************************
