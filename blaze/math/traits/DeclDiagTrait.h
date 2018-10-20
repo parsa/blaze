dfe0725cbdabf9d4b8eb4463640399d433cb1786
Klaus Iglberger <klaus.iglberger@gmail.com>
2018-10-20 16:13:37 +0200

    Improve the specializations for the 'DeclDiagTrait' class template

--- blaze/math/traits/DeclDiagTrait.h
+++ blaze/math/traits/DeclDiagTrait.h
@@ -188,7 +188,7 @@ struct DeclDiagTraitEval< MT
                                         Size_v<MT,1UL> == DefaultSize_v ||
                                         Size_v<MT,0UL> == Size_v<MT,1UL> ) > >
 {
-   using Type = DiagonalMatrix<MT>;
+   using Type = DiagonalMatrix<typename MT::ResultType>;
 };
 /*! \endcond */
 //*************************************************************************************************
