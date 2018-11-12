6b7039aa11080243b585140132251e0963bfe8ad
Klaus Iglberger <klaus.iglberger@gmail.com>
2018-11-12 14:45:50 +0100

    Prevent the use of the 'DiagonalMatrix' adaptor with uniform matrices

--- blaze/math/adaptors/diagonalmatrix/Dense.h
+++ blaze/math/adaptors/diagonalmatrix/Dense.h
@@ -53,6 +53,7 @@
 #include <blaze/math/constraints/Square.h>
 #include <blaze/math/constraints/StorageOrder.h>
 #include <blaze/math/constraints/Symmetric.h>
+#include <blaze/math/constraints/Uniform.h>
 #include <blaze/math/constraints/Upper.h>
 #include <blaze/math/dense/DenseMatrix.h>
 #include <blaze/math/dense/InitializerMatrix.h>
@@ -821,6 +822,7 @@ class DiagonalMatrix<MT,SO,true>
    BLAZE_CONSTRAINT_MUST_NOT_BE_CONST                ( MT );
    BLAZE_CONSTRAINT_MUST_NOT_BE_VOLATILE             ( MT );
    BLAZE_CONSTRAINT_MUST_NOT_BE_EXPRESSION_TYPE      ( MT );
+   BLAZE_CONSTRAINT_MUST_NOT_BE_UNIFORM_TYPE         ( MT );
    BLAZE_CONSTRAINT_MUST_NOT_BE_SYMMETRIC_MATRIX_TYPE( MT );
    BLAZE_CONSTRAINT_MUST_NOT_BE_HERMITIAN_MATRIX_TYPE( MT );
    BLAZE_CONSTRAINT_MUST_NOT_BE_LOWER_MATRIX_TYPE    ( MT );
