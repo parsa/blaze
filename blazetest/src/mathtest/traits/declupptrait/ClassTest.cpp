a07755024da5264493ce43ec93409ddf6044e718
Klaus Iglberger <klaus.iglberger@gmail.com>
2018-12-22 06:26:17 +0100

    Extend the 'DeclUppTrait' class test for the 'ZeroMatrix' class template

--- blazetest/src/mathtest/traits/declupptrait/ClassTest.cpp
+++ blazetest/src/mathtest/traits/declupptrait/ClassTest.cpp
@@ -57,6 +57,7 @@
 #include <blaze/math/UniLowerMatrix.h>
 #include <blaze/math/UniUpperMatrix.h>
 #include <blaze/math/UpperMatrix.h>
+#include <blaze/math/ZeroMatrix.h>
 #include <blaze/util/Complex.h>
 #include <blaze/util/typetraits/Decay.h>
 #include <blaze/util/typetraits/IsSame.h>
@@ -195,7 +196,7 @@ void ClassTest::testMatrixDeclUpp()
    {
       {
          using MT = UniformMatrix<int,rowMajor>;
-         using RT = UpperMatrix< DynamicMatrix<int,rowMajor> >;
+         using RT = UpperMatrix< UniformMatrix<int,rowMajor> >;
          static_assert( IsSame_v< DeclUppTrait_t<MT>, RT >, "Non-matching type detected" );
 
          using Expr = Decay_t< decltype( declupp( std::declval<MT>() ) ) >;
@@ -203,7 +204,7 @@ void ClassTest::testMatrixDeclUpp()
       }
       {
          using MT = UniformMatrix<int,columnMajor>;
-         using RT = UpperMatrix< DynamicMatrix<int,columnMajor> >;
+         using RT = UpperMatrix< UniformMatrix<int,columnMajor> >;
          static_assert( IsSame_v< DeclUppTrait_t<MT>, RT >, "Non-matching type detected" );
 
          using Expr = Decay_t< decltype( declupp( std::declval<MT>() ) ) >;
@@ -256,6 +257,26 @@ void ClassTest::testMatrixDeclUpp()
       {
          using MT = IdentityMatrix<int,columnMajor>;
          using RT = IdentityMatrix<int,columnMajor>;
+         static_assert( IsSame_v< DeclUppTrait_t<MT>, RT >, "Non-matching type detected" );
+
+         using Expr = Decay_t< decltype( declupp( std::declval<MT>() ) ) >;
+         static_assert( StorageOrder_v<Expr> == StorageOrder_v<RT>, "Non-matching storage order detected" );
+      }
+   }
+
+   // ZeroMatrix
+   {
+      {
+         using MT = ZeroMatrix<int,rowMajor>;
+         using RT = UpperMatrix< ZeroMatrix<int,rowMajor> >;
+         static_assert( IsSame_v< DeclUppTrait_t<MT>, RT >, "Non-matching type detected" );
+
+         using Expr = Decay_t< decltype( declupp( std::declval<MT>() ) ) >;
+         static_assert( StorageOrder_v<Expr> == StorageOrder_v<RT>, "Non-matching storage order detected" );
+      }
+      {
+         using MT = ZeroMatrix<int,columnMajor>;
+         using RT = UpperMatrix< ZeroMatrix<int,columnMajor> >;
          static_assert( IsSame_v< DeclUppTrait_t<MT>, RT >, "Non-matching type detected" );
 
          using Expr = Decay_t< decltype( declupp( std::declval<MT>() ) ) >;
