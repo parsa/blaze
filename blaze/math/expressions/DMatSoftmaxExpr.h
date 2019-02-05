62f83f19c2a9738dde47189cb427107578f23d26
Klaus Iglberger <klaus.iglberger@gmail.com>
2019-02-05 13:31:06 +0100

    Add a 'softmax()' overload with support for row- and columnwise evaluation

--- blaze/math/expressions/DMatSoftmaxExpr.h
+++ blaze/math/expressions/DMatSoftmaxExpr.h
@@ -41,6 +41,10 @@
 //*************************************************************************************************
 
 #include <blaze/math/expressions/DenseMatrix.h>
+#include <blaze/math/ReductionFlag.h>
+#include <blaze/math/views/Check.h>
+#include <blaze/math/views/Column.h>
+#include <blaze/math/views/Row.h>
 
 
 namespace blaze {
@@ -61,6 +65,23 @@ namespace blaze {
 // This function computes the softmax function (i.e. the normalized exponential function) for
 // the given dense matrix \a dm (see also https://en.wikipedia.org/wiki/Softmax_function). The
 // resulting dense matrix consists of real values in the range (0..1], which add up to 1.
+
+   \code
+   // Creating the matrix
+   //    ( 1  2  3 )
+   //    ( 4  1  2 )
+   //    ( 3  4  1 )
+   blaze::StaticMatrix<double,3UL,3UL> A{ { 1.0, 2.0, 3.0 }
+                                        , { 4.0, 1.0, 2.0 }
+                                        , { 3.0, 4.0, 1.0 } };
+
+   // Computing the total softmax of A (sum(B) == 1)
+   //    ( 0.0157764  0.0428847  0.116573  )
+   //    ( 0.316878   0.0157764  0.0428847 )
+   //    ( 0.116573   0.316878   0.0157764 )
+   blaze::StaticMatrix<double,3UL,3UL> B;
+   B = softmax( A );
+   \endcode
 */
 template< typename MT  // Type of the dense matrix
         , bool SO >    // Storage order
@@ -69,6 +90,69 @@ auto softmax( const DenseMatrix<MT,SO>& dm )
    auto tmp( evaluate( exp( ~dm ) ) );
    const auto scalar( sum( tmp ) );
    tmp /= scalar;
+   return tmp;
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Computes the row-/columnwise softmax function for the given dense matrix.
+// \ingroup dense_matrix
+//
+// \param dm The given dense matrix for the softmax computation.
+// \return The resulting matrix.
+//
+// This function computes the row-/columnwise softmax function (i.e. the normalized exponential
+// function) for the given dense matrix \a dm (see also https://en.wikipedia.org/wiki/Softmax_function).
+// The resulting dense matrix consists of real values in the range (0..1], which add up to the
+// numbers of rows or columns, respectively.
+
+   \code
+   // Creating the matrix
+   //    ( 1  2  3 )
+   //    ( 4  1  2 )
+   //    ( 3  4  1 )
+   blaze::StaticMatrix<double,3UL,3UL> A{ { 1.0, 2.0, 3.0 }
+                                        , { 4.0, 1.0, 2.0 }
+                                        , { 3.0, 4.0, 1.0 } };
+
+   // Computing the rowwise softmax of A (sum(B) == 3)
+   //    ( 0.0900306  0.244728   0.665241 )
+   //    ( 0.843795   0.0420101  0.114195 )
+   //    ( 0.259496   0.705385   0.035119 )
+   blaze::StaticMatrix<double,3UL,3UL> B;
+   B = softmax<rowwise>( A );
+
+   // Computing the columnwise softmax of A (sum(C) == 3)
+   //    ( 0.035119  0.114195   0.665241  )
+   //    ( 0.705385  0.0420101  0.244728  )
+   //    ( 0.259496  0.843795   0.0900306 )
+   blaze::StaticMatrix<double,3UL,3UL> C;
+   C = softmax<columnwise>( A );
+   \endcode
+*/
+template< bool RF      // Reduction flag
+        , typename MT  // Type of the dense matrix
+        , bool SO >    // Storage order
+auto softmax( const DenseMatrix<MT,SO>& dm )
+{
+   auto tmp( evaluate( exp( ~dm ) ) );
+
+   if( RF == rowwise ) {
+      for( size_t i=0UL; i<tmp.rows(); ++i ) {
+         auto r = row( tmp, i, unchecked );
+         const auto scalar( sum( r ) );
+         r /= scalar;
+      }
+   }
+   else {
+      for( size_t j=0UL; j<tmp.columns(); ++j ) {
+         auto c = column( tmp, j, unchecked );
+         const auto scalar( sum( c ) );
+         c /= scalar;
+      }
+   }
+
    return tmp;
 }
 //*************************************************************************************************
