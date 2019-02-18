c069823ce1801631dda159250103f03428b1bb0a
Klaus Iglberger <klaus.iglberger@gmail.com>
2019-02-18 10:57:14 +0100

    Fix the AVX512BW intrinsic 'equal()' overloads for 8-bit and 16-bit integrals

--- blaze/math/simd/Equal.h
+++ blaze/math/simd/Equal.h
@@ -72,7 +72,7 @@ template< bool RF       // Relaxation flag
 BLAZE_ALWAYS_INLINE bool equal( const SIMDi8<T>& a, const SIMDi8<T>& b ) noexcept
 #if BLAZE_AVX512BW_MODE
 {
-   return _mm512_cmpeq_epi8_mask( (~a).eval().value, (~b).eval().value ) == 0xffffffffffffffff;
+   return _mm512_cmpeq_epi8_mask( (~a).value, (~b).value ) == 0xffffffffffffffff;
 }
 #elif BLAZE_AVX2_MODE
 {
@@ -105,7 +105,7 @@ template< bool RF       // Relaxation flag
 BLAZE_ALWAYS_INLINE bool equal( const SIMDci8<T>& a, const SIMDci8<T>& b ) noexcept
 #if BLAZE_AVX512BW_MODE
 {
-   return _mm512_cmpeq_epi8_mask( (~a).eval().value, (~b).eval().value ) == 0xffffffffffffffff;
+   return _mm512_cmpeq_epi8_mask( (~a).value, (~b).value ) == 0xffffffffffffffff;
 }
 #elif BLAZE_AVX2_MODE
 {
@@ -181,7 +181,7 @@ template< bool RF       // Relaxation flag
 BLAZE_ALWAYS_INLINE bool equal( const SIMDi16<T>& a, const SIMDi16<T>& b ) noexcept
 #if BLAZE_AVX512BW_MODE
 {
-   return _mm512_cmpeq_epi16_mask( (~a).eval().value, (~b).eval().value ) == 0xffffffff;
+   return _mm512_cmpeq_epi16_mask( (~a).value, (~b).value ) == 0xffffffff;
 }
 #elif BLAZE_AVX2_MODE
 {
@@ -214,7 +214,7 @@ template< bool RF       // Relaxation flag
 BLAZE_ALWAYS_INLINE bool equal( const SIMDci16<T>& a, const SIMDci16<T>& b ) noexcept
 #if BLAZE_AVX512BW_MODE
 {
-   return _mm512_cmpeq_epi16_mask( (~a).eval().value, (~b).eval().value ) == 0xffffffff;
+   return _mm512_cmpeq_epi16_mask( (~a).value, (~b).value ) == 0xffffffff;
 }
 #elif BLAZE_AVX2_MODE
 {
