f4e206098b395f820fd13beeefb425475d046812
Klaus Iglberger <klaus.iglberger@gmail.com>
2019-05-04 07:16:59 +0200

    Add specializations of 'KronTrait' for the 'HermitianMatrix' class template

--- blaze/math/adaptors/HermitianMatrix.h
+++ blaze/math/adaptors/HermitianMatrix.h
@@ -60,6 +60,7 @@
 #include <blaze/math/traits/DeclSymTrait.h>
 #include <blaze/math/traits/DeclUppTrait.h>
 #include <blaze/math/traits/DivTrait.h>
+#include <blaze/math/traits/KronTrait.h>
 #include <blaze/math/traits/MapTrait.h>
 #include <blaze/math/traits/MultTrait.h>
 #include <blaze/math/traits/SchurTrait.h>
@@ -1478,6 +1479,30 @@ struct MultTraitEval1< T1, T2
                                    ( IsHermitian_v<T2> && !IsSymmetric_v<T2> && !IsUniform_v<T2> ) > >
 {
    using Type = HermitianMatrix< typename MultTraitEval2<T1,T2>::Type >;
+};
+/*! \endcond */
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  KRONTRAIT SPECIALIZATIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+template< typename T1, typename T2 >
+struct KronTraitEval1< T1, T2
+                     , EnableIf_t< IsMatrix_v<T1> &&
+                                   IsMatrix_v<T2> &&
+                                   ( IsHermitian_v<T1> && IsHermitian_v<T2> ) &&
+                                   !( IsSymmetric_v<T1> && IsSymmetric_v<T2> ) &&
+                                   !( IsZero_v<T1> || IsZero_v<T2> ) > >
+{
+   using Type = HermitianMatrix< typename KronTraitEval2<T1,T2>::Type >;
 };
 /*! \endcond */
 //*************************************************************************************************
