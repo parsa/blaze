0df17af3d7d7223cdf07f052c9c8e309553f007a
Klaus Iglberger <klaus.iglberger@gmail.com>
2019-05-04 07:17:13 +0200

    Add specializations of 'KronTrait' for the 'LowerMatrix' class template

--- blaze/math/adaptors/LowerMatrix.h
+++ blaze/math/adaptors/LowerMatrix.h
@@ -63,6 +63,7 @@
 #include <blaze/math/traits/DeclSymTrait.h>
 #include <blaze/math/traits/DeclUppTrait.h>
 #include <blaze/math/traits/DivTrait.h>
+#include <blaze/math/traits/KronTrait.h>
 #include <blaze/math/traits/MapTrait.h>
 #include <blaze/math/traits/MultTrait.h>
 #include <blaze/math/traits/SchurTrait.h>
@@ -1662,6 +1663,32 @@ struct MultTraitEval1< T1, T2
                                    !( IsZero_v<T1> || IsZero_v<T2> ) > >
 {
    using Type = LowerMatrix< typename MultTraitEval2<T1,T2>::Type >;
+};
+/*! \endcond */
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  KRONTRAIT SPECIALIZATIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+template< typename T1, typename T2 >
+struct KronTraitEval1< T1, T2
+                     , EnableIf_t< IsMatrix_v<T1> &&
+                                   IsMatrix_v<T2> &&
+                                   ( IsLower_v<T1> && IsLower_v<T2> ) &&
+                                   !( IsUniLower_v<T1> && IsUniLower_v<T2> ) &&
+                                   !( IsStrictlyLower_v<T1> || IsStrictlyLower_v<T2> ) &&
+                                   !( IsDiagonal_v<T1> && IsDiagonal_v<T2> ) &&
+                                   !( IsZero_v<T1> || IsZero_v<T2> ) > >
+{
+   using Type = LowerMatrix< typename KronTraitEval2<T1,T2>::Type >;
 };
 /*! \endcond */
 //*************************************************************************************************
