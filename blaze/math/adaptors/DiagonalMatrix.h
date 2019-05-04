2ea13130011c1e0fb22a293450206d2ae3122ce5
Klaus Iglberger <klaus.iglberger@gmail.com>
2019-05-04 07:18:11 +0200

    Add specializations of 'KronTrait' for the 'DiagonalMatrix' class template

--- blaze/math/adaptors/DiagonalMatrix.h
+++ blaze/math/adaptors/DiagonalMatrix.h
@@ -63,6 +63,7 @@
 #include <blaze/math/traits/DeclSymTrait.h>
 #include <blaze/math/traits/DeclUppTrait.h>
 #include <blaze/math/traits/DivTrait.h>
+#include <blaze/math/traits/KronTrait.h>
 #include <blaze/math/traits/MapTrait.h>
 #include <blaze/math/traits/MultTrait.h>
 #include <blaze/math/traits/RowTrait.h>
@@ -1635,6 +1636,30 @@ struct MultTraitEval1< T1, T2
                                    !( IsZero_v<T1> || IsZero_v<T2> ) > >
 {
    using Type = DiagonalMatrix< typename MultTraitEval2<T1,T2>::Type >;
+};
+/*! \endcond */
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  KRONTRAIT SPECIALIZATIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+template< typename T1, typename T2 >
+struct KronTraitEval1< T1, T2
+                     , EnableIf_t< IsMatrix_v<T1> &&
+                                   IsMatrix_v<T2> &&
+                                   ( IsDiagonal_v<T1> && IsDiagonal_v<T2> ) &&
+                                   !( IsIdentity_v<T1> && IsIdentity_v<T2> ) &&
+                                   !( IsZero_v<T1> || IsZero_v<T2> ) > >
+{
+   using Type = DiagonalMatrix< typename KronTraitEval2<T1,T2>::Type >;
 };
 /*! \endcond */
 //*************************************************************************************************
