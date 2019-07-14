a63d6012410d63a04d7642bd264377586e1568ba
Klaus Iglberger <klaus.iglberger@gmail.com>
2019-07-14 16:23:04 +0200

    Add the dense vector/dense vector bitwise AND operator

--- blaze/math/expressions/DVecDVecMapExpr.h
+++ blaze/math/expressions/DVecDVecMapExpr.h
@@ -52,6 +52,7 @@
 #include <blaze/math/expressions/Forward.h>
 #include <blaze/math/expressions/VecVecMapExpr.h>
 #include <blaze/math/functors/Atan2.h>
+#include <blaze/math/functors/Bitand.h>
 #include <blaze/math/functors/Hypot.h>
 #include <blaze/math/functors/Max.h>
 #include <blaze/math/functors/Min.h>
@@ -1347,6 +1348,39 @@ inline decltype(auto)
    BLAZE_FUNCTION_TRACE;
 
    return map( ~lhs, ~rhs, ShiftRV() );
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Bitwise AND operator for two dense vectors (\f$ \vec{a}=\vec{b}&\vec{c} \f$).
+// \ingroup dense_vector
+//
+// \param lhs The left-hand side dense vector for the bitwise AND operation.
+// \param rhs The right-hand side dense vector for the bitwise AND operation.
+// \return The resulting dense vector.
+// \exception std::invalid_argument Vector sizes do not match.
+//
+// This operator represents the bitwise AND of the given two dense vectors:
+
+   \code
+   blaze::DynamicVector<unsigned int> a, b, c;
+   // ... Resizing and initialization
+   c = a & b;
+   \endcode
+
+// In case the current sizes of the two given vectors don't match, a \a std::invalid_argument
+// is thrown.
+*/
+template< typename VT1  // Type of the left-hand side dense vector
+        , typename VT2  // Type of the right-hand side dense vector
+        , bool TF >     // Transpose flag
+inline decltype(auto)
+   operator&( const DenseVector<VT1,TF>& lhs, const DenseVector<VT2,TF>& rhs )
+{
+   BLAZE_FUNCTION_TRACE;
+
+   return map( ~lhs, ~rhs, Bitand() );
 }
 //*************************************************************************************************
 
