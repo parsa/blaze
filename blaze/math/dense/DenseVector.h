77960df4c1fe20c19f056ec9c04fcadb77d0218e
Klaus Iglberger <klaus.iglberger@gmail.com>
2019-07-14 16:23:41 +0200

    Add the dense vector/dense vector bitwise AND assignment operator

--- blaze/math/dense/DenseVector.h
+++ blaze/math/dense/DenseVector.h
@@ -150,6 +150,12 @@ auto operator&=( DenseVector<VT,TF>& vec, ST scalar )
 template< typename VT, bool TF, typename ST >
 auto operator&=( DenseVector<VT,TF>&& vec, ST scalar )
    -> EnableIf_t< IsNumeric_v<ST>, VT& >;
+
+template< typename VT1, typename VT2, bool TF >
+VT1& operator&=( DenseVector<VT1,TF>& lhs, const DenseVector<VT2,TF>& rhs );
+
+template< typename VT1, typename VT2, bool TF >
+VT1& operator&=( DenseVector<VT1,TF>&& lhs, const DenseVector<VT2,TF>& rhs );
 //@}
 //*************************************************************************************************
 
@@ -785,6 +791,64 @@ inline auto operator&=( DenseVector<VT,TF>&& vec, ST scalar )
    -> EnableIf_t< IsNumeric_v<ST>, VT& >
 {
    return operator&=( ~vec, scalar );
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Bitwise AND assignment operator for the bitwise AND of a dense vector
+//        (\f$ \vec{a}&=\vec{b} \f$).
+// \ingroup dense_vector
+//
+// \param lhs The left-hand side dense vector for the bitwise AND operation.
+// \param rhs The right-hand side dense vector for the bitwise AND operation.
+// \return Reference to the dense vector.
+// \exception std::invalid_argument Invalid bitwise AND of restricted vector.
+//
+// In case the vector \a VT is restricted and the assignment would violate an invariant of the
+// vector, a \a std::invalid_argument exception is thrown.
+*/
+template< typename VT1  // Type of the left-hand side dense vector
+        , typename VT2  // Type of the right-hand side dense vector
+        , bool TF >     // Transpose flag
+inline VT1& operator&=( DenseVector<VT1,TF>& lhs, const DenseVector<VT2,TF>& rhs )
+{
+   if( IsRestricted_v<VT1> ) {
+      if( !tryBitandAssign( ~lhs, ~rhs, 0UL ) ) {
+         BLAZE_THROW_INVALID_ARGUMENT( "Invalid bitwise AND of restricted vector" );
+      }
+   }
+
+   decltype(auto) left( derestrict( ~lhs ) );
+
+   smpAssign( left, left & (~rhs) );
+
+   BLAZE_INTERNAL_ASSERT( isIntact( ~lhs ), "Invariant violation detected" );
+
+   return ~lhs;
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Bitwise AND assignment operator for the bitwise AND of a temporary dense vector
+//        (\f$ \vec{a}&=\vec{b} \f$).
+// \ingroup dense_vector
+//
+// \param lhs The left-hand side temporary dense vector for the bitwise AND operation.
+// \param rhs The right-hand side dense vector for the bitwise AND operation.
+// \return Reference to the dense vector.
+// \exception std::invalid_argument Invalid bitwise AND of restricted vector.
+//
+// In case the vector \a VT is restricted and the assignment would violate an invariant of the
+// vector, a \a std::invalid_argument exception is thrown.
+*/
+template< typename VT1  // Type of the left-hand side dense vector
+        , typename VT2  // Type of the right-hand side dense vector
+        , bool TF >     // Transpose flag
+inline VT1& operator&=( DenseVector<VT1,TF>&& lhs, const DenseVector<VT2,TF>& rhs )
+{
+   return operator&=( ~lhs, ~rhs );
 }
 //*************************************************************************************************
 
