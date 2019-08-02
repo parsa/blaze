a4d6e48f5d241fa1cb8aaab03739d17f754706e9
Klaus Iglberger <klaus.iglberger@gmail.com>
2019-08-02 17:17:03 +0200

    Improve the treatment of expression types in the 'IsStrictlyUpper' type trait

--- blaze/math/typetraits/IsStrictlyUpper.h
+++ blaze/math/typetraits/IsStrictlyUpper.h
@@ -43,6 +43,7 @@
 #include <blaze/math/typetraits/IsExpression.h>
 #include <blaze/util/EnableIf.h>
 #include <blaze/util/FalseType.h>
+#include <blaze/util/typetraits/IsSame.h>
 
 
 namespace blaze {
@@ -72,7 +73,7 @@ struct IsStrictlyUpperHelper
 {};
 
 template< typename T >  // Type of the operand
-struct IsStrictlyUpperHelper< T, EnableIf_t< IsExpression_v<T> > >
+struct IsStrictlyUpperHelper< T, EnableIf_t< IsExpression_v<T> && !IsSame_v<T,typename T::ResultType> > >
    : public IsStrictlyUpper< typename T::ResultType >::Type
 {};
 /*! \endcond */
