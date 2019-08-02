64b67b2b87a357f63ce66c10dc88758f8c6390ee
Klaus Iglberger <klaus.iglberger@gmail.com>
2019-08-02 17:16:53 +0200

    Improve the treatment of expression types in the 'IsUniUpper' type trait

--- blaze/math/typetraits/IsUniUpper.h
+++ blaze/math/typetraits/IsUniUpper.h
@@ -43,6 +43,7 @@
 #include <blaze/math/typetraits/IsExpression.h>
 #include <blaze/util/EnableIf.h>
 #include <blaze/util/FalseType.h>
+#include <blaze/util/typetraits/IsSame.h>
 
 
 namespace blaze {
@@ -72,7 +73,7 @@ struct IsUniUpperHelper
 {};
 
 template< typename T >  // Type of the operand
-struct IsUniUpperHelper< T, EnableIf_t< IsExpression_v<T> > >
+struct IsUniUpperHelper< T, EnableIf_t< IsExpression_v<T> && !IsSame_v<T,typename T::ResultType> > >
    : public IsUniUpper< typename T::ResultType >::Type
 {};
 /*! \endcond */
