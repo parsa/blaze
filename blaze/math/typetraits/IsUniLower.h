ce0f64d3f6c7a544f0f7aaabfa01f993929d47d3
Klaus Iglberger <klaus.iglberger@gmail.com>
2019-08-02 17:16:23 +0200

    Improve the treatment of expression types in the 'IsUniLower' type trait

--- blaze/math/typetraits/IsUniLower.h
+++ blaze/math/typetraits/IsUniLower.h
@@ -43,6 +43,7 @@
 #include <blaze/math/typetraits/IsExpression.h>
 #include <blaze/util/EnableIf.h>
 #include <blaze/util/FalseType.h>
+#include <blaze/util/typetraits/IsSame.h>
 
 
 namespace blaze {
@@ -72,7 +73,7 @@ struct IsUniLowerHelper
 {};
 
 template< typename T >  // Type of the operand
-struct IsUniLowerHelper< T, EnableIf_t< IsExpression_v<T> > >
+struct IsUniLowerHelper< T, EnableIf_t< IsExpression_v<T> && !IsSame_v<T,typename T::ResultType> > >
    : public IsUniLower< typename T::ResultType >::Type
 {};
 /*! \endcond */
