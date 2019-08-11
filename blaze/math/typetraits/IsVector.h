e7fdf2181203e04207ce0e000bdb7d0d6d73662b
Klaus Iglberger <klaus.iglberger@gmail.com>
2019-08-11 14:07:58 +0200

    Reduce the include dependencies of the <math/typetraits/IsVector.h> header

--- blaze/math/typetraits/IsVector.h
+++ blaze/math/typetraits/IsVector.h
@@ -40,7 +40,7 @@
 // Includes
 //*************************************************************************************************
 
-#include <blaze/math/expressions/Vector.h>
+#include <blaze/math/expressions/Forward.h>
 #include <blaze/util/FalseType.h>
 #include <blaze/util/TrueType.h>
 
