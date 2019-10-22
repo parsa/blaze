df5b3ca6cb8bdaaa3df40c25353ab87903d966e6
Klaus Iglberger <klaus.iglberger@gmail.com>
2019-10-22 19:53:58 +0200

    Add the 'declunilow()' function for dense matrices

--- blaze/math/DenseMatrix.h
+++ blaze/math/DenseMatrix.h
@@ -60,6 +60,7 @@
 #include <blaze/math/expressions/DMatDeclHermExpr.h>
 #include <blaze/math/expressions/DMatDeclLowExpr.h>
 #include <blaze/math/expressions/DMatDeclSymExpr.h>
+#include <blaze/math/expressions/DMatDeclUniLowExpr.h>
 #include <blaze/math/expressions/DMatDeclUppExpr.h>
 #include <blaze/math/expressions/DMatDetExpr.h>
 #include <blaze/math/expressions/DMatDMatAddExpr.h>
