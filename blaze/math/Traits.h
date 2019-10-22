ff1c895b94055516dd76032f6683fca37a84dda2
Klaus Iglberger <klaus.iglberger@gmail.com>
2019-10-22 19:47:07 +0200

    Add the 'DeclUniLowTrait' class template

--- blaze/math/Traits.h
+++ blaze/math/Traits.h
@@ -50,6 +50,7 @@
 #include <blaze/math/traits/DeclIdTrait.h>
 #include <blaze/math/traits/DeclLowTrait.h>
 #include <blaze/math/traits/DeclSymTrait.h>
+#include <blaze/math/traits/DeclUniLowTrait.h>
 #include <blaze/math/traits/DeclUppTrait.h>
 #include <blaze/math/traits/DeclZeroTrait.h>
 #include <blaze/math/traits/DerestrictTrait.h>
