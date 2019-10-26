fe3d0d5d5ea53e5161a087b48ac9fbcc8081ac70
Klaus Iglberger <klaus.iglberger@gmail.com>
2019-10-26 09:30:36 +0200

    Add a specialization of 'DeclStrLowTrait' for the 'SymmetricMatrix' class template

--- blaze/math/adaptors/SymmetricMatrix.h
+++ blaze/math/adaptors/SymmetricMatrix.h
@@ -57,6 +57,7 @@
 #include <blaze/math/traits/DeclDiagTrait.h>
 #include <blaze/math/traits/DeclHermTrait.h>
 #include <blaze/math/traits/DeclLowTrait.h>
+#include <blaze/math/traits/DeclStrLowTrait.h>
 #include <blaze/math/traits/DeclSymTrait.h>
 #include <blaze/math/traits/DeclUniLowTrait.h>
 #include <blaze/math/traits/DeclUniUppTrait.h>
@@ -1067,6 +1068,25 @@ template< typename MT, bool SO, bool DF, bool NF >
 struct DeclUniLowTrait< SymmetricMatrix<MT,SO,DF,NF> >
 {
    using Type = IdentityMatrix< ElementType_t<MT>, SO >;
+};
+/*! \endcond */
+//*************************************************************************************************
+
+
+
+
+//=================================================================================================
+//
+//  DECLSTRLOWTRAIT SPECIALIZATIONS
+//
+//=================================================================================================
+
+//*************************************************************************************************
+/*! \cond BLAZE_INTERNAL */
+template< typename MT, bool SO, bool DF, bool NF >
+struct DeclStrLowTrait< SymmetricMatrix<MT,SO,DF,NF> >
+{
+   using Type = ZeroMatrix< ElementType_t<MT>, SO >;
 };
 /*! \endcond */
 //*************************************************************************************************
