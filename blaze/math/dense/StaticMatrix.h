d9319efa246c4afd6da5998bb84e0e96f627e943
Klaus Iglberger <klaus.iglberger@gmail.com>
2019-11-14 19:08:51 +0100

    Add a specialization of 'MapTrait' for the 'StaticMatrix' class template

--- blaze/math/dense/StaticMatrix.h
+++ blaze/math/dense/StaticMatrix.h
@@ -6709,6 +6709,22 @@ struct UnaryMapTraitEval2< T, OP
 /*! \cond BLAZE_INTERNAL */
 template< typename T1, typename T2, typename OP >
 struct BinaryMapTraitEval2< T1, T2, OP
+                          , EnableIf_t< IsColumnVector_v<T1> &&
+                                        IsRowVector_v<T2> &&
+                                        ( Size_v<T1,0UL> != DefaultSize_v ) &&
+                                        ( Size_v<T2,0UL> != DefaultSize_v ) > >
+{
+   using ET1 = ElementType_t<T1>;
+   using ET2 = ElementType_t<T2>;
+
+   static constexpr size_t M = Size_v<T1,0UL>;
+   static constexpr size_t N = Size_v<T2,0UL>;
+
+   using Type = StaticMatrix< MapTrait_t<ET1,ET2,OP>, M, N, false >;
+};
+
+template< typename T1, typename T2, typename OP >
+struct BinaryMapTraitEval2< T1, T2, OP
                           , EnableIf_t< IsMatrix_v<T1> &&
                                         IsMatrix_v<T2> &&
                                         ( Size_v<T1,0UL> != DefaultSize_v ||
