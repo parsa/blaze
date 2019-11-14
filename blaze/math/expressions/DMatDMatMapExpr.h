5b4bd575e28a076548a28300d353cc6ff5f84994
Klaus Iglberger <klaus.iglberger@gmail.com>
2019-11-14 19:13:32 +0100

    Fix several small issues in the 'DMatDMatMapExpr' class template

--- blaze/math/expressions/DMatDMatMapExpr.h
+++ blaze/math/expressions/DMatDMatMapExpr.h
@@ -124,9 +124,9 @@ class DMatDMatMapExpr
    //! Compilation switch for the serial evaluation strategy of the map expression.
    /*! The \a useAssign compile time constant expression represents a compilation switch for
        the serial evaluation strategy of the map expression. In case either of the two dense
-       matrix operands requires an intermediate evaluation, \a useAssign will be set to 1 and
-       the addition expression will be evaluated via the \a assign function family. Otherwise
-       \a useAssign will be set to 0 and the expression will be evaluated via the subscript
+       matrix operands requires an intermediate evaluation, \a useAssign will be set to \a true
+       and the addition expression will be evaluated via the \a assign function family. Otherwise
+       \a useAssign will be set to \a false and the expression will be evaluated via the subscript
        operator. */
    static constexpr bool useAssign = ( RequiresEvaluation_v<MT1> || RequiresEvaluation_v<MT2> );
 
@@ -142,9 +142,9 @@ class DMatDMatMapExpr
    //! Helper variable template for the explicit application of the SFINAE principle.
    /*! This variable template is a helper for the selection of the parallel evaluation strategy.
        In case at least one of the two dense matrix operands is not SMP assignable and at least
-       one of the two operands requires an intermediate evaluation, the variable is set to 1 and
-       the expression specific evaluation strategy is selected. Otherwise the variable is set to
-       0 and the default strategy is chosen. */
+       one of the two operands requires an intermediate evaluation, the variable is set to \a true
+       and the expression specific evaluation strategy is selected. Otherwise the variable is set
+       to \a false and the default strategy is chosen. */
    template< typename MT >
    static constexpr bool UseSMPAssign_v =
       ( ( !MT1::smpAssignable || !MT2::smpAssignable ) && useAssign );
@@ -172,7 +172,7 @@ class DMatDMatMapExpr
    //! Composite type of the right-hand side dense matrix expression.
    using RightOperand = If_t< IsExpression_v<MT2>, const MT2, const MT2& >;
 
-   //! Data type of the custom unary operation.
+   //! Data type of the custom binary operation.
    using Operation = OP;
 
    //! Type for the assignment of the left-hand side dense matrix operand.
@@ -214,12 +214,12 @@ class DMatDMatMapExpr
       //
       // \param left Iterator to the initial left-hand side element.
       // \param right Iterator to the initial right-hand side element.
-      // \param op The custom unary operation.
+      // \param op The custom binary operation.
       */
       explicit inline ConstIterator( LeftIteratorType left, RightIteratorType right, OP op )
          : left_ ( left  )  // Iterator to the current left-hand side element
          , right_( right )  // Iterator to the current right-hand side element
-         , op_   ( op    )  // The custom unary operation
+         , op_   ( op    )  // The custom binary operation
       {}
       //*******************************************************************************************
 
@@ -430,7 +430,7 @@ class DMatDMatMapExpr
       //**Member variables*************************************************************************
       LeftIteratorType  left_;   //!< Iterator to the current left-hand side element.
       RightIteratorType right_;  //!< Iterator to the current right-hand side element.
-      OP                op_;     //!< The custom unary operation.
+      OP                op_;     //!< The custom binary operation.
       //*******************************************************************************************
    };
    //**********************************************************************************************
@@ -455,12 +455,12 @@ class DMatDMatMapExpr
    //
    // \param lhs The left-hand side dense matrix operand of the map expression.
    // \param rhs The right-hand side dense matrix operand of the map expression.
-   // \param op The custom unary operation.
+   // \param op The custom binary operation.
    */
    explicit inline DMatDMatMapExpr( const MT1& lhs, const MT2& rhs, OP op ) noexcept
       : lhs_( lhs )  // Left-hand side dense matrix of the map expression
       , rhs_( rhs )  // Right-hand side dense matrix of the map expression
-      , op_ ( op  )  // The custom unary operation
+      , op_ ( op  )  // The custom binary operation
    {}
    //**********************************************************************************************
 
@@ -634,7 +634,7 @@ class DMatDMatMapExpr
    //**Member variables****************************************************************************
    LeftOperand  lhs_;  //!< Left-hand side dense matrix of the map expression.
    RightOperand rhs_;  //!< Right-hand side dense matrix of the map expression.
-   Operation    op_;   //!< The custom unary operation.
+   Operation    op_;   //!< The custom binary operation.
    //**********************************************************************************************
 
    //**Assignment to dense matrices****************************************************************
@@ -1090,8 +1090,8 @@ class DMatDMatMapExpr
 //=================================================================================================
 
 //*************************************************************************************************
-/*!\brief Evaluates the given binary operation on each single element of the dense matrices
-//        \a lhs and \a rhs.
+/*!\brief Elementwise evaluation of the given binary operation on each single element of the
+//        dense matrices \a lhs and \a rhs.
 // \ingroup dense_matrix
 //
 // \param lhs The left-hand side dense matrix operand.
@@ -1100,7 +1100,7 @@ class DMatDMatMapExpr
 // \return The binary operation applied to each single element of \a lhs and \a rhs.
 // \exception std::invalid_argument Matrix sizes do not match.
 //
-// The \a map() function evaluates the given binary operation on each element of the input
+// The \a map() function evaluates the given binary operation on each single element of the input
 // matrices \a lhs and \a rhs. The function returns an expression representing this operation.\n
 // The following example demonstrates the use of the \a map() function:
 
