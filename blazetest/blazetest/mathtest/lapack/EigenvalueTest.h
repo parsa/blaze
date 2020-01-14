620923485f3211cc30f63782017a8603c53f1ceb
Klaus Iglberger <klaus.iglberger@gmail.com>
2020-01-14 03:55:38 +0100

    Update the operation test for the LAPACK 'gges()' function

--- blazetest/blazetest/mathtest/lapack/EigenvalueTest.h
+++ blazetest/blazetest/mathtest/lapack/EigenvalueTest.h
@@ -148,14 +148,14 @@ class EigenvalueTest
 */
 template< typename T >
 const auto select =
-   []( const T* alphar, const T* alphai, const T* beta ) -> int {
+   []( const T* alphar, const T* alphai, const T* beta ) -> blaze::blas_int_t {
       blaze::MAYBE_UNUSED( alphai, beta );
       return *alphar > T(0);
    };
 
 template< typename T >
 const auto select< blaze::complex<T> > =
-   []( const blaze::complex<T>* alpha, const blaze::complex<T>* beta ) -> int {
+   []( const blaze::complex<T>* alpha, const blaze::complex<T>* beta ) -> blaze::blas_int_t {
       blaze::MAYBE_UNUSED( beta );
       return alpha->real() > T(0);
    };
