2957c032de604f6ac7553315fcf5c858457641d9
Klaus Iglberger <klaus.iglberger@gmail.com>
2020-04-18 10:34:55 +0200

    Rework the 'Rand' specialization of the 'Submatrix' class template

--- blaze/math/Submatrix.h
+++ blaze/math/Submatrix.h
@@ -75,101 +75,75 @@ template< typename MT       // Type of the matrix
 class Rand< Submatrix<MT,AF,SO,true,CSAs...> >
 {
  public:
-   //**Randomize functions*************************************************************************
-   /*!\name Randomize functions */
-   //@{
-   template< typename SMT >
-   inline void randomize( SMT&& submatrix ) const;
+   //**********************************************************************************************
+   /*!\brief Randomization of a dense submatrix.
+   //
+   // \param submatrix The submatrix to be randomized.
+   // \return void
+   */
+   template< typename SMT >  // Type of the submatrix
+   inline void randomize( SMT&& submatrix ) const
+   {
+      using blaze::randomize;
 
-   template< typename SMT, typename Arg >
-   inline void randomize( SMT&& submatrix, const Arg& min, const Arg& max ) const;
-   //@}
+      using SubmatrixType = RemoveReference_t<SMT>;
+
+      BLAZE_CONSTRAINT_MUST_BE_SUBMATRIX_TYPE( SubmatrixType );
+      BLAZE_CONSTRAINT_MUST_BE_DENSE_MATRIX_TYPE( SubmatrixType );
+
+      if( SO == rowMajor ) {
+         for( size_t i=0UL; i<submatrix.rows(); ++i ) {
+            for( size_t j=0UL; j<submatrix.columns(); ++j ) {
+               randomize( submatrix(i,j) );
+            }
+         }
+      }
+      else {
+         for( size_t j=0UL; j<submatrix.columns(); ++j ) {
+            for( size_t i=0UL; i<submatrix.rows(); ++i ) {
+               randomize( submatrix(i,j) );
+            }
+         }
+      }
+   }
+   //**********************************************************************************************
+
+   //**********************************************************************************************
+   /*!\brief Randomization of a dense submatrix.
+   //
+   // \param submatrix The submatrix to be randomized.
+   // \param min The smallest possible value for a matrix element.
+   // \param max The largest possible value for a matrix element.
+   // \return void
+   */
+   template< typename SMT    // Type of the submatrix
+           , typename Arg >  // Min/max argument type
+   inline void randomize( SMT&& submatrix, const Arg& min, const Arg& max ) const
+   {
+      using blaze::randomize;
+
+      using SubmatrixType = RemoveReference_t<SMT>;
+
+      BLAZE_CONSTRAINT_MUST_BE_SUBMATRIX_TYPE( SubmatrixType );
+      BLAZE_CONSTRAINT_MUST_BE_DENSE_MATRIX_TYPE( SubmatrixType );
+
+      if( SO == rowMajor ) {
+         for( size_t i=0UL; i<submatrix.rows(); ++i ) {
+            for( size_t j=0UL; j<submatrix.columns(); ++j ) {
+               randomize( submatrix(i,j), min, max );
+            }
+         }
+      }
+      else {
+         for( size_t j=0UL; j<submatrix.columns(); ++j ) {
+            for( size_t i=0UL; i<submatrix.rows(); ++i ) {
+               randomize( submatrix(i,j), min, max );
+            }
+         }
+      }
+   }
    //**********************************************************************************************
 };
-/*! \endcond */
-//*************************************************************************************************
-
-
-//*************************************************************************************************
-/*! \cond BLAZE_INTERNAL */
-/*!\brief Randomization of a dense submatrix.
-//
-// \param submatrix The submatrix to be randomized.
-// \return void
-*/
-template< typename MT       // Type of the dense matrix
-        , AlignmentFlag AF  // Alignment flag
-        , bool SO           // Storage order
-        , size_t... CSAs >  // Compile time submatrix arguments
-template< typename SMT >    // Type of the submatrix
-inline void Rand< Submatrix<MT,AF,SO,true,CSAs...> >::randomize( SMT&& submatrix ) const
-{
-   using blaze::randomize;
-
-   using SubmatrixType = RemoveReference_t<SMT>;
-
-   BLAZE_CONSTRAINT_MUST_BE_SUBMATRIX_TYPE( SubmatrixType );
-   BLAZE_CONSTRAINT_MUST_BE_DENSE_MATRIX_TYPE( SubmatrixType );
-
-   if( SO == rowMajor ) {
-      for( size_t i=0UL; i<submatrix.rows(); ++i ) {
-         for( size_t j=0UL; j<submatrix.columns(); ++j ) {
-            randomize( submatrix(i,j) );
-         }
-      }
-   }
-   else {
-      for( size_t j=0UL; j<submatrix.columns(); ++j ) {
-         for( size_t i=0UL; i<submatrix.rows(); ++i ) {
-            randomize( submatrix(i,j) );
-         }
-      }
-   }
-}
-/*! \endcond */
-//*************************************************************************************************
-
-
-//*************************************************************************************************
-/*! \cond BLAZE_INTERNAL */
-/*!\brief Randomization of a dense submatrix.
-//
-// \param submatrix The submatrix to be randomized.
-// \param min The smallest possible value for a matrix element.
-// \param max The largest possible value for a matrix element.
-// \return void
-*/
-template< typename MT       // Type of the dense matrix
-        , AlignmentFlag AF  // Alignment flag
-        , bool SO           // Storage order
-        , size_t... CSAs >  // Compile time submatrix arguments
-template< typename SMT      // Type of the submatrix
-        , typename Arg >    // Min/max argument type
-inline void Rand< Submatrix<MT,AF,SO,true,CSAs...> >::randomize( SMT&& submatrix,
-                                                                 const Arg& min, const Arg& max ) const
-{
-   using blaze::randomize;
-
-   using SubmatrixType = RemoveReference_t<SMT>;
-
-   BLAZE_CONSTRAINT_MUST_BE_SUBMATRIX_TYPE( SubmatrixType );
-   BLAZE_CONSTRAINT_MUST_BE_DENSE_MATRIX_TYPE( SubmatrixType );
-
-   if( SO == rowMajor ) {
-      for( size_t i=0UL; i<submatrix.rows(); ++i ) {
-         for( size_t j=0UL; j<submatrix.columns(); ++j ) {
-            randomize( submatrix(i,j), min, max );
-         }
-      }
-   }
-   else {
-      for( size_t j=0UL; j<submatrix.columns(); ++j ) {
-         for( size_t i=0UL; i<submatrix.rows(); ++i ) {
-            randomize( submatrix(i,j), min, max );
-         }
-      }
-   }
-}
 /*! \endcond */
 //*************************************************************************************************
 
@@ -196,191 +170,144 @@ template< typename MT       // Type of the dense matrix
 class Rand< Submatrix<MT,AF,SO,false,CSAs...> >
 {
  public:
-   //**Randomize functions*************************************************************************
-   /*!\name Randomize functions */
-   //@{
-   template< typename SMT >
-   inline void randomize( SMT&& submatrix ) const;
+   //**********************************************************************************************
+   /*!\brief Randomization of a sparse submatrix.
+   //
+   // \param submatrix The submatrix to be randomized.
+   // \return void
+   */
+   template< typename SMT >  // Type of the submatrix
+   inline void randomize( SMT&& submatrix ) const
+   {
+      using SubmatrixType = RemoveReference_t<SMT>;
+      using ElementType   = ElementType_t<SubmatrixType>;
 
-   template< typename SMT >
-   inline void randomize( SMT&& submatrix, size_t nonzeros ) const;
+      BLAZE_CONSTRAINT_MUST_BE_SUBMATRIX_TYPE( SubmatrixType );
+      BLAZE_CONSTRAINT_MUST_BE_SPARSE_MATRIX_TYPE( SubmatrixType );
 
-   template< typename SMT, typename Arg >
-   inline void randomize( SMT&& submatrix, const Arg& min, const Arg& max ) const;
+      const size_t m( submatrix.rows()    );
+      const size_t n( submatrix.columns() );
 
-   template< typename SMT, typename Arg >
-   inline void randomize( SMT&& submatrix, size_t nonzeros, const Arg& min, const Arg& max ) const;
-   //@}
+      if( m == 0UL || n == 0UL ) return;
+
+      const size_t nonzeros( rand<size_t>( 1UL, std::ceil( 0.5*m*n ) ) );
+
+      submatrix.reset();
+      submatrix.reserve( nonzeros );
+
+      while( submatrix.nonZeros() < nonzeros ) {
+         submatrix( rand<size_t>( 0UL, m-1UL ), rand<size_t>( 0UL, n-1UL ) ) = rand<ElementType>();
+      }
+   }
+   //**********************************************************************************************
+
+   //**********************************************************************************************
+   /*!\brief Randomization of a sparse submatrix.
+   //
+   // \param submatrix The submatrix to be randomized.
+   // \param nonzeros The number of non-zero elements of the random submatrix.
+   // \return void
+   // \exception std::invalid_argument Invalid number of non-zero elements.
+   */
+   template< typename SMT >  // Type of the submatrix
+   inline void randomize( SMT&& submatrix, size_t nonzeros ) const
+   {
+      using SubmatrixType = RemoveReference_t<SMT>;
+      using ElementType   = ElementType_t<SubmatrixType>;
+
+      BLAZE_CONSTRAINT_MUST_BE_SUBMATRIX_TYPE( SubmatrixType );
+      BLAZE_CONSTRAINT_MUST_BE_SPARSE_MATRIX_TYPE( SubmatrixType );
+
+      const size_t m( submatrix.rows()    );
+      const size_t n( submatrix.columns() );
+
+      if( nonzeros > m*n ) {
+         BLAZE_THROW_INVALID_ARGUMENT( "Invalid number of non-zero elements" );
+      }
+
+      if( m == 0UL || n == 0UL ) return;
+
+      submatrix.reset();
+      submatrix.reserve( nonzeros );
+
+      while( submatrix.nonZeros() < nonzeros ) {
+         submatrix( rand<size_t>( 0UL, m-1UL ), rand<size_t>( 0UL, n-1UL ) ) = rand<ElementType>();
+      }
+   }
+   //**********************************************************************************************
+
+   //**********************************************************************************************
+   /*!\brief Randomization of a sparse submatrix.
+   //
+   // \param submatrix The submatrix to be randomized.
+   // \param min The smallest possible value for a submatrix element.
+   // \param max The largest possible value for a submatrix element.
+   // \return void
+   */
+   template< typename SMT    // Type of the submatrix
+           , typename Arg >  // Min/max argument type
+   inline void randomize( SMT&& submatrix, const Arg& min, const Arg& max ) const
+   {
+      using SubmatrixType = RemoveReference_t<SMT>;
+      using ElementType   = ElementType_t<SubmatrixType>;
+
+      BLAZE_CONSTRAINT_MUST_BE_SUBMATRIX_TYPE( SubmatrixType );
+      BLAZE_CONSTRAINT_MUST_BE_SPARSE_MATRIX_TYPE( SubmatrixType );
+
+      const size_t m( submatrix.rows()    );
+      const size_t n( submatrix.columns() );
+
+      if( m == 0UL || n == 0UL ) return;
+
+      const size_t nonzeros( rand<size_t>( 1UL, std::ceil( 0.5*m*n ) ) );
+
+      submatrix.reset();
+      submatrix.reserve( nonzeros );
+
+      while( submatrix.nonZeros() < nonzeros ) {
+         submatrix( rand<size_t>( 0UL, m-1UL ), rand<size_t>( 0UL, n-1UL ) ) = rand<ElementType>( min, max );
+      }
+   }
+   //**********************************************************************************************
+
+   //**********************************************************************************************
+   /*!\brief Randomization of a sparse submatrix.
+   //
+   // \param submatrix The submatrix to be randomized.
+   // \param nonzeros The number of non-zero elements of the random submatrix.
+   // \param min The smallest possible value for a submatrix element.
+   // \param max The largest possible value for a submatrix element.
+   // \return void
+   // \exception std::invalid_argument Invalid number of non-zero elements.
+   */
+   template< typename SMT    // Type of the submatrix
+           , typename Arg >  // Min/max argument type
+   inline void randomize( SMT&& submatrix, size_t nonzeros, const Arg& min, const Arg& max ) const
+   {
+      using SubmatrixType = RemoveReference_t<SMT>;
+      using ElementType   = ElementType_t<SubmatrixType>;
+
+      BLAZE_CONSTRAINT_MUST_BE_SUBMATRIX_TYPE( SubmatrixType );
+      BLAZE_CONSTRAINT_MUST_BE_SPARSE_MATRIX_TYPE( SubmatrixType );
+
+      const size_t m( submatrix.rows()    );
+      const size_t n( submatrix.columns() );
+
+      if( nonzeros > m*n ) {
+         BLAZE_THROW_INVALID_ARGUMENT( "Invalid number of non-zero elements" );
+      }
+
+      if( m == 0UL || n == 0UL ) return;
+
+      submatrix.reset();
+      submatrix.reserve( nonzeros );
+
+      while( submatrix.nonZeros() < nonzeros ) {
+         submatrix( rand<size_t>( 0UL, m-1UL ), rand<size_t>( 0UL, n-1UL ) ) = rand<ElementType>( min, max );
+      }
+   }
    //**********************************************************************************************
 };
-/*! \endcond */
-//*************************************************************************************************
-
-
-//*************************************************************************************************
-/*! \cond BLAZE_INTERNAL */
-/*!\brief Randomization of a sparse submatrix.
-//
-// \param submatrix The submatrix to be randomized.
-// \return void
-*/
-template< typename MT       // Type of the dense matrix
-        , AlignmentFlag AF  // Alignment flag
-        , bool SO           // Storage order
-        , size_t... CSAs >  // Compile time submatrix arguments
-template< typename SMT >    // Type of the submatrix
-inline void Rand< Submatrix<MT,AF,SO,false,CSAs...> >::randomize( SMT&& submatrix ) const
-{
-   using SubmatrixType = RemoveReference_t<SMT>;
-   using ElementType   = ElementType_t<SubmatrixType>;
-
-   BLAZE_CONSTRAINT_MUST_BE_SUBMATRIX_TYPE( SubmatrixType );
-   BLAZE_CONSTRAINT_MUST_BE_SPARSE_MATRIX_TYPE( SubmatrixType );
-
-   const size_t m( submatrix.rows()    );
-   const size_t n( submatrix.columns() );
-
-   if( m == 0UL || n == 0UL ) return;
-
-   const size_t nonzeros( rand<size_t>( 1UL, std::ceil( 0.5*m*n ) ) );
-
-   submatrix.reset();
-   submatrix.reserve( nonzeros );
-
-   while( submatrix.nonZeros() < nonzeros ) {
-      submatrix( rand<size_t>( 0UL, m-1UL ), rand<size_t>( 0UL, n-1UL ) ) = rand<ElementType>();
-   }
-}
-/*! \endcond */
-//*************************************************************************************************
-
-
-//*************************************************************************************************
-/*! \cond BLAZE_INTERNAL */
-/*!\brief Randomization of a sparse submatrix.
-//
-// \param submatrix The submatrix to be randomized.
-// \param nonzeros The number of non-zero elements of the random submatrix.
-// \return void
-// \exception std::invalid_argument Invalid number of non-zero elements.
-*/
-template< typename MT       // Type of the dense matrix
-        , AlignmentFlag AF  // Alignment flag
-        , bool SO           // Storage order
-        , size_t... CSAs >  // Compile time submatrix arguments
-template< typename SMT >    // Type of the submatrix
-inline void Rand< Submatrix<MT,AF,SO,false,CSAs...> >::randomize( SMT&& submatrix, size_t nonzeros ) const
-{
-   using SubmatrixType = RemoveReference_t<SMT>;
-   using ElementType   = ElementType_t<SubmatrixType>;
-
-   BLAZE_CONSTRAINT_MUST_BE_SUBMATRIX_TYPE( SubmatrixType );
-   BLAZE_CONSTRAINT_MUST_BE_SPARSE_MATRIX_TYPE( SubmatrixType );
-
-   const size_t m( submatrix.rows()    );
-   const size_t n( submatrix.columns() );
-
-   if( nonzeros > m*n ) {
-      BLAZE_THROW_INVALID_ARGUMENT( "Invalid number of non-zero elements" );
-   }
-
-   if( m == 0UL || n == 0UL ) return;
-
-   submatrix.reset();
-   submatrix.reserve( nonzeros );
-
-   while( submatrix.nonZeros() < nonzeros ) {
-      submatrix( rand<size_t>( 0UL, m-1UL ), rand<size_t>( 0UL, n-1UL ) ) = rand<ElementType>();
-   }
-}
-/*! \endcond */
-//*************************************************************************************************
-
-
-//*************************************************************************************************
-/*! \cond BLAZE_INTERNAL */
-/*!\brief Randomization of a sparse submatrix.
-//
-// \param submatrix The submatrix to be randomized.
-// \param min The smallest possible value for a submatrix element.
-// \param max The largest possible value for a submatrix element.
-// \return void
-*/
-template< typename MT       // Type of the dense matrix
-        , AlignmentFlag AF  // Alignment flag
-        , bool SO           // Storage order
-        , size_t... CSAs >  // Compile time submatrix arguments
-template< typename SMT      // Type of the submatrix
-        , typename Arg >    // Min/max argument type
-inline void Rand< Submatrix<MT,AF,SO,false,CSAs...> >::randomize( SMT&& submatrix,
-                                                                  const Arg& min, const Arg& max ) const
-{
-   using SubmatrixType = RemoveReference_t<SMT>;
-   using ElementType   = ElementType_t<SubmatrixType>;
-
-   BLAZE_CONSTRAINT_MUST_BE_SUBMATRIX_TYPE( SubmatrixType );
-   BLAZE_CONSTRAINT_MUST_BE_SPARSE_MATRIX_TYPE( SubmatrixType );
-
-   const size_t m( submatrix.rows()    );
-   const size_t n( submatrix.columns() );
-
-   if( m == 0UL || n == 0UL ) return;
-
-   const size_t nonzeros( rand<size_t>( 1UL, std::ceil( 0.5*m*n ) ) );
-
-   submatrix.reset();
-   submatrix.reserve( nonzeros );
-
-   while( submatrix.nonZeros() < nonzeros ) {
-      submatrix( rand<size_t>( 0UL, m-1UL ), rand<size_t>( 0UL, n-1UL ) ) = rand<ElementType>( min, max );
-   }
-}
-/*! \endcond */
-//*************************************************************************************************
-
-
-//*************************************************************************************************
-/*! \cond BLAZE_INTERNAL */
-/*!\brief Randomization of a sparse submatrix.
-//
-// \param submatrix The submatrix to be randomized.
-// \param nonzeros The number of non-zero elements of the random submatrix.
-// \param min The smallest possible value for a submatrix element.
-// \param max The largest possible value for a submatrix element.
-// \return void
-// \exception std::invalid_argument Invalid number of non-zero elements.
-*/
-template< typename MT       // Type of the dense matrix
-        , AlignmentFlag AF  // Alignment flag
-        , bool SO           // Storage order
-        , size_t... CSAs >  // Compile time submatrix arguments
-template< typename SMT      // Type of the submatrix
-        , typename Arg >    // Min/max argument type
-inline void Rand< Submatrix<MT,AF,SO,false,CSAs...> >::randomize( SMT&& submatrix, size_t nonzeros,
-                                                                  const Arg& min, const Arg& max ) const
-{
-   using SubmatrixType = RemoveReference_t<SMT>;
-   using ElementType   = ElementType_t<SubmatrixType>;
-
-   BLAZE_CONSTRAINT_MUST_BE_SUBMATRIX_TYPE( SubmatrixType );
-   BLAZE_CONSTRAINT_MUST_BE_SPARSE_MATRIX_TYPE( SubmatrixType );
-
-   const size_t m( submatrix.rows()    );
-   const size_t n( submatrix.columns() );
-
-   if( nonzeros > m*n ) {
-      BLAZE_THROW_INVALID_ARGUMENT( "Invalid number of non-zero elements" );
-   }
-
-   if( m == 0UL || n == 0UL ) return;
-
-   submatrix.reset();
-   submatrix.reserve( nonzeros );
-
-   while( submatrix.nonZeros() < nonzeros ) {
-      submatrix( rand<size_t>( 0UL, m-1UL ), rand<size_t>( 0UL, n-1UL ) ) = rand<ElementType>( min, max );
-   }
-}
 /*! \endcond */
 //*************************************************************************************************
 
