447f2d1074f5467fe969e994f1e701b76e24a2c2
Klaus Iglberger <klaus.iglberger@gmail.com>
2020-04-20 19:51:33 +0200

    Document the 'defaultAlignmentFlag' value

--- blaze/system/Alignment.h
+++ blaze/system/Alignment.h
@@ -53,9 +53,26 @@ namespace blaze {
 //=================================================================================================
 
 //*************************************************************************************************
-/*! \cond BLAZE_INTERNAL */
+/*!\brief The default alignment for all vectors and matrices of the Blaze library.
+// \ingroup system
+//
+// This value specifies the default alignment for all vectors and matrices of the Blaze library.
+// In case no explicit alignment is specified with the according vector type, this setting
+// is used.
+
+   \code
+   // Explicit specification of the alignment => the vector element are aligned
+   StaticVector<double,3UL,columnVector,aligned> a;
+
+   // No explicit specification of the alignment => use of the default alignment
+   StaticVector<double,3UL> b;
+   \endcode
+
+// The default alignment is defined via the BLAZE_DEFAULT_ALIGNMENT_FLAG compilation switch
+// (see the \ref alignment section). Valid settings for this value are blaze::aligned and
+// blaze::unaligned.
+*/
 constexpr AlignmentFlag defaultAlignmentFlag = BLAZE_DEFAULT_ALIGNMENT_FLAG;
-/*! \endcond */
 //*************************************************************************************************
 
 } // namespace blaze
