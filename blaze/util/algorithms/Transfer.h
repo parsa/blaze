0557e55e0307b63366664a6c8e0b158d603461d5
Klaus Iglberger <klaus.iglberger@gmail.com>
2020-07-28 06:07:45 +0200

    Add the 'transfer_n()' algorithm

--- blaze/util/algorithms/Transfer.h
+++ blaze/util/algorithms/Transfer.h
@@ -40,6 +40,7 @@
 // Includes
 //*************************************************************************************************
 
+#include <algorithm>
 #include <iterator>
 #include <blaze/util/typetraits/IsAssignable.h>
 
@@ -69,13 +70,42 @@ template< typename InputIterator
         , typename OutputIterator >
 OutputIterator transfer( InputIterator first, InputIterator last, OutputIterator dest )
 {
-   using ValueType = typename std::iterator_traits<InputIterator>::value_type;
+   using T = typename std::iterator_traits<InputIterator>::value_type;
 
-   if( IsNothrowMoveAssignable_v<ValueType> ) {
+   if( IsNothrowMoveAssignable_v<T> ) {
       return std::move( first, last, dest );
    }
    else {
       return std::copy( first, last, dest );
+   }
+}
+//*************************************************************************************************
+
+
+//*************************************************************************************************
+/*!\brief Transfers the elements from the given source range to the destination range.
+// \ingroup algorithms
+//
+// \param first Iterator to the first element of the source range.
+// \param n The number of elements to be moved.
+// \param dest Iterator to the first element of the destination range.
+// \return Output iterator to the element one past the last copied element.
+//
+// This function transfers the elements in the range \f$ [first,first+n) \f$ to the specified
+// destination range. In case the elements provide a no-throw move assignment, the transfer
+// operation is handled via move. Else the elements are copied.
+*/
+template< typename InputIterator
+        , typename OutputIterator >
+OutputIterator transfer_n( InputIterator first, size_t n, OutputIterator dest )
+{
+   using T = typename std::iterator_traits<InputIterator>::value_type;
+
+   if( IsNothrowMoveAssignable_v<T> ) {
+      return std::copy_n( std::make_move_iterator( first ), n, dest );
+   }
+   else {
+      return std::copy_n( first, n, dest );
    }
 }
 //*************************************************************************************************
