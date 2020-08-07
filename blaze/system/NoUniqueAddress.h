9866a93bc689098d5ebab0d0e3f0f228dfbe3eb7
Klaus Iglberger <klaus.iglberger@gmail.com>
2020-08-07 20:23:34 +0200

    Fix a compilation issue of the 'BLAZE_NO_UNIQUE_ADDRESS' macro with Apple's Clang compiler

--- blaze/system/NoUniqueAddress.h
+++ blaze/system/NoUniqueAddress.h
@@ -47,7 +47,7 @@
 // \brief Compiler dependent setup of the [[no_unique_address]] attribute.
 // \ingroup system
 */
-#if ( defined(__GNUC__) && __GNUC__ >= 9 ) || ( defined(__clang__) && __clang_major__ >= 9 )
+#if ( defined(__GNUC__) && __GNUC__ >= 9 ) || ( defined(__clang__) && !defined(__apple_build_version__) && __clang_major__ >= 9 )
 #  define BLAZE_NO_UNIQUE_ADDRESS [[no_unique_address]]
 #else
 #  define BLAZE_NO_UNIQUE_ADDRESS
